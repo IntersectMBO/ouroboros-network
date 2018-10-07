50fed6ccdbe2655f4c2243902b2c0a90ecee57ee
Duncan Coutts <duncan@well-typed.com>
2018-10-08 00:12:41 +0100

    Reorder properties vs generators in module
    The properties are the interesting thing. Put them first.

--- test/Test/ChainProducerState.hs
+++ test/Test/ChainProducerState.hs
@@ -19,17 +19,70 @@ import           Test.Chain
 tests :: TestTree
 tests =
   testGroup "ChainProducerState"
-  [ testProperty "check initial reader state" prop_init_lookup
-  , testProperty "check reader state after updateReader" prop_update_lookup
-  , testProperty "apply readerInstructions" prop_producer_sync
-  , testProperty "swicht fork" prop_switchFork
-  , testGroup "Test Arbitrary instances"
-
+  [ testGroup "Test Arbitrary instances"
     [ testProperty "ChainProducerStateForkTest's generator" prop_arbitrary_ChainProducerStateForkTest
     , testProperty "ChainProducerStateForkTest's shrinker"
       prop_shrink_ChainProducerStateForkTest
     ]
+  , testProperty "check initial reader state" prop_init_lookup
+  , testProperty "check reader state after updateReader" prop_update_lookup
+  , testProperty "apply readerInstructions" prop_producer_sync
+  , testProperty "swicht fork" prop_switchFork
   ]
+
+
+--
+-- Properties
+--
+
+prop_init_lookup :: ChainProducerStateTest -> Bool
+prop_init_lookup (ChainProducerStateTest c _ p) =
+    let (c', rid) = initReader p c in
+    lookupReader c' rid == ReaderState p ReaderBackTo rid
+
+prop_update_lookup :: ChainProducerStateTest -> Bool
+prop_update_lookup (ChainProducerStateTest c rid p) =
+    let c' = updateReader rid p c in
+    lookupReader c' rid == ReaderState p ReaderBackTo rid
+
+prop_producer_sync :: TestBlockChainAndUpdates -> Bool
+prop_producer_sync (TestBlockChainAndUpdates c us) =
+    let producer0        = initChainProducerState c
+        (producer1, rid) = initReader (Chain.headPoint c) producer0
+        Just producer    = applyChainUpdates us producer1
+
+        consumer0        = c
+        consumerUpdates  = iterateReaderUntilDone rid producer
+        Just consumer    = Chain.applyChainUpdates consumerUpdates consumer0
+     in
+        consumer == producerChain producer
+  where
+    iterateReaderUntilDone rid = unfoldr (readerInstruction rid)
+
+prop_switchFork :: ChainProducerStateForkTest -> Bool
+prop_switchFork (ChainProducerStateForkTest cps f) =
+  let cps'  = switchFork f cps
+      cps'' = switchFork (chainState cps') cps'
+  in
+      invChainProducerState cps'
+      && all
+        (uncurry readerInv)
+        (zip (chainReaders cps) (chainReaders cps'))
+  where
+    readerInv :: ReaderState -> ReaderState -> Bool
+    readerInv r r'
+      -- the order of readers has not changed
+       = readerId r == readerId r'
+      -- points only move backward
+      && pointSlot (readerPoint r') <= pointSlot (readerPoint r)
+      -- if reader's point moves back, `readerNext` is changed to `ReaderBackTo`
+      && ((pointSlot (readerPoint r') < pointSlot (readerPoint r)) `imply` (readerNext r' == ReaderBackTo))
+      -- if reader's point is not changed, also next instruction is not changed
+      && ((pointSlot (readerPoint r') == pointSlot (readerPoint r)) `imply` (readerNext r' == readerNext r))
+
+    imply :: Bool -> Bool -> Bool
+    imply a b = not a || b
+
 
 --
 -- Generators
@@ -102,62 +155,6 @@ instance Arbitrary ChainProducerStateForkTest where
         if pointOnChain readerPoint c
           then r
           else r { readerPoint = headPoint c }
-
---
--- Properties
---
-
-prop_init_lookup :: ChainProducerStateTest -> Bool
-prop_init_lookup (ChainProducerStateTest c _ p) =
-    let (c', rid) = initReader p c in
-    lookupReader c' rid == ReaderState p ReaderBackTo rid
-
-prop_update_lookup :: ChainProducerStateTest -> Bool
-prop_update_lookup (ChainProducerStateTest c rid p) =
-    let c' = updateReader rid p c in
-    lookupReader c' rid == ReaderState p ReaderBackTo rid
-
-prop_producer_sync :: TestBlockChainAndUpdates -> Bool
-prop_producer_sync (TestBlockChainAndUpdates c us) =
-    let producer0        = initChainProducerState c
-        (producer1, rid) = initReader (Chain.headPoint c) producer0
-        Just producer    = applyChainUpdates us producer1
-
-        consumer0        = c
-        consumerUpdates  = iterateReaderUntilDone rid producer
-        Just consumer    = Chain.applyChainUpdates consumerUpdates consumer0
-     in
-        consumer == producerChain producer
-  where
-    iterateReaderUntilDone rid = unfoldr (readerInstruction rid)
-
-prop_switchFork :: ChainProducerStateForkTest -> Bool
-prop_switchFork (ChainProducerStateForkTest cps f) =
-  let cps'  = switchFork f cps
-      cps'' = switchFork (chainState cps') cps'
-  in
-      invChainProducerState cps'
-      && all
-        (uncurry readerInv)
-        (zip (chainReaders cps) (chainReaders cps'))
-  where
-    readerInv :: ReaderState -> ReaderState -> Bool
-    readerInv r r'
-      -- the order of readers has not changed
-       = readerId r == readerId r'
-      -- points only move backward
-      && pointSlot (readerPoint r') <= pointSlot (readerPoint r)
-      -- if reader's point moves back, `readerNext` is changed to `ReaderBackTo`
-      && ((pointSlot (readerPoint r') < pointSlot (readerPoint r)) `imply` (readerNext r' == ReaderBackTo))
-      -- if reader's point is not changed, also next instruction is not changed
-      && ((pointSlot (readerPoint r') == pointSlot (readerPoint r)) `imply` (readerNext r' == readerNext r))
-
-    imply :: Bool -> Bool -> Bool
-    imply a b = not a || b
-
---
--- Generator properties
---
 
 prop_arbitrary_ChainProducerStateForkTest :: ChainProducerStateForkTest -> Bool
 prop_arbitrary_ChainProducerStateForkTest (ChainProducerStateForkTest c f)
