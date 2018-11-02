0970f19785b72ab9017e7201d568dec0d350f8ce
Duncan Coutts <duncan@well-typed.com>
2018-11-02 13:48:59 +0000

    Improve documentation for the chain sync protocol types

--- typed-transitions/src/Protocol/ChainSync/Type.hs
+++ typed-transitions/src/Protocol/ChainSync/Type.hs
@@ -1,37 +1,43 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
 
+-- | The type of the chain synchronisation protocol.
+--
+-- Since we are using a typed protocol framework this is in some sense /the/
+-- definition of the protocol: what is allowed and what is not allowed.
+--
 module Protocol.ChainSync.Type where
 
 import Protocol.Core
 
--- | States in the chain sync system.
+
+-- | The states in the state transition diagram of the chain sync protocol.
+--
 data ChainSyncState where
 
   -- | Both client and server are idle. The client can send a request and
   -- the server is waiting for a request.
-  StIdle          :: ChainSyncState
+  StIdle      :: ChainSyncState
 
   -- | The client has sent a next update request. The client is now waiting
   -- for a response, and the server is busy getting ready to send a response.
   -- There are two possibilities here, since the server can send a reply
   -- immediately or it can send an initial await message followed later by
   -- the normal reply.
-  StNext          :: StNextKind -> ChainSyncState
+  StNext      :: StNextKind -> ChainSyncState
 
   -- | The client has sent an intersection request. The client is now waiting
   -- for a response, and the server is busy getting ready to send a response.
-  StIntersect     :: ChainSyncState
+  StIntersect :: ChainSyncState
 
   -- | Both the client and server are in the terminal state. They're done.
-  StDone          :: ChainSyncState
+  StDone      :: ChainSyncState
   --TODO: currently there is no termination mechanism
 
+-- | Sub-cases of the 'StNext' state. This is needed since the server can
+-- either send one reply back, or two.
+--
 data StNextKind where
   StCanAwait  :: StNextKind -- ^ The server can reply or send an await msg.
   StMustReply :: StNextKind -- ^ The server must now reply, having already
@@ -57,20 +63,56 @@ type family ChainSyncStatePartition st client server terminal where
   ChainSyncStatePartition  StDone      client server terminal = terminal
 
 
--- | The actual messages in our protocol.
+-- | The messages in the chain sync protocol.
 --
--- These involve transitions between different states within the 'StChainSync'
--- states.
+-- In this protocol the consumer always initiates things and the producer
+-- replies.
 --
 data ChainSyncMessage header point from to where
-  MsgRequestNext  ::                    ChainSyncMessage header point  StIdle             (StNext StCanAwait)
-  MsgAwaitReply   ::                    ChainSyncMessage header point (StNext StCanAwait) (StNext StMustReply)
-  MsgRollForward  :: header -> point -> ChainSyncMessage header point (StNext _any)        StIdle
-  MsgRollBackward :: point  -> point -> ChainSyncMessage header point (StNext _any)        StIdle
 
+  -- | Request the next update from the producer. The response can be a roll
+  -- forward, a roll back or wait.
+  --
+  MsgRequestNext :: ChainSyncMessage header point StIdle (StNext StCanAwait)
 
-  MsgFindIntersect      :: [point]        -> ChainSyncMessage header point StIdle StIntersect
+  -- | Acknowledge the request but require the consumer to wait for the next
+  -- update. This means that the consumer is synced with the producer, and
+  -- the producer its waiting for its own chain state to change.
+  --
+  MsgAwaitReply :: ChainSyncMessage header point (StNext StCanAwait) (StNext StMustReply)
+
+  -- | Tell the consumer to extend their chain with the given header.
+  --
+  -- The message also tells the consumer about the head point of the producer.
+  --
+  MsgRollForward :: header -> point -> ChainSyncMessage header point (StNext _any) StIdle
+
+  -- | Tell the consumer to roll back to a given point on their chain.
+  --
+  -- The message also tells the consumer about the head point of the producer.
+  --
+  MsgRollBackward :: point  -> point -> ChainSyncMessage header point (StNext _any) StIdle
+
+  -- | Ask the producer to try to find an improved intersection point between
+  -- the consumer and producer's chains. The consumer sends a sequence of
+  -- points and it is up to the producer to find the first intersection point
+  -- on its chain and send it back to the consumer.
+  --
+  MsgFindIntersect :: [point] -> ChainSyncMessage header point StIdle StIntersect
+
+  -- | The reply to the consumer about an intersection found, but /only/ if this
+  -- is an improvement over previously established intersection point. The
+  -- consumer can decide weather to send more points.
+  --
+  -- The message also tells the consumer about the head point of the producer.
+  --
   MsgIntersectImproved  :: point -> point -> ChainSyncMessage header point StIntersect StIdle
+
+  -- | The reply to the consumer that no intersection was found: none of the
+  -- points the consumer supplied are on the producer chain.
+  --
+  -- The message also tells the consumer about the head point of the producer.
+  --
   MsgIntersectUnchanged ::          point -> ChainSyncMessage header point StIntersect StIdle
 
 instance Show (ChainSyncMessage header point from to) where
