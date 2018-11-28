866c7305884e9f3d7e3f7920fd630031c4ea8cd2
Marcin Szamotulski <profunctor@pm.me>
2018-11-28 11:38:12 +0100

    Removed src/shell.nix
    One can use `nix-shell -A typed-transitions` to get nix shell for
    `typed-transitions`.

--- src/shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-  haskellPackages = pkgs.haskell.packages.ghc843;
-  # In case you want quantifie constraints. You'll need a bleeding-edge
-  # nixpkgs.
-  #haskellPackages = pkgs.haskell.packages.ghc861;
-in
-  { "typed-transitions" = haskellPackages.callPackage ./typed-transitions/default.nix { };
-  }
