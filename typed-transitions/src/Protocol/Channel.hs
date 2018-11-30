598266733034b2dc943bd0ebebc068a38709f58f
Alexander Vieth <aovieth@gmail.com>
2018-11-30 12:34:45 -0500

    remove useChannel and friends

--- typed-transitions/src/Protocol/Channel.hs
+++ typed-transitions/src/Protocol/Channel.hs
@@ -8,21 +8,12 @@ module Protocol.Channel
   , Channel
   , uniformChannel
   , fixedInputChannel
-  , FromStream (..)
-  , useChannel
-  , useChannelHomogeneous
   , mvarChannels
   , withMVarChannels
   , channelSendEffect
   ) where
 
 import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
-import Control.Monad (join)
-import qualified Control.Monad.Trans.Class as Trans (lift)
-import Control.Monad.Trans.Free
-import Data.Proxy
-import Protocol.Core
-import Protocol.Transition
 
 -- | Abstract duplex ordered channel.
 -- 'send' and 'recv' produce a new 'Channel m t' so that it's possible to
@@ -72,62 +63,6 @@ fixedInputChannel lst = case lst of
     }
   where
   noopSend trs = const (pure (fixedInputChannel trs))
-
-data FromStream tr k t where
-  -- | The stream ended even though more input is expected.
-  StreamEnd
-    :: (Channel m (SomeTransition tr) -> k (FromStream tr k t))
-    -> FromStream tr k t
-  -- | An unexpected transition come in.
-  StreamUnexpected :: SomeTransition tr -> FromStream tr k t
-  -- | Done using the stream.
-  StreamDone :: t -> FromStream tr k t
-
-instance Show t => Show (FromStream tr k t) where
-  show term = case term of
-    StreamEnd _        -> "StreamEnd"
-    StreamUnexpected _ -> "StreamUnexpected"
-    StreamDone t       -> "StreamDone (" ++ show t ++ ")"
-
--- | Supply input and carry output to/from a 'Peer' by way of a 'Channel'.
--- If the stream ends before the 'Peer' finishes, a continuation is given.
-useChannel
-  :: forall p tr status from to f m t .
-     ( Functor f, Monad m )
-  => Channel m (SomeTransition tr)
-  -> Peer p tr (status from) to f t
-  -> FreeT f m (FromStream tr (FreeT f m) t)
-useChannel chan peer = case peer of
-  PeerDone t -> pure (StreamDone t)
-  PeerLift f -> liftF f >>= useChannel chan
-  PeerAwait k -> do
-    next <- Trans.lift $ recv chan
-    case next of
-      (Nothing, chan') -> pure $ StreamEnd (flip useChannel peer)
-      (Just some, chan') -> case castTransition (Proxy :: Proxy from) some of
-        Unexpected -> pure (StreamUnexpected some)
-        Expected tr -> useChannel chan' (k tr)
-  PeerYield exch next -> do
-    chan' <- Trans.lift $ send chan (SomeTransition (exchangeTransition exch))
-    useChannel chan' next
-
--- | 'useChannel' where the 'Channel' and 'Peer' are in the same monad, and
--- stop the 'Peer' when/if the channel ends.
-useChannelHomogeneous
-  :: forall p tr status from to m t .
-     ( Monad m )
-  => Channel m (SomeTransition tr)
-  -> Peer p tr (status from) to m t
-  -> m (FromStream tr m t)
-useChannelHomogeneous chan peer =
-  fmap fixupFromStream (iterT join (useChannel chan peer))
-  where
-  fixupFromStream it = case it of
-    StreamDone t        -> StreamDone t
-    StreamUnexpected tr -> StreamUnexpected tr
-    StreamEnd k         -> StreamEnd $ \chan ->
-      let it = k chan
-      in  fmap fixupFromStream (iterT join it)
 
 mvarChannels :: IO (Channel IO t, Channel IO t)
 mvarChannels = do
--- typed-transitions/src/Protocol/Transition.hs
+++ typed-transitions/src/Protocol/Transition.hs
@@ -1,51 +1,14 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeInType #-}
 
 module Protocol.Transition
   ( SomeTransition (..)
   , withSomeTransition
-  , TransitionFrom (..)
-  , castTransition
-  , showTransitionFrom
   ) where
 
--- Definitions in this module are useful for working with unityped channels
--- to transport transitions between threads or processes.
--- 'SomeTransition' is a suitable unitype for encoding/decoding, and
--- 'TransitionFrom tr st' is the image of a type-safe cast from
--- 'SomeTransition tr' given a state 'st'.
-
-import Data.Kind (Type)
-import Data.Typeable
-
 data SomeTransition tr where
-  -- | Must ensure Typeable here, because we can't do it at 'castTransition'.
-  -- That would require stating that every type in the state kind is
-  -- typeable. Quantified constraints could help, although a short attempt at
-  -- that did not work.
-  SomeTransition :: ( Typeable from ) => tr from to -> SomeTransition tr
+  SomeTransition :: tr from to -> SomeTransition tr
 
 withSomeTransition :: (forall from to . tr from to -> t) -> SomeTransition tr -> t
 withSomeTransition k (SomeTransition it) = k it
-
-data TransitionFrom tr from where
-  Expected   :: tr from to -> TransitionFrom tr from
-  Unexpected :: TransitionFrom tr from
-
-showTransitionFrom :: TransitionFrom tr from -> String
-showTransitionFrom (Expected _) = "Expected"
-showTransitionFrom (Unexpected) = "Unexpected"
-
-castTransition
-  :: forall st (tr :: st -> st -> Type) (from :: st) .
-     ( Typeable from )
-  => Proxy from
-  -> SomeTransition tr
-  -> TransitionFrom tr from
-castTransition _ (SomeTransition (it :: tr from' to)) = case eqT of
-  Just (Refl :: from :~: from') -> Expected it
-  Nothing -> Unexpected
