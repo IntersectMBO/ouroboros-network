754e104af04ead66744ee30895b3dbf2f26d71da
Alexander Vieth <aovieth@gmail.com>
2018-11-30 17:56:33 -0500

    remove stale codec test in typed-transitions

--- typed-transitions/test/Main.hs
+++ typed-transitions/test/Main.hs
@@ -2,13 +2,11 @@ module Main (main) where
 
 import           Test.Tasty
 
-import qualified Test.Protocol.Codec (tests)
-
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests =
   testGroup "typed-transitions"
-  [ Test.Protocol.Codec.tests
+  [
   ]
