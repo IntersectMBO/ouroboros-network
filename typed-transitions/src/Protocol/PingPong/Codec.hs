272528f2013f0af83855386654c466c54504a5f4
Alexander Vieth <alex@well-typed.com>
2018-12-14 13:11:17 -0500

    reduce typed-transitions dependencies

--- typed-transitions/src/Protocol/PingPong/Codec.hs
+++ typed-transitions/src/Protocol/PingPong/Codec.hs
@@ -6,13 +6,11 @@
 
 module Protocol.PingPong.Codec where
 
-import Data.Text (Text, pack)
-
 import Protocol.Codec
 
 import Protocol.PingPong.Type
 
-pingPongCodec :: Monad m => Codec m Text String String PingPongMessage 'StIdle
+pingPongCodec :: Monad m => Codec m String String String PingPongMessage 'StIdle
 pingPongCodec = pingPongCodecIdle
 
 -- | Here is a complete codec for the ping/pong protocol at 'StIdle.
@@ -33,7 +31,7 @@ pingPongCodec = pingPongCodecIdle
 --   TrGood :: Transition ('Idle param) ('Busy param)
 --
 --
-pingPongCodecIdle :: Monad m => Codec m Text String String PingPongMessage 'StIdle
+pingPongCodecIdle :: Monad m => Codec m String String String PingPongMessage 'StIdle
 pingPongCodecIdle = Codec
   { encode = Encoder $ \tr -> case tr of
       MsgPing -> Encoded "ping" pingPongCodecBusy
@@ -42,7 +40,7 @@ pingPongCodecIdle = Codec
   }
   where
   decodeIdle acc = Fold $ pure $ Partial $ Response
-    { end  = pure $ Left (pack "expected ping or done")
+    { end  = pure $ Left "expected ping or done"
     , more = \strs ->
         let str = mconcat strs
         in  if length acc + length str < 4
@@ -50,10 +48,10 @@ pingPongCodecIdle = Codec
             else Fold $ pure $ Complete [drop 4 (acc ++ str)] $ pure $ case take 4 (acc ++ str) of
               "ping" -> Right $ Decoded MsgPing pingPongCodecBusy
               "done" -> Right $ Decoded MsgDone pingPongCodecDone
-              _      -> Left  $ pack "expected ping"
+              _      -> Left    "expected ping"
     }
 
-pingPongCodecBusy :: Monad m => Codec m Text String String PingPongMessage 'StBusy
+pingPongCodecBusy :: Monad m => Codec m String String String PingPongMessage 'StBusy
 pingPongCodecBusy = Codec
   { encode = Encoder $ \tr -> case tr of
       MsgPong -> Encoded "pong" pingPongCodecIdle
@@ -61,18 +59,18 @@ pingPongCodecBusy = Codec
   }
   where
   decodePong acc = Fold $ pure $ Partial $ Response
-    { end  = pure $ Left (pack "expected pong")
+    { end  = pure $ Left "expected pong"
     , more = \strs ->
         let str = mconcat strs
         in if length acc + length str < 4
            then decodePong (acc ++ str)
            else Fold $ pure $ Complete [drop 4 (acc ++ str)] $ pure $ case take 4 (acc ++ str) of
              "pong" -> Right $ Decoded MsgPong pingPongCodecIdle
-             _      -> Left  $ pack "expected pong"
+             _      -> Left    "expected pong"
     }
 
-pingPongCodecDone :: Monad m => Codec m Text String String PingPongMessage 'StDone
+pingPongCodecDone :: Monad m => Codec m String String String PingPongMessage 'StDone
 pingPongCodecDone = Codec
   { encode = Encoder $ \tr -> case tr of { }
-  , decode = Fold $ pure $ Complete [] $ pure $ Left $ pack "done"
+  , decode = Fold $ pure $ Complete [] $ pure $ Left "done"
   }
