daa2209e815a06c27fb04f82a2ed103d8c856ad2
Marcin Szamotulski <profunctor@pm.me>
2018-12-14 20:56:13 +0100

    nix changes in ouroboros-network and type-transitions
    * use sourceFilesBySuffices
    * and set enableSeparateDocOutput to false

--- ouroboros-network/default.nix
+++ ouroboros-network/default.nix
@@ -1,12 +1,13 @@
 { mkDerivation, array, async, base, bytestring, cborg, clock
 , containers, free, hashable, mtl, network, pipes, process
 , psqueues, QuickCheck, serialise, stdenv, stm, tasty
-, tasty-quickcheck, text, typed-transitions
+, tasty-quickcheck, text, typed-transitions, nixpkgs
 }:
 mkDerivation {
   pname = "ouroboros-network";
   version = "0.1.0.0";
-  src = ./.;
+  src = nixpkgs.lib.sourceFilesBySuffices ./.
+    [ ".hs" "LICENSE" "ouroboros-network.cabal" ];
   libraryHaskellDepends = [
     array async base bytestring cborg clock containers free hashable
     mtl network pipes process psqueues QuickCheck serialise stm text
@@ -19,4 +20,5 @@ mkDerivation {
   description = "A networking layer for the Ouroboros blockchain protocol";
   license = stdenv.lib.licenses.mit;
   hydraPlatforms = stdenv.lib.platforms.none;
+  enableSeparateDocOutput = false;
 }
