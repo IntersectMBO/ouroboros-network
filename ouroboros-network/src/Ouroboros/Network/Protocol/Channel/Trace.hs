edcb0ef9e08aef4a085ed99930b75fc35b0a5238
Alexander Vieth <alex@well-typed.com>
2018-12-17 11:08:51 -0500

    use channelEffect for tracing

--- ouroboros-network/src/Ouroboros/Network/Protocol/Channel/Trace.hs
+++ ouroboros-network/src/Ouroboros/Network/Protocol/Channel/Trace.hs
@@ -4,26 +4,26 @@ import Cardano.BM.BaseTrace
 
 import Protocol.Channel
 
--- TBD log before or after? Before seems better...
+-- | Trace every sent message before sending it, and every receiving message
+-- (or Nothing for end of stream) after receiving it.
+traceChannel
+  :: ( Applicative n, Monad m )
+  => BaseTrace n send
+  -> BaseTrace m (Maybe recv)
+  -> Duplex n m send recv
+  -> Duplex n m send recv
+traceChannel trSend trRecv = channelEffect (traceWith trSend) (traceWith trRecv)
+
 traceSend
-  :: ( Applicative m, Functor n )
+  :: ( Applicative m, Monad n )
   => BaseTrace m send
   -> Duplex m n send recv
   -> Duplex m n send recv
-traceSend bt duplex = duplex
-  { send = \item -> traceWith bt item *> fmap (traceSend bt) (send duplex item)
-  , recv = (fmap . fmap) (traceSend bt) (recv duplex)
-  }
+traceSend tr = channelSendEffect (traceWith tr)
 
--- | Whereas 'traceSend' needs only `Applicative m`, 'traceRecv' needs
--- `Monad m` because it traces a thing produced within `m`.
 traceRecv
-  :: ( Monad m, Functor n )
-  => BaseTrace m (Maybe recv) -- ^ Nothing means end of stream.
+  :: ( Monad m, Applicative n )
+  => BaseTrace m (Maybe recv)
   -> Duplex n m send recv
   -> Duplex n m send recv
-traceRecv bt duplex = duplex
-  { send = fmap (traceRecv bt) . send duplex
-  , recv = recv duplex >>= \(item, duplex') ->
-      traceWith bt item >> pure (item, traceRecv bt duplex')
-  }
+traceRecv tr = channelRecvEffect (traceWith tr)
