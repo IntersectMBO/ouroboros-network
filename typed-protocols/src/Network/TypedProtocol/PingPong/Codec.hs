7eadc3be20f211d2bcbe39bbea917a4a6f437d29
Duncan Coutts <duncan@well-typed.com>
2019-01-28 12:42:12 +0000

    Update PingPong codec
    Use the split client/server style.
    
    We could do a unified style if we introduce a value level representation
    of whether it's the client or server's turn.

--- typed-protocols/src/Network/TypedProtocol/PingPong/Codec.hs
+++ typed-protocols/src/Network/TypedProtocol/PingPong/Codec.hs
@@ -1,35 +1,115 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Network.TypedProtocol.PingPong.Codec where
 
 import           Network.TypedProtocol.Codec
 import           Network.TypedProtocol.PingPong.Type
 
-pingPongCodec
+codecPingPongAsClient
   :: forall m. Monad m
-  => Codec PingPongState String m String
-pingPongCodec = Codec encodeMsg decodeMsg
+  => Codec PingPong AsClient String m String
+codecPingPongAsClient =
+    Codec{encode, decode}
   where
-   encodeMsg :: forall (st :: PingPongState) (st' :: PingPongState). Message st st' -> String
-   encodeMsg MsgPing = "ping"
-   encodeMsg MsgPong = "pong"
-   encodeMsg MsgDone = "done"
+   encode :: forall (st  :: PingPong)
+                    (st' :: PingPong).
+             ClientHasAgency st
+          -> Message st st'
+          -> String
+   encode TokIdle MsgPing = "ping\n"
+   encode TokIdle MsgDone = "done\n"
 
-   decodeMsg :: forall (st :: PingPongState). StateToken st -> m (DecodeStep String String m (SomeMessage st))
-   decodeMsg tok = return $ Partial $ \bytes -> case bytes of
-     Nothing  -> return $ Fail "not enough input"
-     Just bts -> case decodePingPongMessage tok bts of
-       Just msg -> return $ Done msg Nothing
-       Nothing  -> return $ Fail "wrong input"
+   decode :: forall (st :: PingPong).
+             ServerHasAgency st
+          -> m (DecodeStep String String m (SomeMessage st))
+   decode TokBusy =
+     decodeStepConsumeN 5 $ \str trailing ->
+       case str of
+         "pong\n" -> Done (SomeMessage MsgPong) trailing
+         _        -> Fail ("unexpected message: " ++ str)
 
-decodePingPongMessage :: forall (st :: PingPongState).
-                         StateToken st
-                      -> String
-                      -> Maybe (SomeMessage st)
-decodePingPongMessage TokIdle "ping" = Just (SomeMessage MsgPing)
-decodePingPongMessage TokIdle "done" = Just (SomeMessage MsgDone)
-decodePingPongMessage TokBusy "pong" = Just (SomeMessage MsgPong)
-decodePingPongMessage _       _      = Nothing
+
+codecPingPongAsServer
+  :: forall m. Monad m
+  => Codec PingPong AsServer String m String
+codecPingPongAsServer =
+    Codec{encode, decode}
+  where
+   encode :: forall (st  :: PingPong)
+                    (st' :: PingPong).
+             ServerHasAgency st
+          -> Message st st'
+          -> String
+   encode TokBusy MsgPong = "pong\n"
+
+   decode :: forall (st :: PingPong).
+             ClientHasAgency st
+          -> m (DecodeStep String String m (SomeMessage st))
+   decode TokIdle =
+     decodeStepConsumeN 5 $ \str trailing ->
+       case str of
+         "ping\n" -> Done (SomeMessage MsgPing) trailing
+         "done\n" -> Done (SomeMessage MsgDone) trailing
+         _        -> Fail ("unexpected message: " ++ str)
+
+{-
+codecPingPong
+  :: forall pk m. Monad m
+  => Codec PingPong (pk :: PeerKind) String m String
+codecPingPong =
+    Codec{encode, decode}
+  where
+   encode :: forall (st  :: PingPong)
+                    (st' :: PingPong).
+             WeHaveAgency pk st
+          -> Message st st'
+          -> String
+   encode TokIdle MsgPing = "ping\n"
+   encode TokIdle MsgDone = "done\n"
+   encode TokBusy MsgPong = "pong\n"
+
+   decode :: forall (st :: PingPong).
+             TheyHaveAgency pk st
+          -> m (DecodeStep String String m (SomeMessage st))
+   decode stok =
+     decodeStepConsumeN 5 $ \str trailing ->
+       case (stok, str) of
+         (TokBusy, "pong\n") -> Done (SomeMessage MsgPong) trailing
+         (TokIdle, "ping\n") -> Done (SomeMessage MsgPing) trailing
+         (TokIdle, "done\n") -> Done (SomeMessage MsgDone) trailing
+         _                   -> Fail ("unexpected message: " ++ str)
+-}
+
+decodeStepConsumeN :: forall m a.
+                       Monad m
+                    => Int
+                    -> (String -> Maybe String -> DecodeStep String String m a)
+                    -> m (DecodeStep String String m a)
+decodeStepConsumeN n k = go [] n
+  where
+    go :: [String] -> Int -> m (DecodeStep String String m a)
+    go chunks required =
+      return $ Partial $ \mchunk ->
+        case mchunk of
+          Nothing -> return $ Fail "not enough input"
+          Just chunk
+            | length chunk >= required
+           -> let (c,c') = splitAt required chunk in
+              return $ k (concat (reverse (c:chunks)))
+                         (if null c' then Nothing else Just c)
+
+            | otherwise
+           -> go (chunk : chunks) (required - length chunk)
+
+{-
+prop_decodeStepSplitAt n = do
+    chan <- fixedInputChannel ["ping", "pong"]
+    runDecoder chan Nothing decoder
+  where
+    decoder = decodeStepSplitAt n Done)
+-}
