0bd4e6348b882f3bc949aefd11e39a39ce466acf
Duncan Coutts <duncan@well-typed.com>
2019-01-28 12:51:41 +0000

    Move ping/pong examples to Examples module
    We can use these in the tests, but it separates the examples clearly and
    avoids compiling them into the library.

--- typed-protocols/src/Network/TypedProtocol/PingPong/Client.hs
+++ typed-protocols/src/Network/TypedProtocol/PingPong/Client.hs
@@ -43,23 +43,6 @@ data PingPongClient m a where
   SendMsgDone    :: a -> PingPongClient m a
 
 
--- | An example ping-pong client that sends pings as fast as possible forever‽
---
--- This may not be a good idea‼
---
-pingPongClientFlood :: Applicative m => PingPongClient m a
-pingPongClientFlood = SendMsgPing (pure pingPongClientFlood)
-
--- | An example ping-pong client that sends a fixed number of ping messages
--- and then stops.
---
-pingPongClientCount
-  :: Applicative m
-  => Natural
-  -> PingPongClient m ()
-pingPongClientCount 0 = SendMsgDone ()
-pingPongClientCount m = SendMsgPing $ pure (pingPongClientCount (pred m))
-
 -- | Interpret a particular client action sequence into the client side of the
 -- 'PingPong' protocol.
 --
