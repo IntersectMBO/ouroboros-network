06d483cf6ed63747132aed0f4b50cb6f1be91dd6
Duncan Coutts <duncan@well-typed.com>
2019-01-29 15:02:24 +0000

    Move forgetPipelinining to Proofs module and restructure
    Make it a bit more symmetric and more similar to the connectPipelined by
    separating into two mutually recursive 'go' functions.

--- typed-protocols/src/Network/TypedProtocol/Pipelined.hs
+++ typed-protocols/src/Network/TypedProtocol/Pipelined.hs
@@ -39,18 +39,3 @@ data PeerReceiver ps (pk :: PeerKind) (st :: ps) (stdone :: ps) m where
                               -> PeerReceiver ps pk st' stdone m)
                  -> PeerReceiver ps pk st stdone m
 
-forgetPipelinining
-  :: forall (pk :: PeerKind) (st :: ps) m a. Functor m
-  => PeerSender pk st m a
-  -> Peer pk st m a
-forgetPipelinining (SenderEffect mp)   = Effect (forgetPipelinining <$> mp)
-forgetPipelinining (SenderDone stok a) = Done stok a
-forgetPipelinining (PipelinedYield stok msg recv next) = Yield stok msg (go recv next)
- where
-  go :: PeerReceiver pk stCurrent stNext m
-     -> PeerSender pk stNext m a
-     -> Peer pk stCurrent m a
-  go (ReceiverEffect mr) next' = Effect (flip go next' <$> mr)
-  go ReceiverDone        next' = forgetPipelinining next'
-  go (ReceiverAwait stok'' f) next' = Await stok'' (flip go next' . f)
-forgetPipelinining (SenderYield stok msg next) = Yield stok msg (forgetPipelinining next)
