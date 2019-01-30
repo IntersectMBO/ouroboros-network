6e26f1ae137347ad1153a8e7ed48a0f25849f8b1
Marko Dimjašević <marko.dimjasevic@iohk.io>
2019-01-30 14:24:06 +0100

    Issue #168: WIP - implementing applyChainState

--- ouroboros-consensus/src/Ouroboros/Consensus/Protocol/PermBFT.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Protocol/PermBFT.hs
@@ -2,13 +2,14 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Ouroboros.Consensus.Protocol.PermBFT (
   ) where
 
-import           Data.Void
+import           Control.Monad.Trans.Except (except)
 import           GHC.Generics (Generic)
 import           Ouroboros.Consensus.Protocol.Abstract
 
@@ -19,25 +20,57 @@ import           Ouroboros.Consensus.Crypto.DSIGN.Ed448 (Ed448DSIGN)
 import           Ouroboros.Consensus.Crypto.DSIGN.Mock (MockDSIGN)
 import           Ouroboros.Consensus.Util.Condense
 
+import           Chain.Blockchain (BlockchainEnv(..), KeyToQMap, T)
+import           Chain.GenesisBlock (genesisBlock)
+import           Control.State.Transition (applySTS, PredicateFailure, Signal, State, IRC(IRC), TRC(TRC))
+import           Ledger.Core (SlotCount(SlotCount))
+import           Ledger.Delegation (DIEnv)
+import           Types (BC, Block(GBlock, RBlock), ProtParams(..))
+
+
 data PermBft c
 
 instance PermBftCrypto c => OuroborosTag (PermBft c) where
   -- | The BFT payload is just the signature
   newtype Payload (PermBft c) ph = PermBftPayload {
-        something :: SignedDSIGN (PermBftDSIGN c) ph
+        permBftSignature :: SignedDSIGN (PermBftDSIGN c) ph
       }
     deriving (Generic)
 
-  data NodeConfig (PermBft c) = PermBftNodeConfig {
-      unNodeConfig :: Void
+  data NodeConfig (PermBft c) = PermBftNodeConfig
+    { permBftSignKey :: SignKeyDSIGN (PermBftDSIGN c)
+    , protParams     :: ProtParams
+    , kSize          :: SlotCount
+    , tRatio         :: T
     }
 
-  type ValidationErr  (PermBft c) = PermBftValidationErr
+  type ValidationErr  (PermBft c) = [PredicateFailure BC]
   type SupportedBlock (PermBft c) = HasPayload (PermBft c)
   type NodeState      (PermBft c) = ()
-  type LedgerView     (PermBft c) = ()
+  type LedgerView     (PermBft c) = DIEnv
   type IsLeader       (PermBft c) = ()
-  type ChainState     (PermBft c) = ()
+  type ChainState     (PermBft c) = State BC
+
+  mkPayload PermBftNodeConfig{..} _ preheader = do
+    signature <- signedDSIGN preheader permBftSignKey
+    return $ PermBftPayload { permBftSignature = signature }
+
+  checkIsLeader _ _ _ _ = return Nothing
+
+  applyChainState PermBftNodeConfig{..} lv b chainSt@(_, prevBlock, _) =
+    except $
+      if prevBlock == genesisBlock
+        then applySTS $ IRC bcEnv
+        else applySTS $ TRC (bcEnv, chainSt, b)
+   where
+    bcEnv :: BlockchainEnv
+    bcEnv = MkBlockChainEnv
+      { bcEnvPp    = protParams
+      , bcEnvDIEnv = lv
+      , bcEnvK     = kSize
+      , bcEnvT     = tRatio
+      }
+
 
 deriving instance PermBftCrypto c => Show     (Payload (PermBft c) ph)
 deriving instance PermBftCrypto c => Eq       (Payload (PermBft c) ph)
@@ -48,11 +81,8 @@ instance PermBftCrypto c => Serialise (Payload (PermBft c) ph) where
   -- use generic instance
 
 {-------------------------------------------------------------------------------
-  BFT specific types
+  Permissive BFT specific types
 -------------------------------------------------------------------------------}
-
-data PermBftValidationErr = PermBftInvalidSignature
-  deriving (Show)
 
 {-------------------------------------------------------------------------------
   Crypto models
