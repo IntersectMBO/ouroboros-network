3a99ca3575484cc67e4924eca087f95a48bad896
Marko Dimjašević <marko.dimjasevic@iohk.io>
2019-02-05 14:15:42 +0100

    Issue #168: WIP - implementing ledger state

--- ouroboros-consensus/src/Ouroboros/Consensus/Ledger/PermBFT.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Ledger/PermBFT.hs
@@ -1,31 +1,40 @@
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE TypeFamilies            #-}
+{-# LANGUAGE UndecidableInstances    #-}
+
 module Ouroboros.Consensus.Ledger.PermBFT where
 
 import           Ouroboros.Consensus.Ledger.Abstract
-                 ( UpdateLedger(..)
+                 (
+                   BlockProtocol
                  , ProtocolLedgerView(..)
+                 , UpdateLedger(..)
                  )
+import           Ouroboros.Consensus.Protocol.Abstract
+                 (
+                   OuroborosTag
+                 , SupportedBlock
+                 )
+import           Ouroboros.Network.Block (HasHeader (..))
 
-type BlType = (Block (PermBft PermBftMockCrypto))
+import           Types (Block)
 
-
-instance ( Show (LedgerState BlType)
-         , Show (LedgerError BlType)
-         ) => UpdateLedger BlType where
-  data LedgerState BlType =
+instance ( Show (LedgerState Block)
+         , Show (LedgerError Block)
+         ) => UpdateLedger Block where
+  data LedgerState Block =
     MkLedgerState deriving Show
 
-  data LedgerError BlType =
+  data LedgerError Block =
     UndefinedLedgerError deriving Show
 
   -- Apply a block to the ledger state
   applyLedgerState b MkLedgerState = undefined
 
 
-instance ( OuroborosTag (BlockProtocol BlType)
-         , UpdateLedger BlType
-         , HasHeader BlType
-         , SupportedBlock (BlockProtocol BlType) BlType
-         ) => ProtocolLedgerView BlType where
+instance ( OuroborosTag (BlockProtocol Block)
+         , UpdateLedger Block
+         , HasHeader Block
+         , SupportedBlock (BlockProtocol Block) Block
+         ) => ProtocolLedgerView Block where
   protocolLedgerView = undefined
-
-
