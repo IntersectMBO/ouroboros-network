f7b00bacc725fe510593c5be5b99f137109d9dff
Marcin Szamotulski <profunctor@pm.me>
2019-02-06 14:42:05 +0100

    Added identity codec to ChainSync.Codec

--- ouroboros-network/src/Ouroboros/Network/Protocol/ChainSync/Codec.hs
+++ ouroboros-network/src/Ouroboros/Network/Protocol/ChainSync/Codec.hs
@@ -7,6 +7,7 @@
 
 module Ouroboros.Network.Protocol.ChainSync.Codec
   ( codecChainSync
+  , codecChainSyncId
   ) where
 
 import           Control.Monad.Class.MonadST
@@ -27,6 +28,8 @@ import Codec.Serialise.Class (Serialise)
 import qualified Codec.Serialise.Class as CBOR
 
 
+-- | The main CBOR 'Codec' for the 'ChainSync' protocol.
+--
 codecChainSync :: forall header point m.
                   (MonadST m, Serialise header, Serialise point)
                => Codec (ChainSync header point)
@@ -104,3 +107,42 @@ codecChainSync =
 
         _ -> fail ("codecChainSync: unexpected key " ++ show (key, len))
 
+
+-- | An identity 'Codec' for the 'ChainSync' protocol. It does not do any
+-- serialisation. It keeps the typed messages, wrapped in 'AnyMessage'.
+--
+codecChainSyncId :: forall header point m. Monad m
+                 => Codec (ChainSync header point)
+                          String m (AnyMessage (ChainSync header point))
+codecChainSyncId = Codec encode decode
+ where
+  encode :: forall (pr :: PeerRole) st st'.
+            PeerHasAgency pr st
+         -> Message (ChainSync header point) st st'
+         -> AnyMessage (ChainSync header point)
+  encode _ = AnyMessage
+
+  decode :: forall (pr :: PeerRole) st.
+            PeerHasAgency pr st
+         -> m (DecodeStep (AnyMessage (ChainSync header point)) String m (SomeMessage st))
+  decode stok = return $ DecodePartial $ \bytes -> case (stok, bytes) of
+
+    (_, Nothing) -> return $ DecodeFail "codecChainSync: not enough input"
+
+    (ClientAgency TokIdle, Just (AnyMessage msg@MsgRequestNext)) -> return (DecodeDone (SomeMessage msg) Nothing)
+
+    (ServerAgency (TokNext TokCanAwait), Just (AnyMessage msg@MsgAwaitReply)) -> return (DecodeDone (SomeMessage msg) Nothing)
+
+    (ServerAgency (TokNext _), Just (AnyMessage (MsgRollForward h p))) -> return (DecodeDone (SomeMessage (MsgRollForward h p)) Nothing)
+
+    (ServerAgency (TokNext _), Just (AnyMessage (MsgRollBackward p1 p2))) -> return (DecodeDone (SomeMessage (MsgRollBackward p1 p2)) Nothing)
+
+    (ClientAgency TokIdle, Just (AnyMessage (MsgFindIntersect ps))) -> return (DecodeDone (SomeMessage (MsgFindIntersect ps)) Nothing)
+
+    (ServerAgency TokIntersect, Just (AnyMessage (MsgIntersectImproved p1 p2))) -> return (DecodeDone (SomeMessage (MsgIntersectImproved p1 p2)) Nothing)
+
+    (ServerAgency TokIntersect, Just (AnyMessage (MsgIntersectUnchanged p))) -> return (DecodeDone (SomeMessage (MsgIntersectUnchanged p)) Nothing)
+
+    (ClientAgency TokIdle, Just (AnyMessage MsgDone)) -> return (DecodeDone (SomeMessage MsgDone) Nothing)
+
+    (_, _) -> return $ DecodeFail "codecChainSync: no matching message"
