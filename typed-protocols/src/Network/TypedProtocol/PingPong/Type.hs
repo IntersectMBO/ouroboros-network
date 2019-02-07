2b622af7ab5eee004c9f9b33fa8a88f3ba5fb493
Duncan Coutts <duncan@well-typed.com>
2019-02-07 12:19:52 +0000

    Update the ping/pong tests
    Move the direct and connect stuff into the per-protocol Test module, but
    only build the Examples and Test modules as part of the testsuite, not
    as part of the main library.
    
    So the template I'm trying to establish here for the mini-protocols is
    that we have one set of modules for each protocol, in the same place in
    the file system, but some are built into the libraries, and the others
    in the tests.

--- typed-protocols/src/Network/TypedProtocol/PingPong/Type.hs
+++ typed-protocols/src/Network/TypedProtocol/PingPong/Type.hs
@@ -3,13 +3,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE EmptyCase #-}
 
 
 module Network.TypedProtocol.PingPong.Type where
 
 import           Network.TypedProtocol.Core
-import           Network.TypedProtocol.Proofs
 
 
 -- | The ping\/pong protocol and the states in its protocol state machine.
@@ -75,14 +73,4 @@ instance Protocol PingPong where
 
 
 deriving instance Show (Message PingPong from to)
-
-
--- | 'AgencyProofs' for the 'PingPong' protocol
---
-pingPongAgencyProofs :: AgencyProofs PingPong
-pingPongAgencyProofs = AgencyProofs {
-    proofByContradiction_ClientAndServerHaveAgency = \TokIdle tok -> case tok of {},
-    proofByContradiction_NobodyAndClientHaveAgency = \TokDone tok -> case tok of {},
-    proofByContradiction_NobodyAndServerHaveAgency = \TokDone tok -> case tok of {}
-  }
 
