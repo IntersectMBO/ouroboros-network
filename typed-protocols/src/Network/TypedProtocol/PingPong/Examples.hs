f15c172c6aadc1acb38f1b20affd2c7eda680e1a
Duncan Coutts <duncan@well-typed.com>
2019-02-11 19:48:38 +0000

    Update and extend ping/pong examples and tests.
    Would be nice to add pipelined versions of the channel tests.

--- typed-protocols/src/Network/TypedProtocol/PingPong/Examples.hs
+++ typed-protocols/src/Network/TypedProtocol/PingPong/Examples.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE BangPatterns        #-}
 
 module Network.TypedProtocol.PingPong.Examples where
@@ -5,7 +8,7 @@ module Network.TypedProtocol.PingPong.Examples where
 import           Network.TypedProtocol.PingPong.Server
 import           Network.TypedProtocol.PingPong.Client
 
-import           Control.Monad.Class.MonadSTM
+import           Network.TypedProtocol.Pipelined
 
 
 -- | The standard stateless ping-pong server instance.
@@ -28,10 +31,10 @@ pingPongServerCount
   => PingPongServer m Int
 pingPongServerCount = go 0
   where
-   go !c = PingPongServer {
-      recvMsgPing = pure $ go (succ c),
-      recvMsgDone = c
-    }
+    go !c = PingPongServer {
+              recvMsgPing = pure $ go (succ c),
+              recvMsgDone = c
+            }
 
 
 -- | An example ping-pong client that sends pings as fast as possible forever‽
@@ -50,14 +53,99 @@ pingPongClientCount 0 = SendMsgDone ()
 pingPongClientCount n = SendMsgPing (pure (pingPongClientCount (n-1)))
 
 
-pingPongSenderCount
-  :: MonadSTM m
-  => TVar m Int
-  -> Int
-  -> PingPongSender m ()
-pingPongSenderCount var = go
- where
-  go 0 = SendMsgDonePipelined ()
-  go n = SendMsgPingPipelined (atomically $ modifyTVar var succ)
-                              (go (pred n))
+--
+-- Pipelined examples
+--
 
+
+-- | A pipelined ping-pong client that sends eagerly rather than waiting to
+-- collect any replies. This is maximum pipelining in some sense, and
+-- correspondingly it gives minimum choice to the environment (drivers).
+--
+-- It returns the interleaving of ping indexes sent, and collected.
+--
+pingPongClientPipelinedMax
+  :: forall m. Monad m
+  => Int
+  -> PingPongClientPipelined m [Either Int Int]
+pingPongClientPipelinedMax c =
+    PingPongClientPipelined (go [] Zero 0)
+  where
+    go :: [Either Int Int] -> Nat o -> Int
+       -> PingPongSender o Int m [Either Int Int]
+    go acc o        n | n < c
+                      = SendMsgPingPipelined
+                          (return n)
+                          (go (Left n : acc) (Succ o) (succ n))
+    go acc Zero     _ = SendMsgDonePipelined (reverse acc)
+    go acc (Succ o) n = CollectPipelined
+                          Nothing
+                          (\n' -> go (Right n' : acc) o n)
+
+
+-- | A pipelined ping-pong client that sends eagerly but always tries to
+-- collect any replies if they are available.  This allows pipelining but
+-- keeps it to a minimum, and correspondingly it gives maximum choice to the
+-- environment (drivers).
+--
+-- It returns the interleaving of ping indexes sent, and collected.
+--
+pingPongClientPipelinedMin
+  :: forall m. Monad m
+  => Int
+  -> PingPongClientPipelined m [Either Int Int]
+pingPongClientPipelinedMin c =
+    PingPongClientPipelined (go [] Zero 0)
+  where
+    go :: [Either Int Int] -> Nat o -> Int
+       -> PingPongSender o Int m [Either Int Int]
+    go acc (Succ o) n = CollectPipelined
+                          (if n < c then Just (ping acc (Succ o) n)
+                                    else Nothing)
+                          (\n' -> go (Right n' : acc) o n)
+    go acc Zero     n | n < c
+                      = ping acc Zero n
+    go acc Zero     _ = SendMsgDonePipelined (reverse acc)
+
+    ping :: [Either Int Int] -> Nat o -> Int
+         -> PingPongSender o Int m [Either Int Int]
+    ping acc o      n = SendMsgPingPipelined
+                          (return n)
+                          (go (Left n : acc) (Succ o) (succ n))
+
+
+-- | A pipelined ping-pong client that sends eagerly up to some maximum limit
+-- of outstanding requests. It is also always ready to collect any replies if
+-- they are available.  This allows limited pipelining and correspondingly
+-- limited choice to the environment (drivers).
+--
+-- It returns the interleaving of ping indexes sent, and collected.
+--
+pingPongClientPipelinedLimited
+  :: forall m. Monad m
+  => Int -> Int
+  -> PingPongClientPipelined m [Either Int Int]
+pingPongClientPipelinedLimited omax c =
+    PingPongClientPipelined (go [] Zero 0)
+  where
+    go :: [Either Int Int] -> Nat o -> Int
+       -> PingPongSender o Int m [Either Int Int]
+    go acc (Succ o) n = CollectPipelined
+                          (if n < c && int (Succ o) < omax
+                             then Just (ping acc (Succ o) n)
+                             else Nothing)
+                          (\n' -> go (Right n' : acc) o n)
+    go acc Zero     n | n < c
+                      = ping acc Zero n
+    go acc Zero     _ = SendMsgDonePipelined (reverse acc)
+
+    ping :: [Either Int Int] -> Nat o -> Int
+         -> PingPongSender o Int m [Either Int Int]
+    ping acc o      n = SendMsgPingPipelined
+                          (return n)
+                          (go (Left n : acc) (Succ o) (succ n))
+
+    -- this isn't supposed to be efficient, it's just for the example
+    int :: Nat n -> Int
+    int Zero     = 0
+    int (Succ n) = succ (int n)
