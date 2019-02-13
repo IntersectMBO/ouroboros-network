50d5227ac86096e2cf540b097f5a6979fd1adc1a
Marcin Szamotulski <profunctor@pm.me>
2019-02-13 15:06:46 +0100

    Added channel tests for block fetch

--- ouroboros-network/test/Test/Ouroboros/Network/Protocol/BlockFetch.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/Protocol/BlockFetch.hs
@@ -3,9 +3,16 @@
 
 module Test.Ouroboros.Network.Protocol.BlockFetch where
 
-import           Control.Monad.IOSim (SimM, runSim)
+import           Data.ByteString.Lazy (ByteString)
 
+import           Control.Monad.IOSim (SimM, runSim)
+import           Control.Monad.Class.MonadST (MonadST (..))
+import           Control.Monad.Class.MonadSTM (MonadSTM)
+import           Control.Monad.Class.MonadAsync (MonadAsync (..))
+
+import           Network.TypedProtocol.Driver
 import           Network.TypedProtocol.Proofs
+import           Ouroboros.Network.Channel
 
 import           Ouroboros.Network.Chain (Chain, Point)
 import qualified Ouroboros.Network.Chain as Chain
@@ -15,11 +22,13 @@ import qualified Ouroboros.Network.Testing.ConcreteBlock as ConcreteBlock
 import           Ouroboros.Network.Protocol.BlockFetch.Type
 import           Ouroboros.Network.Protocol.BlockFetch.Client
 import           Ouroboros.Network.Protocol.BlockFetch.Server
-import           Ouroboros.Network.Protocol.BlockFetch.Direct (direct)
+import           Ouroboros.Network.Protocol.BlockFetch.Direct
 import           Ouroboros.Network.Protocol.BlockFetch.Examples
+import           Ouroboros.Network.Protocol.BlockFetch.Codec
 
 import           Test.ChainGenerators (TestChainAndPoints (..))
 
+import           Test.QuickCheck
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.QuickCheck (testProperty)
 
@@ -29,6 +38,9 @@ tests =
   testGroup "Ouroboros.Network.Protocol.BlockFetch"
   [ testProperty "direct"  prop_direct
   , testProperty "connect" prop_connect
+  , testProperty "channel ST"          prop_channel_ST
+  , testProperty "channel IO"          prop_channel_IO
+  , testProperty "pipe IO"             prop_pipe_IO
   ]
 
 
@@ -77,6 +89,72 @@ prop_connect (TestChainAndPoints chain points) =
 
 
 --
+-- Experiments using a channel
+--
+
+-- | Run a simple block-fetch client and server using two complementary channels.
+--
+channel_experiment
+  :: forall m.
+     ( MonadST m
+     , MonadSTM m
+     , MonadAsync m
+     )
+  => Channel m ByteString
+  -> Channel m ByteString
+  -> Chain Block
+  -> [Point Block]
+  -> m Property
+channel_experiment clientChannel serverChannel chain points = do
+  let client = blockFetchClientMap (pointsToRanges chain points)
+      server = blockFetchServer (ConcreteBlock.blockBody <$> rangeRequestsFromChain chain)
+
+  withAsync (runPeer codecBlockFetch serverChannel (blockFetchServerPeer server))
+    $ \serverAsync -> withAsync (runPeer codecBlockFetch clientChannel (blockFetchClientPeer client))
+      $ \clientAsync -> do
+
+      bodies' <- wait clientAsync
+      cancel serverAsync
+
+      return ((reverse <$> bodies') === Right (concat (receivedBlockBodies chain points)))
+
+-- |
+-- Run @'channel_experiment'@ in the simulation monad.
+--
+prop_channel_ST
+  :: TestChainAndPoints
+  -> Property
+prop_channel_ST (TestChainAndPoints chain points) =
+  let r = runSim $ do
+        (ca, cb) <- createConnectedChannels
+        channel_experiment ca cb chain points
+  in case r of
+    Right p -> p
+    _       -> property False
+
+
+-- |
+-- Run @'channel_experiment'@ in the IO monad.
+--
+prop_channel_IO
+  :: TestChainAndPoints
+  -> Property
+prop_channel_IO (TestChainAndPoints chain points) = ioProperty $ do
+  (ca, cb) <- createConnectedChannels
+  channel_experiment ca cb chain points
+
+-- |
+-- Run @'channel_experiment'@ in the IO monad using local pipes.
+--
+prop_pipe_IO
+  :: TestChainAndPoints
+  -> Property
+prop_pipe_IO (TestChainAndPoints chain points) = ioProperty $ do
+  (ca, cb) <- createPipeConnectedChannels
+  channel_experiment ca cb chain points
+
+
+--
 -- Auxilary functions
 --
 
@@ -119,4 +197,3 @@ receivedBlockBodies chain points =
       case Chain.selectBlockRange chain from to of
         Nothing -> []
         Just bs -> map ConcreteBlock.blockBody bs
-
