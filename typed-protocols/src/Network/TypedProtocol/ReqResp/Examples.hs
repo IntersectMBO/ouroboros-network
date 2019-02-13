c70347412b865204479a04f37151af8edbe57377
Duncan Coutts <duncan@well-typed.com>
2019-02-13 13:57:24 +0000

    Update ReqResp examples and direct tests.
    And extend the ReqResp tests to follow the template established by the
    PingPong tests.
    
    The natural structure for the ReqResp examples used in the tests is that
    the client does a map, while the serve does a mapAcuumL. This is nice in
    the tests since we can use mapAccumL as the reference spec.

--- typed-protocols/src/Network/TypedProtocol/ReqResp/Examples.hs
+++ typed-protocols/src/Network/TypedProtocol/ReqResp/Examples.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE BangPatterns        #-}
 
 module Network.TypedProtocol.ReqResp.Examples where
 
 import           Network.TypedProtocol.ReqResp.Server
 import           Network.TypedProtocol.ReqResp.Client
+
+import           Network.TypedProtocol.Pipelined
 
 
 -- | A request\/response server instance that computes a 'Data.List.mapAccumL'
@@ -21,7 +26,6 @@ reqRespServerMapAccumL f !acc =
     }
 
 
-
 -- | An example request\/response client that sends the given list of requests
 -- and collects the list of responses.
 --
@@ -35,3 +39,45 @@ reqRespClientMap = go []
       SendMsgReq req $ \resp ->
       return (go (resp:resps) reqs)
 
+
+--
+-- Pipelined example
+--
+
+-- | An example request\/response client that sends the given list of requests
+-- and collects the list of responses.
+--
+-- It is pipelined and tries to collect any replies if they are available.
+-- This allows pipelining but keeps it to a minimum, and correspondingly it
+-- gives maximum choice to the environment (drivers).
+--
+-- In theory, with enough and large enough requests and responses, this should
+-- be able to saturate any channel of any bandwidth and latency, because it
+-- should be able to have both peers send essentially continuously.
+--
+reqRespClientMapPipelined :: forall req resp m.
+                             Monad m
+                          => [req]
+                          -> ReqRespClientPipelined req resp m [resp]
+reqRespClientMapPipelined reqs0 =
+    ReqRespClientPipelined (go [] Zero reqs0)
+  where
+    go :: [resp] -> Nat o -> [req] -> ReqRespSender req resp o resp m [resp]
+    go resps Zero reqs =
+      case reqs of
+        []        -> SendMsgDonePipelined (reverse resps)
+        req:reqs' -> sendReq resps Zero req reqs'
+
+    go resps (Succ o) reqs =
+      CollectPipelined
+        (case reqs of
+           []        -> Nothing
+           req:reqs' -> Just (sendReq resps (Succ o) req reqs'))
+        (\resp -> go (resp:resps) o reqs)
+
+    sendReq :: [resp] -> Nat o -> req -> [req]
+            -> ReqRespSender req resp o resp m [resp]
+    sendReq resps o req reqs' =
+      SendMsgReqPipelined req
+        (\resp -> return resp)
+        (go resps (Succ o) reqs')
