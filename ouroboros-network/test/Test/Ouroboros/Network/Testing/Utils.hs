c705cab0beed7b78be3c2fe69ffd545eb0b543d8
Marcin Szamotulski <profunctor@pm.me>
2019-02-19 10:17:46 +0100

    Added splits2 and splits2 to Testing.Utils

--- ouroboros-network/test/Test/Ouroboros/Network/Testing/Utils.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/Testing/Utils.hs
@@ -4,6 +4,7 @@ module Test.Ouroboros.Network.Testing.Utils where
 import           Codec.CBOR.Encoding as CBOR (Encoding)
 import qualified Codec.CBOR.Write as CBOR
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LBS
 
 import           Protocol.Channel
 
@@ -20,3 +21,15 @@ tmvarChannels = do
     leftChan  = uniformDuplex (atomically . putTMVar left . CBOR.toStrictByteString)  (fmap Just (atomically $ takeTMVar right))
     rightChan = uniformDuplex (atomically . putTMVar right . CBOR.toStrictByteString) (fmap Just (atomically $ takeTMVar left))
   pure (leftChan, rightChan)
+
+
+-- | Generate all 2-splits of a string.
+splits2 :: LBS.ByteString -> [[LBS.ByteString]]
+splits2 bs = zipWith (\a b -> [a,b]) (LBS.inits bs) (LBS.tails bs)
+
+-- | Generate all 3-splits of a string.
+splits3 :: LBS.ByteString -> [[LBS.ByteString]]
+splits3 bs =
+    [ [a,b,c]
+    | (a,bs') <- zip (LBS.inits bs)  (LBS.tails bs)
+    , (b,c)   <- zip (LBS.inits bs') (LBS.tails bs') ]
