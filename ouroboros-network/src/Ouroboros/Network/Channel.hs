1cd9938fa1faf9c5cc82ffe207631c23f6b7dcd0
Duncan Coutts <duncan@well-typed.com>
2019-02-20 12:59:18 +0000

    Add new Channel implementations
    One is for inter-thread communication between the mux and the
    mini-protocol driver threads. This uses lazy ByteStrings but ensures
    that the evaluation is done on the writer side and not the reader side.
    This will ensure that the mux reader doesn't block while it evaluates
    message encoding.
    
    Also add a way to make a channel from a socket, and a util for creating
    a socket pair. This is mostly for testing, but could also be used for
    demos that run a mini-protocol over a socket directly, without any mux.

--- ouroboros-network/src/Ouroboros/Network/Channel.hs
+++ ouroboros-network/src/Ouroboros/Network/Channel.hs
@@ -1,18 +1,67 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+
+-- | An extension of 'Network.TypedProtocol.Channel', with additional 'Channel'
+-- implementations.
+--
 module Ouroboros.Network.Channel
   ( module Network.TypedProtocol.Channel
+  , createBufferConnectedChannels
   , createPipeConnectedChannels
+  , createSocketConnectedChannels
   , withFifosAsChannel
+  , socketAsChannel
   ) where
 
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString               as BS
+import qualified Data.ByteString.Lazy          as LBS
+import qualified Data.ByteString.Lazy.Internal as LBS (smallChunkSize)
 import qualified System.Process as IO (createPipe)
 import qualified System.IO      as IO
                    ( withFile, IOMode(..) )
+import qualified Network.Socket            as Socket hiding (send, recv)
+import qualified Network.Socket.ByteString as Socket
+
+import           Control.Monad.Class.MonadSTM
 
 import           Network.TypedProtocol.Channel
 
+
+-- | Create a pair of 'Channel's that are connected internally.
+--
+-- This is intended for inter-thread communication, such as between a
+-- multiplexing thread and a thread running a peer.
+--
+-- It uses lazy 'ByteString's but it ensures that data written to the channel
+-- is /fully evaluated/ first. This ensures that any work to serialise the data
+-- takes place on the /writer side and not the reader side/.
+--
+createBufferConnectedChannels :: forall m. MonadSTM m
+                              => m (Channel m LBS.ByteString,
+                                    Channel m LBS.ByteString)
+createBufferConnectedChannels = do
+    bufferA <- newEmptyTMVarM
+    bufferB <- newEmptyTMVarM
+
+    return (buffersAsChannel bufferB bufferA,
+            buffersAsChannel bufferA bufferB)
+  where
+    buffersAsChannel bufferRead bufferWrite =
+        Channel{send, recv}
+      where
+        send :: LBS.ByteString -> m ()
+        send x = sequence_ [ atomically (putTMVar bufferWrite c)
+                           | !c <- LBS.toChunks x ]
+                           -- Evaluate the chunk c /before/ doing the STM
+                           -- transaction to write it to the buffer.
+
+        recv :: m (Maybe LBS.ByteString)
+        recv   = Just . LBS.fromStrict <$> atomically (takeTMVar bufferRead)
+
+
 -- | Create a local pipe, with both ends in this process, and expose that as
--- a pair of 'ByteChannel's, one for each end.
+-- a pair of 'Channel's, one for each end.
 --
 -- This is primarily for testing purposes since it does not allow actual IPC.
 --
@@ -43,3 +92,44 @@ withFifosAsChannel fifoPathRead fifoPathWrite action =
     IO.withFile fifoPathWrite IO.WriteMode $ \hndWrite ->
       let channel = handlesAsChannel hndRead hndWrite
        in action channel
+
+
+-- | Make a 'Channel' from a 'Socket'. The socket must be a stream socket
+--- type and status connected.
+---
+socketAsChannel :: Socket.Socket -> Channel IO LBS.ByteString
+socketAsChannel socket =
+    Channel{send, recv}
+  where
+    send :: LBS.ByteString -> IO ()
+    send chunks =
+     -- Use vectored writes.
+     Socket.sendMany socket (LBS.toChunks chunks)
+     -- TODO: limit write sizes, or break them into multiple sends.
+
+    recv :: IO (Maybe LBS.ByteString)
+    recv = do
+      -- We rely on the behaviour of stream sockets that a zero length chunk
+      -- indicates EOF.
+      chunk <- Socket.recv socket LBS.smallChunkSize
+      if BS.null chunk
+        then return Nothing
+        else return (Just (LBS.fromStrict chunk))
+
+
+--- | Create a local socket, with both ends in this process, and expose that as
+--- a pair of 'ByteChannel's, one for each end.
+---
+--- This is primarily for testing purposes since it does not allow actual IPC.
+---
+createSocketConnectedChannels :: Socket.Family -- ^ Usually AF_UNIX or AF_INET
+                              -> IO (Channel IO LBS.ByteString,
+                                     Channel IO LBS.ByteString)
+createSocketConnectedChannels family = do
+   -- Create a socket pair to make both ends of a bidirectional channel
+   (socketA, socketB) <- Socket.socketPair family Socket.Stream
+                                           Socket.defaultProtocol
+
+   return (socketAsChannel socketA,
+           socketAsChannel socketB)
+
