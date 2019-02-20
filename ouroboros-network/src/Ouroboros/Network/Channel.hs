5eedc28b4f33e49998f7237d9ce3169271b72fd1
Duncan Coutts <duncan@well-typed.com>
2019-02-20 11:31:56 +0000

    Switch Channel impls to use lazy ByteStrings
    This fits with our bearer I/O strategy and codecs.
    
    Specifically, switch handlesAsChannel to use lazy ByteString. Also
    adjust createPipeConnectedChannels.
    
    Also move withFifosAsChannel from typed-protocols library to
    ouroboros-network. It's a bit specific.

--- ouroboros-network/src/Ouroboros/Network/Channel.hs
+++ ouroboros-network/src/Ouroboros/Network/Channel.hs
@@ -1,10 +1,13 @@
 module Ouroboros.Network.Channel
   ( module Network.TypedProtocol.Channel
   , createPipeConnectedChannels
+  , withFifosAsChannel
   ) where
 
-import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LBS
 import qualified System.Process as IO (createPipe)
+import qualified System.IO      as IO
+                   ( withFile, IOMode(..) )
 
 import           Network.TypedProtocol.Channel
 
@@ -13,8 +16,8 @@ import           Network.TypedProtocol.Channel
 --
 -- This is primarily for testing purposes since it does not allow actual IPC.
 --
-createPipeConnectedChannels :: IO (Channel IO ByteString,
-                                   Channel IO ByteString)
+createPipeConnectedChannels :: IO (Channel IO LBS.ByteString,
+                                   Channel IO LBS.ByteString)
 createPipeConnectedChannels = do
     -- Create two pipes (each one is unidirectional) to make both ends of
     -- a bidirectional channel
@@ -23,3 +26,20 @@ createPipeConnectedChannels = do
 
     return (handlesAsChannel hndReadA hndWriteA,
             handlesAsChannel hndReadB hndWriteB)
+
+-- | Open a pair of Unix FIFOs, and expose that as a 'Channel'.
+--
+-- The peer process needs to open the same files but the other way around,
+-- for writing and reading.
+--
+-- This is primarily for the purpose of demonstrations that use communication
+-- between multiple local processes. It is Unix specific.
+--
+withFifosAsChannel :: FilePath -- ^ FIFO for reading
+                   -> FilePath -- ^ FIFO for writing
+                   -> (Channel IO LBS.ByteString -> IO a) -> IO a
+withFifosAsChannel fifoPathRead fifoPathWrite action =
+    IO.withFile fifoPathRead  IO.ReadMode  $ \hndRead  ->
+    IO.withFile fifoPathWrite IO.WriteMode $ \hndWrite ->
+      let channel = handlesAsChannel hndRead hndWrite
+       in action channel
