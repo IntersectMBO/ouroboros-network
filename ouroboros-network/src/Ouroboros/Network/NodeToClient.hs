7cd6b53299b0116e94a4b9a6ff848e26d6c21e22
Duncan Coutts <duncan@well-typed.com>
2019-02-26 22:37:51 +0000

    Add place-holder modules for Node-to-{Node,Client} protocols
    Initially they just contain comments on the intention, and the
    enumerations of the sets of mini-protocols.
    
    This replaces the original fixed enumeration in the Mux.
    
    The node-to-node and node-to-client protocol numbers are non-overlapping
    for the benefit of tools like our wireshark plugin.

---
+++ ouroboros-network/src/Ouroboros/Network/NodeToClient.hs
@@ -0,0 +1,32 @@
+
+-- | This is the starting point for a module that will bring together the
+-- overall node to client protocol, as a collection of mini-protocols.
+--
+module Ouroboros.Network.NodeToClient (
+  NodeToClientProtocols(..)
+  ) where
+
+import Ouroboros.Network.Mux.Types (ProtocolEnum(..))
+
+
+-- | An index type used with the mux to enumerate all the mini-protocols that
+-- make up the overall node-to-client protocol.
+--
+data NodeToClientProtocols = ChainSyncWithBlocks
+                        -- | ClientRPC  -- TODO
+  deriving (Eq, Ord, Enum, Bounded, Show)
+
+-- | These are the actual wire format protocol numbers.
+--
+-- These are chosen to not overlap with the node to node protocol numbers.
+-- This is not essential for correctness, but is helpful to allow a single
+-- shared implementation of tools that can analyse both protocols, e.g.
+-- wireshark plugins.
+--
+instance ProtocolEnum NodeToClientProtocols where
+
+  fromProtocolEnum ChainSyncWithBlocks = 5
+
+  toProtocolEnum 5 = Just ChainSyncWithBlocks
+  toProtocolEnum _ = Nothing
+
