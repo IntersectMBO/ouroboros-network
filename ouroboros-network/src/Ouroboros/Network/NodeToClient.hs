b353a5fe2e686c0304808b8ef984235088f7e8d2
Karl Knutsson <karl.knutsson@iohk.io>
2019-02-27 14:43:26 +0100

    compile fix

--- ouroboros-network/src/Ouroboros/Network/NodeToClient.hs
+++ ouroboros-network/src/Ouroboros/Network/NodeToClient.hs
@@ -13,7 +13,6 @@ import Ouroboros.Network.Mux.Types (ProtocolEnum(..))
 -- make up the overall node-to-client protocol.
 --
 data NodeToClientProtocols = ChainSyncWithBlocks
-                        -- | ClientRPC  -- TODO
   deriving (Eq, Ord, Enum, Bounded, Show)
 
 -- | These are the actual wire format protocol numbers.
