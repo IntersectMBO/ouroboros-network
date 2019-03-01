5d4394159b1d7fb2faaa2b49fe89990ef10a34e0
Marcin Szamotulski <profunctor@pm.me>
2019-03-01 12:17:44 +0100

    tx-submission example client & servers

--- ouroboros-network/src/Ouroboros/Network/Protocol/TxSubmission/Examples.hs
+++ ouroboros-network/src/Ouroboros/Network/Protocol/TxSubmission/Examples.hs
@@ -1,32 +1,265 @@
 {-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Ouroboros.Network.Protocol.TxSubmission.Examples where
 
+import           Data.List (find)
+import           Numeric.Natural (Natural)
+
+import           Network.TypedProtocol.Pipelined
+
+import           Ouroboros.Network.Protocol.TxSubmission.Client
 import           Ouroboros.Network.Protocol.TxSubmission.Server
 
 
 -- |
 -- An example @'TxSubmissionServer'@ which sends trasactions from a fixed pool
--- of transactions.  It returns a list of pairs @(hash, tx)@ send back to the
--- client.
+-- of transactions.  It returns a list of @tx@ sent back to the client.
+--
+-- It is only ment to be used in test.  The server will error if a client will
+-- ask for a transaction which is not in the pool or if a client will ask for
+-- the same trasaction twice.
 --
 txSubmissionServerFixed
   :: forall hash tx m.
-     Applicative m
-  => [hash]
-  -> (hash -> m tx)
-  -> TxSubmissionServer hash tx m [(hash, tx)]
-txSubmissionServerFixed hashes0 getTx = TxSubmissionServer (pure $ handlers [] hashes0)
+     ( Applicative m
+     , Eq hash
+     )
+  => [tx]
+  -> (tx -> hash)
+  -> TxSubmissionServer hash tx m [tx]
+txSubmissionServerFixed txs0 txHash = TxSubmissionServer (pure $ handlers [] txs0)
     where
-      handlers :: [(hash, tx)] -> [hash] -> TxSubmissionHandlers hash tx m [(hash, tx)]
-      handlers !as hs =
+      handlers :: [tx] -> [tx] -> TxSubmissionHandlers hash tx m [tx]
+      handlers !sent txs =
         TxSubmissionHandlers {
           getHashes = \n -> 
-            let (resp, hs') = splitAt (fromIntegral n) hs
-            in pure (resp, handlers as hs'),
-          getTx    = \hash -> (\tx -> (tx, handlers ((hash, tx):as) hs)) <$> getTx hash,
-          done     = as
+            let (resp, txs') = splitAt (fromIntegral n) txs
+            in pure (map txHash resp, handlers sent txs'),
+          getTx    = \hash -> case find (\tx -> txHash tx == hash) txs0 of
+                        Nothing -> error "no such transaction"
+                        Just tx -> pure (tx, handlers (tx:sent) txs),
+          done     = reverse sent
         }
 
+-- |
+-- Auxilary data type which allows to collect requests and responses of
+-- tx-submission protocol.
+--
+data ReqOrResp hash tx
+  = ReqHashes Natural
+  | RespHashes [hash]
+  | ReqTx hash
+  | RespTx tx
+  deriving (Eq, Show)
 
+-- |
+-- A non pipelined tx-submission client.  It send @'MsgGetHashes'@ awaits for
+-- the response and then requests each transaction awaiting for it before
+-- sending the next @'MsgGetTx'@ request.
+--
+txSubmissionClient
+  :: forall hash tx m.
+     Applicative m
+  => [Natural] -- ^ each element corresponds to a single @'MsgGetHashes'@ request
+  -> TxSubmissionClientPipelined hash tx m [ReqOrResp hash tx]
+txSubmissionClient ns0 = TxSubmissionClientPipelined (sender [] ns0)
+    where
+      sender
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> TxSubmissionSender hash tx Z (Collection hash tx) m [ReqOrResp hash tx]
+      sender txs []     = SendMsgDone (reverse txs)
+      sender txs (n:ns) = SendMsgGetHashes n
+                            $ \hs -> pure (getHashes (RespHashes hs:ReqHashes n:txs) ns hs)
+
+      getHashes
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> [hash]
+        -> TxSubmissionSender hash tx Z (Collection hash tx) m [ReqOrResp hash tx]
+      getHashes txs ns []     = sender txs ns
+      getHashes txs ns (h:hs) = SendMsgGetTx h
+                                  (pure $ CollectPipelined Nothing
+                                            $ \c -> case c of
+                                              Left _   -> pure $ getHashes txs      ns hs
+                                              Right tx -> pure $ getHashes (RespTx tx:ReqTx h:txs) ns hs )
+
+-- |
+-- A piplined tx-submission client that sends @'MsgGetTx'@ eagerly but always tries to
+-- collect any replies as soon as they are available.  This keeps pipelining to
+-- bare minimum, and gives maximum choice to the environment (drivers).
+--
+-- It returns the interleaving of requests and received trasactions.
+--
+txSubmissionClientPipelinedMin
+  :: forall hash tx m.
+     Applicative m
+  => [Natural]
+  -> TxSubmissionClientPipelined hash tx m [ReqOrResp hash tx]
+txSubmissionClientPipelinedMin ns0 = TxSubmissionClientPipelined (sender [] ns0)
+    where
+      sender
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> TxSubmissionSender hash tx Z (Collection hash tx) m [ReqOrResp hash tx]
+      sender txs []     = SendMsgDone (reverse txs)
+      sender txs (n:ns) = SendMsgGetHashes n
+                            $ \hs -> pure (getHashes (RespHashes hs:ReqHashes n:txs) ns hs Zero)
+
+      getHashes
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> [hash]
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      getHashes txs ns []         (Succ o) = CollectPipelined
+                                                Nothing
+                                                (\c -> case c of
+                                                  Left _   -> error "txSubmissionClientPipelinedMin"
+                                                  Right tx -> pure $ getHashes (RespTx tx:txs) ns [] o)
+
+      getHashes txs ns hs@(h:hs') (Succ o) = CollectPipelined
+                                               (Just $ requestMoreTx txs ns h hs' (Succ o))
+                                               (\c -> case c of
+                                                 Left _   -> error "txSubmissionClientPiplinedMin"
+                                                 Right tx -> pure $ getHashes (RespTx tx:txs) ns hs o)
+      
+      getHashes txs ns (h:hs)     Zero     = requestMoreTx txs ns h hs Zero
+
+      getHashes txs ns []         Zero     = sender txs ns
+
+      
+      requestMoreTx 
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> hash
+        -> [hash]
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      requestMoreTx txs ns h hs o = SendMsgGetTx h (pure $ getHashes (ReqTx h:txs) ns hs (Succ o))
+
+
+-- |
+-- An example tx-submission client which sends @'MsgGetHashes'@ awaits for the
+-- response, and then piplines requests for each received hash.  The responses
+-- are collected before next @'MsgGetHashes'@.
+--
+txSubmissionClientPipelinedMax
+  :: forall hash tx m.
+     Applicative m
+  => [Natural] -- ^ each element corresponds to a single @'MsgGetHashes'@ request
+  -> TxSubmissionClientPipelined hash tx m [ReqOrResp hash tx]
+txSubmissionClientPipelinedMax ns0 = TxSubmissionClientPipelined (sender [] ns0)
+    where
+      sender
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> TxSubmissionSender hash tx Z (Collection hash tx) m [ReqOrResp hash tx]
+      sender txs []     = SendMsgDone (reverse txs)
+      sender txs (n:ns) = SendMsgGetHashes n 
+                            $ \hs -> pure (getHashes (RespHashes hs:ReqHashes n:txs) ns hs Zero)
+
+      getHashes
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> [hash]
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      getHashes txs ns []     Zero     = sender txs ns
+      getHashes txs ns (h:hs) o        = SendMsgGetTx h (pure $ getHashes (ReqTx h:txs) ns hs (Succ o))
+      getHashes txs ns []     (Succ o) = CollectPipelined Nothing
+                                        (\c -> case c of
+                                          Left  _  -> pure $ getHashes txs             ns [] o
+                                          Right tx -> pure $ getHashes (RespTx tx:txs) ns [] o)
+
+-- |
+-- Like @'txSubmissionClientPipelinedMin'@ but it also pipelines @'MsgGetHashes'@.
+-- We pipeline the request for more hashes whenever we already get half of the
+-- transactions.
+--
+-- It returns the interleaving of requests and received trasactions.
+--
+txSubmissionClientPipelinedAllMin
+  :: forall hash tx m.
+     ( Applicative m
+     , Eq hash
+     )
+  => [Natural]
+  -> TxSubmissionClientPipelined hash tx m [ReqOrResp hash tx]
+txSubmissionClientPipelinedAllMin ns0 = TxSubmissionClientPipelined (sender [] ns0)
+    where
+      middle :: [as] -> Maybe as
+      middle as = let l = length as
+                      i = l `div` 2
+                  in if i < l then Just (as !! i) else Nothing
+
+      sender
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> TxSubmissionSender hash tx Z (Collection hash tx) m [ReqOrResp hash tx]
+      sender txs []     = SendMsgDone (reverse txs)
+      sender txs (n:ns) = SendMsgGetHashes n
+                            $ \hs -> pure (getHashes (RespHashes hs:ReqHashes n:txs) ns hs (middle hs) Zero)
+
+      senderPipelined
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> [hash]
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      senderPipelined txs (n:ns) hs o = SendMsgGetHashesPipelined n
+                                                         (pure $ getHashes (ReqHashes n:txs) ns hs Nothing (Succ o)) 
+
+      senderPipelined txs []     hs@(_:_) (o@Succ{}) = getHashes txs [] hs Nothing o
+
+      senderPipelined txs []     hs@(_:_) Zero       = getHashes txs [] hs Nothing Zero
+
+      senderPipelined txs []     []       Zero       = SendMsgDone (reverse txs)
+
+      senderPipelined _   []     []       Succ{}     = error "ups, impossible is possible!"
+
+
+      -- Eagerly collect responses; send @'MsgGetTx'@, after we reached the
+      -- marked hash, we pipeline @'MsgGetHashes'@ using @'senderPipelined'@
+      getHashes
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> [hash]
+        -> Maybe hash -- ^ hash at which we do @'senderPipelined'@
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      getHashes txs ns (h:hs) hash o        | hash == Just h
+                                            = senderPipelined txs ns (h:hs) o
+
+      getHashes txs ns []     hash (Succ o) = CollectPipelined
+                                                Nothing
+                                                (\c -> case c of
+                                                  Left hs  -> pure $ getHashes (RespHashes hs:txs) ns hs (middle hs) o
+                                                  Right tx -> pure $ getHashes (RespTx tx:txs) ns [] hash o)
+
+      getHashes txs ns (h:hs) hash (Succ o) = CollectPipelined
+                                                (Just $ requestMoreTx txs ns h hs hash (Succ o))
+                                                (\c -> case c of
+                                                  Left hs'  ->
+                                                    let hsNext = hs ++ hs'
+                                                    in pure $ getHashes (RespHashes hs:txs) ns hsNext (middle hsNext) o
+                                                  Right tx -> pure $ getHashes (RespTx tx:txs) ns hs hash o)
+      
+      getHashes txs ns (h:hs) hash Zero     = requestMoreTx txs ns h hs hash Zero
+
+      getHashes txs ns []     _    Zero     = sender txs ns
+
+      
+      requestMoreTx 
+        :: [ReqOrResp hash tx]
+        -> [Natural]
+        -> hash
+        -> [hash]
+        -> Maybe hash
+        -> Nat n
+        -> TxSubmissionSender hash tx n (Collection hash tx) m [ReqOrResp hash tx]
+      requestMoreTx txs ns h hs hash o = SendMsgGetTx h (pure $ getHashes (ReqTx h:txs) ns hs hash (Succ o))
