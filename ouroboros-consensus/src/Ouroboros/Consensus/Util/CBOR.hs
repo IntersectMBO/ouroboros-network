5408f1d62d9276037a162c1ff8986c5e837ffa2e
Thomas Winant <thomas@well-typed.com>
2019-03-01 11:51:17 +0100

    Add an `EpochFileParser` for CBOR
    See `Ouroboros.Storage.ImmutableDB.Util.cborEpochFileParser'`.
    
    * `Ouroboros.Consensus.Util.CBOR.readIncrementalOffsets` does all the hard
      work.
    
    * Use `Word` instead of `Int` for the size of a block/blob.
    
    * Mention in the docstring of `openDB` the reason for requiring this
      `Word` (previously `Int`) in the output the `EpochFileParser`.
    
    * Move `reconstructSlotOffsets` and `indexBackfill` to
      `Ouroboros.Storage.ImmutableDB.Util`. Add basic unit tests to
      `reconstructSlotOffsets`.
    
    * Generalise to `testBlockEpochFileParser` to `binaryEpochFileParser`:
      `TestBlock` -> `Binary a => a`

--- ouroboros-consensus/src/Ouroboros/Consensus/Util/CBOR.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Util/CBOR.hs
@@ -13,6 +13,7 @@ module Ouroboros.Consensus.Util.CBOR (
     -- * HasFS interaction
   , ReadIncrementalErr(..)
   , readIncremental
+  , readIncrementalOffsets
   ) where
 
 import qualified Codec.CBOR.Read as CBOR
@@ -25,6 +26,7 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import           Data.ByteString.Builder.Extra (defaultChunkSize)
 import           Data.IORef
+import           Data.Word (Word64)
 import           System.IO (IOMode (..))
 
 import           Control.Monad.Class.MonadST
@@ -100,6 +102,7 @@ data ReadIncrementalErr =
 
     -- | Deserialisation was successful, but there was additional data
   | TrailingBytes ByteString
+  deriving (Eq, Show)
 
 -- | Read a file incrementally
 --
@@ -130,6 +133,57 @@ readIncremental hasFS@HasFS{..} fp = withLiftST $ \liftST -> do
                    else Left $ TrailingBytes leftover
     go _ _ (S.Fail _ _ err) =
         return $ Left $ ReadFailed err
+
+    checkEmpty :: ByteString -> Maybe ByteString
+    checkEmpty bs | BS.null bs = Nothing
+                  | otherwise  = Just bs
+
+-- | Read multiple @a@s incrementally from a file.
+--
+-- Return the offset ('Word64') of the start of each @a@ and the size ('Word')
+-- of each @a@. When deserialising fails, return all already deserialised @a@s
+-- and the error.
+--
+-- To be used by 'Ouroboros.Storage.ImmutableDB.Util.cborEpochFileParser''.
+readIncrementalOffsets :: forall m h a. (Serialise a, MonadST m, MonadThrow m)
+                       => HasFS m h
+                       -> FsPath
+                       -> m ([(Word64, (Word, a))], Maybe ReadIncrementalErr)
+                          -- ^ ((the offset of the start of @a@ in the file,
+                          --     (the size of @a@ in bytes,
+                          --      @a@ itself)),
+                          --     error encountered during deserialisation)
+readIncrementalOffsets hasFS@HasFS{..} fp = withLiftST $ \liftST ->
+    withFile hasFS fp ReadMode $ \h ->
+      go liftST h 0 [] Nothing =<< liftST S.deserialiseIncremental
+  where
+    go :: (forall x. ST s x -> m x)
+       -> h
+       -> Word64                -- ^ Offset
+       -> [(Word64, (Word, a))] -- ^ Already deserialised (reverse order)
+       -> Maybe ByteString      -- ^ Unconsumed bytes from last time
+       -> S.IDecode s a
+       -> m ([(Word64, (Word, a))], Maybe ReadIncrementalErr)
+    go liftST h offset deserialised mbUnconsumed dec = case dec of
+      S.Partial k -> do
+        -- First use the unconsumed bytes from a previous read before read
+        -- some more bytes from the file.
+        bs   <- case mbUnconsumed of
+          Just unconsumed -> return unconsumed
+          Nothing         -> hGet h defaultChunkSize
+        dec' <- liftST $ k (checkEmpty bs)
+        go liftST h offset deserialised Nothing dec'
+
+      S.Done leftover size a -> do
+        let nextOffset    = offset + fromIntegral size
+            deserialised' = (offset, (fromIntegral size, a)) : deserialised
+        case checkEmpty leftover of
+          Nothing         -> return (reverse deserialised', Nothing)
+          -- Some more bytes, so try to read the next @a@.
+          Just unconsumed -> liftST S.deserialiseIncremental >>=
+            go liftST h nextOffset deserialised' (Just unconsumed)
+
+      S.Fail _ _ err -> return (reverse deserialised, Just (ReadFailed err))
 
     checkEmpty :: ByteString -> Maybe ByteString
     checkEmpty bs | BS.null bs = Nothing
--- ouroboros-consensus/src/Ouroboros/Storage/ImmutableDB/Util.hs
+++ ouroboros-consensus/src/Ouroboros/Storage/ImmutableDB/Util.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 module Ouroboros.Storage.ImmutableDB.Util
   ( renderFile
   , handleUser
@@ -13,13 +14,22 @@ module Ouroboros.Storage.ImmutableDB.Util
   , readAll
   , hGetRightSize
   , validateIteratorRange
+  , reconstructSlotOffsets
+  , indexBackfill
+  , cborEpochFileParser'
   ) where
 
+import           Codec.Serialise (Serialise)
+import           Control.Exception (assert)
 import           Control.Monad (when)
+import           Control.Monad.Class.MonadST (MonadST)
+import           Control.Monad.Class.MonadThrow (MonadThrow)
 
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as BS
+import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
 import qualified Data.Text as T
 
 import           GHC.Stack (HasCallStack, callStack, popCallStack)
@@ -28,9 +38,13 @@ import           Text.Printf (printf)
 import           Text.Read (readMaybe)
 
 import           Ouroboros.Consensus.Util (whenJust)
+import           Ouroboros.Consensus.Util.CBOR (ReadIncrementalErr (..),
+                     readIncrementalOffsets)
 
 import           Ouroboros.Storage.FS.API
 import           Ouroboros.Storage.FS.API.Types
+import           Ouroboros.Storage.ImmutableDB.CumulEpochSizes
+                     (RelativeSlot (..))
 import           Ouroboros.Storage.ImmutableDB.Types
 import           Ouroboros.Storage.Util.ErrorHandling (ErrorHandling (..))
 import qualified Ouroboros.Storage.Util.ErrorHandling as EH
@@ -143,3 +157,88 @@ validateIteratorRange err next mbStart mbEnd = do
       -- Check that the end is not >= the next expected slot
       when (end >= next) $
         throwUserError err $ ReadFutureSlotError end next
+
+-- | Given a list of increasing 'SlotOffset's together with the 'Word' (blob
+-- size) and 'RelativeSlot' corresponding to the offset, reconstruct a
+-- non-empty list of (decreasing) slot offsets.
+--
+-- The input list (typically returned by 'EpochFileParser') is assumed to be
+-- valid: __strictly__ monotonically increasing offsets as well as
+-- __strictly__ monotonically increasing relative slots.
+--
+-- The 'RelativeSlot's are used to detect empty/unfilled slots that will
+-- result in repeated offsets in the output, indicating that the size of the
+-- slot is 0.
+--
+-- The output list will always have 0 as last element.
+reconstructSlotOffsets :: [(SlotOffset, (Word, RelativeSlot))]
+                       -> NonEmpty SlotOffset
+reconstructSlotOffsets = go 0 [] 0
+  where
+    go :: SlotOffset
+       -> [SlotOffset]
+       -> RelativeSlot
+       -> [(SlotOffset, (Word, RelativeSlot))]
+       -> NonEmpty SlotOffset
+    go offsetAfterLast offsets expectedRelSlot ((offset, (len, relSlot)):olrs') =
+      assert (offsetAfterLast == offset) $
+      assert (relSlot >= expectedRelSlot) $
+      let backfill = indexBackfill relSlot expectedRelSlot offset
+      in go (offset + fromIntegral len) (offset : backfill <> offsets)
+            (succ relSlot) olrs'
+    go offsetAfterLast offsets _lastRelSlot [] = offsetAfterLast NE.:| offsets
+
+
+-- | Return the slots to backfill the index file with.
+--
+-- A situation may arise in which we \"skip\" some relative slots, and we
+-- write into the DB, for example, every other relative slot. In this case, we
+-- need to backfill the index file with offsets for the skipped relative
+-- slots. Similarly, before we start a new epoch, we must backfill the index
+-- file of the current epoch file to indicate that it is finalised.
+--
+-- For example, say we have written \"a\" to relative slot 0 and \"bravo\" to
+-- relative slot 1. We have the following index file:
+--
+-- > slot:     0   1   2
+-- >         ┌───┬───┬───┐
+-- > offset: │ 0 │ 1 │ 6 │
+-- >         └───┴───┴───┘
+--
+-- Now we want to store \"haskell\" in relative slot 4, skipping 2 and 3. We
+-- first have to backfill the index by repeating the last offset for the two
+-- missing slots:
+--
+-- > slot:     0   1   2   3   4
+-- >         ┌───┬───┬───┬───┬───┐
+-- > offset: │ 0 │ 1 │ 6 │ 6 │ 6 │
+-- >         └───┴───┴───┴───┴───┘
+--
+-- After backfilling (writing the offset 6 twice), we can write the next
+-- offset:
+--
+-- > slot:     0   1   2   3   4   5
+-- >         ┌───┬───┬───┬───┬───┬───┐
+-- > offset: │ 0 │ 1 │ 6 │ 6 │ 6 │ 13│
+-- >         └───┴───┴───┴───┴───┴───┘
+--
+-- For the example above, the output of this funciton would thus be: @[6, 6]@.
+--
+indexBackfill :: RelativeSlot  -- ^ The slot to write to (>= next expected slot)
+              -> RelativeSlot  -- ^ The next expected slot to write to
+              -> SlotOffset    -- ^ The last 'SlotOffset' written to
+              -> [SlotOffset]
+indexBackfill (RelativeSlot slot) (RelativeSlot nextExpected) lastOffset =
+    replicate gap lastOffset
+  where
+    gap = fromIntegral $ slot - nextExpected
+
+
+-- | CBOR-based 'EpochFileParser' that can be used with
+-- 'Ouroboros.Storage.ImmutableDB.Impl.openDB'.
+cborEpochFileParser' :: forall m h a. (Serialise a, MonadST m, MonadThrow m)
+                     => HasFS m h
+                     -> EpochFileParser ReadIncrementalErr m (Word, a)
+                        -- ^ The 'Word' is the size in bytes of the
+                        -- corresponding @a@.
+cborEpochFileParser' hasFS = EpochFileParser (readIncrementalOffsets hasFS)
