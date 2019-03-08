a14bf7ddf2880a987d8aa65e206ccaa71dca99c4
kderme <k.dermenz@gmail.com>
2019-03-08 11:14:20 +0200

    Introduce isMember api call for volatileDB

--- ouroboros-consensus/test-storage/Test/Ouroboros/Storage/VolatileDB/StateMachine.hs
+++ ouroboros-consensus/test-storage/Test/Ouroboros/Storage/VolatileDB/StateMachine.hs
@@ -42,6 +42,7 @@ import           Test.Tasty.QuickCheck (testProperty)
 import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadThrow
 
+import qualified Ouroboros.Consensus.Util.Classify as C
 import           Ouroboros.Storage.FS.API
 import qualified Ouroboros.Storage.FS.Sim.MockFS as Mock
 import           Ouroboros.Storage.FS.Sim.STM
@@ -52,20 +53,21 @@ import           Ouroboros.Storage.VolatileDB.Impl (openDB)
 import           Test.Ouroboros.Storage.Util
 import           Test.Ouroboros.Storage.VolatileDB.Model
 
-newtype At t (r :: Type -> Type) = At t
+newtype At t (r :: Type -> Type) = At {unAt :: t}
   deriving (Generic)
 
 -- | Alias for 'At'
 type (:@) t r = At t r
 
 data Success
-  = Unit ()
-  | Blob (Maybe ByteString)
-  | Bl   Bool
+  = Unit     ()
+  | Blob     (Maybe ByteString)
+  | Bl       Bool
+  | IsMember [Bool] -- We compare two functions based on their results on a list of inputs.
   deriving (Eq, Show)
 
 newtype Resp = Resp {getResp :: Either (VolatileDBError MyBlockId) Success}
-    deriving (Show, Eq)
+    deriving (Eq, Show)
 
 data Cmd
     = IsOpen
@@ -74,6 +76,7 @@ data Cmd
     | GetBlock MyBlockId
     | PutBlock MyBlockId
     | GarbageCollect MyBlockId
+    | AskIfMember [MyBlockId]
     deriving (Show)
 
 deriving instance Generic1          (At Cmd)
@@ -88,7 +91,6 @@ deriving instance Show1 r => Show (Resp :@ r)
 
 instance Show (Model r) where
     show = show . toShow
-
 
 deriving instance Show (ModelShow r)
 deriving instance Generic (DBModel MyBlockId)
@@ -110,6 +112,7 @@ instance CommandNames (At Cmd) where
         IsOpen           -> "IsOpen"
         Close            -> "Close"
         ReOpen           -> "ReOpen"
+        AskIfMember _    -> "AskIfMember"
     cmdNames _ = ["not", "suported", "yet"]
 
 data Model (r :: Type -> Type) = Model
@@ -180,6 +183,9 @@ runDB db cmd = case cmd of
     IsOpen -> Bl <$> isOpenDB db
     Close -> Unit <$> closeDB db
     ReOpen -> Unit <$> reOpenDB db
+    AskIfMember bids -> do
+        isMember <- getIsMember db
+        return $ IsMember $ isMember <$> bids
 
 
 sm :: MonadCatch m => VolatileDB MyBlockId m -> DBModel MyBlockId -> ModelDBPure -> StateMachine Model (At Cmd) m (At Resp)
@@ -218,6 +224,8 @@ postconditionImpl model cmd resp =
 generatorImpl :: Model Symbolic -> Maybe (Gen (At Cmd Symbolic))
 generatorImpl Model {..} = Just $ At <$> do
     sl <- arbitrary
+    let lastGC = latestGarbaged dbModel
+    ls <- filter (newer lastGC . toSlot) <$> arbitrary
     frequency
         [ (3, return $ GetBlock sl)
         , (3, return $ PutBlock sl)
@@ -225,7 +233,12 @@ generatorImpl Model {..} = Just $ At <$> do
         , (1, return $ IsOpen)
         , (1, return $ Close)
         , (1, return $ ReOpen)
+        , (if null ls then 0 else 1, return $ AskIfMember ls)
         ]
+
+newer :: Ord a => Maybe a -> a -> Bool
+newer Nothing _   = True
+newer (Just a) a' = a' >= a
 
 shrinkerImpl :: Model Symbolic -> At Cmd Symbolic -> [At Cmd Symbolic]
 shrinkerImpl _ _ = []
@@ -240,19 +253,70 @@ mockImpl model cmd = At <$> return mockResp
 
 knownLimitation :: Model Symbolic -> Cmd :@ Symbolic -> Logic
 knownLimitation model (At cmd) = case cmd of
-    GetBlock bid -> isLimitation (latestGarbaged $ dbModel model) (toSlot bid)
-    PutBlock bid -> isLimitation (latestGarbaged $ dbModel model) (toSlot bid)
+    GetBlock bid -> Boolean $ isLimitation (latestGarbaged $ dbModel model) (toSlot bid)
+    PutBlock bid -> Boolean $ isLimitation (latestGarbaged $ dbModel model) (toSlot bid)
     GarbageCollect _sl -> Bot
     IsOpen -> Bot
     Close -> Bot
     ReOpen -> Bot
+    AskIfMember bids -> exists ((\b -> isLimitation (latestGarbaged $ dbModel model) (toSlot b)) <$> bids) Boolean
     where
-        isLimitation :: (Show slot, Ord slot) => Maybe slot -> slot -> Logic
-        isLimitation Nothing _sl       = Bot
-        isLimitation (Just slot') slot = slot' .>  slot
+        isLimitation :: (Show slot, Ord slot) => Maybe slot -> slot -> Bool
+        isLimitation Nothing _sl       = False
+        isLimitation (Just slot') slot = slot' >  slot
 
 mkDBModel :: MonadState (DBModel MyBlockId) m => (DBModel MyBlockId, VolatileDB MyBlockId (ExceptT (VolatileDBError MyBlockId) m))
 mkDBModel = openDBModel EH.exceptT
+
+-- | Predicate on events
+type EventPred = C.Predicate (Event Symbolic) Tag
+
+-- | Convenience combinator for creating classifiers for successful commands
+successful :: (    Event Symbolic
+                -> Success
+                -> Either Tag EventPred
+              )
+           -> EventPred
+successful f = C.predicate $ \ev -> case eventMockResp ev of
+    Resp (Left  _ ) -> Right $ successful f
+    Resp (Right ok) -> f ev ok
+
+-- | Tag commands
+--
+-- Tagging works on symbolic events, so that we can tag without doing real IO.
+-- TODO(kde) This needs extending as part of #251
+tag :: [Event Symbolic] -> [Tag]
+tag = C.classify
+    [ tagGetBinaryBlobJust
+    ]
+  where
+
+    tagGetBinaryBlobJust :: EventPred
+    tagGetBinaryBlobJust = successful $ \ev r -> case r of
+        Blob (Just _) | GetBlock {} <- unAt $ eventCmd ev ->
+            Left TagGetBinaryBlobJust
+        _ -> Right tagGetBinaryBlobJust
+
+isMemberTrue :: [Event Symbolic] -> Int
+isMemberTrue events = sum $ count <$> events
+    where
+        count :: Event Symbolic -> Int
+        count e = case eventMockResp e of
+            Resp (Left _)              -> 0
+            Resp (Right (IsMember ls)) -> length $ filter id ls
+            Resp (Right _)             -> 0
+
+isMemberTrue' :: [Event Symbolic] -> Int
+isMemberTrue' events = sum $ f <$> events
+    where
+        f :: Event Symbolic -> Int
+        f e = case eventMockResp e of
+            Resp (Left _)              -> 0
+            Resp (Right (IsMember ls)) -> if length (filter id ls) > 0 then 1 else 0
+            Resp (Right _)             -> 0
+
+data Tag = TagGetBinaryBlobJust
+         deriving Show
 
 prop_sequential :: Property
 prop_sequential =
@@ -264,14 +328,29 @@ prop_sequential =
               (hist, _model, res) <- runCommands sm' cmds
               run $ closeDB db
               return (hist, res)
-
         fsVar <- run $ atomically (newTVar Mock.empty)
         (hist, res) <- test (simHasFS EH.monadCatch fsVar)
-        prettyCommands smUnused hist $
-            checkCommandNames cmds (res === Ok)
+        prettyCommands smUnused hist
+            $ tabulate "Tags" (map show $ tag (execCmds (initModel smUnused) cmds))
+            $ tabulate "IsMember: Total number of True's" ([show $ isMemberTrue (execCmds (initModel smUnused) cmds)])
+            $ tabulate "IsMember: At least one True" ([show $ isMemberTrue' (execCmds (initModel smUnused) cmds)])
+            $ res === Ok
     where
         (dbm, vdb) = mkDBModel
         smUnused = sm (error "semantics and DB used during command generation") dbm vdb
+
+execCmd :: Model Symbolic
+        -> Command (At Cmd) (At Resp)
+        -> Event Symbolic
+execCmd model (Command cmdErr resp _vars) = lockstep model cmdErr resp
+
+execCmds :: Model Symbolic -> Commands (At Cmd) (At Resp) -> [Event Symbolic]
+execCmds model (Commands cs) = go model cs
+    where
+        go :: Model Symbolic -> [Command (At Cmd) (At Resp)] -> [Event Symbolic]
+        go _ []        = []
+        go m (c : css) = let ev = execCmd m c in ev : go (eventAfter ev) css
+
 
 tests :: TestTree
 tests = testGroup "Volatile" [
