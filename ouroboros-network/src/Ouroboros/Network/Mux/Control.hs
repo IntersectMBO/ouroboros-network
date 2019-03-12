c27e42949edb4d4389d09fdd7ef451e2ff42d2ca
Karl Knutsson <karl.knutsson@iohk.io>
2019-03-12 10:46:00 +0100

    Change MsgInitReq from list to a map
    Switch from a list of Version to a Map inorder to prevent
    sending of duplicate Versions.

--- ouroboros-network/src/Ouroboros/Network/Mux/Control.hs
+++ ouroboros-network/src/Ouroboros/Network/Mux/Control.hs
@@ -12,14 +12,13 @@ import           Codec.CBOR.Write (toLazyByteString)
 import           Codec.Serialise.Class
 import           Control.Exception
 import           Control.Monad
-
 import qualified Data.Map.Strict as M
-import           Data.Maybe (mapMaybe)
+import           Data.Maybe (catMaybes)
 import           GHC.Stack
 
 import           Ouroboros.Network.Mux.Types
 
-data ControlMsg = MsgInitReq [Version]
+data ControlMsg = MsgInitReq (M.Map VersionNumber Version)
                 | MsgInitRsp Version
                 | MsgInitFail String
                 deriving Show
@@ -29,17 +28,17 @@ encodeCtrlMsg (MsgInitReq versions) = CBOR.encodeListLen 2 <> CBOR.encodeWord 0 
 encodeCtrlMsg (MsgInitRsp version)  = CBOR.encodeListLen 2 <> CBOR.encodeWord 1 <> encode version
 encodeCtrlMsg (MsgInitFail msg)     = CBOR.encodeListLen 2 <> CBOR.encodeWord 2 <> encode msg
 
-encodeVersionMap :: [Version] -> CBOR.Encoding
+encodeVersionMap :: M.Map VersionNumber Version -> CBOR.Encoding
 encodeVersionMap versions =
-    CBOR.encodeMapLen (fromIntegral $ length versions)
-    <> foldr (\v b -> encode v <> b) mempty versions
+    CBOR.encodeMapLen (fromIntegral $ M.size versions)
+    <> foldr (\v b -> encode v <> b) mempty (M.elems versions)
 
 decodeCtrlMsg :: HasCallStack => CBOR.Decoder s ControlMsg
 decodeCtrlMsg = do
     _ <- CBOR.decodeListLen
     key <- CBOR.decodeWord
     case key of
-         0 -> MsgInitReq . mapMaybe decodeVersionTerm . M.toList <$> decodeVersionMap
+         0 -> MsgInitReq . M.mapMaybeWithKey decodeVersionTerm <$> decodeVersionMap
          1 -> MsgInitRsp <$> decode
          2 -> MsgInitFail <$> decode
          a -> throw $ MuxError MuxControlUnknownMessage
@@ -47,28 +46,30 @@ decodeCtrlMsg = do
                       callStack
 
   where
-    decodeVersionMap :: CBOR.Decoder s (M.Map Word CBOR.Term)
+    decodeVersionMap :: CBOR.Decoder s (M.Map VersionNumber CBOR.Term)
     decodeVersionMap = do
         n <- CBOR.decodeMapLen
         let decodeEntry = do
               !k <- CBOR.decodeWord
               !v <- CBOR.decodeTerm
-              return (k, v)
-        fmap M.fromList (replicateM n decodeEntry)
+              case k of
+                   0 -> return $ Just (VersionNumber0, v)
+                   1 -> return $ Just (VersionNumber1, v)
+                   _ -> return Nothing
+        fmap (M.fromList . catMaybes) (replicateM n decodeEntry)
 
-    decodeVersionTerm (0, t) =
+    decodeVersionTerm VersionNumber0 t =
         let bs = toLazyByteString (CBOR.encodeTerm t)
             v_e  = CBOR.deserialiseFromBytes (Version0 . NetworkMagic <$> decode) bs in
         case v_e of
              Left  e      -> throw e
              Right (_, v) -> Just v
 
-    decodeVersionTerm (1, t) =
+    decodeVersionTerm VersionNumber1 t =
         let bs = toLazyByteString (CBOR.encodeTerm t)
             v_e  = CBOR.deserialiseFromBytes (Version1 . NetworkMagic <$> decode) bs in
         case v_e of
              Left  e      -> throw e
              Right (_, v) -> Just v
 
-    decodeVersionTerm _ = Nothing
 
