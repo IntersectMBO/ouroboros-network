ab07c1f4e80f3d59fd5418ee4eeaee0eed34ea68
Karl Knutsson <karl.knutsson@iohk.io>
2019-03-13 16:13:42 +0100

    Split out Muxcontrol testcases into a separate file

--- ouroboros-network/test/Test/Mux.hs
+++ ouroboros-network/test/Test/Mux.hs
@@ -11,15 +11,11 @@ module Test.Mux (
     , version1) where
 
 import           Codec.CBOR.Decoding (decodeBytes)
-import           Codec.CBOR.Encoding (Encoding, encodeBytes, encodeListLen,
-                     encodeMapLen, encodeWord)
-import           Codec.CBOR.Read (deserialiseFromBytes)
-import           Codec.CBOR.Write (toLazyByteString)
+import           Codec.CBOR.Encoding (encodeBytes)
 import           Codec.Serialise (Serialise (..))
 import           Control.Monad
 import qualified Data.Binary.Put as Bin
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Map as M
 import           Data.Word
 import           Test.QuickCheck
 import           Test.Tasty (TestTree, testGroup)
@@ -36,7 +32,6 @@ import           Network.TypedProtocol.ReqResp.Server
 
 import           Ouroboros.Network.Channel
 import qualified Ouroboros.Network.Mux as Mx
-import           Ouroboros.Network.Mux.Control
 import           Ouroboros.Network.Mux.Types (MuxBearer)
 import qualified Ouroboros.Network.Mux.Types as Mx
 import           Ouroboros.Network.Protocol.ReqResp.Codec
@@ -49,10 +44,6 @@ tests =
   , testProperty "starvation"                     prop_mux_starvation
   , testProperty "unknown miniprotocol"           prop_mux_unknown_miniprot
   , testProperty "too short header"               prop_mux_short_header
-  , testProperty "Muxcontrol encode/decode"       prop_mux_encode_decode
-  , testProperty "Muxcontrol unknown version rsp" prop_unknown_version_rsp
-  , testProperty "Muxcontrol unknown version req" prop_unknown_version_req
-  , testProperty "Muxcontrol unknown message"     prop_unknown_version_key
   ]
 
 version0 :: Mx.Version
@@ -123,23 +114,6 @@ instance Arbitrary InvalidSDU where
 
         return $ InvalidSDU (Mx.RemoteClockModel ts) mid len
 
-
-instance Arbitrary Mx.NetworkMagic where
-    arbitrary = Mx.NetworkMagic <$> arbitrary
-
-instance Arbitrary Mx.Version where
-    arbitrary = do
-        nm <- arbitrary
-        elements [Mx.Version0 nm, Mx.Version1 nm]
-
-instance Arbitrary ControlMsg where
-    arbitrary = do
-        vs <- arbitrary
-        v  <- arbitrary
-        f  <- arbitrary
-        elements [ MsgInitReq (M.fromList $ map (\a -> (Mx.versionToVersionNumber a, a)) vs)
-                 , MsgInitRsp v
-                 , MsgInitFail f]
 
 data MuxSTMCtx ptcl m = MuxSTMCtx {
       writeQueue :: TBQueue m BL.ByteString
@@ -502,68 +476,4 @@ failingServer  = do
 
     return (server_r, server_res)
 
--- | Verify that decoder . encoder = id for ControlMsg.
-prop_mux_encode_decode :: ControlMsg
-                       -> Property
-prop_mux_encode_decode msg =
-    let bs = toLazyByteString $ encodeCtrlMsg msg
-        res_e = deserialiseFromBytes decodeCtrlMsg bs in
-    case res_e of
-         Left  _   -> property False
-         Right res -> return msg === res
-
--- | Verify error handling for MsgInitRsp with an invalid version.
-prop_unknown_version_rsp :: Word
-                         -> Word
-                         -> Property
-prop_unknown_version_rsp version len = version > 1 && len > 0 && len < 0xffff ==> ioProperty $ do
-    let blob = toLazyByteString $ invalidControlMessage 1 version len
-
-    res <- try $ return $! deserialiseFromBytes decodeCtrlMsg blob
-    case res of
-         Left e  ->
-             case fromException e of
-                  Just me -> return $ Mx.errorType me == Mx.MuxControlUnknownVersion
-                  Nothing -> return False
-
-         Right _ -> return False
-
--- | Verify that unknown versions are skipped in MsgInitReq messages.
-prop_unknown_version_req :: Word
-                         -> Word
-                         -> Mx.Version
-                         -> Property
-prop_unknown_version_req version len validVersion = version > 1 && len > 0 && len < 0xffff ==>
-    let blob = toLazyByteString enc
-        res = deserialiseFromBytes decodeCtrlMsg blob in
-    case res of
-         Left _ -> property False
-         Right (_, MsgInitReq resVersions) -> M.elems resVersions === [validVersion]
-         Right _  -> property False
-  where
-    enc =
-        let term = BL.replicate (fromIntegral len) 0xa in
-        encodeListLen 2 <> encodeWord 0 <> encodeMapLen 2
-        <> foldr (\v b -> b <> encode v) (encodeWord 0xcafebabe <> encode term) [validVersion]
-
--- | Verify error handling for ControlMsg with invalid key.
-prop_unknown_version_key :: Word
-                         -> Word
-                         -> Property
-prop_unknown_version_key key len = key > 2 && len > 0 && len < 0xffff ==> ioProperty $ do
-    let blob = toLazyByteString $ invalidControlMessage key 1 len
-
-    res <- try $ return $! deserialiseFromBytes decodeCtrlMsg blob
-    case res of
-         Left e  ->
-             case fromException e of
-                  Just me -> return $ Mx.errorType me == Mx.MuxControlUnknownMessage
-                  Nothing -> return False
-
-         Right _ -> return False
-
-invalidControlMessage :: Word -> Word -> Word -> Encoding
-invalidControlMessage k v len =
-    let term = BL.replicate (fromIntegral len) 0xa in
-    encodeListLen 2 <> encodeWord k <> encode v <> encode term
 
