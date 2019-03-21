5f06280d3de7dab1dae98875fa1ba3d78ae202ca
Edsko de Vries <edsko@well-typed.com>
2019-03-21 15:05:27 +0100

    QC utilities for working with SimM

---
+++ ouroboros-network-testing/src/Ouroboros/Network/Testing/QuickCheck.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Ouroboros.Network.Testing.QuickCheck (
+    runSimGen
+  , monadicSim
+  ) where
+
+import           Test.QuickCheck
+import           Test.QuickCheck.Gen.Unsafe (Capture (..), capture)
+import           Test.QuickCheck.Monadic
+
+import           Control.Monad.IOSim
+
+-- | 'SimM' analogue of 'runSTGen'
+--
+-- > runSTGen  :: (forall s. Gen (ST   s a)) -> Gen a
+-- > runSimGen :: (forall s. Gen (SimM s a)) -> Gen a
+runSimGen :: (forall s. Gen (SimM s a)) -> Gen a
+runSimGen f = do
+    Capture eval <- capture
+    return $ runSimOrThrow (eval f)
+
+-- | 'SimM; analogue of 'monadicST'
+--
+-- > monadicST  :: Testable a => (forall s. PropertyM (ST   s) a) -> Property
+-- > monadicSim :: Testable a => (forall s. PropertyM (SimM s) a) -> Property
+monadicSim :: Testable a => (forall s. PropertyM (SimM s) a) -> Property
+monadicSim m = property (runSimGen (monadic' m))
