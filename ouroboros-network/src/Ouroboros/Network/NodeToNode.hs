7267a665b4167f012f55e5224eae48a5c059854b
Marcin Szamotulski <profunctor@pm.me>
2019-05-27 23:42:10 +0200

    Expand NodeToNode interface

--- ouroboros-network/src/Ouroboros/Network/NodeToNode.hs
+++ ouroboros-network/src/Ouroboros/Network/NodeToNode.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures    #-}
 {-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE TypeFamilies      #-}
 
 -- | This is the starting point for a module that will bring together the
@@ -13,8 +14,12 @@ module Ouroboros.Network.NodeToNode (
   , NodeToNodeVersionData (..)
   , DictVersion (..)
   , nodeToNodeCodecCBORTerm
+
+  , nodeToNodeConnectTo
+  , withServerNodeToNode
   ) where
 
+import           Control.Concurrent.Async (Async)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Typeable (Typeable)
@@ -25,8 +30,13 @@ import qualified Codec.CBOR.Term as CBOR
 import           Codec.Serialise (Serialise (..))
 import           Codec.SerialiseTerm
 
+import qualified Network.Socket as Socket
+
+import           Ouroboros.Network.Protocol.Handshake.Type
 import           Ouroboros.Network.Protocol.Handshake.Version
-import           Ouroboros.Network.Mux.Types (ProtocolEnum(..))
+import           Ouroboros.Network.Mux.Types (ProtocolEnum(..), MiniProtocolLimits (..))
+import           Ouroboros.Network.Mux.Interface
+import           Ouroboros.Network.Socket
 
 
 -- | An index type used with the mux to enumerate all the mini-protocols that
@@ -63,6 +73,12 @@ instance ProtocolEnum NodeToNodeProtocols where
   toProtocolEnum 4 = Just TxSubmission
   toProtocolEnum _ = Nothing
 
+instance MiniProtocolLimits NodeToNodeProtocols where
+  -- TODO: provide sensible limits
+  -- https://github.com/input-output-hk/ouroboros-network/issues/575
+  maximumMessageSize  _ = 0xffffffff
+  maximumIngressQueue _ = 0xffffffff
+
 -- |
 -- Enumeration of node to node protocol versions.
 --
@@ -95,3 +111,32 @@ nodeToNodeCodecCBORTerm = CodecCBORTerm {encodeTerm, decodeTerm}
       decodeTerm (CBOR.TInt x) | x >= 0 && x <= 0xffff = Right (NodeToNodeVersionData $ fromIntegral x)
                                | otherwise             = Left $ T.pack $ "networkMagic out of bound: " <> show x
       decodeTerm t             = Left $ T.pack $ "unknown encoding: " ++ show t
+
+
+-- |
+-- A specialised version of @'Ouroboros.Network.Socket.connectTo'@.
+--
+nodeToNodeConnectTo
+  :: Versions NodeToNodeVersion DictVersion (MuxApplication InitiatorApp NodeToNodeProtocols IO)
+  -> Maybe Socket.AddrInfo
+  -> Socket.AddrInfo
+  -> IO ()
+nodeToNodeConnectTo =
+  connectTo
+    (\(DictVersion codec) -> encodeTerm codec)
+    (\(DictVersion codec) -> decodeTerm codec)
+
+-- |
+-- A specialised version of @'Ouroboros.Network.Socket.withServerNode'@
+--
+withServerNodeToNode
+  :: Socket.AddrInfo
+  -> (forall vData. DictVersion vData -> vData -> vData -> Accept)
+  -> Versions NodeToNodeVersion DictVersion (AnyMuxResponderApp NodeToNodeProtocols IO)
+  -> (Async () -> IO t)
+  -> IO t
+withServerNodeToNode addr =
+  withServerNode
+    addr 
+    (\(DictVersion codec) -> encodeTerm codec)
+    (\(DictVersion codec) -> decodeTerm codec)
