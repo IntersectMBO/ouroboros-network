79c7664010622ceb5fbf24907623d067de3ffc3d
Marcin Szamotulski <profunctor@pm.me>
2019-06-13 09:17:15 +0200

    Updated test-consensus

--- ouroboros-consensus/test-consensus/Test/Dynamic/Network.hs
+++ ouroboros-consensus/test-consensus/Test/Dynamic/Network.hs
@@ -1,9 +1,13 @@
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NamedFieldPuns      #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections       #-}
-{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE NamedFieldPuns       #-}
+{-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TupleSections        #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 -- | Setup network
 module Test.Dynamic.Network (
@@ -18,24 +22,32 @@ import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import           Data.Typeable (Typeable)
+import           Data.Functor.Identity
 
 import           Control.Monad.Class.MonadAsync
 import           Control.Monad.Class.MonadFork (MonadFork)
 import           Control.Monad.Class.MonadSay
+import           Control.Monad.Class.MonadST
 import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadThrow
 import           Control.Monad.Class.MonadTime
 import           Control.Monad.Class.MonadTimer
 
+import           Network.TypedProtocol.Core
+import           Network.TypedProtocol.Codec (Codec, CodecFailure, isoCodec)
+
 import           Ouroboros.Network.Channel
-import           Ouroboros.Network.Codec (AnyMessage, Codec)
+import           Ouroboros.Network.Codec (AnyMessage (..))
 
 import           Ouroboros.Network.Block
 import           Ouroboros.Network.Chain
-import           Ouroboros.Network.Protocol.BlockFetch.Codec
-import           Ouroboros.Network.Protocol.BlockFetch.Type
 import           Ouroboros.Network.Protocol.ChainSync.Codec
+import           Ouroboros.Network.Mux.Interface
+import           Ouroboros.Network.NodeToNode
+
 import           Ouroboros.Network.Protocol.ChainSync.Type
+import           Ouroboros.Network.Protocol.BlockFetch.Type
+import           Ouroboros.Network.Protocol.BlockFetch.Codec
 
 import           Ouroboros.Consensus.BlockchainTime
 import           Ouroboros.Consensus.ChainSyncClient (ClockSkew (..))
@@ -58,6 +70,131 @@ import qualified Ouroboros.Storage.ChainDB.API as ChainDB
 import qualified Ouroboros.Storage.ChainDB.Mock as ChainDB
 
 
+-- | Existential wrapper which let us encode / decode using identity
+-- codecs.
+--
+data AnyProtocolMessage blk where
+     ChainSyncMsg :: Message (ChainSync (Header blk) (Point blk)) st st' -> AnyProtocolMessage blk
+     BlockFetchMsg :: Message (BlockFetch blk) st st' -> AnyProtocolMessage blk
+
+-- This instance constraint is no smaller than the instance head, and thus
+-- @UndecidableInstances@ extension is required
+--
+instance ( Show blk
+         , Show (Header blk)
+         , StandardHash blk
+         ) => Show (AnyProtocolMessage blk) where
+    show (ChainSyncMsg msg) = show msg
+    show (BlockFetchMsg msg) = show msg
+
+
+codecChainSyncId_ :: forall blk m. Monad m
+                  => Codec (ChainSync (Header blk) (Point blk))
+                           CodecFailure m (AnyProtocolMessage blk)
+codecChainSyncId_ = isoCodec fromAnyChainSyncMessage toAnyChainSyncMessage codecChainSyncId
+    where
+      fromAnyChainSyncMessage :: AnyMessage (ChainSync (Header blk) (Point blk)) -> AnyProtocolMessage blk
+      fromAnyChainSyncMessage (AnyMessage msg) = ChainSyncMsg msg
+
+      toAnyChainSyncMessage :: AnyProtocolMessage blk -> AnyMessage (ChainSync (Header blk) (Point blk))
+      toAnyChainSyncMessage (ChainSyncMsg msg) = AnyMessage msg
+      -- it means we sent a message through wrong channel
+      toAnyChainSyncMessage _                  = error "toAnyChainSyncMessage: message type missmatch"
+
+
+codecBlockFetchId_ :: forall blk m. Monad m
+                   => Codec (BlockFetch blk)
+                            CodecFailure m (AnyProtocolMessage blk)
+codecBlockFetchId_ = isoCodec fromAnyBlockFetchMessage toAnyBlockFetchMessage codecBlockFetchId
+    where
+      fromAnyBlockFetchMessage :: AnyMessage (BlockFetch blk) -> AnyProtocolMessage blk
+      fromAnyBlockFetchMessage (AnyMessage msg) = BlockFetchMsg msg
+
+      toAnyBlockFetchMessage :: AnyProtocolMessage blk -> AnyMessage (BlockFetch blk)
+      toAnyBlockFetchMessage (BlockFetchMsg msg) = AnyMessage msg
+      -- it means we sent a message through wrong channel
+      toAnyBlockFetchMessage _                   = error "toAnyBlockFetchMessage: message type missmatch"
+
+-- | Interface provided by 'ouroboros-network'.  At the moment
+-- 'ouroboros-network' only provides this interface in 'IO' backed by sockets,
+-- we cook up here one using 'NodeChans'.
+--
+data NetworkInterface m up = NetworkInterface {
+      -- | Like 'Ouroboros.Network.NodeToNode.nodeToNodeConnectTo'
+      --
+      niConnectTo :: up -> m ()
+
+      -- | Like 'Ouroboros.Network.NodeToNode.withServerNodeToNode'
+      --
+    , niWithServerNode :: forall t.  (Async m () -> m t) -> m t
+    }
+
+-- | Create 'NetworkInterface' from a map of channels between nodes.
+--
+-- TODO: move this function to 'ouroboros-network'.
+--
+createNetworkInterface
+    :: forall m nodeId blk.
+       ( MonadAsync m
+       , MonadFork  m
+       , MonadMask  m
+       , MonadSay   m
+       , StandardHash blk
+       , Show blk
+       , Show (Header blk)
+       , Ord nodeId
+       , Show nodeId
+       )
+    => NodeChans m nodeId blk -- ^ map of channels between nodes
+    -> [nodeId]               -- ^ list of nodes which we want to serve
+    -> nodeId                 -- ^ our nodeId
+    -> NetworkApplication m Identity nodeId blk (AnyProtocolMessage blk) () ()
+    -> NetworkInterface m nodeId
+createNetworkInterface chans nodeIds us NetworkApplication {naMuxInitiatorApp, naMuxResponderApp} = NetworkInterface
+    { niConnectTo = \them -> do
+        Identity app <- runSharedState $ naMuxInitiatorApp them
+        case app of
+          MuxInitiatorApplication f -> do
+            let nodeChan = chans Map.! them Map.! us
+
+                fn ptcl@ChainSyncWithHeadersPtcl =
+                  void $ f ptcl
+                       $ loggingChannel (TalkingToProducer us them)
+                       $ chainSyncProducer nodeChan
+                fn ptcl@BlockFetchPtcl =
+                  void $ f ptcl
+                       $ loggingChannel (TalkingToProducer us them)
+                       $ blockFetchProducer nodeChan
+
+            as <- traverse (async . fn) [minBound .. maxBound]
+            -- wait for all the threads, if any throws an exception, cancel all
+            -- of them; this is consistent with
+            -- 'Ouroboros.Network.Socket.connectTo'.
+            void $ waitAnyCancel as
+
+    , niWithServerNode = \k -> do
+        ts :: [Async m ()] <- fmap concat $ forM (filter (/= us) nodeIds) $ \them -> do
+          let nodeChan = chans Map.! us Map.! them
+              fn :: NodeToNodeProtocols -> m ()
+              fn ptcl@ChainSyncWithHeadersPtcl = do
+                case runIdentity naMuxResponderApp of
+                  MuxResponderApplication f -> do
+                    void $ f ptcl
+                         $ loggingChannel (TalkingToConsumer us them)
+                         $ chainSyncConsumer nodeChan
+              fn ptcl@BlockFetchPtcl =
+                case runIdentity naMuxResponderApp of
+                  MuxResponderApplication f -> do
+                    void $ f ptcl
+                         $ loggingChannel (TalkingToConsumer us them)
+                         $ blockFetchConsumer nodeChan
+          forM [minBound .. maxBound] (async . fn)
+
+        -- if any thread raises an exception, we kill all of them
+        waitAnyCancel ts >>= k . fst
+    }
+
+
 -- | Setup fully-connected topology, where every node is both a producer
 -- and a consumer
 --
@@ -68,6 +205,7 @@ broadcastNetwork :: forall m c ext.
                     , MonadFork  m
                     , MonadMask  m
                     , MonadSay   m
+                    , MonadST    m
                     , MonadTime  m
                     , MonadTimer m
                     , MonadThrow (STM m)
@@ -97,7 +235,7 @@ broadcastNetwork registry btime numCoreNodes pInfo initRNG numSlots = do
                       nodeAddrs  = map fst (Map.elems nodeUtxo)
                 ]
 
-    chans :: NodeChans m (SimpleBlock c ext) <- createCommunicationChannels
+    chans :: NodeChans m NodeId (SimpleBlock c ext) <- createCommunicationChannels
 
     varRNG <- atomically $ newTVar initRNG
 
@@ -143,33 +281,20 @@ broadcastNetwork registry btime numCoreNodes pInfo initRNG numSlots = do
             }
 
       node <- nodeKernel nodeParams
-      let network = initNetworkLayer nodeParams node
+      let app = consensusNetworkApps
+                  Identity
+                  nullTracer
+                  nullTracer
+                  codecChainSyncId_
+                  codecBlockFetchId_
+                  nodeParams
+                  node
 
-      forM_ (filter (/= us) nodeIds) $ \them -> do
-        let mkCommsDown :: Show bytes
-                        => (NodeChan m (SimpleBlock c ext) -> Channel m bytes)
-                        -> Codec ps e m bytes -> NodeComms m ps e bytes
-            mkCommsDown getChan codec = NodeComms {
-                ncCodec    = codec
-              , ncWithChan = \cc -> cc $
-                  loggingChannel (TalkingToConsumer us them) $
-                    getChan (chans Map.! us Map.! them)
-              }
-            mkCommsUp :: Show bytes
-                      => (NodeChan m (SimpleBlock c ext) -> Channel m bytes)
-                      -> Codec ps e m bytes -> NodeComms m ps e bytes
-            mkCommsUp getChan codec = NodeComms {
-                ncCodec    = codec
-              , ncWithChan = \cc -> cc $
-                  loggingChannel (TalkingToProducer us them) $
-                    getChan (chans Map.! them Map.! us)
-              }
-        addDownstream network
-          (mkCommsDown chainSyncConsumer  codecChainSyncId)
-          (mkCommsDown blockFetchConsumer codecBlockFetchId)
-        addUpstream network them
-          (mkCommsUp   chainSyncProducer  codecChainSyncId)
-          (mkCommsUp   blockFetchProducer codecBlockFetchId)
+          ni :: NetworkInterface m NodeId
+          ni = createNetworkInterface chans nodeIds us app
+
+      void $ forkLinked registry (niWithServerNode ni wait)
+      forM_ (filter (/= us) nodeIds) $ \them -> forkLinked registry (niConnectTo ni them)
 
       return (coreNodeId, node)
 
@@ -196,7 +321,7 @@ broadcastNetwork registry btime numCoreNodes pInfo initRNG numSlots = do
     getUtxo :: ExtLedgerState (SimpleBlock c ext) -> Utxo
     getUtxo = mockUtxo . simpleLedgerState . ledgerState
 
-    createCommunicationChannels :: m (NodeChans m (SimpleBlock c ext))
+    createCommunicationChannels :: m (NodeChans m NodeId (SimpleBlock c ext))
     createCommunicationChannels = fmap Map.fromList $ forM nodeIds $ \us ->
       fmap ((us, ) . Map.fromList) $ forM (filter (/= us) nodeIds) $ \them -> do
         (chainSyncConsumer,  chainSyncProducer)  <- createConnectedChannels
@@ -208,22 +333,16 @@ broadcastNetwork registry btime numCoreNodes pInfo initRNG numSlots = do
   Internal auxiliary
 -------------------------------------------------------------------------------}
 
--- | Communication channel used for the Chain Sync protocol
-type ChainSyncChannel m blk = Channel m (AnyMessage (ChainSync (Header blk) (Point blk)))
-
--- | Communication channel used for the Block Fetch protocol
-type BlockFetchChannel m blk = Channel m (AnyMessage (BlockFetch blk))
-
 -- | The communication channels from and to each node
 data NodeChan m blk = NodeChan
-  { chainSyncConsumer  :: ChainSyncChannel  m blk
-  , chainSyncProducer  :: ChainSyncChannel  m blk
-  , blockFetchConsumer :: BlockFetchChannel m blk
-  , blockFetchProducer :: BlockFetchChannel m blk
+  { chainSyncConsumer  :: Channel m (AnyProtocolMessage blk)
+  , chainSyncProducer  :: Channel m (AnyProtocolMessage blk)
+  , blockFetchConsumer :: Channel m (AnyProtocolMessage blk)
+  , blockFetchProducer :: Channel m (AnyProtocolMessage blk)
   }
 
 -- | All connections between all nodes
-type NodeChans m blk = Map NodeId (Map NodeId (NodeChan m blk))
+type NodeChans m nodeId blk = Map nodeId (Map nodeId (NodeChan m blk))
 
 
 {-------------------------------------------------------------------------------
@@ -265,21 +384,21 @@ genTx addrs u = do
 -------------------------------------------------------------------------------}
 
 -- | Message sent by or to a producer
-data TalkingToProducer pid = TalkingToProducer {
-      producerUs   :: NodeId
+data TalkingToProducer nodeId pid = TalkingToProducer {
+      producerUs   :: nodeId
     , producerThem :: pid
     }
   deriving (Show)
 
 -- | Message sent by or to a consumer
-data TalkingToConsumer cid = TalkingToConsumer {
-      consumerUs   :: NodeId
+data TalkingToConsumer nodeId cid = TalkingToConsumer {
+      consumerUs   :: nodeId
     , consumerThem :: cid
     }
   deriving (Show)
 
-instance Condense pid => Condense (TalkingToProducer pid) where
+instance (Condense nodeId, Condense pid) => Condense (TalkingToProducer nodeId pid) where
   condense TalkingToProducer{..} = condense (producerUs, producerThem)
 
-instance Condense pid => Condense (TalkingToConsumer pid) where
+instance (Condense nodeId, Condense pid) => Condense (TalkingToConsumer nodeId pid) where
   condense TalkingToConsumer{..} = condense (consumerUs, consumerThem)
