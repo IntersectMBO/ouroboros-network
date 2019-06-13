02e37a6c70f1c1e6fcf32989271874813b1201bd
Marcin Szamotulski <profunctor@pm.me>
2019-06-13 09:13:57 +0200

    Ouroboros.Consensus.NodeNetwork: use ouroboros-network stack

--- ouroboros-consensus/src/Ouroboros/Consensus/NodeNetwork.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/NodeNetwork.hs
@@ -12,31 +12,30 @@
 {-# OPTIONS_GHC -Wredundant-constraints -Werror=missing-fields #-}
 
 module Ouroboros.Consensus.NodeNetwork (
-    NetworkProvides(..)
-  , NodeComms(..)
-  , initNetworkLayer
+    NetworkApplication(..)
+  , consensusNetworkApps
+  , SharedState (..)
+  , runSharedState
   ) where
 
 import           Control.Monad (void)
-import           Data.Functor.Contravariant (contramap)
+import           Data.Void (Void)
 
 import           Control.Monad.Class.MonadAsync
-import           Control.Monad.Class.MonadFork (MonadFork)
 import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadThrow
 import           Control.Monad.Class.MonadTime
 import           Control.Tracer
 
 import           Network.TypedProtocol.Driver
-import           Ouroboros.Network.Channel as Network
 import           Ouroboros.Network.Codec
+import           Ouroboros.Network.Mux.Interface
+import           Ouroboros.Network.NodeToNode
 
 import           Ouroboros.Network.Block
 import           Ouroboros.Network.BlockFetch
-import           Ouroboros.Network.BlockFetch.Client (BlockFetchClient,
-                     blockFetchClient)
-import           Ouroboros.Network.Protocol.BlockFetch.Server
-                     (BlockFetchServer (..), blockFetchServerPeer)
+import           Ouroboros.Network.BlockFetch.Client (blockFetchClient)
+import           Ouroboros.Network.Protocol.BlockFetch.Server (blockFetchServerPeer)
 import           Ouroboros.Network.Protocol.BlockFetch.Type (BlockFetch)
 import           Ouroboros.Network.Protocol.ChainSync.Client
 import           Ouroboros.Network.Protocol.ChainSync.Server
@@ -49,142 +48,138 @@ import           Ouroboros.Consensus.ChainSyncServer
 import           Ouroboros.Consensus.Ledger.Abstract
 import           Ouroboros.Consensus.Util.Condense
 import           Ouroboros.Consensus.Util.Orphans ()
-import           Ouroboros.Consensus.Util.ThreadRegistry
 
 import qualified Ouroboros.Storage.ChainDB.API as ChainDB
 
 import           Ouroboros.Consensus.Node
 
 
-{-------------------------------------------------------------------------------
-  Network layer
--------------------------------------------------------------------------------}
+data SharedState m s a = SharedState (m s) (s -> a)
 
--- TODO something nicer than eCS, eBF, bytesCS, and bytesBF. Mux them over one
--- channel.
-data NetworkProvides m up blk = NetworkProvides {
-      -- | Notify network layer of new upstream node
-      --
-      -- NOTE: Eventually it will be the responsibility of the network layer
-      -- itself to register and deregister peers.
-      addUpstream   :: forall eCS eBF bytesCS bytesBF.
-                       (Exception eCS, Exception eBF)
-                    => up
-                    -> NodeComms m (ChainSync (Header blk) (Point blk)) eCS bytesCS
-                       -- Communication for the Chain Sync protocol
-                    -> NodeComms m (BlockFetch blk)                     eBF bytesBF
-                       -- Communication for the Block Fetch protocol
-                    -> m ()
+runSharedState :: Monad m => SharedState m s a -> m a
+runSharedState (SharedState ms f) = ms >>= pure . f
 
-      -- | Notify network layer of a new downstream node
+-- | Network applications that are the consensus layer will run over
+-- a 'NetworkInterface'.
+--
+data NetworkApplication m versions up blk bytes a b = NetworkApplication {
+      -- | Mux application which runs with upstream peers.
       --
-      -- NOTE: Eventually it will be the responsibility of the network layer
-      -- itself to register and deregister peers.p
-    , addDownstream :: forall eCS eBF bytesCS bytesBF.
-                       (Exception eCS, Exception eBF)
-                    => NodeComms m (ChainSync (Header blk) (Point blk)) eCS bytesCS
-                       -- Communication for the Chain Sync protocol
-                    -> NodeComms m (BlockFetch blk)                     eBF bytesBF
-                       -- Communication for the Block Fetch protocol
-                    -> m ()
+      naMuxInitiatorApp :: up
+                        -> SharedState m (TMVar m ())
+                            (versions
+                              (MuxApplication
+                                InitiatorApp
+                                NodeToNodeProtocols
+                                m bytes a Void))
+
+      -- | Mux application which serves downstream peers.
+      --
+      -- TODO: use 'AnyMuxResponderApp', but it requires better connection
+      -- management on the networking side.  This way we avoid simultaneously
+      -- opened connections for now.
+      --
+    , naMuxResponderApp :: versions
+                            (MuxApplication
+                              ResponderApp
+                              NodeToNodeProtocols
+                              m bytes Void b)
     }
 
-
--- | Required by the network layer to initiate comms to a new node
-data NodeComms m ps e bytes = NodeComms {
-      -- | Codec used for the protocol
-      ncCodec    :: Codec ps e m bytes
-
-      -- | Construct a channel to the node
-      --
-      -- This is in CPS style to allow for resource allocation.
-    , ncWithChan :: forall a. (Channel m bytes -> m a) -> m a
-    }
-
-
-initNetworkLayer
-    :: forall m up blk.
-       ( MonadSTM   m
-       , MonadAsync m
-       , MonadFork  m
+-- | Example function which creates consensus mux applications, this is useful
+-- for testing but also untill we have only a single version in
+-- 'NodeToNodeVersions'.
+--
+consensusNetworkApps
+    :: forall m versions up blk failure bytes.
+       ( MonadAsync m
        , MonadCatch m
-       , MonadMask  m
        , MonadThrow (STM m)
        , MonadTime  m
-       , Ord up
        , ProtocolLedgerView blk
-       , TraceConstraints up blk
+       , Condense (Header blk)
+       , Condense (ChainHash blk)
+       , Ord up
+       , Condense up
+       , Exception failure
        )
-    => NodeParams m up blk
+    => (forall a. a -> versions a)
+    -> Tracer m (TraceSendRecv (ChainSync (Header blk) (Point blk)))
+    -> Tracer m (TraceSendRecv (BlockFetch blk))
+    -> Codec (ChainSync (Header blk) (Point blk)) failure m bytes
+    -> Codec (BlockFetch blk) failure m bytes
+    -> NodeParams m up blk
     -> NodeKernel m up blk
-    -> NetworkProvides m up blk
-initNetworkLayer NodeParams {..} NodeKernel{..} = NetworkProvides {..}
+    -> NetworkApplication m versions up blk bytes () ()
+consensusNetworkApps singletonV traceChainSync traceBlockFetch codecChainSync codecBlockFetch NodeParams {..} kernel =
+    NetworkApplication {
+        naMuxInitiatorApp = initiatorApp,
+        naMuxResponderApp = singletonV (simpleMuxResponderApplication responderApp)
+      }
   where
-    addDownstream :: (Exception eCS, Exception eBF)
-                  => NodeComms m (ChainSync (Header blk) (Point blk)) eCS bytesCS
-                  -> NodeComms m (BlockFetch blk)                     eBF bytesBF
-                  -> m ()
-    addDownstream ncCS ncBF = do
-      -- TODO use subregistry here?
-      let NodeComms csCodec csWithChan = ncCS
-          NodeComms bfCodec bfWithChan = ncBF
-      void $ forkLinked threadRegistry $ bfWithChan $ \chan ->
-        runPeer nullTracer bfCodec chan $
-          blockFetchServerPeer nrBlockFetchServer
-      void $ forkLinked threadRegistry $ csWithChan $ \chan ->
-        runPeer nullTracer csCodec chan $
-          chainSyncServerPeer nrChainSyncServer
+    initiatorApp :: up
+                 -> SharedState m (TMVar m ())
+                      (versions
+                        (MuxApplication
+                          InitiatorApp
+                          NodeToNodeProtocols
+                          m bytes () Void))
 
-    addUpstream :: (Exception eCS, Exception eBF)
-                => up
-                -> NodeComms m (ChainSync (Header blk) (Point blk)) eCS bytesCS
-                -> NodeComms m (BlockFetch blk)                     eBF bytesBF
-                -> m ()
-    addUpstream up ncCS ncBF = do
-      -- TODO use subregistry here?
-      let NodeComms csCodec csWithChan = ncCS
-          NodeComms bfCodec bfWithChan = ncBF
+    initiatorApp up = SharedState newEmptyTMVarM $ \clientRegistered ->
+      singletonV $ MuxInitiatorApplication $
+        \ptcl channel -> case ptcl of
+          ChainSyncWithHeadersPtcl -> do
+            -- The block fetch logic thread in the background wants there to be a
+            -- block fetch client thread for each chain sync candidate it sees. So
+            -- start the chain sync client after the block fetch thread was
+            -- registered to make sure it never sees a chain sync candidate without
+            -- a corresponding block fetch client thread.
+            atomically $ takeTMVar clientRegistered
 
-      clientRegistered <- newEmptyTMVarM
+            void $ runPeer
+              traceChainSync
+              codecChainSync
+              channel
+              $ chainSyncClientPeer
+              $ chainSyncClient
+                  (tracePrefix "CSClient" (Just up) tracer)
+                  cfg
+                  btime
+                  maxClockSkew
+                  (ChainDB.getCurrentChain chainDB)
+                  (ChainDB.getCurrentLedger chainDB)
+                  (getNodeCandidates kernel)
+                  up
 
-      void $ forkLinked threadRegistry $ bfWithChan $ \chan ->
-        bracketFetchClient getFetchClientRegistry up $ \clientCtx -> do
-          atomically $ putTMVar clientRegistered ()
-          runPipelinedPeer nullTracer bfCodec chan $
-            nrBlockFetchClient clientCtx
+          BlockFetchPtcl ->
+            bracketFetchClient (getFetchClientRegistry kernel) up $ \clientCtx -> do
 
-      -- The block fetch logic thread in the background wants there to be a
-      -- block fetch client thread for each chain sync candidate it sees. So
-      -- start the chain sync client after the block fetch thread was
-      -- registered to make sure it never sees a chain sync candidate without
-      -- a corresponding block fetch client thread.
-      atomically $ takeTMVar clientRegistered
+              atomically $ putTMVar clientRegistered ()
 
-      void $ forkNonTerminating threadRegistry $ csWithChan $ \chan ->
-        runPeer nullTracer csCodec chan $
-          chainSyncClientPeer (nrChainSyncClient up)
+              runPipelinedPeer
+                traceBlockFetch
+                codecBlockFetch
+                channel
+                (blockFetchClient clientCtx)
 
-    nrChainSyncClient :: up -> Consensus ChainSyncClient blk m
-    nrChainSyncClient up = chainSyncClient
-      (tracePrefix "CSClient" (Just up))
-      cfg
-      btime
-      maxClockSkew
-      (ChainDB.getCurrentChain chainDB)
-      (ChainDB.getCurrentLedger chainDB)
-      getNodeCandidates
-      up
 
-    nrChainSyncServer :: ChainSyncServer (Header blk) (Point blk) m ()
-    nrChainSyncServer =
-      chainSyncServer (tracePrefix "CSServer" Nothing) chainDB
+    responderApp :: NodeToNodeProtocols
+                 -> MuxPeer failure m bytes ()
 
-    nrBlockFetchClient :: BlockFetchClient (Header blk) blk m ()
-    nrBlockFetchClient = blockFetchClient
-      -- Note the tracer for the changes in the fetch client state
-      -- is passed to the blockFetchLogic.
-      -- The message level tracer is passed to runPipelinedPeer.
+    responderApp ChainSyncWithHeadersPtcl =
+      MuxPeer
+        traceChainSync
+        codecChainSync
+        (chainSyncServerPeer
+          $ chainSyncServer
+            (tracePrefix "CSServer" (Nothing :: Maybe up) tracer)
+            chainDB)
 
-    nrBlockFetchServer :: BlockFetchServer blk m ()
-    nrBlockFetchServer =
-      blockFetchServer (tracePrefix "BFServer" Nothing) chainDB
+    responderApp BlockFetchPtcl =
+      MuxPeer
+        traceBlockFetch
+        codecBlockFetch
+        (blockFetchServerPeer
+          $ blockFetchServer
+            (tracePrefix "BFServer" (Nothing :: Maybe up) tracer)
+            chainDB)
