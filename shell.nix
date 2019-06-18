d1373672ec5575e1f2cd0a6d772db5decf6d9630
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-06-18 16:53:13 +0300

    shell.nix:  review suggestions

--- shell.nix
+++ shell.nix
@@ -4,14 +4,14 @@ let
   default = import ./default.nix {};
 in
 default.nix-tools._raw.shellFor {
-  packages    = p: map (x: p."${x}") [
-    "io-sim"
-    "io-sim-classes"
-    "ouroboros-consensus"
-    "ouroboros-network"
-    "typed-transitions"
+  packages    = ps: with ps; [
+    io-sim
+    io-sim-classes
+    ouroboros-consensus
+    ouroboros-network
+    typed-transitions
   ];
-  withHoogle  = withHoogle;
+  inherit withHoogle;
   buildInputs = with default.nix-tools._raw; [
     cabal-install.components.exes.cabal
   ];
