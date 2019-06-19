7165dc8e01186192c512d599121f34c560c83bdd
Neil Davies <nel.davies@pnsol.com>
2019-06-19 14:24:34 +0100

    initial notebook

---
+++ DeltaQ/run-me.sh
@@ -0,0 +1,1 @@
+nix-shell --run "(cd jupyter-notebooks;jupyter-lab)"
---
+++ DeltaQ/shell.nix
@@ -0,0 +1,30 @@
+let
+
+  jupyter = import (builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    rev = "";
+    }) {};
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+
+  iHaskell = jupyter.kernels.iHaskellWith {
+    name = "haskell";
+    packages = p: with p; [ 
+#    ihaskell-charts
+     ihaskell-plot
+#     ihaskell-hatex
+#     ihaskell-diagrams ihaskell-graphviz ihaskell-magic
+#     ihaskell-aeson ihaskell-gnuplot ihaskell-widgets
+#    formatting hvega
+   ];
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython iHaskell ];
+    };
+in
+  jupyterEnvironment.env
