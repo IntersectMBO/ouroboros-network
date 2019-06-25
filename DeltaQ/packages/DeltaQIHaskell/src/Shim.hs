86402eb7f52823c5a9e34777da8586b7156ba14a
Neil Davies <nel.davies@pnsol.com>
2019-06-25 14:36:07 +0100

    added support for rendering ∆Q into LaTeX - so that results typeset properly

--- DeltaQ/packages/DeltaQIHaskell/src/Shim.hs
+++ DeltaQ/packages/DeltaQIHaskell/src/Shim.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Shim
 where
 
 import DeltaQ.Algebra.Class
-import Text.LaTeX
+import Text.LaTeX hiding (Bottom)
+import Text.LaTeX.Packages.AMSMath
+import Text.LaTeX.Base.Class (comm0)
 
-instance LaTeX DeltaQ 
+asLaTeX :: DeltaQ p d n -> LaTeX
+asLaTeX dq  =  math $ 
+  case dq of
+    Bottom           -> bot --  '⊥'
+    Unit             -> comm0 "emptyset" --  '∅'
+    Delay _          -> "DELAY"
+    ProbChoice p a b -> undefined
+    Convolve a b     -> undefined    
