83f98870d2cf24a0a0452e573ff392a700672029
Neil Davies <nel.davies@pnsol.com>
2019-06-26 15:55:58 +0100

    With SimpleUniform Delay printer

--- DeltaQ/packages/DeltaQIHaskell/shell.nix
+++ DeltaQ/packages/DeltaQIHaskell/shell.nix
@@ -4,12 +4,16 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, DeltaQIllustration, HaTeX, stdenv }:
+  f = { mkDerivation, base, DeltaQIllustration, HaTeX, ihaskell
+      , ihaskell-hatex, stdenv
+      }:
       mkDerivation {
         pname = "DeltaQIHaskell";
         version = "0.1.0.0";
         src = ./.;
-        libraryHaskellDepends = [ base DeltaQIllustration HaTeX ];
+        libraryHaskellDepends = [
+          base DeltaQIllustration HaTeX ihaskell ihaskell-hatex
+        ];
         description = "Interface module to isolate DeltaQ libraries from IHaskell/Jupyter requirements";
         license = stdenv.lib.licenses.bsd3;
       };
--- DeltaQ/packages/DeltaQIHaskell/src/Shim.hs
+++ DeltaQ/packages/DeltaQIHaskell/src/Shim.hs
@@ -1,28 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
-module Shim
+module Shim ()
 where
 
-import DeltaQ.Algebra.Class
-import Text.LaTeX hiding (Bottom)
-import Text.LaTeX.Packages.AMSMath
-import Text.LaTeX.Base.Class
 import Data.Ratio
+import DeltaQ.Algebra.Class
+import DeltaQ.Algebra.DelayModel.SimpleUniform
+import IHaskell.Display
+import IHaskell.Display.Hatex()
+import Text.LaTeX hiding (Bottom)
+import Numeric
+import Text.LaTeX.Base.Class
+import Text.LaTeX.Packages.AMSMath
 
-asLaTeX :: Texy t => t -> LaTeX
-asLaTeX = execLaTeXM . mathDisplay . texy
 
-instance (RealFrac p) => Texy (DeltaQ p n d) where
+instance (RealFrac p, Texy (n d)) => IHaskellDisplay (DeltaQ p n d) where
+  display = display . execLaTeXM . mathDisplay . texy
+
+
+instance (RealFrac p, Texy (n d)) => Texy (DeltaQ p n d) where
   texy x
    = case x of
        Bottom           -> bot --  '⊥' / bottom
        Unit             -> comm0 "emptyset" --  '∅' / perfection
-       Delay _          -> "DELAY"
+       Delay d          -> texy d
        ProbChoice p a b -> pchoice p (condBracket a) (condBracket b) -- '⇋' / choice
        Convolve a b     -> oplus (condBracket a) (condBracket b) -- '⊕' / convolve
     where
       pchoice p = between $ (lrharp p)
-
+      
       lrharp p
         = let r = approxRational p (fromRational $ toRational 1e-6)
           in (operatorname (commS "leftrightharpoons"))
@@ -33,3 +38,12 @@ instance (RealFrac p) => Texy (DeltaQ p n d) where
           ProbChoice _ _ _ -> autoParens $ texy x
           Convolve _ _     -> autoParens $ texy x
           _                -> texy x
+
+instance (RealFloat r) => Texy (SimpleUniform r) where
+  texy x = 
+    case x of
+      DiracDelta t -> delta <> lfloor <> fmt t <> rceil
+      UniformD   t -> (commS "sqcap") <> autoSquareBrackets (fmt t)
+   where
+      fmt x = fromString $ showGFloatAlt (Just 2) x ""
+
