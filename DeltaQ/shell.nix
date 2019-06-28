7293d5c5420e20dffcf384b3af2476eb048f9ac0
Neil Davies <nel.davies@pnsol.com>
2019-06-28 09:27:15 +0100

    added some more ihaskell-renderers

--- DeltaQ/shell.nix
+++ DeltaQ/shell.nix
@@ -15,25 +15,24 @@ let
       let  dq1 = callCabal2nix "DeltaQIllustration"  ./packages/DeltaQIllustration {Chart=Chart;};
            dq2 = callCabal2nix "DeltaQIHaskell"      ./packages/DeltaQIHaskell {DeltaQIllustration = dq1;};
            Chart = (callHackageDirect {pkg    = "Chart";
-                                       ver    = "1.9.1";     
+                                       ver    = "1.9.1";
                                        sha256 = "0aiq9r78yhma1xzhwl2xlzwqb2c59k6a4jjvdpninwm6lngac3s7";}) {};
-      
+
            Chart-cairo = (callHackageDirect {pkg    = "Chart-cairo";
-                                       ver    = "1.9.1";     
+                                       ver    = "1.9.1";
                                        sha256 = "0ambbkxbndjqxr0c4pysn87b3z0f8zpgvldj4qjbyazadna9k807";}) {Chart=Chart;};
           # ihaskell-charts needs some special treatment to get around broken packages
           ih-charts = callHackage "ihaskell-charts" "0.3.0.1" {Chart=Chart;Chart-cairo=Chart-cairo;};
       in [
-    # the pretty renders 
-    ihaskell-hatex ihaskell-diagrams ihaskell-graphviz ihaskell-aeson ihaskell-hvega
-    ihaskell-widgets
-    #  ihaskell-plot ihaskell-gnuplot  ihaskell-magic
+    # the pretty renders
+    ihaskell-hatex ihaskell-diagrams ihaskell-graphviz ihaskell-aeson ihaskell-hvega ihaskell-magic
+    # note next line may have more "broken" dependencies in them
+    #  ihaskell-plot ihaskell-gnuplot ihaskell-widgets
     # current special case for Chart and its dependencies
     Chart ih-charts
     formatting hvega
     dq1
     dq2
-
    ];
   };
 
