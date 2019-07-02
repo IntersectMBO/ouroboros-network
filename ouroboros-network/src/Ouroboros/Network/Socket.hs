a7b015cbda431ac34e822fd9d1102806cd4dabb5
Karl Knutsson <karl.knutsson@iohk.io>
2019-07-02 15:26:27 +0200

    Permit InitiatorAndResponderApp to open connections

--- ouroboros-network/src/Ouroboros/Network/Socket.hs
+++ ouroboros-network/src/Ouroboros/Network/Socket.hs
@@ -212,7 +212,7 @@ connectToNode encodeData decodeData versions localAddr remoteAddr =
 --
 -- Exceptions thrown by @'MuxApplication'@ are rethrown by @'connectTo'@.
 connectToNode'
-  :: forall ptcl vNumber extra a b.
+  :: forall appType ptcl vNumber extra a b.
      ( Mx.ProtocolEnum ptcl
      , Ord ptcl
      , Enum ptcl
@@ -224,10 +224,11 @@ connectToNode'
      , Show vNumber
      , Show ptcl
      , Mx.MiniProtocolLimits ptcl
+     , HasInitiator appType ~ True
      )
   => (forall vData. extra vData -> vData -> CBOR.Term)
   -> (forall vData. extra vData -> CBOR.Term -> Either Text vData)
-  -> Versions vNumber extra (MuxApplication InitiatorApp ptcl IO BL.ByteString a b)
+  -> Versions vNumber extra (MuxApplication appType ptcl IO BL.ByteString a b)
   -- ^ application to run over the connection
   -> Socket.Socket
   -> IO ()
