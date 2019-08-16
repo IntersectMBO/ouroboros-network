b75c0d2ca58a9e5a200d599d8eb1ee8144a48a66
Alexander Vieth <alex@well-typed.com>
2019-08-16 18:16:56 -0400

    fix CDDL definitions
    In `test/CDDL.hs` there are some type synonyms for the protocols, including
    
    ```
    type CS = ChainSync DummyBytes DummyBytes
    ```
    
    That's a chain sync codec where the point type is the same as the header type.
    What it always _should_ have been is
    
    ```
    type CS = ChainSync DummyBytes (Point DummyBytes)
    ```
    
    but this was never noticed, because the CDDL tests never actually tested this
    part of the codec! The CDDL definition for a point was
    
    ```
    point = bytes .cbor any
    ```
    
    although there was a different one for block fetch (which I remember adding)
    which looks like
    
    ```
    point = origin / [slotNo, blockHash]
    origin = []
    slotNo = uint
    blockHash = null ; because the tests use () for hashes
    ```
    
    I've consolidated this.
    
    And finally, with this pull request, CBOR-in-CBOR is used for header and
    block encodings, so I had to update the CDDL file to use
    
    ```
    \#6.24(bytes . cbor bytes)
    ```.

--- ouroboros-network/test/CDDL.hs
+++ ouroboros-network/test/CDDL.hs
@@ -13,12 +13,15 @@ import Control.Exception.Base (throw)
 import qualified Data.ByteString.Lazy as BS
 import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy.Char8 as Char8 (putStrLn, unpack, lines)
+import Data.List (intercalate)
 import Data.Word (Word32)
 import qualified Codec.Serialise.Class as Serialise
 import Codec.CBOR.Decoding (decodeWord, decodeListLenOf, decodeBytes)
+import qualified Codec.CBOR.Decoding as CBOR
 import qualified Codec.CBOR.Term as CBOR (Term)
 import Codec.CBOR.Read
 
+import Ouroboros.Network.Block (Point)
 import Ouroboros.Network.Protocol.ChainSync.Type as CS
 import Ouroboros.Network.Protocol.ChainSync.Codec (codecChainSync)
 import Network.TypedProtocol.ReqResp.Type as ReqResp
@@ -34,7 +37,7 @@ import Ouroboros.Network.Block (HeaderHash)
 import Network.TypedProtocol.Codec
 
 -- the concrete types used for the test
-type CS = ChainSync DummyBytes DummyBytes
+type CS = ChainSync DummyBytes (Point DummyBytes)
 type RR = ReqResp DummyBytes DummyBytes
 type BF = BlockFetch DummyBytes
 type HS = Handshake Word32 CBOR.Term
@@ -88,44 +91,71 @@ decodeTopTerm input
 -- Decode a message. Throw an error if the message is not decodeable.
 decodeMsg :: (Word, ByteString) -> IO ()
 decodeMsg (tag, input) = case tag of
-    0 -> tryParsers "chainSync"  chainSyncParsers
-    1 -> tryParsers "reqResp"    reqRespParsers
-    2 -> tryParsers "pingPong"   pingPongParsers
-    3 -> tryParsers "blockFetch" blockFetchParsers
+    0 -> tryParsers ["chainSync"]  chainSyncParsers
+    1 -> tryParsers ["reqResp"]    reqRespParsers
+    2 -> tryParsers ["pingPong"]   pingPongParsers
+    3 -> tryParsers ["blockFetch"] blockFetchParsers
     4 -> return () -- "txSubmissionMessage" in branch
-    5 -> tryParsers "handshake"  handshakeParsers
+    5 -> tryParsers ["handshake"]  handshakeParsers
+    -- Point and list of point.
+    6 -> case deserialiseFromBytes decodePoint input of
+           Left err -> error ("point: " ++ show err)
+           Right (_, _) -> pure ()
+    7 -> case deserialiseFromBytes decodePoints input of
+           Left err -> error ("points: " ++ show err)
+           Right (_, _) -> pure ()
     _ -> error "unkown tag"
     where
-        tryParsers :: String -> [IO Bool] -> IO ()
-        tryParsers name [] = error $ "parse failed : " ++ name
-        tryParsers name (h:t) = h >>= \case
-            True -> return ()
-            False -> tryParsers name t
+
+        decodePoint :: CBOR.Decoder s (Point DummyBytes)
+        decodePoint = Serialise.decode
+
+        decodePoints :: CBOR.Decoder s [Point DummyBytes]
+        decodePoints = decodeList decodePoint
+
+        decodeList :: CBOR.Decoder s a -> CBOR.Decoder s [a]
+        decodeList dec = do
+          mn <- CBOR.decodeListLenOrIndef
+          case mn of
+            Nothing -> CBOR.decodeSequenceLenIndef (flip (:)) [] reverse   dec
+            Just n  -> CBOR.decodeSequenceLenN     (flip (:)) [] reverse n dec
+
+        -- typed-protocols codecs are parameterized on the tokens which
+        -- serve as singletons to identify the state types. For each message
+        -- we know which protocol it should belong to, but not which state,
+        -- so we just run all of them at expect that at least one passes.
+        -- This isn't ideal but it's better than nothing. In case they all
+        -- fail, error messages for each are given in order.
+        tryParsers :: [String] -> [IO (Maybe String)] -> IO ()
+        tryParsers traces [] = error $ intercalate "\n" (reverse ("parse failed: " : traces))
+        tryParsers traces (h:t) = h >>= \case
+            Nothing  -> return ()
+            Just str -> tryParsers (str : traces) t
 
         runCodec
           :: IO (DecodeStep ByteString DeserialiseFailure IO (SomeMessage st))
           -> ByteString
-          -> IO Bool
+          -> IO (Maybe String)
         runCodec cont bs = cont >>= \case
             DecodeDone _msg _rest -> error "runCodec: codec is DecodeDone"
-            DecodeFail _f -> error "runCodec: codec is DecodeFail"
-            DecodePartial next -> (next $ Just bs) >>= \case
-                DecodePartial _ -> return False
+            DecodeFail _f         -> error "runCodec: codec is DecodeFail"
+            DecodePartial next    -> (next $ Just bs) >>= \case
+                DecodePartial _      -> return (Just "expecting more input")
                 DecodeDone _msg rest -> case rest of
-                    Nothing -> return True
-                    Just _r  -> return False
-                DecodeFail _f -> return False
+                    Nothing   -> return Nothing
+                    Just _r   -> return (Just "leftover input")
+                DecodeFail _f -> return (Just (show _f))
 
         run :: forall ps (pr :: PeerRole) (st :: ps).
                 Codec ps DeserialiseFailure IO ByteString
-             -> PeerHasAgency pr st -> IO Bool
+             -> PeerHasAgency pr st -> IO (Maybe String)
         run codec state = runCodec ((decode codec) state) input
 
         runCS = run codecCS
 
         codecCS :: MonoCodec CS
         codecCS = codecChainSync Serialise.encode (fmap const Serialise.decode)
-                                 Serialise.encode             Serialise.decode
+                                 Serialise.encode (Serialise.decode :: CBOR.Decoder s (Point DummyBytes))
 
         chainSyncParsers = [
               runCS (ClientAgency CS.TokIdle)
@@ -151,7 +181,7 @@ decodeMsg (tag, input) = case tag of
 
         codecBF :: MonoCodec BF
         codecBF = codecBlockFetch Serialise.encode (fmap const Serialise.decode)
-                                  Serialise.encode             Serialise.decode
+                                  Serialise.encode (Serialise.decode :: CBOR.Decoder s (HeaderHash DummyBytes))
 
         blockFetchParsers = [
               runBlockFetch (ClientAgency BlockFetch.TokIdle)
