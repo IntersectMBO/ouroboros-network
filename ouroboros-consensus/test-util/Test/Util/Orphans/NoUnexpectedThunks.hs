3bec4de3bd20b71f1ef1da97a0ed56f14cd3966b
Edsko de Vries <edsko@well-typed.com>
2019-10-02 17:42:56 +0200

    NoUnexpectedThunks support for StrictMVar

--- ouroboros-consensus/test-util/Test/Util/Orphans/NoUnexpectedThunks.hs
+++ ouroboros-consensus/test-util/Test/Util/Orphans/NoUnexpectedThunks.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Test.Util.Orphans.NoUnexpectedThunks () where
 
-import           Ouroboros.Consensus.Util.MonadSTM.NormalForm
 import           Control.Monad.IOSim
 import           Control.Monad.ST.Lazy
 import           Control.Monad.ST.Unsafe (unsafeSTToIO)
+import           Ouroboros.Consensus.Util.MonadSTM.NormalForm
 
 import           Cardano.Prelude (NoUnexpectedThunks (..), ThunkInfo (..))
 
@@ -18,4 +19,10 @@ instance NoUnexpectedThunks a => NoUnexpectedThunks (StrictTVar (SimM s) a) wher
   showTypeOf _ = "StrictTVar SimM"
   whnfNoUnexpectedThunks ctxt tvar = do
       a <- unsafeSTToIO $ lazyToStrictST $ execReadTVar (toLazyTVar tvar)
+      noUnexpectedThunks ctxt a
+
+instance NoUnexpectedThunks a => NoUnexpectedThunks (StrictMVar (SimM s) a) where
+  showTypeOf _ = "StrictMVar SimM"
+  whnfNoUnexpectedThunks ctxt StrictMVar { tvar } = do
+      a <- unsafeSTToIO $ lazyToStrictST $ execReadTVar tvar
       noUnexpectedThunks ctxt a
