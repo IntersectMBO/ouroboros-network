c037e738c2d953701620134f5bc8cf9bf78fdf2d
Alexander Vieth <alex@well-typed.com>
2019-10-03 14:40:47 -0400

    topography graph edges are Last semigroup
    No multiple edges. Requires a patched algebraic-graphs that doesn't
    demand monoid nor equality on its edge labels.

--- peer-selection/src/DeltaQ/Topography.hs
+++ peer-selection/src/DeltaQ/Topography.hs
@@ -5,48 +5,15 @@
 module DeltaQ.Topography where
 
 import Algebra.Graph.Labelled.AdjacencyMap
+import Data.Semigroup (Last (..))
 
---import DeltaQ.OutcomesTypes
+edge :: e -> Last e
+edge = Last
 
--- | A monoid suitable for use with algebraic-graphs, which treats mempty of
--- the edge label as "no edge". It's not OK to use, for instance, SimpleGS as
--- the edge label directly. If multiple edges are found, these will be combined,
--- so that one Bottom edge makes all edges Bottom. What's more, mempty is
--- perfection, which just does not correspond to "no edge".
-newtype Edges e = Edges { getEdges :: [e] }
-  deriving (Show)
-
-instance Semigroup (Edges e) where
-  left <> right = Edges (getEdges left <> getEdges right)
-
-instance Monoid (Edges e) where
-  mappend = (<>)
-  mempty  = Edges mempty
-
-instance Eq e => Eq (Edges e) where
-  left == right = getEdges left == getEdges right
-
-instance Ord e => Ord (Edges e) where
-  left `compare` right = getEdges left `compare` getEdges right
-
-instance Functor Edges where
-  fmap f = Edges . fmap f . getEdges
-
-edge :: e -> Edges e
-edge = Edges . pure
-
--- | An adjacency map graph with 'Edges edge' as its edge label.
-type Topography edge vertex = AdjacencyMap (Edges edge) vertex
-
-{-
-instance (QualityAttenuationMeasure a, Monoid a, Eq b, Ord b) => 
-         TomographyGraph (Topography a b) where
-  type QAMeasure (Topography a b) = a
-  type NodeName  (Topography a b) = b
-
-  linkAttenutation a n1 n2 = edgeLabel n1 n2 a
-  nodes                    = vertexSet
--}
+-- | An adjacency map graph with at most one edge per pair of vertices. The
+-- @Last@ semigroup is used to favour later graphs when one is constructed
+-- compositionally.
+type Topography edge vertex = AdjacencyMap (Last edge) vertex
 
 -- | remove the supplied bi-directional link from the connectivity,
 --   ignore if link not present.
@@ -57,7 +24,7 @@ maskLinks  rm tg
     expand xs = concat [[(a,b), (b,a)] | (a,b) <- xs]
 
 -- | transform the link annotation for a given topology
-updateLinkAnnotation :: ( Ord a, Eq edge' )
+updateLinkAnnotation :: ( Ord a )
                      => (a -> a -> edge -> edge')
                      -> Topography edge a -> Topography edge' a
 updateLinkAnnotation f = edges . map f' . edgeList
