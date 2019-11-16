ee20d3278cd0cb89a1fa7a2cd83caa777c2cbd87
Neil Davies <Neil.Davies@pnsol.com>
2019-11-16 09:09:23 +0000

    sample folding logic

--- network-mux/src/Network/Mux/DeltaQTraceTransformer.hs
+++ network-mux/src/Network/Mux/DeltaQTraceTransformer.hs
@@ -1,21 +1,28 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Network.Mux.DeltaQTraceTransformer
 where
 
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.Maybe
+import           Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
+import           Data.Maybe
 
-import Control.Monad.Class.MonadTime
-import Network.Mux.Types
+import           Control.Monad.Class.MonadTime
+import           Network.Mux.Types
 
 -- Map time intervals to real numbers, for the arithmetic.
 newtype SISec  = S  Float -- this is all the precision we need,
+  deriving (Eq, Ord, Num)
 newtype SISec2 = S2 Float -- are there performance reasons to use Double?
+  deriving (Eq, Ord, Num)
 
+squareSISec :: SISec -> SISec2
+squareSISec (S x) = S2 $ x * x
+
+-- the observation step
 step :: StatsA -> RemoteClockModel -> Time -> Int -> (Maybe Sample, StatsA)
 step s remoteTS localTS obsSize
  | isNothing (referenceTimePoint s)
-   = step (s { referenceTimePoint = Just (remoteTS, localTS)
+   = step (s { referenceTimePoint = Just $! (remoteTS, localTS)
              , nextSampleAt       = sampleInterval `addTime` localTS
              , timeLastObs        = localTS -- for single observation in sample case
              })
@@ -29,7 +36,6 @@ step s remoteTS localTS obsSize
         (_, s') = step initialStatsA remoteTS localTS obsSize
     in (Just sample, s')
 
-
 calcTransitTime :: (RemoteClockModel, Time)
                 -> RemoteClockModel
                 -> Time
@@ -38,9 +44,13 @@ calcTransitTime _rt _rts _lts
   = undefined
 
 recordObservation :: StatsA -> Time -> Int -> SISec -> StatsA
-recordObservation _s _obsTime _obsSize _transitTime
-  = undefined
-
+recordObservation s obsTime obsSize transitTime
+  = let f Nothing  = Just $! makePerSizeRecord transitTime
+        f (Just a) = Just $! makePerSizeRecord transitTime <> a
+    in s { timeLastObs     = obsTime
+         , numObservations = succ (numObservations s)
+         , observables     = IM.alter f obsSize (observables s)
+         }
 
 constructSample :: StatsA -> Sample
 constructSample _ = undefined
@@ -56,8 +66,22 @@ data StatsA = StatsA
     -- per observation
   , numObservations    :: !Int
   , timeLastObs        :: !Time
-  , observables        :: !(IntMap (SISec, Int, SISec, SISec2))
+  , observables        :: !(IntMap PerSizeRecord)
   }
+
+data PerSizeRecord = PSR
+  { minTransitTime   :: !SISec
+  , count            :: !Int
+  , sumTransitTime   :: !SISec
+  , sumTransitTimeSq :: !SISec2
+  }
+
+instance Semigroup PerSizeRecord where
+  a <> b = PSR { minTransitTime   = (minTransitTime a) `min` (minTransitTime b)
+               , count            = count a + count b
+               , sumTransitTime   = sumTransitTime a + sumTransitTime b
+               , sumTransitTimeSq = sumTransitTimeSq a + sumTransitTimeSq b
+               }
 
 initialStatsA :: StatsA
 initialStatsA = StatsA
@@ -70,7 +94,15 @@ initialStatsA = StatsA
   where
     noTime = Time 0
 
--- May want to make some of these configuration variables
+makePerSizeRecord :: SISec -> PerSizeRecord
+makePerSizeRecord tt = PSR
+  { minTransitTime   = tt
+  , count            = 1
+  , sumTransitTime   = tt
+  , sumTransitTimeSq = squareSISec tt
+  }
+
+-- May want to make this a configuration variable
 
 sampleInterval :: DiffTime
 sampleInterval = 10
