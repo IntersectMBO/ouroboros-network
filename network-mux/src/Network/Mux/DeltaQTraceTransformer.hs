b804129afac43963c7c6258ebd1ba5fe0fb077c7
Neil Davies <nel.davies@pnsol.com>
2019-11-17 08:57:20 +0000

    Added reasoning on numerical stability and hence intermediate representation

--- network-mux/src/Network/Mux/DeltaQTraceTransformer.hs
+++ network-mux/src/Network/Mux/DeltaQTraceTransformer.hs
@@ -84,6 +84,39 @@ data StatsA = StatsA
   , observables        :: !(IntMap PerSizeRecord)
   }
 
+
+-- This _may_ not be the best representation, but it does appear to be
+-- an adequate one. There are known issues with numerical stabilty for
+-- this representation approach in floating point arithmetic where the
+-- values being measured are "large" and the variablity in the sampled
+-- population is "small" (i.e the inherent rounding effect of floating
+-- point arithmetic has an effect).
+-- 
+-- This is very unlikely to cause an issue here as:
+--
+--   a) the modulo model of the RemoteClockModel (and hence the
+--   establishment of a clock reference offset for a given sample)
+--   means that we are only ever recording differences - thus any
+--   absolute clock differences get factored out.
+--
+--   b) the transit delay for a measurement will be small, (probablly
+--   not even credible) worst case ~10^3 / 10^4 seconds, the finite
+--   mantissa of IEEE754
+--   (https://en.wikipedia.org/wiki/IEEE_754#Representation_and_encoding_in_memory)
+--   even for 32 bit representation (24bits / 7.22 decimal digits)
+--   represents an bound on the inherent measured population
+--   variability.
+--
+--   c) longer term clock drift is covered here by the restablishing
+--   of the clock reference offsets every sampling period. Given a
+--   reasonable sampling period (say 10 seconds) clock drift ( <
+--   100ppm) can't amount to a significant error over such a period.
+--
+-- To conclude, reasonable model of delay is < 1second, the precision
+-- of delay measurement is 10^-6 - this all fits nicely withing a
+-- IEEE754 32bit representation with its 7.22 decimal digit
+-- mantissa. Haskell `Float`s are adequate for this purpose.
+
 data PerSizeRecord = PSR
   { minTransitTime   :: !SISec
   , count            :: !Int
