d273fb430821f696cabe73301536a560542a43da
Edsko de Vries <edsko@well-typed.com>
2019-11-18 09:17:14 +0100

    Use human readable DB marker format
    Also move to separate module, and refer to as DB marker rather than
    something related to protocol magic ID (the latter is merely an
    implementation detail of the former).

--- ouroboros-consensus/test-consensus/Test/Consensus/Node.hs
+++ ouroboros-consensus/test-consensus/Test/Consensus/Node.hs
@@ -2,18 +2,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Test.Consensus.Node (tests) where
 
-import qualified Codec.CBOR.Write as CBOR
 import           Data.Bifunctor (second)
-import           Data.ByteString (ByteString)
 import qualified Data.Map.Strict as Map
 
 import           Control.Monad.IOSim (runSimOrThrow)
 
-import           Cardano.Binary (toCBOR)
 import           Cardano.Crypto (ProtocolMagicId (..))
 
-import           Ouroboros.Consensus.Node (ChainDbCheckError (..),
-                     checkProtocolMagicId, protocolMagicIdFile)
+import           Ouroboros.Consensus.Node.DbMarker
 
 import           Ouroboros.Storage.FS.API.Types
 import qualified Ouroboros.Storage.Util.ErrorHandling as EH
@@ -28,7 +24,7 @@ import           Test.Util.FS.Sim.STM (runSimFS)
 
 tests :: TestTree
 tests = testGroup "Node"
-    [ testGroup "checkProtocolMagicId"
+    [ testGroup "checkDbMarker"
       [ testCase "match"        test_checkProtocolMagicId_match
       , testCase "mismatch"     test_checkProtocolMagicId_mismatch
       , testCase "empty folder" test_checkProtocolMagicId_empty_folder
@@ -39,7 +35,7 @@ tests = testGroup "Node"
     ]
 
 {-------------------------------------------------------------------------------
-  checkProtocolMagicId
+  checkDbMarker
 -------------------------------------------------------------------------------}
 
 expectedProtocolMagicId :: ProtocolMagicId
@@ -50,25 +46,22 @@ mountPoint = MountPoint "root"
 
 fullPath :: FilePath
 fullPath = fsToFilePath
-    mountPoint (fsPathFromList [protocolMagicIdFile])
+    mountPoint (fsPathFromList [dbMarkerFile])
 
-runCheck :: Files -> (Either ChainDbCheckError (), Files)
+runCheck :: Files -> (Either DbMarkerError (), Files)
 runCheck files = runSimOrThrow $ do
     fmap (second Mock.mockFiles) $
       runSimFS EH.monadCatch Mock.empty { Mock.mockFiles = files } $ \hasFS ->
-        checkProtocolMagicId hasFS mountPoint expectedProtocolMagicId
-
-protocolMagicIdToBS :: ProtocolMagicId -> ByteString
-protocolMagicIdToBS = CBOR.toStrictByteString . toCBOR
+        checkDbMarker hasFS mountPoint expectedProtocolMagicId
 
 test_checkProtocolMagicId_match :: Assertion
 test_checkProtocolMagicId_match = res @?= Right ()
   where
     fs = Folder $ Map.fromList
-      [ (protocolMagicIdFile, File $ protocolMagicIdToBS expectedProtocolMagicId)
-      , ("immutable",         Folder mempty)
-      , ("ledger",            Folder mempty)
-      , ("volatile",          Folder mempty)
+      [ (dbMarkerFile, File $ dbMarkerContents expectedProtocolMagicId)
+      , ("immutable",  Folder mempty)
+      , ("ledger",     Folder mempty)
+      , ("volatile",   Folder mempty)
       ]
     (res, _) = runCheck fs
 
@@ -76,10 +69,10 @@ test_checkProtocolMagicId_mismatch :: Assertion
 test_checkProtocolMagicId_mismatch = res @?= Left e
   where
     fs = Folder $ Map.fromList
-      [ (protocolMagicIdFile, File $ protocolMagicIdToBS actual)
-      , ("immutable",         Folder mempty)
-      , ("ledger",            Folder mempty)
-      , ("volatile",          Folder mempty)
+      [ (dbMarkerFile, File $ dbMarkerContents actual)
+      , ("immutable",  Folder mempty)
+      , ("ledger",     Folder mempty)
+      , ("volatile",   Folder mempty)
       ]
     (res, _) = runCheck fs
     actual = ProtocolMagicId 10
@@ -91,12 +84,12 @@ test_checkProtocolMagicId_mismatch = res @?= Left e
 test_checkProtocolMagicId_empty_folder :: Assertion
 test_checkProtocolMagicId_empty_folder = do
     res @?= Right ()
-    expectedFs' @?= fs'
+    fs' @?= expectedFs'
   where
     fs = Folder mempty
     (res, fs') = runCheck fs
     expectedFs' = Folder $ Map.fromList
-      [ (protocolMagicIdFile, File $ protocolMagicIdToBS expectedProtocolMagicId) ]
+      [ (dbMarkerFile, File $ dbMarkerContents expectedProtocolMagicId) ]
 
 test_checkProtocolMagicId_missing :: Assertion
 test_checkProtocolMagicId_missing = res @?= Left e
@@ -105,28 +98,28 @@ test_checkProtocolMagicId_missing = res @?= Left e
       [ ("passwords.txt", File "qwerty\n123456\n")
       ]
     (res, _) = runCheck fs
-    e = NoProtocolMagicIdAndNotEmpty fullPath
+    e = NoDbMarkerAndNotEmpty fullPath
 
 test_checkProtocolMagicId_corrupt :: Assertion
 test_checkProtocolMagicId_corrupt = res @?= Left e
   where
     fs = Folder $ Map.fromList
-      [ (protocolMagicIdFile, File "garbage")
-      , ("immutable",         Folder mempty)
-      , ("ledger",            Folder mempty)
-      , ("volatile",          Folder mempty)
+      [ (dbMarkerFile, File "garbage")
+      , ("immutable",  Folder mempty)
+      , ("ledger",     Folder mempty)
+      , ("volatile",   Folder mempty)
       ]
     (res, _) = runCheck fs
-    e = CorruptProtocolMagicId fullPath
+    e = CorruptDbMarker fullPath
 
 test_checkProtocolMagicId_empty :: Assertion
 test_checkProtocolMagicId_empty = res @?= Left e
   where
     fs = Folder $ Map.fromList
-      [ (protocolMagicIdFile, File "")
-      , ("immutable",         Folder mempty)
-      , ("ledger",            Folder mempty)
-      , ("volatile",          Folder mempty)
+      [ (dbMarkerFile, File "")
+      , ("immutable",  Folder mempty)
+      , ("ledger",     Folder mempty)
+      , ("volatile",   Folder mempty)
       ]
     (res, _) = runCheck fs
-    e = CorruptProtocolMagicId fullPath
+    e = CorruptDbMarker fullPath
