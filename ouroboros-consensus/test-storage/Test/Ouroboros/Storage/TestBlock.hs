90cdfa7983fa21734b7edc1a6f8e7586856fc9cf
Edsko de Vries <edsko@well-typed.com>
2019-11-26 08:06:57 +0100

    Return of the chain tick
    This undoes the changes from #1226. See #1291 and (new) docstring for
    `applyChainTick` for motivation.
    
    Note that I've changed the contract of `applyBlock` slightly: it is
    respoinsible for processing the _entire_ block, including any chain
    ticking; the chain tick function is needed only if we want do only
    _part_ of the block processing.
    
    It also extends the Byron chain tick function; see detailed discussion
    in the comments.

--- ouroboros-consensus/test-storage/Test/Ouroboros/Storage/TestBlock.hs
+++ ouroboros-consensus/test-storage/Test/Ouroboros/Storage/TestBlock.hs
@@ -318,6 +318,8 @@ instance UpdateLedger TestBlock where
 
   ledgerConfigView _ = LedgerConfig
 
+  applyChainTick _ _ = return
+
   applyLedgerBlock _ tb@TestBlock{..} TestLedger{..}
     | blockPrevHash tb /= lastAppliedHash
     = throwError $ InvalidHash lastAppliedHash (blockPrevHash tb)
