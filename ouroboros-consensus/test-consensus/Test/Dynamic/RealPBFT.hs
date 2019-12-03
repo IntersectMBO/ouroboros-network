ba3a0ee0e844e6647010527a6ef4eba29f337722
Thomas Winant <thomas@well-typed.com>
2019-12-03 13:17:39 +0100

    Optionally generate an EBB for each epoch in the RealPBFT tests

--- ouroboros-consensus/test-consensus/Test/Dynamic/RealPBFT.hs
+++ ouroboros-consensus/test-consensus/Test/Dynamic/RealPBFT.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE GADTs               #-}
+{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RankNTypes          #-}
@@ -9,10 +10,12 @@ module Test.Dynamic.RealPBFT (
     tests
   ) where
 
+import           Control.Monad (void, when)
 import           Data.Foldable (find)
+import           Data.Functor ((<&>))
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import           Data.Maybe (fromJust)
+import           Data.Maybe (fromJust, isJust)
 import           Data.Time (Day (..), UTCTime (..))
 
 import           Numeric.Search.Range (searchFromTo)
@@ -20,26 +23,40 @@ import           Test.QuickCheck
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
+import           Ouroboros.Network.Block (ChainHash (..), blockNo)
 import           Ouroboros.Network.MockChain.Chain (Chain)
 import qualified Ouroboros.Network.MockChain.Chain as Chain
+import           Ouroboros.Network.Point (WithOrigin (..))
 
+import           Ouroboros.Consensus.Block (headerHash)
 import           Ouroboros.Consensus.BlockchainTime
 import           Ouroboros.Consensus.Ledger.Byron (ByronBlock)
+import           Ouroboros.Consensus.Ledger.Byron.Forge (forgeEBB)
 import           Ouroboros.Consensus.Node.ProtocolInfo
 import           Ouroboros.Consensus.Node.ProtocolInfo.Byron (plcCoreNodeId)
+import           Ouroboros.Consensus.Node.Run (nodeIsEBB)
 import           Ouroboros.Consensus.NodeId
+import           Ouroboros.Consensus.NodeKernel (NodeKernel (..))
 import           Ouroboros.Consensus.Protocol
+import           Ouroboros.Consensus.Util.IOLike (IOLike)
 import           Ouroboros.Consensus.Util.Random
+import           Ouroboros.Consensus.Util.ResourceRegistry
+
+import qualified Ouroboros.Storage.ChainDB as ChainDB
+import           Ouroboros.Storage.Common (EpochNo (..), EpochSize (..))
+import           Ouroboros.Storage.EpochInfo
 
 import qualified Cardano.Chain.Common as Common
 import qualified Cardano.Chain.Delegation as Delegation
 import qualified Cardano.Chain.Genesis as Genesis
+import qualified Cardano.Chain.ProtocolConstants as Constants
+import qualified Cardano.Chain.Slotting as Slotting
 import qualified Cardano.Chain.Update as Update
 import qualified Cardano.Crypto as Crypto
 import qualified Test.Cardano.Chain.Genesis.Dummy as Dummy
 
 import           Test.Dynamic.General
-import           Test.Dynamic.Network (NodeOutput (..))
+import           Test.Dynamic.Network (NodeHook (..), NodeOutput (..))
 import qualified Test.Dynamic.Ref.RealPBFT as Ref
 import           Test.Dynamic.Util
 import           Test.Dynamic.Util.NodeJoinPlan
@@ -74,9 +91,10 @@ tests = testGroup "Dynamic chain generation"
               ]
             , nodeTopology = meshNodeTopology ncn
             }
-    , testProperty "simple Real PBFT convergence" $ \seed ->
+            OnlyGenesisEBB
+    , testProperty "simple Real PBFT convergence" $ \generateEBBs seed ->
         forAllShrink genRealPBFTTestConfig shrinkRealPBFTTestConfig $ \testConfig ->
-        prop_simple_real_pbft_convergence testConfig seed
+        prop_simple_real_pbft_convergence testConfig generateEBBs seed
     ]
 
 prop_setup_coreNodeId ::
@@ -95,20 +113,44 @@ prop_setup_coreNodeId numCoreNodes coreNodeId =
     genesisSecrets :: Genesis.GeneratedSecrets
     (genesisConfig, genesisSecrets) = generateGenesisConfig numCoreNodes
 
-prop_simple_real_pbft_convergence :: TestConfig
-                                  -> Seed
-                                  -> Property
+data GenerateEBBs
+  = OnlyGenesisEBB
+    -- ^ Only the node leading the first slot will produce an EBB in that slot
+    -- (instead of a regular block). No other EBBs will be produced.
+  | EachEpochAnEBB
+    -- ^ At the start of each epoch, each node (independently) produces an EBB
+    -- that fits onto its current tip.
+  deriving (Eq, Show)
+
+instance Arbitrary GenerateEBBs where
+  arbitrary = elements [OnlyGenesisEBB, EachEpochAnEBB]
+  shrink OnlyGenesisEBB = []
+  shrink EachEpochAnEBB = [OnlyGenesisEBB]
+
 prop_simple_real_pbft_convergence
-  testConfig@TestConfig{numCoreNodes, numSlots} seed =
+  :: TestConfig
+  -> GenerateEBBs
+  -> Seed
+  -> Property
+prop_simple_real_pbft_convergence
+  testConfig@TestConfig{numCoreNodes, numSlots} generateEBBs seed =
+    tabulate "generate EBBS" [show generateEBBs] $
     prop_general k
         testConfig
         (Just $ roundRobinLeaderSchedule numCoreNodes numSlots)
         testOutput
     .&&. not (all Chain.null finalChains)
+    .&&. checkEBBs
   where
     k =
       (SecurityParam . Common.unBlockCount) $
       (Genesis.gdK . Genesis.configGenesisData) $
+      genesisConfig
+
+    epochSize :: EpochSize
+    epochSize =
+      (EpochSize . Slotting.unEpochSlots) $
+      Constants.kEpochSlots . Genesis.gdK . Genesis.configGenesisData $
       genesisConfig
 
     testOutput =
@@ -116,15 +158,67 @@ prop_simple_real_pbft_convergence
             (\nid -> protocolInfo
                        (mkProtocolRealPBFT numCoreNodes nid
                                            genesisConfig genesisSecrets))
-            testConfig seed mempty
+            testConfig seed
+            (case generateEBBs of
+               OnlyGenesisEBB -> mempty
+               EachEpochAnEBB -> NodeHook ebbProducer)
 
     finalChains :: [Chain ByronBlock]
     finalChains = Map.elems $ nodeOutputFinalChain <$> testOutputNodes testOutput
+
+    lastEpoch :: Chain ByronBlock -> EpochNo
+    lastEpoch ch = case Chain.headSlot ch of
+      Origin  -> 0
+      At slot -> EpochNo (unSlotNo slot `div` unEpochSize epochSize)
+
+    checkEBBs :: Property
+    checkEBBs = case generateEBBs of
+      OnlyGenesisEBB -> property True
+      EachEpochAnEBB -> conjoin
+        [ counterexample
+            ("Expected EBBs in chain of node " <> show n <> ": " <>
+             show expectedEBBs <>
+             "\nActual EBBs in chain of node " <> show n <> ": " <>
+             show actualEBBs)
+            (expectedEBBs === actualEBBs)
+        | (n, ch) <- Map.toList $
+            nodeOutputFinalChain <$> testOutputNodes testOutput
+          -- Count the EBBs in the chain
+        , let actualEBBs = length $ filter (isJust . nodeIsEBB) $
+                Chain.toNewestFirst ch
+              -- An EBB for each epoch (we start counting at 0, so + 1)
+              expectedEBBs = fromIntegral (unEpochNo (lastEpoch ch)) + 1
+        ]
 
     genesisConfig  :: Genesis.Config
     genesisSecrets :: Genesis.GeneratedSecrets
     (genesisConfig, genesisSecrets) = generateGenesisConfig numCoreNodes
 
+ebbProducer :: forall m peer. IOLike m
+            => ResourceRegistry m
+            -> BlockchainTime m
+            -> NodeKernel m peer ByronBlock
+            -> EpochInfo m
+            -> m ()
+ebbProducer registry btime nodeKernel epochInfo =
+    void $ forkLinkedThread registry $ go 0
+  where
+    cfg     = getNodeConfig nodeKernel
+    chainDB = getChainDB nodeKernel
+
+    go :: EpochNo -> m ()
+    go epoch = do
+      -- The first slot in @epoch@
+      ebbSlotNo <- epochInfoFirst epochInfo epoch
+      over      <- blockUntilSlot btime ebbSlotNo
+      when (over && ebbSlotNo > 0) $
+        error $ "slot was already passed: " <> show ebbSlotNo
+      (ebbBlockNo, prevHash) <- ChainDB.getTipHeader chainDB <&> \case
+        Nothing  -> (0,           GenesisHash)
+        Just hdr -> (blockNo hdr, BlockHash (headerHash hdr))
+      let ebb = forgeEBB cfg ebbSlotNo ebbBlockNo prevHash
+      ChainDB.addBlock chainDB ebb
+      go (epoch + 1)
 
 mkProtocolRealPBFT :: NumCoreNodes
                    -> CoreNodeId
