17e23db5b956da860b6dced3ba8d82a6eb528831
Marcin Szamotulski <profunctor@pm.me>
2020-01-02 14:54:37 +0100

    wsaWaitForCompletion: like waitForCompletion but for sockets

--- Win32-network/src/System/Win32/Async/Internal.hs
+++ Win32-network/src/System/Win32/Async/Internal.hs
@@ -35,3 +35,25 @@ waitForCompletion asyncIO = do
           || errCode == eRROR_PIPE_CONNECTED
           then return 0
           else Win32.failWith ("waitForCompletion: " ++ show errCode) errCode
+
+
+wsaWaitForCompletion :: (StablePtr (MVar (Either WSAErrCode Int)) -> IO ())
+                     -> IO Int
+wsaWaitForCompletion asyncIO = do
+    wait_var <- newEmptyMVar
+    -- the pointer is freed in 'c_GetQueuedCompletionStatus'
+    wait_var_ptr <- newStablePtr wait_var
+    asyncIO wait_var_ptr
+    errCode <- wsaGetLastError
+    if errCode == wSA_IO_PENDING
+      then do
+        -- The operation is initiated asynchronously.
+        -- We wait for the completion of the operation.
+        res' <- takeMVar wait_var
+        case res' of
+            Right num_bytes
+                   -> return num_bytes
+            Left e -> wsaFailWith ("wsaWaitForCompletion: " ++ show e) e
+      else
+        -- An error occurred.
+        wsaFailWith ("wsaWaitForCompletion: " ++ show errCode) errCode
