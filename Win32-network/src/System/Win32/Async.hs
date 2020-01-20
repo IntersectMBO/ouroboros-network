e7d201aa75df4fa6f27405fde3b2f3448d92cd52
Marcin Szamotulski <profunctor@pm.me>
2020-01-20 10:00:56 +0100

    Win32-network: export ByteString operations from System.Win32.Async

--- Win32-network/src/System/Win32/Async.hs
+++ Win32-network/src/System/Win32/Async.hs
@@ -3,9 +3,11 @@ module System.Win32.Async
   , module System.Win32.Async.ErrCode
   , module System.Win32.Async.IOManager
   , module System.Win32.Async.Socket
+  , module System.Win32.Async.Socket.ByteString
   ) where
 
 import System.Win32.Async.IOManager
 import System.Win32.Async.File
 import System.Win32.Async.ErrCode
 import System.Win32.Async.Socket
+import System.Win32.Async.Socket.ByteString
