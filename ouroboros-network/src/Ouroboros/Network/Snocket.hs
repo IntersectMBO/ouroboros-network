221ab6073f2ec2ba0def3c534fbdac0cf22f6b05
Marcin Szamotulski <profunctor@pm.me>
2020-01-21 18:02:58 +0100

    socketSnocket: Snocket interface for Berkeley sockets

--- ouroboros-network/src/Ouroboros/Network/Snocket.hs
+++ ouroboros-network/src/Ouroboros/Network/Snocket.hs
@@ -7,9 +7,13 @@ module Ouroboros.Network.Snocket
   ( Accept (..)
   , AddressFamily (..)
   , Snocket (..)
+  , SocketSnocket
+  , socketSnocket
+  , rawSocketSnocket
   ) where
 
 import           Control.Exception
+import           Control.Monad (when)
 import           Control.Tracer (Tracer)
 import           Network.Socket (Socket, SockAddr)
 import qualified Network.Socket as Socket
@@ -19,6 +23,7 @@ import qualified System.Win32.Async      as Win32.Async
 
 import           Network.Mux.Types (MuxBearer)
 import           Network.Mux.Trace (MuxTrace)
+import qualified Network.Mux.Bearer.Socket as Mx
 
 import           Ouroboros.Network.IOManager
 
@@ -129,3 +134,116 @@ data Snocket m fd addr = Snocket {
 
   , toBearer      :: Tracer m MuxTrace -> fd -> (MuxBearer m)
   }
+
+
+socketAddrFamily
+    :: Socket.SockAddr
+    -> AddressFamily Socket.SockAddr
+socketAddrFamily (Socket.SockAddrInet  _ _    ) = SocketFamily Socket.AF_INET
+socketAddrFamily (Socket.SockAddrInet6 _ _ _ _) = SocketFamily Socket.AF_INET6
+socketAddrFamily (Socket.SockAddrUnix _       ) = SocketFamily Socket.AF_UNIX
+
+type SocketSnocket = Snocket IO Socket SockAddr
+
+-- | Create a 'Snocket' for the given 'Socket.Family'. In the 'bind' method set
+-- 'Socket.ReuseAddr` and 'Socket.ReusePort'.
+--
+socketSnocket
+  :: AssociateWithIOCP
+  -- ^ associate the socket with I/O CP.  We use it when we create a new socket
+  -- and when we accept a connection.
+  --
+  -- Though it could be used in `open`, but that is going to be used in
+  -- a bracket so it's better to keep it simple.
+  --
+  -> SocketSnocket
+socketSnocket iocp = Snocket {
+      getLocalAddr   = Socket.getSocketName
+    , getRemoteAddr  = Socket.getPeerName
+    , addrFamily     = socketAddrFamily
+    , open           = openSocket
+    , openToConnect  = \addr -> openSocket (socketAddrFamily addr)
+    , connect        = \s a -> do
+#if !defined(mingw32_HOST_OS)
+        Socket.connect s a
+#else
+        Win32.Async.connect s a
+#endif
+    , bind = \sd addr -> do
+        let SocketFamily fml = socketAddrFamily addr
+        when (fml == Socket.AF_INET ||
+              fml == Socket.AF_INET6) $ do
+          Socket.setSocketOption sd Socket.ReuseAddr 1
+#if !defined(mingw32_HOST_OS)
+          -- not supported on Windows 10
+          Socket.setSocketOption sd Socket.ReusePort 1
+#endif
+        when (fml == Socket.AF_INET6)
+          -- An AF_INET6 socket can be used to talk to both IPv4 and IPv6 end points, and
+          -- it is enabled by default on some systems. Disabled here since we run a separate
+          -- IPv4 server instance if configured to use IPv4.
+          $ Socket.setSocketOption sd Socket.IPv6Only 1
+
+        Socket.bind sd addr
+    , listen   = \s -> Socket.listen s 1
+    , accept   = berkeleyAccept iocp
+    , close    = Socket.close
+    , toBearer = Mx.socketAsMuxBearer
+    }
+  where
+    openSocket :: AddressFamily SockAddr -> IO Socket
+    openSocket (SocketFamily family_) = do
+      sd <- Socket.socket family_ Socket.Stream Socket.defaultProtocol
+      associateWithIOCP iocp (Right sd)
+        -- open is designed to be used in `bracket`, and thus it's called with
+        -- async exceptions masked.  The 'associteWithIOCP' is a blocking
+        -- operation and thus it may throw.
+        `catch` \(e :: IOException) -> do
+          Socket.close sd
+          throwIO e
+        `catch` \(SomeAsyncException _) -> do
+          Socket.close sd
+          throwIO e
+      return sd
+
+
+
+-- | Create a snocket for the given 'Socket.Family'.  This snocket does not set
+-- any options on the underlaying socket.
+--
+rawSocketSnocket
+  :: AssociateWithIOCP
+  -> SocketSnocket
+rawSocketSnocket iocp = Snocket {
+      getLocalAddr  = Socket.getSocketName
+    , getRemoteAddr = Socket.getPeerName
+    , addrFamily    = socketAddrFamily
+    , connect       = \s a -> do
+#if !defined(mingw32_HOST_OS)
+        Socket.connect s a
+#else
+        Win32.Async.connect s a
+#endif
+    , bind          = \fd addr -> Socket.bind fd addr
+    , listen        = flip Socket.listen 1
+    , accept        = berkeleyAccept iocp
+    , open          = openSocket
+    , openToConnect = \addr -> openSocket (socketAddrFamily addr)
+    , close         = Socket.close
+    , toBearer      = Mx.socketAsMuxBearer
+    }
+  where
+    openSocket :: AddressFamily SockAddr -> IO Socket
+    openSocket (SocketFamily family_) = do
+      sd <- Socket.socket family_ Socket.Stream Socket.defaultProtocol
+      associateWithIOCP iocp (Right sd)
+        -- open is designed to be used in `bracket`, and thus it's called with
+        -- async exceptions masked.  The 'associteWithIOCP' is a blocking
+        -- operation and thus it may throw.
+        `catch` \(e :: IOException) -> do
+          Socket.close sd
+          throwIO e
+        `catch` \(SomeAsyncException _) -> do
+          Socket.close sd
+          throwIO e
+      return sd
