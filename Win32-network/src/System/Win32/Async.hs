ff0a40768c6fc0af28ef43a659b338bcd2f2c746
Marcin Szamotulski <profunctor@pm.me>
2020-01-31 12:44:52 +0100

    System.Win32.Async - do not export Socket.ByteString
    Let the user of the library choose wether to use
    'System.Win32.Async.Socket.ByteString' or
    'System.Win32.Async.Socket.ByteString.Lazy'.

--- Win32-network/src/System/Win32/Async.hs
+++ Win32-network/src/System/Win32/Async.hs
@@ -8,11 +8,9 @@ module System.Win32.Async
   , module System.Win32.Async.ErrCode
   , module System.Win32.Async.IOManager
   , module System.Win32.Async.Socket
-  , module System.Win32.Async.Socket.ByteString
   ) where
 
 import System.Win32.Async.IOManager
 import System.Win32.Async.File
 import System.Win32.Async.ErrCode
 import System.Win32.Async.Socket
-import System.Win32.Async.Socket.ByteString
