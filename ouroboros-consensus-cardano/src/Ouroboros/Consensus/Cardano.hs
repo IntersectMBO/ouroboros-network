6e78b1ab40085861e6392573269de9afa9ed9442
Edsko de Vries <edsko@well-typed.com>
2020-02-19 14:51:39 +0100

    ExtConfig is once again gone.

--- ouroboros-consensus-cardano/src/Ouroboros/Consensus/Cardano.hs
+++ ouroboros-consensus-cardano/src/Ouroboros/Consensus/Cardano.hs
@@ -40,7 +40,6 @@ import           Ouroboros.Consensus.Node.Run
 import           Ouroboros.Consensus.NodeId (CoreNodeId)
 import           Ouroboros.Consensus.Protocol.Abstract as X
 import           Ouroboros.Consensus.Protocol.BFT as X
-import           Ouroboros.Consensus.Protocol.ExtConfig as X
 import           Ouroboros.Consensus.Protocol.LeaderSchedule as X
 import           Ouroboros.Consensus.Protocol.PBFT as X
 import           Ouroboros.Consensus.Util
