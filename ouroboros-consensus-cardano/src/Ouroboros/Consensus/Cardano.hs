cb80eb23c5122f6b07dc873f9cafa89baeff2e8b
Edsko de Vries <edsko@well-typed.com>
2020-02-19 14:21:44 +0100

    Add sanity check for the protocol definitions
    Turns out, the sanity check failed for praos, since we had a bug.

--- ouroboros-consensus-cardano/src/Ouroboros/Consensus/Cardano.hs
+++ ouroboros-consensus-cardano/src/Ouroboros/Consensus/Cardano.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs         #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Ouroboros.Consensus.Cardano (
     -- * Supported protocols
     ProtocolMockBFT
@@ -8,6 +10,7 @@ module Ouroboros.Consensus.Cardano (
   , ProtocolRealPBFT
     -- * Abstract over the various protocols
   , Protocol(..)
+  , verifyProtocol
     -- * Data required to run a protocol
   , protocolInfo
     -- * Evidence that we can run all the supported protocols
@@ -15,9 +18,12 @@ module Ouroboros.Consensus.Cardano (
   , module X
   ) where
 
+import           Data.Type.Equality
+
 import qualified Cardano.Chain.Genesis as Genesis
 import qualified Cardano.Chain.Update as Update
 
+import           Ouroboros.Consensus.Block
 import           Ouroboros.Consensus.BlockchainTime
 import           Ouroboros.Consensus.Byron.Ledger
 import           Ouroboros.Consensus.Byron.Node as X
@@ -41,10 +47,15 @@ import           Ouroboros.Consensus.Util
 
 {-------------------------------------------------------------------------------
   Supported protocols
+
+  We list these as explicit definitions here (rather than derived through
+  'BlockProtocol'), and then /verify/ in 'verifyProtocol' that these definitions
+  match. This provides an additional sanity check that we are not accidentally
+  breaking any assumptions made in @cardano-node@.
 -------------------------------------------------------------------------------}
 
 type ProtocolMockBFT        = Bft BftMockCrypto
-type ProtocolMockPraos      = ExtConfig PraosMockCrypto AddrDist
+type ProtocolMockPraos      = ExtConfig (Praos PraosMockCrypto) AddrDist
 type ProtocolLeaderSchedule = WithLeaderSchedule (Praos PraosCryptoUnused)
 type ProtocolMockPBFT       = ExtConfig (PBft PBftMockCrypto) (PBftLedgerView PBftMockCrypto)
 type ProtocolRealPBFT       = ExtConfig (PBft PBftByronCrypto) ByronConfig
@@ -54,21 +65,25 @@ type ProtocolRealPBFT       = ExtConfig (PBft PBftByronCrypto) ByronConfig
 -------------------------------------------------------------------------------}
 
 -- | Consensus protocol to use
-data Protocol blk where
+data Protocol blk p where
   -- | Run BFT against the mock ledger
   ProtocolMockBFT
     :: NumCoreNodes
     -> CoreNodeId
     -> SecurityParam
     -> SlotLengths
-    -> Protocol (SimpleBftBlock SimpleMockCrypto BftMockCrypto)
+    -> Protocol
+         (SimpleBftBlock SimpleMockCrypto BftMockCrypto)
+         ProtocolMockBFT
 
   -- | Run Praos against the mock ledger
   ProtocolMockPraos
     :: NumCoreNodes
     -> CoreNodeId
     -> PraosParams
-    -> Protocol (SimplePraosBlock SimpleMockCrypto PraosMockCrypto)
+    -> Protocol
+         (SimplePraosBlock SimpleMockCrypto PraosMockCrypto)
+         ProtocolMockPraos
 
   -- | Run Praos against the mock ledger but with an explicit leader schedule
   ProtocolLeaderSchedule
@@ -76,13 +91,17 @@ data Protocol blk where
     -> CoreNodeId
     -> PraosParams
     -> LeaderSchedule
-    -> Protocol (SimplePraosRuleBlock SimpleMockCrypto)
+    -> Protocol
+         (SimplePraosRuleBlock SimpleMockCrypto)
+         ProtocolLeaderSchedule
 
   -- | Run PBFT against the mock ledger
   ProtocolMockPBFT
     :: PBftParams
     -> CoreNodeId
-    -> Protocol (SimplePBftBlock SimpleMockCrypto PBftMockCrypto)
+    -> Protocol
+         (SimplePBftBlock SimpleMockCrypto PBftMockCrypto)
+         ProtocolMockPBFT
 
   -- | Run PBFT against the real ledger
   ProtocolRealPBFT
@@ -91,14 +110,23 @@ data Protocol blk where
     -> Update.ProtocolVersion
     -> Update.SoftwareVersion
     -> Maybe PBftLeaderCredentials
-    -> Protocol ByronBlock
+    -> Protocol
+         ByronBlock
+         ProtocolRealPBFT
+
+verifyProtocol :: Protocol blk p -> (p :~: BlockProtocol blk)
+verifyProtocol ProtocolMockBFT{}        = Refl
+verifyProtocol ProtocolMockPraos{}      = Refl
+verifyProtocol ProtocolLeaderSchedule{} = Refl
+verifyProtocol ProtocolMockPBFT{}       = Refl
+verifyProtocol ProtocolRealPBFT{}       = Refl
 
 {-------------------------------------------------------------------------------
   Data required to run a protocol
 -------------------------------------------------------------------------------}
 
 -- | Data required to run the selected protocol
-protocolInfo :: Protocol blk -> ProtocolInfo blk
+protocolInfo :: Protocol blk p -> ProtocolInfo blk
 protocolInfo (ProtocolMockBFT nodes nid params slotLengths) =
     protocolInfoBft nodes nid params slotLengths
 
@@ -118,7 +146,7 @@ protocolInfo (ProtocolRealPBFT gc mthr prv swv mplc) =
   Evidence that we can run all the supported protocols
 -------------------------------------------------------------------------------}
 
-runProtocol :: Protocol blk -> Dict (RunNode blk)
+runProtocol :: Protocol blk p -> Dict (RunNode blk)
 runProtocol ProtocolMockBFT{}        = Dict
 runProtocol ProtocolMockPraos{}      = Dict
 runProtocol ProtocolLeaderSchedule{} = Dict
