37a2bb7a23503cb4ac8c4fa3ec4dd6612bf7e4e8
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-02-27 13:19:14 -0600

    Make all fields strict for BlockConfig in Test.Util.TestBlock

--- ouroboros-consensus/ouroboros-consensus-test-infra/src/Test/Util/TestBlock.hs
+++ ouroboros-consensus/ouroboros-consensus-test-infra/src/Test/Util/TestBlock.hs
@@ -242,13 +242,13 @@ instance Condense (ChainHash TestBlock) where
   condense (BlockHash h) = show h
 
 data instance BlockConfig TestBlock = TestBlockConfig {
-      testBlockSlotLengths :: SlotLengths
+      testBlockSlotLengths :: !SlotLengths
 
       -- | Number of core nodes
       --
       -- We need this in order to compute the 'ValidateView', which must
       -- conjure up a validation key out of thin air
-    , testBlockNumCoreNodes :: NumCoreNodes
+    , testBlockNumCoreNodes :: !NumCoreNodes
     }
   deriving (Generic, NoUnexpectedThunks)
 
