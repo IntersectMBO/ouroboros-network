fcb5c36fd50313d008cf27723b47109280aabffa
Duncan Coutts <duncan@well-typed.com>
2020-03-02 11:07:30 +0000

    mux: introduce a protocol thread status var for tracking
    Use it to check for incorrect use of runMiniProtocol. It is updated when
    we try to start a thread, and when a thread completes.

--- network-mux/src/Network/Mux/Types.hs
+++ network-mux/src/Network/Mux/Types.hs
@@ -26,6 +26,7 @@ module Network.Mux.Types (
     , MiniProtocolDir (..)
     , protocolDirEnum
     , MiniProtocolState (..)
+    , MiniProtocolStatus (..)
     , MuxBearer (..)
     , muxBearerAsChannel
     , MuxSDU (..)
@@ -192,8 +193,12 @@ protocolDirEnum ResponderDirection     = ResponderDir
 
 data MiniProtocolState mode m = MiniProtocolState {
        miniProtocolInfo         :: MiniProtocolInfo mode,
-       miniProtocolIngressQueue :: IngressQueue m
+       miniProtocolIngressQueue :: IngressQueue m,
+       miniProtocolStatusVar    :: StrictTVar m MiniProtocolStatus
      }
+
+data MiniProtocolStatus = StatusIdle | StatusRunning
+  deriving Eq
 
 data MuxSDUHeader = MuxSDUHeader {
       mhTimestamp :: !RemoteClockModel
