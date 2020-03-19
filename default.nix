c6214fb6feaeb9c3f40180479e616e38589096d7
Nicholas Clarke <nick@topos.org.uk>
2020-03-19 09:51:07 +0100

    Do full ledger validation.
    Since this is running nightly now, we might as well do full validation of the ledger, rather than just the ImmDB

--- default.nix
+++ default.nix
@@ -22,6 +22,7 @@ let
   validate-mainnet = import ./nix/validate-mainnet.nix {
     inherit pkgs;
     byron-db-converter = haskellPackages.ouroboros-consensus-byron.components.exes.db-converter;
+    onlyImmDB = false;
   };
 
   self = {
