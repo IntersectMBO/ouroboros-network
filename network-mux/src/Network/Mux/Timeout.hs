a584b8376bee64ce5617b0c0592822009511278f
Duncan Coutts <duncan@well-typed.com>
2020-04-09 20:25:09 +0100

    Experiment with yet another timeout implementation

--- network-mux/src/Network/Mux/Timeout.hs
+++ network-mux/src/Network/Mux/Timeout.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes          #-}
 
 -- | An alternative implementation of 'System.Timeout.timeout' for platforms
 -- (i.e. Windows) where the standard implementation is too expensive.
@@ -35,58 +36,58 @@ import Control.Monad.Class.MonadTimer hiding (timeout)
 -- @timeout@ can /only do so from one thread at once/.
 --
 withTimeoutSerial
-  :: forall m b. (MonadAsync m, MonadFork m, MonadTimer m, MonadThrow m)
-  => ((forall a. DiffTime -> m a -> m a) -> m b) -> m b
+  :: forall m b. (MonadAsync m, MonadFork m, MonadTime m, MonadTimer m, MonadCatch m)
+  => ((forall a. DiffTime -> m a -> m (Maybe a)) -> m b) -> m b
 withTimeoutSerial body = do
 
-    -- Shared var for the current timeout, shared between the thread using
+    -- Shared var for the current timer, shared between the thread using
     -- timeouts and the thread monitoring the timeouts.
-    tv <- newTVarM Nothing
+    timerStateVar <- newTVarM TimerIdle
 
-    -- Which thread to kill if at timeout fires.
+    -- Which thread to kill if a timeout fires.
     tid <- myThreadId
 
-    let notifyNewTimeout t =
-          -- Post the timeout to the monitoring thread.
-          atomically $ do
-
-            -- Assertion to ensure we only use the timeout serially
-            existing <- readTVar tv
-            case existing of
-              Nothing -> return ()
-              Just t'  -> do
-                state <- readTimeout t'
-                case state of
-                  TimeoutCancelled -> return ()
-                  _ -> fail "withTimeoutSerial: timeout used concurrently"
-
-            writeTVar tv (Just t)
-        
-        -- The @timeout@ action we pass to the body.
-        timeout :: forall a. DiffTime -> m a -> m a
-        timeout d action =
+    -- The @timeout@ action we pass to the body.
+    let timeout :: forall a. DiffTime -> m a -> m (Maybe a)
+        timeout delay action =
           bracket
-            (do t <- newTimeout d
-                notifyNewTimeout t
-                return t)
-            cancelTimeout
-            (\_ -> action)
-   
+            (do cancelVar <- newTVarM False
+                now <- getMonotonicTime
+                let !deadline = addTime delay now
+                atomically $ writeTVar timerStateVar (TimerActive deadline cancelVar)
+                return cancelVar)
+            (\cancelVar -> atomically $ writeTVar cancelVar True)
+            (\_ -> fmap Just action)
+          `catch` \TimeoutException -> return Nothing
+
     -- Ensure the monitoring thread gets terminated when the body completes
-    withAsync (monitoringThread tid tv) $ \_ ->
+    withAsync (monitoringThread tid timerStateVar) $ \_ ->
       body timeout
 
   where
-    monitoringThread :: ThreadId m -> TVar m (Maybe (Timeout m)) -> m ()
-    monitoringThread tid tv =
+    monitoringThread :: ThreadId m -> TVar m (TimerState m) -> m ()
+    monitoringThread tid timerStateVar =
       forever $ do
-        fired <- atomically $ do
-                  mt <- readTVar tv
-                  case mt of
-                    Nothing -> retry
-                    Just t  -> awaitTimeout t
-        when fired $
+        -- Grab the next timeout snapshot
+        -- TODO: this is equivalent to an MVar with overwriting
+        (deadline, cancelVar) <-
+          atomically $ do
+            timeoutState <- readTVar timerStateVar
+            case timeoutState of
+              TimerIdle                      -> retry
+              TimerActive deadline cancelVar -> do
+                writeTVar timerStateVar TimerIdle
+                return (deadline, cancelVar)
+
+        now <- getMonotonicTime
+        let delay = diffTime deadline now
+        threadDelay delay
+        cancelled <- atomically $ readTVar cancelVar
+        unless cancelled $
           throwTo tid TimeoutException
+
+data TimerState m = TimerIdle
+                  | TimerActive !Time !(TVar m Bool)
 
 data TimeoutException = TimeoutException deriving Show
 
