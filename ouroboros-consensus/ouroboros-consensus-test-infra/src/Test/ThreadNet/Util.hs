f55e081896a0e5576e006be0b7d2aad21838afb4
Nicolas Frisby <nick.frisby@iohk.io>
2020-05-11 17:00:36 -0700

    test-infra: make the common prefix error message quieter

--- ouroboros-consensus/ouroboros-consensus-test-infra/src/Test/ThreadNet/Util.hs
+++ ouroboros-consensus/ouroboros-consensus-test-infra/src/Test/ThreadNet/Util.hs
@@ -63,23 +63,20 @@ shortestLength = fromIntegral . minimum . map Chain.length . Map.elems
 prop_all_common_prefix :: (HasHeader b, Condense b, Eq b)
                        => Word64 -> [Chain b] -> Property
 prop_all_common_prefix _ []     = property True
-prop_all_common_prefix l (c:cs) = conjoin [prop_common_prefix l c d | d <- cs]
+prop_all_common_prefix l (c:cs) = conjoin [prop_common_prefix l n c d | (d, n) <- zip cs [1..] ]
 
 prop_common_prefix :: forall b. (HasHeader b, Condense b, Eq b)
-                   => Word64 -> Chain b -> Chain b -> Property
-prop_common_prefix l x y = go x y .&&. go y x
+                   => Word64 -> Word64 -> Chain b -> Chain b -> Property
+prop_common_prefix l n x y = go 0 n x y .&&. go n 0 y x
   where
-    go c d =
-        let (l', c') = findPrefix c d
+    go nc nd c d =
+        let (l', _) = findPrefix c d
             e        = "after dropping "
                  <> show l'
-                 <> " blocks from "
-                 <> showChain c
-                 <> ",\n\nthe resulting "
-                 <> showChain c'
-                 <> "\n\nis a prefix of "
-                 <> showChain d
-                 <> ",\n\nbut only "
+                 <> " block(s) from the final chain of node " <> show nc
+                 <> ",\nthe resulting chain is a prefix of "
+                 <> "the final chain of node " <> show nd
+                 <> ",\nbut only "
                  <> show l
                  <> " block(s) should have been necessary"
         in  counterexample e $ l' <= l
@@ -89,8 +86,8 @@ prop_common_prefix l x y = go x y .&&. go y x
         | otherwise         = let (l', c'') = findPrefix (Chain.dropLastBlocks 1 c') d
                               in  (l' + 1, c'')
 
-    showChain :: Chain b -> String
-    showChain c = condense c
+    _showChain :: Chain b -> String
+    _showChain c = condense c
                   <> "\n(length "
                   <> show (Chain.length c)
                   <> case Chain.lastSlot c of
