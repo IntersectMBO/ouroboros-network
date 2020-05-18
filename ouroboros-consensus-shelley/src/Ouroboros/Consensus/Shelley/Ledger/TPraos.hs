ff9b016bcbc7eccf6942e97e1607cbcc5aa2352b
Nicholas Clarke <nick@topos.org.uk>
2020-05-18 12:08:44 +0200

    Chain selection using VRF values
    This branch implements a variant of chain selection which considers the
    verifiably random leader value when choosing between forks of equal
    length. The chain whose tip has the lowest leader value is preferred.

--- ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Ledger/TPraos.hs
+++ ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Ledger/TPraos.hs
@@ -5,15 +5,17 @@
 -- TODO where to put this?
 module Ouroboros.Consensus.Shelley.Ledger.TPraos () where
 
+import           Cardano.Crypto.VRF.Class (certifiedNatural)
+
 import           Ouroboros.Consensus.Block
 import           Ouroboros.Consensus.Protocol.Signed
-
-import qualified Shelley.Spec.Ledger.BlockChain as SL
-import qualified Shelley.Spec.Ledger.OCert as SL
-
 import           Ouroboros.Consensus.Shelley.Ledger.Block
 import           Ouroboros.Consensus.Shelley.Ledger.Config ()
 import           Ouroboros.Consensus.Shelley.Protocol
+
+import qualified Shelley.Spec.Ledger.BlockChain as SL
+import qualified Shelley.Spec.Ledger.Keys as SL
+import qualified Shelley.Spec.Ledger.OCert as SL
 
 {-------------------------------------------------------------------------------
   Support for Transitional Praos consensus algorithm
@@ -24,11 +26,17 @@ type instance BlockProtocol (ShelleyBlock c) = TPraos c
 instance TPraosCrypto c => BlockSupportsProtocol (ShelleyBlock c) where
   validateView _cfg (ShelleyHeader hdr _) = hdr
 
-  selectView _ (ShelleyHeader hdr _) = ChainSelectView
-    { csvChainLength = SL.bheaderBlockNo . SL.bhbody $ hdr
-    , csvIssuer      = SL.bheaderVk . SL.bhbody $ hdr
-    , csvIssueNo     = SL.ocertN . SL.bheaderOCert . SL.bhbody $ hdr
-    }
+  selectView _ (ShelleyHeader hdr _) =
+    ChainSelectView
+      { csvChainLength = SL.bheaderBlockNo . SL.bhbody $ hdr
+      , csvLeaderVRF =
+          SL.fromNatural . certifiedNatural
+            . SL.bheaderL
+            . SL.bhbody
+            $ hdr
+      , csvIssuer = SL.bheaderVk . SL.bhbody $ hdr
+      , csvIssueNo = SL.ocertN . SL.bheaderOCert . SL.bhbody $ hdr
+      }
 
 -- TODO correct place for these two?
 type instance Signed (Header (ShelleyBlock c)) = SL.BHBody c
