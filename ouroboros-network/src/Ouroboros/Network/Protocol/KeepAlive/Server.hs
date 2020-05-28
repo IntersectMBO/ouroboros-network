8789fa293bc9661a470ead1ce20d995b002acb1e
Marcin Szamotulski <profunctor@pm.me>
2020-05-28 15:25:43 +0200

    KeepAlive protocol

---
+++ ouroboros-network/src/Ouroboros/Network/Protocol/KeepAlive/Server.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Ouroboros.Network.Protocol.KeepAlive.Server (
+    KeepAliveServer (..),
+    keepAliveServerPeer
+  ) where
+
+import           Network.TypedProtocol.Core
+import           Ouroboros.Network.Protocol.KeepAlive.Type
+
+
+data KeepAliveServer m a = KeepAliveServer {
+    recvMsgKeepAlive :: m (KeepAliveServer m a),
+
+    recvMsgDone      :: m a
+  }
+
+
+keepAliveServerPeer
+    :: Functor m
+    => KeepAliveServer m a
+    -> Peer KeepAlive AsServer StClient m a
+keepAliveServerPeer KeepAliveServer { recvMsgKeepAlive, recvMsgDone } =
+    Await (ClientAgency TokClient) $ \msg ->
+      case msg of
+        MsgDone -> Effect $ Done TokDone <$> recvMsgDone
+
+        MsgKeepAlive ->
+          Effect $
+            fmap (\server ->
+                    Yield (ServerAgency TokServer)
+                          MsgKeepAliveResponse
+                          (keepAliveServerPeer server))
+                 recvMsgKeepAlive
---
+++ ouroboros-network/src/Ouroboros/Network/Protocol/KeepAlive/Type.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE EmptyCase         #-}
+{-# LANGUAGE PolyKinds         #-}
+
+-- | The type of the keep alive protocol.
+--
+-- The keep alive protocol is used for
+--
+-- * sending keep alive messages
+-- * making round trip measurements
+--
+-- Each side will run its own version of the keep alive protocol.  It should be
+-- configured so that any intermediate state (such as in customer premise
+-- equipment or in a carrier grade NAT) is kept alive. This has to be a per-node
+-- configuration element as this is about the properties of that nodes network
+-- connectivity.
+--
+-- For making round trip measurements its in the interest of the other side to
+-- reply promptly.
+--
+module Ouroboros.Network.Protocol.KeepAlive.Type where
+
+import Network.TypedProtocol.Core
+
+
+-- | A kind to identify our protocol, and the types of the states in the state
+-- transition diagram of the protocol.
+--
+data KeepAlive where
+
+    -- | The client can send a request and the server is waiting for a request.
+    --
+    StClient :: KeepAlive
+
+    -- | The server is responsible for sending response back.
+    --
+    StServer :: KeepAlive
+
+    -- | Both the client and server are in the terminal state. They're done.
+    --
+    StDone   :: KeepAlive
+
+
+instance Protocol KeepAlive where
+    -- | The messages in the keep alive protocol.
+    --
+    -- In this protocol the consumer initiates and the producer replies.
+    --
+    data Message KeepAlive from to where
+
+      -- | Send a keep alive message.
+      --
+      MsgKeepAlive
+        :: Message KeepAlive StClient StServer
+
+      -- | Keep alive response.
+      --
+      MsgKeepAliveResponse
+        :: Message KeepAlive StServer StClient
+
+      -- | The client side terminating message of the protocol.
+      --
+      MsgDone
+        :: Message KeepAlive StClient StDone
+
+    data ClientHasAgency st where
+      TokClient :: ClientHasAgency StClient
+
+    data ServerHasAgency st where
+      TokServer :: ServerHasAgency StServer
+
+    data NobodyHasAgency st where
+      TokDone   :: NobodyHasAgency StDone
+
+    exclusionLemma_ClientAndServerHaveAgency TokClient tok = case tok of {}
+    exclusionLemma_NobodyAndClientHaveAgency TokDone   tok = case tok of {}
+    exclusionLemma_NobodyAndServerHaveAgency TokDone   tok = case tok of {}
+
+
+instance Show (Message KeepAlive from to) where
+    show MsgKeepAlive         = "MsgKeepAlive"
+    show MsgKeepAliveResponse = "MsgKeepAliveResponse"
+    show MsgDone              = "MsgDone"
+
+instance Show (ClientHasAgency (st :: KeepAlive)) where
+    show TokClient = "TokClient"
+
+instance Show (ServerHasAgency (st :: KeepAlive)) where
+    show TokServer = "TokServer"
