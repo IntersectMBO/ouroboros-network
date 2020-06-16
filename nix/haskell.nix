0efcdfad30a2fa8d92e5f312d5d12ced8e98ec40
Nicholas Clarke <nick@topos.org.uk>
2020-06-16 14:09:22 +0200

    CDDL fix

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -77,6 +77,7 @@ let
       } else {
         packages.ouroboros-network.flags.cddl = true;
         packages.ouroboros-network.components.tests.test-cddl.build-tools = [pkgs.cddl pkgs.cbor-diag];
+        packages.ouroboros-network.components.tests.test-cddl.preCheck = "export HOME=`pwd`";
       })
     ];
     configureArgs = lib.optionalString stdenv.hostPlatform.isWindows "--disable-tests";
