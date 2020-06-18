038f6cffbf315e95296e99c15410436cd39b7cc6
Marcin Szamotulski <profunctor@pm.me>
2020-06-18 17:33:01 +0200

    connection-manager: simulation test

--- ouroboros-network-framework/test/Main.hs
+++ ouroboros-network-framework/test/Main.hs
@@ -2,8 +2,9 @@ module Main (main) where
 
 import           Test.Tasty
 
-import qualified Test.Ouroboros.Network.Server2 as Server2
+import qualified Test.Ouroboros.Network.ConnectionManager as ConnectionManager
 import qualified Test.Ouroboros.Network.Driver as Driver
+import qualified Test.Ouroboros.Network.Server2 as Server2
 import qualified Test.Ouroboros.Network.Socket as Socket
 import qualified Test.Ouroboros.Network.Subscription as Subscription
 import qualified Test.Ouroboros.Network.RateLimiting as RateLimiting
@@ -14,7 +15,8 @@ main = defaultMain tests
 tests :: TestTree
 tests =
   testGroup "ouroboros-network-framework"
-  [ Driver.tests
+  [ ConnectionManager.tests
+  , Driver.tests
   , Server2.tests
   , Socket.tests
   , Subscription.tests
