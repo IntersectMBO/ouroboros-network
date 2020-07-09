ba41aa1fb8cae7245ade59ba5550fb55f7a040a1
Thomas Winant <thomas@well-typed.com>
2020-07-09 16:46:59 +0200

    Fix problems with paths to golden folders
    The Nix CI was not finding them, creating the golden results each time.
    Moreover, `cabal test` was making the directories relative to each subpackage
    whereas `cabal run` was making the directories relative to the root.
    
    Copy a solution found by our friends from cardano-wallet.

--- ouroboros-consensus-shelley-test/test/Test/Consensus/Shelley/Golden.hs
+++ ouroboros-consensus-shelley-test/test/Test/Consensus/Shelley/Golden.hs
@@ -1,21 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Test.Consensus.Shelley.Golden (tests) where
-
-import           System.FilePath ((</>))
 
 import           Ouroboros.Consensus.Shelley.Ledger.NetworkProtocolVersion
 import           Ouroboros.Consensus.Shelley.Node ()
 
 import           Test.Tasty
 
+import           Test.Util.Paths
 import           Test.Util.Serialisation.Golden
 
 import           Test.Consensus.Shelley.Examples
 
 tests :: TestTree
-tests = goldenTest_all codecConfig goldenDir examples
-  where
-    goldenDir = "ouroboros-consensus-shelley-test" </> "test" </> "golden"
+tests = goldenTest_all codecConfig $(getGoldenDir) examples
 
 instance ToGoldenDirectory ShelleyNodeToNodeVersion
   -- Use defaults
