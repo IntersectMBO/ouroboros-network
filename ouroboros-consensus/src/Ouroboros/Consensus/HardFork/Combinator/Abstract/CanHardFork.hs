46419c4ba3588c81a311a739b15afb3f464516bc
Edsko de Vries <edsko@well-typed.com>
2020-07-17 10:25:20 +0200

    Inject transactions from older eras
    Closes #2371.

--- ouroboros-consensus/src/Ouroboros/Consensus/HardFork/Combinator/Abstract/CanHardFork.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/HardFork/Combinator/Abstract/CanHardFork.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds               #-}
 {-# LANGUAGE FlexibleContexts        #-}
 {-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE TypeOperators           #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 
 module Ouroboros.Consensus.HardFork.Combinator.Abstract.CanHardFork (
@@ -10,11 +11,16 @@ module Ouroboros.Consensus.HardFork.Combinator.Abstract.CanHardFork (
 import           Data.SOP.Strict
 import           Data.Typeable
 
+import           Ouroboros.Consensus.TypeFamilyWrappers
 import           Ouroboros.Consensus.Util.SOP
 
 import           Ouroboros.Consensus.HardFork.Combinator.Abstract.SingleEraBlock
+import           Ouroboros.Consensus.HardFork.Combinator.InjectTxs
 import           Ouroboros.Consensus.HardFork.Combinator.Protocol.ChainSel
 import           Ouroboros.Consensus.HardFork.Combinator.Translation
+import           Ouroboros.Consensus.HardFork.Combinator.Util.InPairs (InPairs,
+                     RequiringBoth)
+import qualified Ouroboros.Consensus.HardFork.Combinator.Util.InPairs as InPairs
 import           Ouroboros.Consensus.HardFork.Combinator.Util.Tails (Tails)
 import qualified Ouroboros.Consensus.HardFork.Combinator.Util.Tails as Tails
 
@@ -25,7 +31,9 @@ import qualified Ouroboros.Consensus.HardFork.Combinator.Util.Tails as Tails
 class (All SingleEraBlock xs, Typeable xs, IsNonEmpty xs) => CanHardFork xs where
   hardForkEraTranslation :: EraTranslation xs
   hardForkChainSel       :: Tails AcrossEraSelection xs
+  hardForkInjectTxs      :: InPairs (RequiringBoth WrapLedgerConfig InjectTx) xs
 
 instance SingleEraBlock blk => CanHardFork '[blk] where
   hardForkEraTranslation = trivialEraTranslation
   hardForkChainSel       = Tails.mk1
+  hardForkInjectTxs      = InPairs.mk1
---
+++ ouroboros-consensus/src/Ouroboros/Consensus/HardFork/Combinator/InjectTxs.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE EmptyCase        #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE RecordWildCards  #-}
+
+module Ouroboros.Consensus.HardFork.Combinator.InjectTxs (
+    InjectTx(..)
+  , cannotInjectTx
+  , matchTx
+  ) where
+
+import           Data.Bifunctor
+import           Data.Functor.Product
+import           Data.SOP.Strict
+
+import           Ouroboros.Consensus.HardFork.Combinator.State.Types
+import           Ouroboros.Consensus.HardFork.Combinator.Util.InPairs
+                     (InPairs (..))
+import           Ouroboros.Consensus.HardFork.Combinator.Util.Match
+import           Ouroboros.Consensus.HardFork.Combinator.Util.Telescope
+                     (Telescope (..))
+import qualified Ouroboros.Consensus.HardFork.Combinator.Util.Telescope as Telescope
+import           Ouroboros.Consensus.Ledger.SupportsMempool
+
+{-------------------------------------------------------------------------------
+  Match a transaction with a ledger
+-------------------------------------------------------------------------------}
+
+data InjectTx blk blk' = InjectTx {
+      injectTxWith :: GenTx blk  -> Maybe (GenTx blk')
+    }
+
+cannotInjectTx :: InjectTx blk blk'
+cannotInjectTx = InjectTx $ const Nothing
+
+-- | Match transaction with a ledger, attempting to inject where possible
+matchTx' ::
+     InPairs InjectTx xs
+  -> NS GenTx xs
+  -> Telescope g f xs
+  -> Either (Mismatch GenTx f xs)
+            (Telescope g (Product GenTx f) xs)
+matchTx' = go
+  where
+    go :: InPairs InjectTx xs
+       -> NS GenTx xs
+       -> Telescope g f xs
+       -> Either (Mismatch GenTx f xs)
+                 (Telescope g (Product GenTx f) xs)
+    go _            (Z x) (TZ f)   = Right $ TZ (Pair x f)
+    go (PCons _ is) (S x) (TS g f) = bimap MS (TS g) $ go is x f
+    go _            (S x) (TZ f)   = Left $ MR x f
+    go PNil            _  (TS _ f) = case f of {}
+    go (PCons i is) (Z x) (TS g f) =
+        case injectTxWith i x of
+          Nothing -> Left $ ML x (Telescope.tip f)
+          Just x' -> bimap MS (TS g) $ go is (Z x') f
+
+matchTx ::
+     SListI xs
+  => InPairs InjectTx xs
+  -> NS GenTx xs
+  -> HardForkState_ g f xs
+  -> Either (Mismatch GenTx (Current f) xs)
+            (HardForkState_ g (Product GenTx f) xs)
+matchTx is tx =
+      fmap (HardForkState . hmap distrib)
+    . matchTx' is tx
+    . getHardForkState
+  where
+    distrib :: Product GenTx (Current f) blk -> Current (Product GenTx f) blk
+    distrib (Pair tx' Current{..}) = Current {
+          currentStart = currentStart
+        , currentState = Pair tx' currentState
+        }
