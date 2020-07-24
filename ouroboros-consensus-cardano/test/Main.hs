fa13c2d12ef78c43d80c04f14ce84a4a118620b6
Thomas Winant <thomas@well-typed.com>
2020-07-24 11:44:49 +0200

    Optimise translating the Byron UTxO to the Shelley UTxO
    Instead of going from compact Byron Tx -> normal Byron Tx -> normal Shelley Tx
    -> compact Shelley Tx, go from compact Byron Tx -> compact Shelley Tx.
    
    The crux is to avoid deserialising and reserialising the address in the `TxOut`.
    
    I measured the time it took to translate the Byron ledger to the Shelley ledger
    when syncing mainnet_candidate2:
    
    Before: 3.7s
    After:  1.2s

--- ouroboros-consensus-cardano/test/Main.hs
+++ ouroboros-consensus-cardano/test/Main.hs
@@ -6,6 +6,7 @@ import           Test.Util.Nightly
 import qualified Test.Consensus.Cardano.ByronCompatibility (tests)
 import qualified Test.Consensus.Cardano.Golden (tests)
 import qualified Test.Consensus.Cardano.Serialisation (tests)
+import qualified Test.Consensus.Cardano.Translation (tests)
 import qualified Test.ThreadNet.Cardano (tests)
 
 main :: IO ()
@@ -17,5 +18,6 @@ tests =
   [ Test.Consensus.Cardano.ByronCompatibility.tests
   , Test.Consensus.Cardano.Golden.tests
   , Test.Consensus.Cardano.Serialisation.tests
+  , Test.Consensus.Cardano.Translation.tests
   , Test.ThreadNet.Cardano.tests
   ]
