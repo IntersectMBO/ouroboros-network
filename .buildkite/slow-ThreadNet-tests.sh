5bc6b59d1e300294d2ef1e9ed50f7a2017bea71f
Nicolas Frisby <nick.frisby@iohk.io>
2020-10-12 08:33:53 -0700

    DEBUG just the RealTPraos nightly

--- .buildkite/slow-ThreadNet-tests.sh
+++ .buildkite/slow-ThreadNet-tests.sh
@@ -24,11 +24,9 @@ set -euo pipefail
 # overhead and also more reliable percentages in their QuickCheck statistics.
 rows=(
     # From the slowest individual invocation ...
-    '1 Cardano    1500'  # ~70 minutes per invocation
-    '2 RealTPraos 200'   # ~30 minutes per invocation (but high variance)
-    '4 RealTPraos 100'   # ~15 minutes per invocation (but high variance)
-    '5 Cardano    200'   # ~9  minutes per invocation
-    # ... to fastest individual invocation
+      '2 RealTPraos 200'   # ~30 minutes per invocation (but high variance)
+      '4 RealTPraos 100'   # ~15 minutes per invocation (but high variance)
+      # ... to fastest individual invocation
     #
     # And the number of invocations is non-decreasing.
 )
