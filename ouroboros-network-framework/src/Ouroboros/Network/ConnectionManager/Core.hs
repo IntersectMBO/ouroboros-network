7bc2f0a5c58f5c1b15ecd9901c5752ebc0592924
Marcin Szamotulski <profunctor@pm.me>
2021-01-21 14:14:04 +0100

    connection-manager: server

--- ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
@@ -47,6 +47,8 @@ import           Network.Mux.Trace (MuxTrace, WithMuxBearer (..))
 
 import           Ouroboros.Network.ConnectionId
 import           Ouroboros.Network.ConnectionManager.Types
+import           Ouroboros.Network.InboundGovernor.ControlChannel
+import           Ouroboros.Network.MuxMode
 import           Ouroboros.Network.Snocket
 import           Ouroboros.Network.Server.RateLimiting (AcceptedConnectionsLimit (..))
 
@@ -343,6 +345,9 @@ withConnectionManager
     -- ^ Callback which runs in a thread dedicated for a given connection.
     -> (handleError -> HandleErrorType)
     -- ^ classify 'handleError's
+    -> InResponderMode muxMode (ControlChannel m (NewConnection peerAddr handle))
+    -- ^ On outbound duplex connections we need to notify the server about
+    -- a new connection.
     -> (ConnectionManager muxMode socket peerAddr handle handleError m -> m a)
     -- ^ Continuation which receives the 'ConnectionManager'.  It must not leak
     -- outside of scope of this callback.  Once it returns all resources
@@ -365,6 +370,7 @@ withConnectionManager ConnectionManagerArguments {
                           connectionHandler
                         }
                       classifyHandleError
+                      inboundGovernorControlChannel
                       k = do
     (stateVar ::  StrictTMVar m (ConnectionManagerState peerAddr handle handleError version m))
       <- atomically $  do
@@ -1112,6 +1118,10 @@ withConnectionManager ConnectionManagerArguments {
                         Duplex -> do
                           let connState' = OutboundDupState connId connThread handle Ticking
                           writeTVar connVar connState'
+                          case inboundGovernorControlChannel of
+                            InResponderMode controlChannel ->
+                              newOutboundConnection controlChannel connId dataFlow handle
+                            NotInResponderMode -> return ()
                           return (mkTransition connState connState')
                     traceWith
                       trTracer
