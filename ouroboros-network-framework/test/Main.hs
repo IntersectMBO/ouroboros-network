a05335e31786c21de1a7b590e84e4db9dd53b5cb
Marcin Szamotulski <profunctor@pm.me>
2021-01-22 11:09:09 +0100

    connection-manager: server IO tests
    * unidirectional: two connection managers; first, running in initiator
      only mode, which connects to the other.
    * bidirectional: two connection manager using a single bidirectional TCP
      connection

--- ouroboros-network-framework/test/Main.hs
+++ ouroboros-network-framework/test/Main.hs
@@ -5,6 +5,7 @@ import           Test.Tasty
 import qualified Test.Data.Monoid.Synchronisation as Synchronisation
 import qualified Test.Network.TypedProtocol.PingPong.Codec as PingPong
 import qualified Test.Network.TypedProtocol.ReqResp.Codec as ReqResp
+import qualified Test.Ouroboros.Network.Server2 as Server2
 import qualified Test.Ouroboros.Network.Driver as Driver
 import qualified Test.Ouroboros.Network.Socket as Socket
 import qualified Test.Ouroboros.Network.Subscription as Subscription
@@ -19,6 +20,7 @@ tests =
   [ PingPong.tests
   , ReqResp.tests
   , Driver.tests
+  , Server2.tests
   , Socket.tests
   , Subscription.tests
   , RateLimiting.tests
