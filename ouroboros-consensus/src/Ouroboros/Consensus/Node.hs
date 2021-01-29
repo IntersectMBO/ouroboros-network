6e19fd8eff30181bb7ad95cfa8d91c2e5aba47f7
Marcin Szamotulski <profunctor@pm.me>
2021-01-29 12:57:46 +0100

    ouroboros-consensus: using updated diffusion interface

--- ouroboros-consensus/src/Ouroboros/Consensus/Node.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Node.hs
@@ -30,9 +30,7 @@ module Ouroboros.Consensus.Node (
   , ConnectionId (..)
   , DiffusionArguments (..)
   , DiffusionTracers (..)
-  , DnsSubscriptionTarget (..)
   , HardForkBlockchainTimeArgs (..)
-  , IPSubscriptionTarget (..)
   , LastShutDownWasClean (..)
   , LowLevelRunNodeArgs (..)
   , MaxTxCapacityOverride (..)
@@ -59,6 +57,7 @@ import           Data.Hashable (Hashable)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Typeable (Typeable)
+import           Data.Void (Void)
 import           System.FilePath ((</>))
 import           System.Random (StdGen, newStdGen, randomIO, randomRIO)
 
@@ -88,11 +87,11 @@ import qualified Ouroboros.Consensus.Network.NodeToClient as NTC
 import qualified Ouroboros.Consensus.Network.NodeToNode as NTN
 import           Ouroboros.Consensus.Node.DbLock
 import           Ouroboros.Consensus.Node.DbMarker
-import           Ouroboros.Consensus.Node.ErrorPolicy
 import           Ouroboros.Consensus.Node.InitStorage
 import           Ouroboros.Consensus.Node.NetworkProtocolVersion
 import           Ouroboros.Consensus.Node.ProtocolInfo
 import           Ouroboros.Consensus.Node.Recovery
+import           Ouroboros.Consensus.Node.RethrowPolicy
 import           Ouroboros.Consensus.Node.Run
 import           Ouroboros.Consensus.Node.Tracers
 import           Ouroboros.Consensus.NodeKernel
@@ -186,7 +185,7 @@ data LowLevelRunNodeArgs m addrNTN addrNTC versionDataNTN versionDataNTC blk = L
              addrNTN        addrNTC
              versionDataNTN versionDataNTC
              m
-        -> m ()
+        -> m Void
 
     , llrnVersionDataNTC :: versionDataNTC
 
@@ -207,7 +206,7 @@ run :: forall blk.
      RunNode blk
   => RunNodeArgs IO RemoteAddress LocalAddress blk
   -> StdRunNodeArgs IO blk
-  -> IO ()
+  -> IO Void
 run args stdArgs = stdLowLevelRunNodeArgsIO args stdArgs >>= runWith args
 
 -- | Start a node.
@@ -223,7 +222,7 @@ runWith :: forall m addrNTN addrNTC versionDataNTN versionDataNTC blk.
      )
   => RunNodeArgs m addrNTN addrNTC blk
   -> LowLevelRunNodeArgs m addrNTN addrNTC versionDataNTN versionDataNTC blk
-  -> m ()
+  -> m Void
 runWith RunNodeArgs{..} LowLevelRunNodeArgs{..} =
 
     llrnWithCheckedDB $ \(LastShutDownWasClean lastShutDownWasClean) ->
@@ -299,7 +298,6 @@ runWith RunNodeArgs{..} LowLevelRunNodeArgs{..} =
                                     (miniProtocolParameters nodeKernelArgs)
                                     ntnApps
                                     ntcApps
-                                    nodeKernel
 
       llrnRunDataDiffusion registry diffusionApplications
   where
@@ -347,27 +345,26 @@ runWith RunNodeArgs{..} LowLevelRunNodeArgs{..} =
           -> NodeToClientVersion
           -> NTC.Apps m (ConnectionId addrNTC) ByteString ByteString ByteString ()
          )
-      -> NodeKernel m (ConnectionId addrNTN) (ConnectionId addrNTC) blk
       -> DiffusionApplications
            addrNTN addrNTC
            versionDataNTN versionDataNTC
            m
-    mkDiffusionApplications miniProtocolParams ntnApps ntcApps kernel =
+    mkDiffusionApplications miniProtocolParams ntnApps ntcApps =
       DiffusionApplications {
-          daResponderApplication = combineVersions [
-              simpleSingletonVersions
-                version
-                llrnVersionDataNTN
-                (NTN.responder miniProtocolParams version $ ntnApps blockVersion)
-            | (version, blockVersion) <- Map.toList llrnNodeToNodeVersions
-            ]
-        , daInitiatorApplication = combineVersions [
-              simpleSingletonVersions
-                version
-                llrnVersionDataNTN
-                (NTN.initiator miniProtocolParams version $ ntnApps blockVersion)
-            | (version, blockVersion) <- Map.toList llrnNodeToNodeVersions
-            ]
+          daApplicationInitiatorMode = combineVersions
+             [ simpleSingletonVersions
+                 version
+                 llrnVersionDataNTN
+                 (NTN.initiator miniProtocolParams version $ ntnApps blockVersion)
+             | (version, blockVersion) <- Map.toList llrnNodeToNodeVersions
+             ]
+        , daApplicationInitiatorResponderMode = combineVersions
+             [ simpleSingletonVersions
+                 version
+                 llrnVersionDataNTN
+                 (NTN.initiatorAndResponder miniProtocolParams version $ ntnApps blockVersion)
+             | (version, blockVersion) <- Map.toList llrnNodeToNodeVersions
+             ]
         , daLocalResponderApplication = combineVersions [
               simpleSingletonVersions
                 version
@@ -375,8 +372,9 @@ runWith RunNodeArgs{..} LowLevelRunNodeArgs{..} =
                 (NTC.responder version $ ntcApps blockVersion version)
             | (version, blockVersion) <- Map.toList llrnNodeToClientVersions
             ]
-        , daErrorPolicies = consensusErrorPolicy (Proxy @blk)
-        , daLedgerPeersCtx = LedgerPeersConsensusInterface (getPeersFromCurrentLedgerAfterSlot kernel)
+        , daMiniProtocolParameters = miniProtocolParams
+        , daRethrowPolicy = consensusRethrowPolicy (Proxy @blk)
+        , daLocalRethrowPolicy = mempty
         }
 
 -- | Did the ChainDB already have existing clean-shutdown marker on disk?
@@ -602,7 +600,7 @@ stdRunDataDiffusion ::
        RemoteAddress LocalAddress
        NodeToNodeVersionData NodeToClientVersionData
        IO
-  -> IO ()
+  -> IO Void
 stdRunDataDiffusion = runDataDiffusion
 
 -- | Higher-level arguments that can determine the 'LowLevelRunNodeArgs' under
