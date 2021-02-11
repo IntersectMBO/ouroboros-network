cd27ec46ce2e6ab130ac85c84ec4b6808fb5b69a
Karl Knutsson <karl.knutsson@iohk.io>
2021-02-11 08:44:10 +0000

    Force the list of ledger peers to WHNF.

--- ouroboros-consensus/src/Ouroboros/Consensus/NodeKernel.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/NodeKernel.hs
@@ -24,6 +24,9 @@ module Ouroboros.Consensus.NodeKernel (
   , initNodeKernel
   ) where
 
+
+import           Cardano.Prelude (forceElemsToWHNF)
+
 import           Control.Monad
 import           Control.Monad.Except
 import           Data.Bifunctor (second)
@@ -747,6 +750,7 @@ getPeersFromCurrentLedger kernel p = do
       guard (p immutableLedger)
       return
         $ map (second (fmap stakePoolRelayAddress))
+        $ forceElemsToWHNF
         $ getPeers immutableLedger
 
 -- | Like 'getPeersFromCurrentLedger' but with a \"after slot number X\"
