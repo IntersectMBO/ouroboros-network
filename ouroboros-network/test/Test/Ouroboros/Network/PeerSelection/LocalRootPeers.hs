e673bc2b2ca14834d3b96ef532846586f8d368f2
Armando Santos <armando@well-typed.com>
2021-03-23 10:39:45 +0000

    Added test for the fromGroups function.

--- ouroboros-network/test/Test/Ouroboros/Network/PeerSelection/LocalRootPeers.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/PeerSelection/LocalRootPeers.hs
@@ -31,13 +31,14 @@ import           Test.Tasty.QuickCheck (testProperty)
 
 tests :: TestTree
 tests =
-  testGroup "LocalRootPeers"
-  [ testProperty "arbitrary"    prop_arbitrary_LocalRootPeers
-  , testProperty "fromGroups"   prop_fromToGroups
-  , testProperty "shrink"       prop_shrink_LocalRootPeers
-  , testProperty "clampToLimit" prop_clampToLimit
-  ]
-
+    testGroup
+        "LocalRootPeers"
+        [ testProperty "arbitrary" prop_arbitrary_LocalRootPeers,
+          testProperty "fromToGroups" prop_fromToGroups,
+          testProperty "fromGroups" prop_fromGroups,
+          testProperty "shrink" prop_shrink_LocalRootPeers,
+          testProperty "clampToLimit" prop_clampToLimit
+        ]
 
 -- | Check that we can forcibly bring the localRootPeers into compliance
 -- with the governor's state invariant, which requires that the local
@@ -47,90 +48,93 @@ tests =
 -- See sanePeerSelectionTargets.
 prop_clampToLimit :: LocalRootPeers PeerAddr -> PeerSelectionTargets -> Property
 prop_clampToLimit localRootPeers targets =
-
-
-    let sizeLimit       = targetNumberOfKnownPeers targets
+    let sizeLimit = targetNumberOfKnownPeers targets
         localRootPeers' = LocalRootPeers.clampToLimit sizeLimit localRootPeers
-
      in counterexample ("sizeLimit   = " ++ show sizeLimit) $
-
-        LocalRootPeers.size localRootPeers'
-          === min sizeLimit
-                 (LocalRootPeers.size localRootPeers)
-
+            LocalRootPeers.size localRootPeers'
+                === min
+                    sizeLimit
+                    (LocalRootPeers.size localRootPeers)
 
 arbitraryLocalRootPeers :: Set PeerAddr -> Gen (LocalRootPeers PeerAddr)
 arbitraryLocalRootPeers peeraddrs = do
-
     -- divide into a few disjoint groups
-    ngroups     <- choose (1, 5 :: Int)
+    ngroups <- choose (1, 5 :: Int)
     gassignment <- vectorOf (Set.size peeraddrs) (choose (1, ngroups))
-    advertise   <- vectorOf (Set.size peeraddrs) arbitrary
-    let groups = Map.elems $
-                 Map.fromListWith Map.union
-                   [ (gn, Map.singleton p a)
-                   | (p, gn, a) <- zip3 (Set.toList peeraddrs)
-                                         gassignment
-                                         advertise
-                   ]
+    advertise <- vectorOf (Set.size peeraddrs) arbitrary
+    let groups =
+            Map.elems $
+                Map.fromListWith
+                    Map.union
+                    [ (gn, Map.singleton p a)
+                      | (p, gn, a) <-
+                            zip3
+                                (Set.toList peeraddrs)
+                                gassignment
+                                advertise
+                    ]
     targets <- mapM (\g -> choose (0, Map.size g)) groups
 
     return (LocalRootPeers.fromGroups (zip targets groups))
 
-
 instance Arbitrary (LocalRootPeers PeerAddr) where
-
     arbitrary = do
-        peeraddrs <- Set.map (PeerAddr . getNonNegative)
-                 <$> scale (`div` 4) arbitrary
+        peeraddrs <-
+            Set.map (PeerAddr . getNonNegative)
+                <$> scale (`div` 4) arbitrary
         arbitraryLocalRootPeers peeraddrs
 
     shrink lrps =
         map LocalRootPeers.fromGroups (shrink (toGroups' lrps))
 
-toGroups' :: Ord peeraddr
-          => LocalRootPeers peeraddr
-          -> [(Int, Map peeraddr PeerAdvertise)]
+toGroups' ::
+    Ord peeraddr =>
+    LocalRootPeers peeraddr ->
+    [(Int, Map peeraddr PeerAdvertise)]
 toGroups' lrps =
     [ (t, Map.fromSet (m Map.!) g)
-    | let m = LocalRootPeers.toMap lrps
-    , (t, g) <- LocalRootPeers.toGroups lrps ]
+      | let m = LocalRootPeers.toMap lrps,
+        (t, g) <- LocalRootPeers.toGroups lrps
+    ]
 
-restrictKeys :: Ord peeraddr
-             => LocalRootPeers peeraddr
-             -> Set peeraddr
-             -> LocalRootPeers peeraddr
+restrictKeys ::
+    Ord peeraddr =>
+    LocalRootPeers peeraddr ->
+    Set peeraddr ->
+    LocalRootPeers peeraddr
 restrictKeys lrps ks =
     LocalRootPeers.fromGroups
-  . map (\(t,g) -> (t, Map.restrictKeys g ks))
-  . toGroups'
-  $ lrps
+        . map (\(t, g) -> (t, Map.restrictKeys g ks))
+        . toGroups'
+        $ lrps
 
 prop_arbitrary_LocalRootPeers :: LocalRootPeers PeerAddr -> Property
 prop_arbitrary_LocalRootPeers lrps =
-    tabulate "total size"    [size] $
-    tabulate "num groups"    [numGroups] $
-    tabulate "group size"    sizeGroups $
-    tabulate "targets"       targets $
-
-    LocalRootPeers.invariant lrps
-  where
-    size       = renderRanges 5 (LocalRootPeers.size lrps)
-    numGroups  = show (length (LocalRootPeers.toGroups lrps))
-    sizeGroups = map (show . Set.size . snd) (LocalRootPeers.toGroups lrps)
-    targets    = [ case () of
-                    _ | t == 0          -> "none"
+    tabulate "total size" [size]
+        $ tabulate "num groups" [numGroups]
+        $ tabulate "group size" sizeGroups
+        $ tabulate "targets" targets
+        $ LocalRootPeers.invariant lrps
+    where
+        size = renderRanges 5 (LocalRootPeers.size lrps)
+        numGroups = show (length (LocalRootPeers.toGroups lrps))
+        sizeGroups = map (show . Set.size . snd) (LocalRootPeers.toGroups lrps)
+        targets =
+            [ case () of
+                  _
+                      | t == 0 -> "none"
                       | t == Set.size g -> "all"
-                      | otherwise       -> "some"
-                 | (t, g) <- LocalRootPeers.toGroups lrps ]
-
+                      | otherwise -> "some"
+              | (t, g) <- LocalRootPeers.toGroups lrps
+            ]
 
 prop_shrink_LocalRootPeers :: LocalRootPeers PeerAddr -> Bool
 prop_shrink_LocalRootPeers =
     all LocalRootPeers.invariant . shrink
 
-
 prop_fromToGroups :: LocalRootPeers PeerAddr -> Bool
 prop_fromToGroups lrps =
     LocalRootPeers.fromGroups (toGroups' lrps) == lrps
 
+prop_fromGroups :: [(Int, Map PeerAddr PeerAdvertise)] -> Bool
+prop_fromGroups = LocalRootPeers.invariant . LocalRootPeers.fromGroups
