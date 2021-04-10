cc1328813dfc384d27759898b2ee6cdd9a91e70c
Duncan Coutts <duncan@well-typed.com>
2021-04-10 18:20:10 +0100

    Improve the Script shrinker
    Rather than using a general shrinker for NonEmpty lists, we use a
    specific one. We can take advantage of the property that the scripts are
    executed in order, so if an error occurs, only script actions performed
    before the error are relevant.
    
    This means we can shrink from the back to the front, rather than trying
    deleting arbitrary elements, and we be a bit more agressive with
    shrinking by bigger jumps.
    
    Since these scripts are used in several places in the governor mock
    environemtn, this change makes a fairly substantial improvement to the
    number of shrinking steps.

--- ouroboros-network/test/Test/QuickCheck/Utils.hs
+++ ouroboros-network/test/Test/QuickCheck/Utils.hs
@@ -3,10 +3,23 @@
 --
 module Test.QuickCheck.Utils (
 
+    -- * Generator and shrinker utils
+    shrinkListElems,
+
     -- * Reporting utils
     renderRanges,
 
   ) where
+
+
+-- | Like 'shrinkList' but only shrink the elems, don't drop elements.
+--
+-- Useful when you want a custom strategy for dropping elements.
+--
+shrinkListElems :: (a -> [a]) -> [a] -> [[a]]
+shrinkListElems _   []     = []
+shrinkListElems shr (x:xs) = [ x':xs | x'  <- shr x ]
+                          ++ [ x:xs' | xs' <- shrinkListElems shr xs ]
 
 
 -- | Use in 'tabulate' to help summarise data into buckets.
