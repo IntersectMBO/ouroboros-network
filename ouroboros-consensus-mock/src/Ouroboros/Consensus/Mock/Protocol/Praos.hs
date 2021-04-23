bf6a909d12bad89513ef9af324aa1faec15aa167
Javier Sagredo <jasataco@gmail.com>
2021-04-23 16:25:25 +0200

    Add documentation in Ouroboros.Consensus.Mock.
    Adding various comments of different kinds to the mock modules, hopefully for
    helping the reader get an overview of how things work there (and in Praos in
    general).

--- ouroboros-consensus-mock/src/Ouroboros/Consensus/Mock/Protocol/Praos.hs
+++ ouroboros-consensus-mock/src/Ouroboros/Consensus/Mock/Protocol/Praos.hs
@@ -81,18 +81,87 @@ import           Ouroboros.Consensus.Protocol.Abstract
 import           Ouroboros.Consensus.Protocol.Signed
 import           Ouroboros.Consensus.Util.Condense
 
+-- The Praos paper can be located at https://ia.cr/2017/573
+--
+-- A block as defined in Praos (section 3, definition 2, then extended in Fig 9)
+-- consist of a tuple:
+--
+-- > B = (slⱼ, st, d, B_πj, ρ, σⱼ)
+--
+-- where
+-- - slⱼ: the slot at which the block was generated.
+--        Named 'simpleSlotNo' in the 'SimpleStdHeader' block header.
+--        As far as consensus is concerned this will be accessed through
+--        'biSlot' in each block in the 'ChainDepState'.
+--
+-- - st: state, a string ∈ {0,1}^λ, which holds the hash of the previous block.
+--       Named 'simplePrev' in the 'SimpleStdHeader' block header.
+--
+-- - d: data (transaction data in most cases).
+--      Named 'simpleBody' inside 'SimpleBlock'.
+--
+-- - B_πj: block proof consisting of (Uᵢ, y, π).
+--         Named 'praosY' inside 'PraosExtraFields'.
+--      - y: a VRF output used to confirm that Uᵢ was the slot leader.
+--      - π: the VRF proof of the above value.
+--
+--      > (y, π) ≜ VRF_evalProve(η, slⱼ, TEST) see Fig 9
+--
+-- - ρ: the block nonce consisting of (ρ_y, ρ_π), to capture entropy from the
+--      block forging process.
+--      Named 'praosRho' inside 'PraosExtraFields'.
+--      - ρ_y: a VRF output used to confirm this block captured all the previous
+--             entropy.
+--      - ρ_π: the VRF proof of the above value.
+--
+--      > (ρ_y, ρ_π) ≜ VRF_evalProve(η, slⱼ, NONCE) see Fig 9
+--
+-- - σⱼ: a signature on (st, d, slⱼ, B_πj, ρ) with the signing key
+--       for the slot slⱼ for the stakeholder Uᵢ.
+--       Named 'praosSignature' in 'PraosFields'.
+--
+-- Protocol parameters:
+-- - k: maximum number of blocks we can rollback.
+--      Named 'praosSecurityParam' in 'PraosParams'.
+-- - R: number of slots per epoch.
+--      Named 'praosSlotsPerEpoch' in 'PraosParams'.
+-- - f: the active slots coefficient, specifies roughly the proportion of
+--      occupied slots per epoch.
+--      Named 'praosLeaderF' in 'PraosParams'.
+--
+-- Some values you will encounter:
+-- - η: The epoch's nonce. Captures entropy from the block chain. See Fig 8 in
+--      praos paper for where it is used and Fig 10 for how it is defined.
+--      Defined as the hash of the η from the previous epoch, this epoch number
+--      and the ρ of the first 2/3 of the blocks in the previous epoch.
+--      Commonly named through the code as 'eta'.
+--
+--      > η_e ≜ HASH(η_{e-1} || e || ρ_{e-1,0} ... ρ_{e-1, 2R/3})
+--
+-- - Tᵢ: the leader threshold for a specific stakeholder considering its
+--       relative stake (therefore depending on the slot). Defined in the Praos
+--       paper in Figure 4 using the definition for ϕ_f(αᵢ) from section 3.3.
+--       Named 't' in the code but essentially computed by 'leaderThreshold' in
+--       'rhoYT'.
+--
+--       > Tᵢ ≜ 2^(l_VRF) * pᵢ
+--       > pᵢ = ϕ_f(αᵢ) ≜ 1 - (1 - f)^(αᵢ)
+
 {-------------------------------------------------------------------------------
   Fields required by Praos in the header
 -------------------------------------------------------------------------------}
 
 -- | The fields that Praos required in the header
-data PraosFields c toSign = PraosFields {
-      praosSignature   :: SignedKES (PraosKES c) toSign
-    , praosExtraFields :: PraosExtraFields c
+data PraosFields crypto typeBeingSigned = PraosFields {
+      praosSignature   :: SignedKES (PraosKES crypto) typeBeingSigned
+    , praosExtraFields :: PraosExtraFields crypto
     }
   deriving (Generic)
 
 instance (PraosCrypto c, Typeable toSign) => NoThunks (PraosFields c toSign)
+
+deriving instance PraosCrypto c => Show (PraosFields c toSign)
+deriving instance PraosCrypto c => Eq   (PraosFields c toSign)
 
 -- | Fields that should be included in the signature
 data PraosExtraFields c = PraosExtraFields {
@@ -104,6 +173,15 @@ data PraosExtraFields c = PraosExtraFields {
 
 instance PraosCrypto c => NoThunks (PraosExtraFields c)
 
+deriving instance PraosCrypto c => Show (PraosExtraFields c)
+deriving instance PraosCrypto c => Eq   (PraosExtraFields c)
+
+-- | A validate view is an association from the (@signed@) value to the
+-- @PraosFields@ that contains the signature that sign it.
+--
+-- In this mock implementation, this could have been simplified to use
+-- @SignedSimplePraos@ but from the consensus point of view, it is not relevant
+-- which actual value is being signed, that's why we use the existential.
 data PraosValidateView c =
     forall signed. Cardano.Crypto.KES.Class.Signable (PraosKES c) signed
                 => PraosValidateView (PraosFields c signed) signed
@@ -121,6 +199,10 @@ praosValidateView getFields hdr =
   Forging
 -------------------------------------------------------------------------------}
 
+-- | The key used for the given period or a stub Poisoned value.
+--
+-- A key will be poisoned if it failed to evolve by @updateKES@, and will remain
+-- poisoned forever after that.
 data HotKey c =
     HotKey
       !Period  -- ^ Absolute period of the KES key
@@ -161,6 +243,10 @@ evolveKey slotNo hotKey = case hotKey of
    targetPeriod :: Period
    targetPeriod = fromIntegral $ unSlotNo slotNo
 
+-- | Create a PraosFields using a proof, a key and the data to be signed.
+--
+-- It is done by signing whatever is extracted from the extra fields by @mkToSign@
+-- and storing the signature and the extra fields on a @PraosFields@.
 forgePraosFields :: ( PraosCrypto c
                     , Cardano.Crypto.KES.Class.Signable (PraosKES c) toSign
                     , HasCallStack
@@ -172,12 +258,12 @@ forgePraosFields :: ( PraosCrypto c
 forgePraosFields PraosProof{..} hotKey mkToSign =
     case hotKey of
       HotKey kesPeriod key -> PraosFields {
-          praosSignature   = signedKES () kesPeriod (mkToSign signedFields) key
-        , praosExtraFields = signedFields
+          praosSignature   = signedKES () kesPeriod (mkToSign fieldsToSign) key
+        , praosExtraFields = fieldsToSign
         }
       HotKeyPoisoned -> error "trying to sign with a poisoned key"
   where
-    signedFields = PraosExtraFields {
+    fieldsToSign = PraosExtraFields {
         praosCreator = praosLeader
       , praosRho     = praosProofRho
       , praosY       = praosProofY
@@ -218,6 +304,8 @@ latestEvolvedStakeDistAsOfEpoch (PraosEvolvingStake x) e =
   Praos specific types
 -------------------------------------------------------------------------------}
 
+-- |The two VRF invocation modes, NONCE (rho) and TEST (y). See the comment at
+-- the top of the module for an explanation of these.
 data VRFType = NONCE | TEST
     deriving (Show, Eq, Ord, Generic, NoThunks)
 
@@ -227,12 +315,14 @@ instance ToCBOR VRFType where
   -- This is a cheat, and at some point we probably want to decide on Serialise/ToCBOR
   toCBOR = encode
 
+-- |Proofs certifying ρ and y for a given slot and eta.
 data PraosProof c = PraosProof {
       praosProofRho :: CertifiedVRF (PraosVRF c) (Natural, SlotNo, VRFType)
     , praosProofY   :: CertifiedVRF (PraosVRF c) (Natural, SlotNo, VRFType)
     , praosLeader   :: CoreNodeId
     }
 
+-- | An error that can arise during validation
 data PraosValidationError c =
       PraosInvalidSlot SlotNo SlotNo
     | PraosUnknownCoreId CoreNodeId
@@ -262,16 +352,22 @@ deriving instance PraosCrypto c => NoThunks (BlockInfo c)
   Protocol proper
 -------------------------------------------------------------------------------}
 
+-- | An uninhabited type representing the Praos protocol.
 data Praos c
 
 -- | Praos parameters that are node independent
 data PraosParams = PraosParams {
       praosLeaderF       :: !Double
+      -- ^ f, the active slots coefficient, defined in 3.3 in the Praos paper.
     , praosSecurityParam :: !SecurityParam
+      -- ^ k, maximum number of blocks we can rollback
     , praosSlotsPerEpoch :: !Word64
+      -- ^ R, slots in each epoch, defined in section 3 in the Praos paper.
     }
   deriving (Generic, NoThunks)
 
+-- | The configuration that will be provided to every node when running the
+-- MockPraos protocol.
 data instance ConsensusConfig (Praos c) = PraosConfig
   { praosParams        :: !PraosParams
   , praosInitialEta    :: !Natural
@@ -282,11 +378,56 @@ data instance ConsensusConfig (Praos c) = PraosConfig
   }
   deriving (Generic)
 
+instance PraosCrypto c => NoThunks (ConsensusConfig (Praos c))
+
+slotEpoch :: ConsensusConfig (Praos c) -> SlotNo -> EpochNo
+slotEpoch PraosConfig{praosParams = PraosParams{..}} s =
+    runIdentity $ epochInfoEpoch epochInfo s
+  where
+    epochInfo = fixedSizeEpochInfo (EpochSize praosSlotsPerEpoch)
+
+epochFirst :: ConsensusConfig (Praos c) -> EpochNo -> SlotNo
+epochFirst PraosConfig{..} e =
+    runIdentity $ epochInfoFirst epochInfo e
+  where
+    epochInfo = fixedSizeEpochInfo (EpochSize praosSlotsPerEpoch)
+    PraosParams{..} = praosParams
+
+-- |The chain dependent state, in this case as it is a mock, we just will store
+-- a list of BlockInfos that allow us to look into the past.
 newtype PraosChainDepState c = PraosChainDepState {
       praosHistory :: [BlockInfo c]
     }
   deriving stock   (Eq, Show)
   deriving newtype (NoThunks, Serialise)
+
+infosSlice :: SlotNo -> SlotNo -> [BlockInfo c] -> [BlockInfo c]
+infosSlice from to xs = takeWhile (\b -> biSlot b >= from)
+                      $ dropWhile (\b -> biSlot b > to) xs
+
+infosEta :: forall c. (PraosCrypto c)
+         => ConsensusConfig (Praos c)
+         -> [BlockInfo c]
+         -> EpochNo
+         -> Natural
+infosEta l _  0 =
+    praosInitialEta l
+infosEta l@PraosConfig{praosParams = PraosParams{..}} xs e =
+    let e'   = e - 1
+        -- the η from the previous epoch
+        eta' = infosEta l xs e'
+        -- the first slot in previous epoch
+        from = epochFirst l e'
+        -- 2/3 of the slots per epoch
+        n    = div (2 * praosSlotsPerEpoch) 3
+        -- the last of the 2/3 of slots in this epoch
+        to   = SlotNo $ unSlotNo from + n
+        -- the list of rhos from the first block in this epoch until the one at
+        -- 2/3 of the slots. Note it is reversed, i.e. start at the oldest.
+        rhos = reverse [biRho b | b <- infosSlice from to xs]
+    in  bytesToNatural
+          . hashToBytes
+          $ hashWithSerialiser @(PraosHash c) toCBOR (eta', e, rhos)
 
 -- | Ticking the Praos chain dep state has no effect
 --
@@ -298,10 +439,13 @@ newtype PraosChainDepState c = PraosChainDepState {
 -- We do however need access to the ticked stake distribution.
 data instance Ticked (PraosChainDepState c) = TickedPraosChainDepState {
       tickedPraosLedgerView      :: Ticked (LedgerView (Praos c))
+      -- ^ The ticked ledger view.
     , untickedPraosChainDepState :: PraosChainDepState c
+      -- ^ The unticked chain dependent state, containing the full history.
     }
 
 instance PraosCrypto c => ConsensusProtocol (Praos c) where
+
   protocolSecurityParam = praosSecurityParam . praosParams
 
   type LedgerView    (Praos c) = ()
@@ -312,6 +456,8 @@ instance PraosCrypto c => ConsensusProtocol (Praos c) where
   type CanBeLeader   (Praos c) = CoreNodeId
 
   checkIsLeader cfg@PraosConfig{..} nid slot (TickedPraosChainDepState _u  cds) =
+      -- See Figure 4 of the Praos paper.
+      -- In order to be leader, y must be < Tᵢ
       if fromIntegral (getOutputVRFNatural (certifiedOutput y)) < t
       then Just PraosProof {
                praosProofRho = rho
@@ -403,62 +549,15 @@ instance PraosCrypto c => ConsensusProtocol (Praos c) where
   -- (Standard) Praos uses the standard chain selection rule, so no need to
   -- override (though see note regarding clock skew).
 
-instance PraosCrypto c => NoThunks (ConsensusConfig (Praos c))
-  -- use generic instance
-
-slotEpoch :: ConsensusConfig (Praos c) -> SlotNo -> EpochNo
-slotEpoch PraosConfig{..} s =
-    runIdentity $ epochInfoEpoch epochInfo s
-  where
-    epochInfo = fixedSizeEpochInfo (EpochSize praosSlotsPerEpoch)
-    PraosParams{..} = praosParams
-
-blockInfoEpoch :: ConsensusConfig (Praos c) -> BlockInfo c -> EpochNo
-blockInfoEpoch l = slotEpoch l . biSlot
-
-epochFirst :: ConsensusConfig (Praos c) -> EpochNo -> SlotNo
-epochFirst PraosConfig{..} e =
-    runIdentity $ epochInfoFirst epochInfo e
-  where
-    epochInfo = fixedSizeEpochInfo (EpochSize praosSlotsPerEpoch)
-    PraosParams{..} = praosParams
-
-infosSlice :: SlotNo -> SlotNo -> [BlockInfo c] -> [BlockInfo c]
-infosSlice from to xs = takeWhile (\b -> biSlot b >= from)
-                      $ dropWhile (\b -> biSlot b > to) xs
-
-infosEta :: forall c. (PraosCrypto c)
-         => ConsensusConfig (Praos c)
-         -> [BlockInfo c]
-         -> EpochNo
-         -> Natural
-infosEta l _  0 = praosInitialEta l
-infosEta l xs e =
-    let e'   = e - 1
-        eta' = infosEta l xs e'
-        from = epochFirst l e'
-        n    = div (2 * praosSlotsPerEpoch) 3
-        to   = SlotNo $ unSlotNo from + n
-        rhos = reverse [biRho b | b <- infosSlice from to xs]
-    in  bytesToNatural
-          . hashToBytes
-          $ hashWithSerialiser @(PraosHash c) toCBOR (eta', e, rhos)
-  where
-    PraosParams{..} = praosParams l
-
-infosStake :: ConsensusConfig (Praos c) -> [BlockInfo c] -> EpochNo -> StakeDist
-infosStake s@PraosConfig{..} xs e = case ys of
-    []                  -> praosInitialStake
-    (BlockInfo{..} : _) -> biStake
-  where
-    e' = if e >= 2 then EpochNo (unEpochNo e - 2) else 0
-    ys = dropWhile (\b -> blockInfoEpoch s b > e') xs
-
+-- | Probability for stakeholder Uᵢ to be elected in slot
+-- slⱼ considering its relative stake αᵢ.
 phi :: ConsensusConfig (Praos c) -> Rational -> Double
 phi PraosConfig{..} alpha = 1 - (1 - praosLeaderF) ** fromRational alpha
   where
     PraosParams{..} = praosParams
 
+-- | Compute Tᵢ for a given stakeholder @n@ at a @SlotNo@. Will be computed from
+-- 'praosEvolvingStake' (or taken from 'praosInitialStake' if checking epoch 0).
 leaderThreshold :: forall c. PraosCrypto c
                 => ConsensusConfig (Praos c)
                 -> [BlockInfo c]
