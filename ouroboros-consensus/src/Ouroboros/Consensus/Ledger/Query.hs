ef4aa2922d2230da1f3c2a3025829693a0d1ffba
David Eichmann <EichmannD@gmail.com>
2021-04-28 17:39:19 +0100

    Add GetPartialLedgerConfig query

--- ouroboros-consensus/src/Ouroboros/Consensus/Ledger/Query.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Ledger/Query.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes            #-}
@@ -22,6 +23,9 @@ import           Data.Maybe (isJust)
 import           Ouroboros.Network.Protocol.LocalStateQuery.Type
                      (ShowQuery (..))
 
+import           Cardano.Binary
+import           Ouroboros.Consensus.Config (topLevelConfigLedger)
+import           Ouroboros.Consensus.HardFork.Combinator.PartialConfig
 import           Ouroboros.Consensus.Ledger.Extended
 import           Ouroboros.Consensus.Node.Serialisation
                      (SerialiseNodeToClient (..), SerialiseResult (..))
@@ -34,48 +38,79 @@ import           Ouroboros.Consensus.Util.DepPair
 
 -- | Different queries supported by the ledger for all block types, indexed
 -- by the result type.
-data Query blk result = BlockQuery (BlockQuery blk result)
+data Query blk result where
+  BlockQuery :: BlockQuery blk result -> Query blk result
+  GetPartialLedgerConfig :: Query blk (PartialLedgerConfig blk)
 
 instance (ShowProxy (BlockQuery blk)) => ShowProxy (Query blk) where
   showProxy (Proxy :: Proxy (Query blk)) = "Query (" ++ showProxy (Proxy @(BlockQuery blk)) ++ ")"
 
 instance (ShowQuery (BlockQuery blk)) => ShowQuery (Query blk) where
-  showResult (BlockQuery blockQuery) = showResult blockQuery
+  showResult query result = case query of
+    BlockQuery blockQuery  -> showResult blockQuery result
+    GetPartialLedgerConfig -> "PartialLedgerConfig {..}"
 
 instance Eq (SomeSecond BlockQuery blk) => Eq (SomeSecond Query blk) where
   SomeSecond (BlockQuery blockQueryA) == SomeSecond (BlockQuery blockQueryB)
     = SomeSecond blockQueryA == SomeSecond blockQueryB
+  SomeSecond (BlockQuery _) == SomeSecond _
+    = False
+  SomeSecond GetPartialLedgerConfig == SomeSecond GetPartialLedgerConfig
+    = True
+  SomeSecond GetPartialLedgerConfig == SomeSecond _
+    = False
 
 instance Show (SomeSecond BlockQuery blk) => Show (SomeSecond Query blk) where
-  show (SomeSecond (BlockQuery blockQueryA)) = "Query " ++ show (SomeSecond blockQueryA)
+  show (SomeSecond query) = case query of
+    BlockQuery blockQueryA -> "Query " ++ show (SomeSecond blockQueryA)
+    GetPartialLedgerConfig -> "GetPartialLedgerConfig"
 
 instance SerialiseNodeToClient blk (SomeSecond BlockQuery blk) => SerialiseNodeToClient blk (SomeSecond Query blk) where
-  encodeNodeToClient codecConfig blockVersion (SomeSecond (BlockQuery blockQuery))
-    = encodeNodeToClient
-        @blk
-        @(SomeSecond BlockQuery blk)
-        codecConfig
-        blockVersion
-        (SomeSecond blockQuery)
+  encodeNodeToClient codecConfig blockVersion (SomeSecond query)
+    = case query of
+      BlockQuery blockQuery ->
+        encodeTag 0 <> encodeNodeToClient
+                        @blk
+                        @(SomeSecond BlockQuery blk)
+                        codecConfig
+                        blockVersion
+                        (SomeSecond blockQuery)
+      GetPartialLedgerConfig ->
+        encodeTag 1
 
   decodeNodeToClient codecConfig blockVersion = do
-    SomeSecond blockQuery <- decodeNodeToClient
-        @blk
-        @(SomeSecond BlockQuery blk)
-        codecConfig
-        blockVersion
-    return (SomeSecond (BlockQuery blockQuery))
+    tag <- decodeTag
+    case tag of
+      0 -> do
+        SomeSecond x <- decodeNodeToClient
+            @blk
+            @(SomeSecond BlockQuery blk)
+            codecConfig
+            blockVersion
+        return (SomeSecond (BlockQuery x))
+      1 -> return (SomeSecond GetPartialLedgerConfig)
+      _ -> fail $ "SomeSecond Query blk: unknown tag " ++ show tag
 
-instance SerialiseResult blk (BlockQuery blk) => SerialiseResult blk (Query blk) where
-  encodeResult codecConfig blockVersion (BlockQuery blockQuery) result
-    = encodeResult codecConfig blockVersion blockQuery result
+instance ( SerialiseResult blk (BlockQuery blk)
+         , SerialiseNodeToClient blk (PartialLedgerConfig blk)
+         ) => SerialiseResult blk (Query blk) where
+  encodeResult codecConfig blockVersion query result = case query of
+    BlockQuery blockQuery -> encodeResult codecConfig blockVersion blockQuery result
+    GetPartialLedgerConfig -> encodeNodeToClient codecConfig blockVersion result
 
-  decodeResult codecConfig blockVersion (BlockQuery query)
-    = decodeResult codecConfig blockVersion query
+  decodeResult codecConfig blockVersion query = case query of
+    BlockQuery blockQuery -> decodeResult codecConfig blockVersion blockQuery
+    GetPartialLedgerConfig -> decodeNodeToClient codecConfig blockVersion
 
 instance SameDepIndex (BlockQuery blk) => SameDepIndex (Query blk) where
   sameDepIndex (BlockQuery blockQueryA) (BlockQuery blockQueryB)
     = sameDepIndex blockQueryA blockQueryB
+  sameDepIndex (BlockQuery _) _
+    = Nothing
+  sameDepIndex GetPartialLedgerConfig GetPartialLedgerConfig
+    = Just Refl
+  sameDepIndex GetPartialLedgerConfig _
+    = Nothing
 
 deriving instance Show (BlockQuery blk result) => Show (Query blk result)
 
@@ -83,6 +118,8 @@ deriving instance Show (BlockQuery blk result) => Show (Query blk result)
 answerQuery :: QueryLedger blk => ExtLedgerCfg blk -> Query blk result -> ExtLedgerState blk -> result
 answerQuery cfg query st = case query of
   BlockQuery blockQuery -> answerBlockQuery cfg blockQuery st
+  GetPartialLedgerConfig -> undefined "TODO_add_this_to_the_HasPartialLedgerConfig_class"
+    $ topLevelConfigLedger $ getExtLedgerCfg cfg
 
 -- | Different queries supported by the ledger, indexed by the result type.
 data family BlockQuery blk :: Type -> Type
