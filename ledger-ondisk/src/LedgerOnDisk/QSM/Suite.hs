2455cb8d7ceed6c78a362dcbd9f8dfe197d0487c
Douglas Wilson <douglas.wilson@gmail.com>
2021-05-07 16:30:28 +0100

    s/ioProperty/unsafePerformIO

--- ledger-ondisk/src/LedgerOnDisk/QSM/Suite.hs
+++ ledger-ondisk/src/LedgerOnDisk/QSM/Suite.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 -- |
 
 module LedgerOnDisk.QSM.Suite where
@@ -8,15 +9,16 @@ import qualified LedgerOnDisk.QSM.Model
 import           LedgerOnDisk.Simple
 import Test.StateMachine.Lockstep.Simple
 import Data.IORef
+import System.IO.Unsafe
+import GHC.Base (noinline)
+
 
 prop_stateMachine_SimpleT :: Property
-prop_stateMachine_SimpleT = ioProperty $ do
-  ref <- newIORef $ initialState mempty
-
-  let
-    smt0 = LedgerOnDisk.QSM.Model.stateMachineTest $ flip runSimpleTWithIORef ref
-    smt = smt0 { cleanup = \x -> cleanup smt0 x *> writeIORef ref (initialState mempty) }
-  pure $ prop_sequential smt Nothing
+prop_stateMachine_SimpleT = let
+  !ref = noinline . unsafePerformIO $ newIORef $ initialState mempty
+  smt0 = LedgerOnDisk.QSM.Model.stateMachineTest $ flip runSimpleTWithIORef ref
+  smt = smt0 { cleanup = \x -> cleanup smt0 x *> writeIORef ref (initialState mempty) }
+  in prop_sequential smt (Just 10)
 
 tests :: TestTree
 tests = testGroup "quickcheck state machine"
