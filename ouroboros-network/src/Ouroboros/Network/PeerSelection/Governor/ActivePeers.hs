46af4c7de1fb53283ae6e987452c337f3daba663
Duncan Coutts <duncan@well-typed.com>
2021-05-11 12:31:49 +0100

    p2p-governor: extend the pick policy with a couple example attributes.
    This establishes the pattern and these two may also be actually useful.

--- ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/ActivePeers.hs
+++ ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/ActivePeers.hs
@@ -87,7 +87,7 @@ belowTargetLocal actions
   = Guarded Nothing $ do
       selectedToPromote <-
         Set.unions <$> sequence
-          [ pickPeers
+          [ pickPeers st
               policyPickWarmPeersToPromote
               membersAvailableToPromote
               numMembersToPromote
@@ -168,7 +168,7 @@ belowTargetOther actions
   , not (Set.null availableToPromote)
   , numPeersToPromote > 0
   = Guarded Nothing $ do
-      selectedToPromote <- pickPeers
+      selectedToPromote <- pickPeers st
                              policyPickWarmPeersToPromote
                              availableToPromote
                              numPeersToPromote
@@ -350,7 +350,7 @@ aboveTargetLocal actions
   = Guarded Nothing $ do
       selectedToDemote <-
         Set.unions <$> sequence
-          [ pickPeers
+          [ pickPeers st
               policyPickHotPeersToDemote
               membersAvailableToDemote
               numMembersToDemote
@@ -413,7 +413,7 @@ aboveTargetOther actions
                               Set.\\ LocalRootPeers.keysSet localRootPeers
   , not (Set.null availableToDemote)
   = Guarded Nothing $ do
-      selectedToDemote <- pickPeers
+      selectedToDemote <- pickPeers st
                             policyPickHotPeersToDemote
                             availableToDemote
                             numPeersToDemote
