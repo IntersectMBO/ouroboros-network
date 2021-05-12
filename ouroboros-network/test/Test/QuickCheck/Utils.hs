b853c3edf74441a76087a01eb723c31e81c740ec
Duncan Coutts <duncan@well-typed.com>
2021-05-12 14:58:29 +0200

    p2p-governor: Change the PickScript to pick elements not offsets.
    The previous representation of pick scripts used [Int], a list of
    offsets to pick. These are offsets within the set that the governor
    gives to us. This is nice in one sense, that it can be interpreted to
    make sense irrespecive of what set the governor gives us. The downside
    is that its shrinking behaviour is quite odd and hence poor. It is odd
    because the modulo behaviour means the choices are highly unstable, and
    that does not sit well with shrinking.
    
    So we switch to an alternative representation where the pick script has
    a set of elements that it wants to pick. We arrange to select this set
    as an arbitrary subset of the total set of elements, which are the peers
    in the graph in the mock environment.
    
    The advantage of this is that the shrinking behaviour is much clearer.
    The choices become stable as the other things are shrunk. The slight
    downside is that the sets we get to pick from are chosen by the
    governor at runtime, which can often be a small subset of the total
    peers available in the graph. But for the script we have to pick the
    subset up front. So to resolve this we need to pick quite large sets up
    front so that the intersection between the set we generated, and the set
    the governor asks us to pick from will frequently have a non-trivial
    intersection. So the approach we take is to generate random subsets that
    roughtly half the size of the total set.

--- ouroboros-network/test/Test/QuickCheck/Utils.hs
+++ ouroboros-network/test/Test/QuickCheck/Utils.hs
@@ -4,12 +4,31 @@
 module Test.QuickCheck.Utils (
 
     -- * Generator and shrinker utils
+    arbitrarySubset,
     shrinkListElems,
 
     -- * Reporting utils
     renderRanges,
 
   ) where
+
+import           Data.Set (Set)
+import qualified Data.Set as Set
+
+import           Test.QuickCheck
+
+-- | Pick a subset of a set, using a 50:50 chance for each set element.
+--
+arbitrarySubset :: Ord a => Set a -> Gen (Set a)
+arbitrarySubset s = do
+    picks <- vectorOf (Set.size s) (arbitrary :: Gen Bool)
+    let s' = Set.fromList
+           . map snd
+           . filter fst
+           . zip picks
+           . Set.toList
+           $ s
+    return s'
 
 
 -- | Like 'shrinkList' but only shrink the elems, don't drop elements.
