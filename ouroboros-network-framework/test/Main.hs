4c168ecb470d7e9d12e783edf4a8ad4083c52dc5
Marcin Szamotulski <profunctor@pm.me>
2021-05-13 08:52:52 +0200

    Data.Monoid.Synchronisation
    Introduce new type wrappers:
    * 'FirstToFinish'
    * 'LastToFinish'
    
    which behaves like 'Alt' and 'Ap', but with more useful names.

--- ouroboros-network-framework/test/Main.hs
+++ ouroboros-network-framework/test/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import           Test.Tasty
 
+import qualified Test.Data.Monoid.Synchronisation as Synchronisation
 import qualified Test.Network.TypedProtocol.PingPong.Codec as PingPong
 import qualified Test.Network.TypedProtocol.ReqResp.Codec as ReqResp
 import qualified Test.Ouroboros.Network.Driver as Driver
@@ -21,6 +22,7 @@ tests =
   , Socket.tests
   , Subscription.tests
   , RateLimiting.tests
+  , Synchronisation.tests
   ]
 
 
