0b95377efdb9bab4802039b0caa42832625a4fe1
Marcin Szamotulski <profunctor@pm.me>
2021-05-17 18:07:29 +0200

    connection-manager: added readState method

--- ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
@@ -491,57 +491,81 @@ withConnectionManager ConnectionManagerArguments {
           freshIdSupply <- newFreshIdSupply (Proxy :: Proxy m)
           return (freshIdSupply, v)
 
-    let connectionManager :: ConnectionManager muxMode socket peerAddr
+    let readState
+          :: m (Map peerAddr AbstractState)
+        readState =
+          atomically $ do
+              state <- readTMVar stateVar
+              traverse ( fmap (abstractState . Known)
+                       . readTVar
+                       . connVar
+                       )
+                       state
+
+        connectionManager :: ConnectionManager muxMode socket peerAddr
                                                handle handleError m
         connectionManager =
           case connectionHandler of
             WithInitiatorMode outboundHandler ->
-              ConnectionManager
-                (WithInitiatorMode
-                  OutboundConnectionManager {
-                      ocmRequestConnection =
-                        requestOutboundConnectionImpl freshIdSupply stateVar outboundHandler,
-                      ocmUnregisterConnection =
-                        unregisterOutboundConnectionImpl stateVar
-                    })
+              ConnectionManager {
+                getConnectionManager =
+                  WithInitiatorMode
+                    OutboundConnectionManager {
+                        ocmRequestConnection =
+                          requestOutboundConnectionImpl freshIdSupply stateVar
+                                                        outboundHandler,
+                        ocmUnregisterConnection =
+                          unregisterOutboundConnectionImpl stateVar
+                      },
+                readState
+              }
 
             WithResponderMode inboundHandler ->
-              ConnectionManager
-                (WithResponderMode
-                  InboundConnectionManager {
-                      icmIncludeConnection =
-                        includeInboundConnectionImpl freshIdSupply stateVar inboundHandler,
-                      icmUnregisterConnection =
-                        unregisterInboundConnectionImpl stateVar,
-                      icmPromotedToWarmRemote =
-                        promotedToWarmRemoteImpl stateVar,
-                      icmDemotedToColdRemote =
-                        demotedToColdRemoteImpl stateVar,
-                      icmNumberOfConnections =
-                        readTMVar stateVar >>= countPrunableConnections
-                    })
+              ConnectionManager {
+                getConnectionManager =
+                  WithResponderMode
+                    InboundConnectionManager {
+                        icmIncludeConnection =
+                          includeInboundConnectionImpl freshIdSupply stateVar
+                                                       inboundHandler,
+                        icmUnregisterConnection =
+                          unregisterInboundConnectionImpl stateVar,
+                        icmPromotedToWarmRemote =
+                          promotedToWarmRemoteImpl stateVar,
+                        icmDemotedToColdRemote =
+                          demotedToColdRemoteImpl stateVar,
+                        icmNumberOfConnections =
+                          readTMVar stateVar >>= countPrunableConnections
+                      },
+                readState
+              }
 
             WithInitiatorResponderMode outboundHandler inboundHandler ->
-              ConnectionManager
-                (WithInitiatorResponderMode
-                  OutboundConnectionManager {
-                      ocmRequestConnection =
-                        requestOutboundConnectionImpl freshIdSupply stateVar outboundHandler,
-                      ocmUnregisterConnection =
-                        unregisterOutboundConnectionImpl stateVar
-                    }
-                  InboundConnectionManager {
-                      icmIncludeConnection =
-                        includeInboundConnectionImpl freshIdSupply stateVar inboundHandler,
-                      icmUnregisterConnection =
-                        unregisterInboundConnectionImpl stateVar,
-                      icmPromotedToWarmRemote =
-                        promotedToWarmRemoteImpl stateVar,
-                      icmDemotedToColdRemote =
-                        demotedToColdRemoteImpl stateVar,
-                      icmNumberOfConnections =
-                        readTMVar stateVar >>= countPrunableConnections
-                    })
+              ConnectionManager {
+                getConnectionManager =
+                  WithInitiatorResponderMode
+                    OutboundConnectionManager {
+                        ocmRequestConnection =
+                          requestOutboundConnectionImpl freshIdSupply stateVar
+                                                        outboundHandler,
+                        ocmUnregisterConnection =
+                          unregisterOutboundConnectionImpl stateVar
+                      }
+                    InboundConnectionManager {
+                        icmIncludeConnection =
+                          includeInboundConnectionImpl freshIdSupply stateVar
+                                                       inboundHandler,
+                        icmUnregisterConnection =
+                          unregisterInboundConnectionImpl stateVar,
+                        icmPromotedToWarmRemote =
+                          promotedToWarmRemoteImpl stateVar,
+                        icmDemotedToColdRemote =
+                          demotedToColdRemoteImpl stateVar,
+                        icmNumberOfConnections =
+                          readTMVar stateVar >>= countPrunableConnections
+                      },
+                readState
+              }
 
     k connectionManager
       `finally` do
