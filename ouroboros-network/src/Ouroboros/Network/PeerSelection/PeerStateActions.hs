e8e81485b22852fa10fe1c075c8987e17e22c93f
Marcin Szamotulski <profunctor@pm.me>
2021-05-25 15:30:10 +0200

    handshake: simplify types of client / server
    Unify the error type for handshake client and server.

--- ouroboros-network/src/Ouroboros/Network/PeerSelection/PeerStateActions.hs
+++ ouroboros-network/src/Ouroboros/Network/PeerSelection/PeerStateActions.hs
@@ -53,8 +53,6 @@ import           Ouroboros.Network.Channel (fromChannel)
 import           Ouroboros.Network.Mux
 import           Ouroboros.Network.Protocol.Handshake
                    ( HandshakeException
-                   , HandshakeClientProtocolError
-                   , RefuseReason
                    )
 import           Ouroboros.Network.PeerSelection.Governor
                    ( PeerStateActions (..)
@@ -472,11 +470,11 @@ peerSelectionActionExceptionFromException x = do
 data EstablishConnectionException versionNumber
       -- | Handshake client failed
     = ClientException
-        !(HandshakeException (HandshakeClientProtocolError versionNumber))
+        !(HandshakeException versionNumber)
 
       -- | Handshake server failed
     | ServerException
-        !(HandshakeException (RefuseReason versionNumber))
+        !(HandshakeException versionNumber)
   deriving Show
 
 instance ( Show versionNumber
