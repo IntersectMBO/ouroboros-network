af704e922eaea512f6b1e2c379ffe90ebd351ed3
Marcin Szamotulski <profunctor@pm.me>
2021-06-18 11:18:58 +0200

    Wedge data type
    See https://hackage.haskell.org/package/smash/docs/Data-Wedge.html#t:Wedge

---
+++ ouroboros-network-framework/src/Data/Wedge.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DeriveFunctor  #-}
+{-# LANGUAGE DeriveFoldable #-}
+
+module Data.Wedge where
+
+import           Control.Monad (ap)
+
+import           Data.Bifunctor
+import           Data.Bifoldable
+import           Data.Bitraversable
+
+-- | A wedge product
+-- <https://hackage.haskell.org/package/smash/docs/Data-Wedge.html#t:Wedge>
+--
+data Wedge a b =
+    Nowhere
+  | Here a
+  | There b
+  deriving (Eq, Ord, Foldable, Functor, Show)
+
+instance Bifunctor Wedge where
+    bimap _ _ Nowhere   = Nowhere
+    bimap f _ (Here a)  = Here  (f a)
+    bimap _ g (There b) = There (g b)
+
+instance Bifoldable Wedge where
+    bifoldMap _ _ Nowhere   = mempty
+    bifoldMap f _ (Here a)  = f a
+    bifoldMap _ g (There b) = g b
+
+instance Bitraversable Wedge where
+    bitraverse _ _ Nowhere   = pure Nowhere
+    bitraverse f _ (Here a)  = Here <$> f a
+    bitraverse _ g (There b) = There <$> g b
+
+instance Applicative (Wedge a) where
+    pure  = return
+    (<*>) = ap
+
+instance Monad (Wedge a) where
+    return = There
+
+    Nowhere >>= _ = Nowhere
+    Here a  >>= _ = Here a
+    There a >>= f = f a
