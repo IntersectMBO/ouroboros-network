51a02cc6971fa2202a8102a46ff19b7b4ed4639a
Marcin Szamotulski <profunctor@pm.me>
2021-07-12 12:59:07 +0200

    verifyAbstracTransition: transitions from TerminatedSt
    Allow transitions from `TermiantedSt` to `ReservedOutboundSt` or
    `UnnegotiatedSt Inbound` state.

--- ouroboros-network-framework/test/Test/Ouroboros/Network/Server2.hs
+++ ouroboros-network-framework/test/Test/Ouroboros/Network/Server2.hs
@@ -1571,7 +1571,7 @@ prop_multinode_Sim serverAcc (ArbDataFlow dataFlow) script =
                          -> AllProperty
                           . (counterexample $!
                               (  "\nUnexpected transition: "
-                              ++ ppTransition tr)
+                              ++ show tr)
                               )
                           . verifyAbstractTransition
                           $ tr
