363499221a862301a9346fad3f3d47b0f0271d72
Marcin Szamotulski <profunctor@pm.me>
2021-07-12 12:59:07 +0200

    verifyAbstracTransition: transitions from TerminatedSt
    Allow transitions from `TermiantedSt` to `ReservedOutboundSt` or
    `UnnegotiatedSt Inbound` state.

--- ouroboros-network-framework/test/Test/Ouroboros/Network/ConnectionManager.hs
+++ ouroboros-network-framework/test/Test/Ouroboros/Network/ConnectionManager.hs
@@ -1720,6 +1720,7 @@ verifyAbstractTransition Transition { fromState, toState } =
       --
 
       -- @Reserve@
+      (TerminatedSt, ReservedOutboundSt) -> True
       (UnknownConnectionSt, ReservedOutboundSt) -> True
       -- @Connected@
       (ReservedOutboundSt, UnnegotiatedSt Outbound) -> True
@@ -1758,6 +1759,7 @@ verifyAbstractTransition Transition { fromState, toState } =
       --
 
       -- @Accepted@
+      (TerminatedSt, UnnegotiatedSt Inbound) -> True
       (UnknownConnectionSt, UnnegotiatedSt Inbound) -> True
       -- @Overwritten@
       (ReservedOutboundSt, UnnegotiatedSt Inbound) -> True
