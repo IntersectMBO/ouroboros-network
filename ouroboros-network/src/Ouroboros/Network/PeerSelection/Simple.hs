7b4cb0f4b64b6b32fc8b8f55f443857ff165647f
Marcin Szamotulski <profunctor@pm.me>
2021-07-16 13:51:32 +0200

    diffusion: run diffusion in an abstract monad
    Also introduce `DiffusionInterfaces` record which provides all the
    interfaces (e.g. snockets, dns resolver, etc) needed to run data
    diffusion.

--- ouroboros-network/src/Ouroboros/Network/PeerSelection/Simple.hs
+++ ouroboros-network/src/Ouroboros/Network/PeerSelection/Simple.hs
@@ -14,10 +14,11 @@ module Ouroboros.Network.PeerSelection.Simple
 
 import           Data.Foldable (toList)
 import           Control.Monad.Class.MonadAsync
+import           Control.Monad.Class.MonadThrow
 import           Control.Monad.Class.MonadTime
+import           Control.Monad.Class.MonadTimer
 import           Control.Monad.Class.MonadSTM.Strict
 import           Control.Tracer (Tracer)
-import           Control.Exception (IOException)
 
 import           Data.Map (Map)
 import           Data.Set (Set)
@@ -34,31 +35,38 @@ import           Ouroboros.Network.PeerSelection.RootPeersDNS
 
 
 withPeerSelectionActions
-  :: forall peeraddr peerconn a.
-     Ord peeraddr
-  => Tracer IO (TraceLocalRootPeers peeraddr IOException)
-  -> Tracer IO TracePublicRootPeers
+  :: forall peeraddr peerconn resolver exception m a.
+     ( MonadAsync m
+     , MonadDelay m
+     , MonadThrow m
+     , Ord peeraddr
+     , Exception exception
+     )
+  => Tracer m (TraceLocalRootPeers peeraddr exception)
+  -> Tracer m TracePublicRootPeers
   -> (IP -> Socket.PortNumber -> peeraddr)
-  -> TimeoutFn IO
-  -> STM IO PeerSelectionTargets
-  -> STM IO [(Int, Map RelayAddress PeerAdvertise)]
+  -> DNSActions resolver exception m
+  -> TimeoutFn m
+  -> STM m PeerSelectionTargets
+  -> STM m [(Int, Map RelayAddress PeerAdvertise)]
   -- ^ local root peers
-  -> STM IO [RelayAddress]
+  -> STM m [RelayAddress]
   -- ^ public root peers
-  -> PeerStateActions peeraddr peerconn IO
-  -> (NumberOfPeers -> STM IO ())
-  -> STM IO (Maybe (Set peeraddr, DiffTime))
-  -> (Maybe (Async IO Void)
-      -> PeerSelectionActions peeraddr peerconn IO
-      -> IO a)
+  -> PeerStateActions peeraddr peerconn m
+  -> (NumberOfPeers -> STM m ())
+  -> STM m (Maybe (Set peeraddr, DiffTime))
+  -> (Maybe (Async m Void)
+      -> PeerSelectionActions peeraddr peerconn m
+      -> m a)
   -- ^ continuation, recieves a handle to the local roots peer provider thread
   -- (only if local root peers where non-empty).
-  -> IO a
+  -> m a
 withPeerSelectionActions
   localRootTracer
   publicRootTracer
   toPeerAddr
-  timeout
+  dnsActions
+  timeoutFn
   readTargets
   readLocalRootPeers
   readPublicRootPeers
@@ -70,7 +78,7 @@ withPeerSelectionActions
     let peerSelectionActions = PeerSelectionActions {
             readPeerSelectionTargets = readTargets,
             readLocalRootPeers = toList <$> readTVar localRootsVar,
-            requestPublicRootPeers = requestLedgerPeers ioDNSActions,
+            requestPublicRootPeers = requestLedgerPeers,
             requestPeerGossip = \_ -> pure [],
             peerStateActions
           }
@@ -78,34 +86,32 @@ withPeerSelectionActions
       (localRootPeersProvider
         localRootTracer
         toPeerAddr
-        timeout
+        timeoutFn
         DNS.defaultResolvConf
         localRootsVar
         readLocalRootPeers
-        ioDNSActions)
+        dnsActions)
       (\thread -> k (Just thread) peerSelectionActions)
   where
     -- We first try to get poublic root peers from the ledger, but if it fails
     -- (for example because the node hasn't synced far enough) we fall back
     -- to using the manually configured bootstrap root peers.
-    requestLedgerPeers :: DNSActions DNS.Resolver IOException IO
-                       -> Int -> IO (Set peeraddr, DiffTime)
-    requestLedgerPeers dnsActions n = do
+    requestLedgerPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestLedgerPeers n = do
         atomically $ reqLedgerPeers $ NumberOfPeers $ fromIntegral n
         peers_m <- atomically getLedgerPeers
         case peers_m of
-             Nothing -> requestPublicRootPeers dnsActions n
+             Nothing    -> requestPublicRootPeers n
              Just peers -> return peers
 
     -- For each call we re-initialise the dns library which forces reading
     -- `/etc/resolv.conf`:
     -- https://github.com/input-output-hk/cardano-node/issues/731
-    requestPublicRootPeers :: DNSActions DNS.Resolver IOException IO
-                           -> Int -> IO (Set peeraddr, DiffTime)
-    requestPublicRootPeers dnsActions n =
+    requestPublicRootPeers :: Int -> m (Set peeraddr, DiffTime)
+    requestPublicRootPeers n =
       publicRootPeersProvider publicRootTracer
                               toPeerAddr
-                              timeout
+                              timeoutFn
                               DNS.defaultResolvConf
                               readPublicRootPeers
                               dnsActions
