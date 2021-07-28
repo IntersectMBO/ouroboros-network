c81f148a50f879c9696cd198073065c37707c634
Marcin Szamotulski <profunctor@pm.me>
2021-07-28 20:51:53 +0200

    typed-protocols-examples: added CBOR codecs
    Include CBOR codecs & tests for:
    * `PingPong` mini-protocol
    * `ReqResp`  mini-protocol
    
    This patch makes `typed-protocols-examples` contain all the
    implementation details for `PingPong` and `ReqResp` protocols.

--- ouroboros-network-framework/test/Main.hs
+++ ouroboros-network-framework/test/Main.hs
@@ -2,8 +2,6 @@ module Main (main) where
 
 import           Test.Tasty
 
-import qualified Test.Network.TypedProtocol.PingPong.Codec as PingPong
-import qualified Test.Network.TypedProtocol.ReqResp.Codec as ReqResp
 import qualified Test.Ouroboros.Network.Driver as Driver
 import qualified Test.Ouroboros.Network.Socket as Socket
 import qualified Test.Ouroboros.Network.Subscription as Subscription
@@ -15,9 +13,7 @@ main = defaultMain tests
 tests :: TestTree
 tests =
   testGroup "ouroboros-network-framework"
-  [ PingPong.tests
-  , ReqResp.tests
-  , Driver.tests
+  [ Driver.tests
   , Socket.tests
   , Subscription.tests
   , RateLimiting.tests
