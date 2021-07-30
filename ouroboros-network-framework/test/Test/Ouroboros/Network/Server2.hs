094aa21dcbb4585ae59e8f32cfc5afdc663566cb
Marcin Szamotulski <profunctor@pm.me>
2021-07-30 17:58:24 +0200

    handshake: more convenient type of unversionedProtocolDataCodec

--- ouroboros-network-framework/test/Test/Ouroboros/Network/Server2.hs
+++ ouroboros-network-framework/test/Test/Ouroboros/Network/Server2.hs
@@ -76,8 +76,7 @@ import qualified Ouroboros.Network.InboundGovernor.ControlChannel as Server
 import           Ouroboros.Network.Mux
 import           Ouroboros.Network.MuxMode
 import           Ouroboros.Network.Protocol.Handshake
-import           Ouroboros.Network.Protocol.Handshake.Codec ( cborTermVersionDataCodec
-                                                            , noTimeLimitsHandshake)
+import           Ouroboros.Network.Protocol.Handshake.Codec (noTimeLimitsHandshake)
 import           Ouroboros.Network.Protocol.Handshake.Unversioned
 import           Ouroboros.Network.Protocol.Handshake.Version (Acceptable (..))
 import           Ouroboros.Network.RethrowPolicy
@@ -320,7 +319,7 @@ withInitiatorOnlyConnectionManager name timeouts trTracer snocket localAddr next
             -- TraceSendRecv
             haHandshakeTracer = (name,) `contramap` nullTracer,
             haHandshakeCodec = unversionedHandshakeCodec,
-            haVersionDataCodec = cborTermVersionDataCodec unversionedProtocolDataCodec,
+            haVersionDataCodec = unversionedProtocolDataCodec,
             haAcceptVersion = acceptableVersion,
             haTimeLimits = noTimeLimitsHandshake
           }
@@ -495,7 +494,7 @@ withBidirectionalConnectionManager name timeouts trTracer snocket socket localAd
               -- TraceSendRecv
               haHandshakeTracer = WithName name `contramap` nullTracer,
               haHandshakeCodec = unversionedHandshakeCodec,
-              haVersionDataCodec = cborTermVersionDataCodec unversionedProtocolDataCodec,
+              haVersionDataCodec = unversionedProtocolDataCodec,
               haAcceptVersion = acceptableVersion,
               haTimeLimits = noTimeLimitsHandshake
             }
