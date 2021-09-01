451efc593f35a2f0f3d7959fb8b01e370e4f9363
Douglas Wilson <douglas.wilson@gmail.com>
2021-09-01 09:36:44 +0100

    Single mutable timeline. Rearange DB to use type familty over FD
    Rearranged Cursors a bit.

--- ledger-ondisk/src/LedgerOnDisk/KVHandle/ClassWithTimelines.hs
+++ ledger-ondisk/src/LedgerOnDisk/KVHandle/ClassWithTimelines.hs
@@ -29,6 +29,7 @@ import Control.Lens
 import Data.Set (Set)
 import Data.HashMap.Strict (HashMap)
 import Data.Kind
+import Data.Int
 
 import Data.Map.Monoidal (MonoidalMap(MonoidalMap))
 import Data.Map (Map)
@@ -114,9 +115,9 @@ type instance SemigroupMap Map = KeysOrdValuesSemigroups
 type instance SemigroupMap HashMap = KeysHashableValuesSemigroups
 type instance SemigroupMap NullMap = UnrestrictedKeysAndValues
 type instance SemigroupMap Keys = KeysAreOrd
-type instance SemigroupMap CursorRequest = UnrestrictedKeysAndValues
 type instance SemigroupMap DiffMap = KeysAreOrd
 type instance SemigroupMap PTMap = KeysAreOrd
+type instance SemigroupMap (CursorRequest h) = UnrestrictedKeysAndValues
 
 class (Ord k) => KeysAreOrd k v
 instance (Ord k) => KeysAreOrd k v
@@ -136,106 +137,99 @@ instance (KeysAreOrd k v, ValuesAreSemigroups k v) => KeysOrdValuesSemigroups k 
 class (KeysAreHashable k v, ValuesAreSemigroups k v) => KeysHashableValuesSemigroups k v
 instance (KeysAreHashable k v, ValuesAreSemigroups k v) => KeysHashableValuesSemigroups k v
 
+data CursorRequest0 h k v
+  = NewCursor
+    { snapshot :: !(Snapshot h)
+    , initialNumRows :: !Int64
+    }
+  | ReadCursor
+    { resumeFrom :: !(Cursor h k v)
+    , numRows :: !Int64
+    }
+  | DisposeCursor !(Cursor h k v)
 
+deriving stock instance (Eq (Cursor h k v), Eq (Snapshot h))
+  => Eq (CursorRequest0 h k v)
 
-data CursorRequest0 k v where
-  NewCursor ::
-    { initialNumRows :: !Int
-    } -> CursorRequest0 k v
-  ReadCursor ::
-    { numRows :: !Int
-    } -> CursorRequest0 k v
-  DisposeCursor :: CursorRequest0 k v
-  deriving stock (Show, Eq)
+deriving stock instance (Show (Cursor h k v), Show (Snapshot h))
+  => Show (CursorRequest0 h k v)
 
-newtype CursorRequest k v = CursorRequest { getCursorRequest :: Map CursorId (CursorRequest0 k v) }
-  deriving stock (Show, Eq)
-  deriving (Semigroup, Monoid) via MonoidalMap CursorId (Semi.Last (CursorRequest0 k v))
+newtype CursorRequest s k v = CursorRequest
+  { getCursorRequest :: Map CursorId (CursorRequest0 s k v) }
+  deriving (Semigroup, Monoid) via MonoidalMap CursorId (Semi.Last (CursorRequest0 s k v))
 
-data CursorResponse0 k v where
-  CursorFinished :: CursorResponse0 k v
-  CursorInvalidated :: CursorResponse0 k v
-  CursorRows ::
-    { numRows :: !Int
+deriving stock instance (Eq (Cursor h k v), Eq (Snapshot h)) => Eq (CursorRequest h k v)
+deriving stock instance (Show (Cursor h k v), Show (Snapshot h)) => Show (CursorRequest h k v)
+
+data CursorResponse0 h k v
+  = CursorFinished
+  | CursorRows
+    { remainingRows :: !Int64
+    , returnedRows :: !Int64
+    , nextCursor :: !(Cursor h k v)
     , rows :: ![(k, v)]
-    } -> CursorResponse0 k v
-  deriving stock (Eq, Show)
+    }
 
-newtype CursorResponse k v = CursorResponse { getCursorResponse :: Map CursorId (CursorResponse0 k v) }
-  deriving stock (Show, Eq)
-  deriving (Semigroup, Monoid) via MonoidalMap CursorId (Semi.Last (CursorResponse0 k v))
+deriving stock instance (Eq (Cursor h k v), Eq k, Eq v) => Eq (CursorResponse0 h k v)
+deriving stock instance (Show (Cursor h k v), Show k, Show v) => Show (CursorResponse0 h k v)
 
-data DBRequest0 state = DBRequest0
-  { dbrqKeys :: !(OnDiskMappings state Keys)
-  , dbrqCursors :: !(OnDiskMappings state CursorRequest)
+newtype CursorResponse h k v = CursorResponse
+  { getCursorResponse :: Map CursorId (CursorResponse0 h k v) }
+  deriving (Semigroup, Monoid) via MonoidalMap CursorId (Semi.Last (CursorResponse0 h k v))
+
+deriving stock instance (Eq (Cursor h k v), Eq k, Eq v) => Eq (CursorResponse h k v)
+deriving stock instance (Show (Cursor h k v), Show k, Show v) => Show (CursorResponse h k v)
+
+data DBRequest dbhandle = DBRequest
+  { cursorRequests :: !(Map CursorId (DBOnDiskMappings dbhandle (CursorRequest dbhandle)))
+  , kvRequests :: !(Map (Maybe (Snapshot dbhandle)) (DBOnDiskMappings dbhandle Keys))
   }
 
-instance
-  ( Semigroup (OnDiskMappings state Keys)
-  , Semigroup (OnDiskMappings state CursorRequest)
-  ) => Semigroup (DBRequest0 state) where
-  x <> y = DBRequest0
-    { dbrqKeys = dbrqKeys x <> dbrqKeys y
-    , dbrqCursors = dbrqCursors x <> dbrqCursors y
-    }
+deriving stock instance
+  ( Eq (DBOnDiskMappings dbhandle (CursorRequest dbhandle))
+  , Eq (DBOnDiskMappings dbhandle Keys)
+  , Eq (Snapshot dbhandle)
+  ) => Eq (DBRequest dbhandle)
 
-instance
-  ( Monoid (OnDiskMappings state Keys)
-  , Monoid (OnDiskMappings state CursorRequest)
-  ) => Monoid (DBRequest0 state) where
-  mempty = DBRequest0
-    { dbrqKeys = mempty
-    , dbrqCursors = mempty
-    }
-
-newtype DBRequest state dbhandle = DBRequest { getDBRequest :: Map (Timeline state dbhandle) (DBRequest0 state) }
-
-deriving stock instance (Eq (Timeline state dbhandle), Eq (DBRequest0 state))
-  => Eq (DBRequest state dbhandle)
-deriving stock instance (Show (Timeline state dbhandle), Show (DBRequest0 state))
-  => Show (DBRequest state dbhandle)
-
-deriving via (MonoidalMap (Timeline state dbhandle) (DBRequest0 state))
-  instance (Ord (Timeline state dbhandle), Semigroup (DBRequest0 state)) => Semigroup (DBRequest state dbhandle)
-
-deriving via (MonoidalMap (Timeline state dbhandle) (DBRequest0 state))
-  instance (Ord (Timeline state dbhandle), Semigroup (DBRequest0 state)) => Monoid (DBRequest state dbhandle)
+deriving stock instance
+  ( Show (DBOnDiskMappings dbhandle (CursorRequest dbhandle))
+  , Show (DBOnDiskMappings dbhandle Keys)
+  , Show (Snapshot dbhandle)
+  ) => Show (DBRequest dbhandle)
 
 -- | The return value for 'submit'
-data DBResponse state dbhandle a = DBResponse
+data DBResponse dbhandle a = DBResponse
   { payload :: a -- ^ An arbitrary return value. Easy access via 'extract'. Passed directly from 'DBOpResult'
-  , commitedToCurrentTimeline :: !Bool -- ^ Were mutations applied to the "current" timeline? i.e. the timeline passed to 'submit'
-  , snapshot :: !(Maybe (Timeline state dbhandle)) -- ^ If timelines were branched this is the new timeline that was created
+  , snapshot :: !(Maybe (Snapshot dbhandle)) -- ^ The snapshot, if it was requested in 'DbOpResult'
   }
   deriving stock (Functor)
 
-instance Comonad (DBResponse state dbhandle) where
+instance Comonad (DBResponse dbhandle) where
   extract = payload
   duplicate x = x <$ x
 
-
-data DBOpArgs0 state = DBOpArgs0
-  { doaData :: !(OnDiskMappings state PTMap)
-  , doaCursors :: !(OnDiskMappings state CursorResponse)
+data DBOpArgs dbhandle = DBOpArgs
+  { cursorResults :: !(Map CursorId (DBOnDiskMappings dbhandle (CursorResponse dbhandle)))
+  , kvResults :: !(Map (Maybe (Snapshot dbhandle)) (DBOnDiskMappings dbhandle PTMap))
   }
 
-newtype DBOpArgs state dbhandle = DBOpArgs { timelineMap :: Map (Timeline state dbhandle) (DBOpArgs0 state) }
-
 -- | The return value for a DB operation, see 'submit'.
+-- This doesn't depend on a dbhandle, but rather state, which one should think
+-- of as (T dbhandle) for some dbhandle
 data DBOpResult state a = DBOpResult
   { payload :: a -- ^ An arbitrary return value. Easy access via 'extract'
   , changes :: !(OnDiskMappings state DiffMap) -- ^ Mutations to apply.
-  , snapshot :: !BranchTimelines -- ^ Where to apply the mutations, and whether to snapshot the result
+  , snapshot :: !Bool -- ^ Should a snapshot be taken?
   } deriving stock (Functor)
 
--- | Describes how we should apply mutations in a DB operation.
--- These applications are relative to a "current" timeline, see 'submit'
-data BranchTimelines
-  = NewTimeline     -- ^ Do not apply mutations to the current timeline, but fork a new timeline with the changes
-  | CurrentTimelineAndSnapshot -- ^ Apply mutations to the current timeline, and return a new timeline snapshotting the result
-  | CurrentTimeline -- ^ Apply mutations to the current timeline, do not snapshot the result
-  | DiscardChanges -- ^ Do not apply the mutations
-  deriving stock (Eq, Show, Enum, Ord)
+-- -- | Describes how we should apply mutations in a DB operation.
+-- -- These applications are relative to a "current" timeline, see 'submit'
+-- data BranchTimelines
+--   = NewTimeline     -- ^ Do not apply mutations to the current timeline, but fork a new timeline with the changes
+--   | CurrentTimelineAndSnapshot -- ^ Apply mutations to the current timeline, and return a new timeline snapshotting the result
+--   | CurrentTimeline -- ^ Apply mutations to the current timeline, do not snapshot the result
+--   | DiscardChanges -- ^ Do not apply the mutations
+--   deriving stock (Eq, Show, Enum, Ord)
 
 
 instance Comonad (DBOpResult state) where
@@ -243,13 +237,15 @@ instance Comonad (DBOpResult state) where
   duplicate x = x <$ x
 
 
+type DBOnDiskMappings dbhandle = OnDiskMappings (T dbhandle)
+
 -- | An instance of 'DB state dbhandle' witnesses that we can perform db
 -- operations on a dbhandle pertaining to type state.
 class
-  ( HasConstrainedOnDiskMappings (DBKVConstraint state) state
-  , Eq (Timeline state dbhandle)
-  , Monoid (DBRequest state dbhandle)
-  ) => DB state dbhandle | dbhandle -> state where
+  ( HasConstrainedOnDiskMappings (DBKVConstraint dbhandle) (T dbhandle)
+  , Ord (Snapshot dbhandle)
+  , Monoid (DBRequest dbhandle)
+  ) => DB dbhandle where
 
   -- | Databases can put constraints on the keys and values they can store.
   -- For example, we may need a Binary Instance on keys and values and an Ord instance on keys
@@ -260,11 +256,14 @@ class
   -- type DBKVConstraint Foo = OrdKeysBinaryAll
   --
   -- The superclass constraint guarantees  state can only contain appropriate keys and values
-  type DBKVConstraint state :: Type -> Type -> Constraint
+  type DBKVConstraint dbhandle :: Type -> Type -> Constraint
+
+  type T dbhandle :: (Type -> Type -> Type) -> Type
+
+  data Cursor dbhandle k v
 
   -- | The abstract type representing prepared - but not submitted - queries
-  -- TODO rename to ReadHandle
-  data ReadSet state dbhandle
+  data ReadHandle dbhandle
 
   -- | The abstract type naming branches of history.
   -- In general Timelines are mutable. Akin to a git branch rather than a git tag.
@@ -272,29 +271,21 @@ class
   -- distinguishing between mutable and immutable timelines in their
   -- implemtation of `Timeline`
   -- This type is expected to be small, just an Id and maybe some flags
-  data Timeline state dbhandle
+  data Snapshot dbhandle
 
   -- | Initiate an operation by preparing it. The 'DBRequest' holds keys and cursors, each associated with a timeline.
   -- | The consistent view of this data will be provided to the operation when it is submitted
   prepare :: dbhandle
-          -> DBRequest state dbhandle
-          -> IO (ReadSet state dbhandle)
+          -> DBRequest dbhandle
+          -> IO (ReadHandle dbhandle)
 
   -- | Submit a prepared operation
-  submit :: dbhandle
-    -> ReadSet state dbhandle
+  submit :: ReadHandle dbhandle
     -- ^ The ReadSet returned from an earlier call to prepare on this handle
-    -> Timeline state dbhandle
-    -- ^ The timeline in which mutations will be applied. This is the "current"
-    -- timeline refferred to in 'BranchTimelines'
-    -> (DBOpArgs state dbhandle -> DBOpResult state a)
+    -> (DBOpArgs dbhandle -> DBOpResult (T dbhandle) a)
     -- ^ The operation to perform. This function will be supplied with the data that was requested by 'prepare'.
     -- It should return a 'DbOpResult', containing mutations to apply,
     -- instructions on snapshotting, as well as an arbitrary 'a' payload.
-    -> IO (DBResponse state dbhandle a)
+    -> IO (DBResponse dbhandle a)
 
-  -- resetTimeline :: Timeline -> Timeline -> IO ()
-
-  -- or:
-  -- timelines are immutable
-  -- there is a global "current" timeline
+  rollback :: dbhandle -> Snapshot dbhandle -> IO ()
