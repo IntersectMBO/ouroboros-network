c37fbaff247e113bf39d6d1ddf75d5522ab0128c
Karl Knutsson <karl.knutsson@iohk.io>
2021-10-18 17:40:59 +0200

    Disable assert.
    Disable assert which triggers by failing hot demotions.

--- ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/Types.hs
+++ ouroboros-network/src/Ouroboros/Network/PeerSelection/Governor/Types.hs
@@ -405,7 +405,7 @@ assertPeerSelectionState PeerSelectionState{..} =
   . assert (Set.isSubsetOf inProgressPromoteCold coldPeersSet)
   . assert (Set.isSubsetOf inProgressPromoteWarm warmPeersSet)
   . assert (Set.isSubsetOf inProgressDemoteWarm  warmPeersSet)
-  . assert (Set.isSubsetOf inProgressDemoteHot   hotPeersSet)
+  -- . assert (Set.isSubsetOf inProgressDemoteHot   hotPeersSet) TODO: XXX fix assert for failing hot demotion
   . assert (Set.null (Set.intersection inProgressPromoteWarm inProgressDemoteWarm))
   where
     knownPeersSet       = KnownPeers.toSet knownPeers
@@ -415,7 +415,7 @@ assertPeerSelectionState PeerSelectionState{..} =
     activePeersSet      = activePeers
     coldPeersSet        = knownPeersSet Set.\\ establishedPeersSet
     warmPeersSet        = establishedPeersSet Set.\\ activePeersSet
-    hotPeersSet         = activePeersSet
+    -- hotPeersSet         = activePeersSet
 
 
 -- | A view of the status of each established peer, for testing and debugging.
