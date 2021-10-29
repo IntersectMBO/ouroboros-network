085026f090557321b7503293b7bebe96e3541bac
Damian Nadales <damian.nadales@iohk.io>
2021-10-29 13:43:32 +0200

    Add ledger state hydration and DB Changelog extension functions.

--- ouroboros-consensus/src/Ouroboros/Consensus/Storage/LedgerDB/Hybrid.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Storage/LedgerDB/Hybrid.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
 
@@ -9,30 +12,39 @@ module Ouroboros.Consensus.Storage.LedgerDB.Hybrid (
   , DiskDb
   , readDb
   , writeDb
-    -- * Ledger interface
+    -- * Ledger interface.
   , getTableKeysetsForBlock
+    -- * Ledger state hydration.
+  , hydrateLedgerState2
+    -- * DB Changelog extension.
+  , extendDbChangelog
   ) where
 
-class HasOnDiskTables state
+class HasOnDiskTables (state :: (TableType -> * -> * -> *) -> *)
 
-data DbChangelog state
+data DbChangelog (state :: (TableType -> * -> * -> *) -> *)
+data TableType
 
-data TableKeySets state
+data TableKeySets (state :: (TableType -> * -> * -> *) -> *)
 
-data AnnTableKeySets state a
+data AnnTableKeySets (state :: (TableType -> * -> * -> *) -> *) a
 
-data SeqNo state
+data SeqNo (state :: (TableType -> * -> * -> *) -> *)
 
-data TableReadSets state
+data TableReadSets (state :: (TableType -> * -> * -> *) -> *)
 
-data AnnTableReadSets state a
+data AnnTableReadSets (state :: (TableType -> * -> * -> *) -> *) a
 
 type KeySetSanityInfo state = Maybe (SeqNo state)
 type ReadSetSanityInfo state = (KeySetSanityInfo state, SeqNo state)
 
-data TableDiffs state
+data TableDiffs (state :: (TableType -> * -> * -> *) -> *)
 
-data TableSnapshots state
+data TableDiff (t :: TableType) k v
+
+data TableSnapshots (state :: (TableType -> * -> * -> *) -> *)
+
+data EmptyTable (t :: TableType) k v
 
 --------------------------------------------------------------------------------
 -- Operations on keysets.
@@ -46,7 +58,7 @@ rewindTableKeySets ::
 rewindTableKeySets = undefined
 
 forwardTableReadSets ::
-    forall state . HasOnDiskTables state
+      HasOnDiskTables state
   => DbChangelog state
   -> AnnTableReadSets state (ReadSetSanityInfo state)
   -> Maybe (TableReadSets state)
@@ -72,11 +84,64 @@ class DiskDb dbhandle state where
 --------------------------------------------------------------------------------
 
 getTableKeysetsForBlock
-  ::  blk
-  -> state
-  -- ^ This should be a recent state. TODO: explain why.
+  :: forall (state :: (TableType -> * -> * -> *) -> *) blk
+  . blk
+  -> state EmptyTable
+  -- ^ This should be a recent state.
+  --
+  -- TODO: Is it correct to use EmptyTable here?
+  --
+  -- TODO: explain why. This might have to do with incremental computations. if
+  --       the ledger thinks that no-incremental computation should take place
+  --       but it should then the keys the ledger needs won't be in the
+  --       resulting TableKeySets.
   --
   -- TODO: what is the exact type we need here? I can't imagine we use the
-  -- actual ledger state. Would the ledger require a 'DbChangelog' instead?
+  --       actual ledger state. Would the ledger require a 'DbChangelog'
+  --       instead?
   -> TableKeySets state
 getTableKeysetsForBlock = undefined
+
+-- TODO: how to reconcile 'TableKeySets state' and 'Set (TxIn (Crypto era))'.
+
+--------------------------------------------------------------------------------
+-- Ledger state hydration
+--
+-- These are the functions that we'll need to prepare the ledger state for block
+-- application.
+--------------------------------------------------------------------------------
+
+type UnforwardedTableReadSets state = AnnTableReadSets state (ReadSetSanityInfo state)
+data TrackingTable (t :: TableType) k v
+
+hydrateLedgerState1 ::
+     TableReadSets state
+  -> DbChangelog state
+  -> state TrackingTable
+hydrateLedgerState1  = undefined
+
+hydrateLedgerState2 ::
+     HasOnDiskTables state
+  => UnforwardedTableReadSets state
+  -> DbChangelog state
+  -> Maybe (state TrackingTable)
+hydrateLedgerState2 utrs dbcl = do
+  trs <- forwardTableReadSets dbcl utrs
+  pure $ hydrateLedgerState1 trs dbcl
+
+--------------------------------------------------------------------------------
+-- DB Changelog extension
+--------------------------------------------------------------------------------
+
+-- | Apply a state change to the changelog.
+extendDbChangelog :: forall state.
+                     HasOnDiskTables state
+                  => SeqNo state
+                  -- ^ ???
+                  -> state TableDiff
+                  -- ^ ??? This be derived from the ledger state, right
+                  -> Maybe (TableSnapshots state)
+                  -- ^ ???
+                  -> DbChangelog state
+                  -> DbChangelog state
+extendDbChangelog = undefined
