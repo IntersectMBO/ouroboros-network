71269bc71dc952f54646b4b14b4c2229ec5b27ac
Nicolas Frisby <nick.frisby@iohk.io>
2022-01-27 16:37:06 -0800

    WIP extract BackingStore interface

--- ouroboros-consensus/src/Ouroboros/Consensus/Storage/LedgerDB/HD.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/Storage/LedgerDB/HD.hs
@@ -31,12 +31,6 @@ module Ouroboros.Consensus.Storage.LedgerDB.HD (
   , forwardValues
   , mapRewoundKeys
   , rewindKeys
-    -- * Backing store interface
-  , BackingStore (..)
-  , BackingStorePath (..)
-  , BackingStoreValueHandle (..)
-  , bsRead
-  , withBsValueHandle
     -- ** An in-memory backing store
   , newTVarBackingStore
   , TVarBackingStoreClosedExn (..)
@@ -79,6 +73,8 @@ import qualified Ouroboros.Consensus.Storage.FS.API as FS
 import qualified Ouroboros.Consensus.Storage.FS.API.Types as FS
 import           Ouroboros.Consensus.Util.IOLike (IOLike)
 import qualified Ouroboros.Consensus.Util.IOLike as IOLike
+
+import           Ouroboros.Consensus.Storage.LedgerDB.HD.BackingStore
 
 {-------------------------------------------------------------------------------
   Map of values
@@ -286,71 +282,6 @@ forwardValues (UtxoValues values) (UtxoDiff diffs) =
       UedsDel       -> Nothing
       UedsIns       -> error "impossible! duplicate insert of key"
       UedsInsAndDel -> error "impossible! duplicate insert of key"
-
-{-------------------------------------------------------------------------------
-  Backing store interface
--------------------------------------------------------------------------------}
-
--- | A backing store for a map
---
--- TODO Do we also need a backup and restore mechanism? Or does any disk
--- corruption mean the node will always need to sync entirely from scratch?
-data BackingStore m keys values diff = BackingStore {
-    -- | Close the backing store
-    --
-    -- Other methods throw exceptions if called on a closed store.
-    bsClose       :: m ()
-    -- | Create a persistent copy
-    --
-    -- Each backing store implementation will offer a way to initialize itself
-    -- from such a path.
-  , bsCopy        :: FS.SomeHasFS m -> BackingStorePath -> m ()
-    -- | Open a 'BackingStoreValueHandle' capturing the current value of the
-    -- entire database
-  , bsValueHandle :: m (WithOrigin SlotNo, BackingStoreValueHandle m keys values)
-    -- | Apply a valid diff to the contents of the backing store
-  , bsWrite       :: SlotNo -> diff -> m ()
-  }
-
-newtype BackingStorePath = BackingStorePath FS.FsPath
-
--- | An ephemeral handle to an immutable value of the entire database
---
--- The performance cost is usually minimal unless this handle is held open too
--- long.
-data BackingStoreValueHandle m keys values = BackingStoreValueHandle {
-    -- | Close the handle
-    --
-    -- Other methods throw exceptions if called on a closed handle.
-    bsvhClose :: m ()
-    -- | Read the given keys from the handle
-    --
-    -- Absent keys will merely not be present in the result instead of causing a
-    -- failure or an exception.
-  , bsvhRead  :: keys -> m values
-  }
-
--- | A combination of 'bsValueHandle' and 'bsvhRead'
-bsRead ::
-     IOLike m
-  => BackingStore m keys values diff
-  -> keys
-  -> m (WithOrigin SlotNo, values)
-bsRead store keys = withBsValueHandle store $ \slot vh -> do
-    values <- bsvhRead vh keys
-    pure (slot, values)
-
--- | A 'IOLike.bracket'ed 'bsValueHandle'
-withBsValueHandle ::
-     IOLike m
-  => BackingStore m keys values diff
-  -> (WithOrigin SlotNo -> BackingStoreValueHandle m keys values -> m a)
-  -> m a
-withBsValueHandle store kont =
-    IOLike.bracket
-      (bsValueHandle store)
-      (bsvhClose . snd)
-      (uncurry kont)
 
 {-------------------------------------------------------------------------------
   An in-memory backing store
