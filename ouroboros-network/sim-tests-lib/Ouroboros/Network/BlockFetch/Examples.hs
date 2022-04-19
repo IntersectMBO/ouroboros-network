8866197a304e3b3a438f6a24ce05548a7c0d40a0
Marcin Szamotulski <coot@coot.me>
2022-04-19 22:26:11 +0200

    block-fetch tests: fixed termination tests
    The first assertion of `fetchDecisionsForStateSnapshot` was triggered.
    We need first kill the fetch thread before the client side terminates.

--- ouroboros-network/sim-tests-lib/Ouroboros/Network/BlockFetch/Examples.hs
+++ ouroboros-network/sim-tests-lib/Ouroboros/Network/BlockFetch/Examples.hs
@@ -14,6 +14,7 @@ module Ouroboros.Network.BlockFetch.Examples
 
 import Codec.Serialise (Serialise (..))
 import Data.ByteString.Lazy qualified as LBS
+import Data.Foldable (traverse_)
 import Data.List as List (foldl')
 import Data.Map (Map)
 import Data.Map.Strict qualified as Map
@@ -38,11 +39,10 @@ import Ouroboros.Network.Block
 
 import Network.TypedProtocol.Peer.Client
 
-import Ouroboros.Network.ControlMessage (ControlMessageSTM)
-
 import Ouroboros.Network.BlockFetch
 import Ouroboros.Network.BlockFetch.Client
 import Ouroboros.Network.Channel
+import Ouroboros.Network.ControlMessage
 import Ouroboros.Network.DeltaQ
 import Ouroboros.Network.Driver
 import Ouroboros.Network.NodeToNode (NodeToNodeVersion (..))
@@ -100,11 +100,14 @@ blockFetchExample0 decisionTracer clientStateTracer clientMsgTracer
       labelThread threadId "driver"
       driver blockHeap
 
-    -- Order of shutdown here is important for this example: must kill off the
-    -- fetch thread before the peer threads.
-    _ <- waitAnyCancel $ [ fetchAsync, driverAsync,
-                           clientAsync, serverAsync,
-                           syncClientAsync, keepAliveAsync]
+    -- Order of shutdown is important for this example: must kill the fetch
+    -- thread before the peer threads, or otherwise the first assertion in
+    -- `fetchDecisionsForStateSnapshot` can be triggered.
+    atomically $ controlMessageSTM >>= check . (Terminate ==)
+    cancel fetchAsync
+    _ <- waitAnyCancel [ driverAsync, clientAsync, serverAsync
+                       , syncClientAsync, keepAliveAsync
+                       ]
     return ()
 
   where
