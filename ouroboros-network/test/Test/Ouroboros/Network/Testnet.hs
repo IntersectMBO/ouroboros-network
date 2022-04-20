3861d5a13cd3285030d7c977e575902b80892666
Armando Santos <armando@well-typed.com>
2022-04-20 10:53:38 +0100

    Fix comments

--- ouroboros-network/test/Test/Ouroboros/Network/Testnet.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/Testnet.hs
@@ -413,9 +413,6 @@ prop_diffusion_target_established_local defaultBearerInfo diffScript =
 -- the logs for all nodes running will all appear in the trace and the test
 -- property should only be valid while a given node is up and running.
 --
--- We do not need separate above and below variants of this property since it
--- is not possible to exceed the target.
---
 prop_diffusion_target_active_below :: AbsBearerInfo
                                    -> DiffusionScript
                                    -> Property
@@ -568,9 +565,6 @@ prop_diffusion_target_active_below defaultBearerInfo diffScript =
 -- the logs for all nodes running will all appear in the trace and the test
 -- property should only be valid while a given node is up and running.
 --
--- We do not need separate above and below variants of this property since it
--- is not possible to exceed the target.
---
 prop_diffusion_target_active_local_above :: AbsBearerInfo
                                          -> DiffusionScript
                                          -> Property
@@ -694,9 +688,6 @@ prop_diffusion_target_active_local_above defaultBearerInfo diffScript =
 -- but for running on Diffusion. This means it has to have in consideration
 -- that the logs for all nodes running will all appear in the trace and the test
 -- property should only be valid while a given node is up and running.
---
--- We do not need separate above and below variants of this property since it
--- is not possible to exceed the target.
 --
 prop_diffusion_cm_valid_transitions :: AbsBearerInfo
                                     -> DiffusionScript
