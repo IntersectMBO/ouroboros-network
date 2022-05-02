fac87cdf794c48b31652e588bf7a143b490521fd
Alexander Esgen <alexander.esgen@iohk.io>
2022-05-02 13:46:37 +0200

    Update HLS to 1.7.0.0

--- nix/hls.nix
+++ nix/hls.nix
@@ -8,7 +8,20 @@ let
   planConfig = {
     compiler-nix-name = pkgs.localConfig.ghcVersion;
     # get the sources from niv
-    src = sources.hls-released;
+    src = pkgs.applyPatches {
+      name = "hls";
+      src = sources.hls-released;
+      # see https://github.com/input-output-hk/haskell.nix/pull/1457
+      patches = [
+        (pkgs.fetchpatch {
+          url =
+            "https://raw.githubusercontent.com/input-output-hk/haskell.nix/4ee7270856a6ba4617c7e51a6c619f365faad894/patches/ghcide-1.7-unboxed-tuple-fix-issue-1455.patch";
+          sha256 = "118cy4jkfhhwxr0bc0g56l4vyp3svdck9cw1r8jc9nlv898iflyp";
+          stripLen = 1;
+          extraPrefix = "ghcide/";
+        })
+      ];
+    };
     configureArgs = "--disable-benchmarks";
     modules = [{ enableSeparateDataOutput = true; }];
   };
