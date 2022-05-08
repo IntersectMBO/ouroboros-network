8df123be3917fd7ec37ff055e7d293fadc8f79e2
Marcin Szamotulski <coot@coot.me>
2022-05-08 10:19:29 +0200

    ouroboros-protocol-tests: stateful cborg utilities

--- ouroboros-network-protocols/testlib/Test/Ouroboros/Network/Testing/Utils.hs
+++ ouroboros-network-protocols/testlib/Test/Ouroboros/Network/Testing/Utils.hs
@@ -11,6 +11,7 @@ import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy qualified as LBS
 
 import Network.TypedProtocol.Codec
+import Network.TypedProtocol.Stateful.Codec qualified as Stateful
 
 import Test.QuickCheck
 
@@ -52,6 +53,44 @@ prop_codec_valid_cbor_encoding
   -> Property
 prop_codec_valid_cbor_encoding Codec {encode} (AnyMessageAndAgency stok msg) =
     case deserialise [] (encode stok msg) of
+      Left  e     -> counterexample (show e) False
+      Right terms -> property (CBOR.validFlatTerm terms)
+  where
+    deserialise :: [CBOR.TermToken]
+                -> ByteString
+                -> Either CBOR.DeserialiseFailure [CBOR.TermToken]
+    deserialise !as bs =
+      case CBOR.deserialiseFromBytes CBOR.decodeTermToken bs of
+        Left e -> Left e
+        Right (bs', a) | LBS.null bs'
+                       -> Right (reverse (a : as))
+                       | otherwise
+                       -> deserialise (a : as) bs'
+
+prop_codec_st_cborM
+  :: forall ps f m.
+     ( Monad m
+     , Eq (AnyMessage ps)
+     )
+  => Stateful.Codec ps CBOR.DeserialiseFailure f m LBS.ByteString
+  -> Stateful.AnyMessage ps f
+  -> m Bool
+prop_codec_st_cborM codec (Stateful.AnyMessage _ f' msg)
+    = case CBOR.deserialiseFromBytes CBOR.decodeTerm $ Stateful.encode codec f' msg of
+        Left _err               -> return False
+        Right (leftover, _term) -> return $ LBS.null leftover
+
+-- | This property checks that the encoder is producing a valid CBOR.  It
+-- encodes to 'ByteString' using 'encode' and decodes a 'FlatTerm' from the
+-- bytestring which is the fed into 'CBOR.validFlatTerm'.
+--
+prop_codec_st_valid_cbor_encoding
+  :: forall ps f.
+     Stateful.Codec ps CBOR.DeserialiseFailure f IO ByteString
+  -> Stateful.AnyMessage ps f
+  -> Property
+prop_codec_st_valid_cbor_encoding Stateful.Codec {Stateful.encode} (Stateful.AnyMessage _ f msg) =
+    case deserialise [] (encode f msg) of
       Left  e     -> counterexample (show e) False
       Right terms -> property (CBOR.validFlatTerm terms)
   where
