7a3c3093bf1bf4d2e8bb40173aeb43c4eccf59db
Alexander Esgen <alexander.esgen@iohk.io>
2022-05-17 14:24:28 +0200

    Set locale for stylish-haskell
    This hopefully fixes errors like
    
    > stylish-haskell: ./path/to/file.hs: commitBuffer: invalid argument (invalid character)

--- scripts/ci/check-stylish.sh
+++ scripts/ci/check-stylish.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
+export LC_ALL=C.UTF-8
 # TODO the export of the <= operator TxLimits crashes stylish-haskell
 fd -p ouroboros-consensus -e hs -E Setup.hs -E ouroboros-consensus/src/Ouroboros/Consensus/Mempool/TxLimits.hs -X stylish-haskell -c .stylish-haskell.yaml -i
