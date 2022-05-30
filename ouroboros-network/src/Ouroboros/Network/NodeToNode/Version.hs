fe363f34acf5a81cba16b85d19c36a28b667fb95
Marcin Szamotulski <coot@coot.me>
2022-05-30 09:58:31 +0200

    Improved DiffusionMode haddocks

--- ouroboros-network/src/Ouroboros/Network/NodeToNode/Version.hs
+++ ouroboros-network/src/Ouroboros/Network/NodeToNode/Version.hs
@@ -69,9 +69,19 @@ nodeToNodeVersionCodec = CodecCBORTerm { encodeTerm, decodeTerm }
 
 
 
--- | The flag which indicates wheather the node runs only initiator or both
--- initiator or responder node.   It does not however specify weather the node
--- is using duplex connections, this is implicit see 'NodeToNodeV_4'
+-- | The flag which indicates whether the node runs only initiator or both
+-- initiator or responder node.
+--
+-- This data structure has two proposes:
+--
+-- * instruct the diffusion layer if it should listen on incoming connections;
+--
+-- * it is communicated via 'NodeToNodeVersionData' during handshake
+--   negotiation. In non-p2p mode we always send 'InitiatorOnlyDiffusionMode',
+--   in p2p mode we send exactly what the diffusion is given.  In non-p2p mode
+--   every connection outbound port is ephemeral, the remote side cannot connect
+--   to it, however in p2p mode the outbound port is actually the port on which
+--   the node is listening (if it runs in 'InitiatorAndResponderDiffusionMode').
 --
 data DiffusionMode
     = InitiatorOnlyDiffusionMode
