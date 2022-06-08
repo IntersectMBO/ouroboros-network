041ffda02bfaf60d77cfe05965fa94b3ad68a113
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-06-08 21:13:47 +0300

    Update cardano-base and cardano-ledger. Removed VRF10 crypto

--- ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Crypto.hs
+++ ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Crypto.hs
@@ -2,28 +2,6 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 -- | Module defining the crypto primitives used throughout Shelley based eras.
-module Ouroboros.Consensus.Shelley.Crypto (
-    StandardCrypto
-  , VRF10Crypto
-  ) where
+module Ouroboros.Consensus.Shelley.Crypto (StandardCrypto) where
 
-import           Cardano.Crypto.DSIGN (Ed25519DSIGN)
-import           Cardano.Crypto.Hash (Blake2b_256)
-import           Cardano.Crypto.KES (CompactSum7KES)
-import           Cardano.Crypto.VRF.PraosBatchCompat (PraosBatchCompatVRF)
-import           Cardano.Ledger.Crypto (Crypto (..), StandardCrypto)
-
--- | Crypto to be used from the Babbage era onwards. This incorporates a few
--- changes from 'StandardCrypto':
---
--- - Update to IETF VRF draft 10
--- - Use a smaller KES proof
-
-data VRF10Crypto
-
-instance Crypto VRF10Crypto where
-  type ADDRHASH _ = ADDRHASH StandardCrypto
-  type HASH _ = HASH StandardCrypto
-  type DSIGN _ = DSIGN StandardCrypto
-  type VRF _ = PraosBatchCompatVRF
-  type KES _ = CompactSum7KES Ed25519DSIGN Blake2b_256
+import           Cardano.Ledger.Crypto (StandardCrypto)
