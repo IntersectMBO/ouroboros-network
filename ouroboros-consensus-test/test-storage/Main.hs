2bbf3999d47d0aee43417a97c425bac170a0fee8
Damian Nadales <damian.only@gmail.com>
2022-07-15 14:19:28 +0200

    Add a heartbeat command to prevent CI from aborting
    We how print a character every 30 seconds in the storage tests. This is
    needed because the `OnDisk` tests take longer due to the introduction of
    UTxO HD.

--- ouroboros-consensus-test/test-storage/Main.hs
+++ ouroboros-consensus-test/test-storage/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE NumericUnderscores #-}
 module Main (main) where
 
+import           Control.Concurrent (threadDelay)
+import           Control.Concurrent.Async (race_)
+import           Control.Monad (forever)
 import qualified System.Directory as Dir
+import           System.IO (hFlush, stdout)
 import           System.IO.Temp
 import           Test.Tasty
 
@@ -8,10 +13,13 @@ import qualified Test.Ouroboros.Storage
 import           Test.Util.TestEnv
 
 main :: IO ()
-main = do
-  sysTmpDir <- Dir.getTemporaryDirectory
-  withTempDirectory sysTmpDir "cardano-s-m" $ \tmpDir ->
-    defaultMainWithTestEnv defaultTestEnvConfig (tests tmpDir)
+main = runTests `race_` heartbeat
+  where
+    runTests = do
+      sysTmpDir <- Dir.getTemporaryDirectory
+      withTempDirectory sysTmpDir "cardano-s-m" $ \tmpDir ->
+        defaultMainWithTestEnv defaultTestEnvConfig (tests tmpDir)
+    heartbeat = forever $ threadDelay (30 * 1_000_000)  >> putChar '.' >> hFlush stdout
 
 tests :: FilePath -> TestTree
 tests tmpDir =
