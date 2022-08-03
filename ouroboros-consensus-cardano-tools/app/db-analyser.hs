57f62f9fc33e033ce460184deaf9a48ebda2208c
Michael Karg <michael.karg@iohk.io>
2022-08-03 12:00:22 +0200

    cardano-tools: provide command line options and usage in comments
    (cherry picked from commit 56d049d6bc4ccf6fefe03ae23c9f66990af24bbf)

--- ouroboros-consensus-cardano-tools/app/db-analyser.hs
+++ ouroboros-consensus-cardano-tools/app/db-analyser.hs
@@ -3,6 +3,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | Database analysis tool.
+--
+-- Usage: db-analyser --db PATH [--verbose]
+--                    [--only-immutable-db [--analyse-from SLOT_NUMBER]]
+--                    [--validate-all-blocks | --minimum-block-validation] COMMAND
+--                    [--show-slot-block-no | --count-tx-outputs |
+--                      --show-block-header-size | --show-block-txs-size |
+--                      --show-ebbs | --store-ledger SLOT_NUMBER | --count-blocks |
+--                      --checkThunks BLOCK_COUNT | --trace-ledger |
+--                      --repro-mempool-and-forge INT]
+--                    [--num-blocks-to-process INT]
 module Main (main) where
 
 import           DBAnalyser.Parsers
--- ouroboros-consensus-cardano-tools/app/db-synthesizer.hs
+++ ouroboros-consensus-cardano-tools/app/db-synthesizer.hs
@@ -1,4 +1,25 @@
--- | This tool aims to synthesize a valid ChainDB, replicating cardano-node's UX
+-- | This tool synthesizes a valid ChainDB, replicating cardano-node's UX
+--
+-- Usage: db-synthesizer --config FILE --db PATH
+--                       [--shelley-operational-certificate FILE]
+--                       [--shelley-vrf-key FILE] [--shelley-kes-key FILE]
+--                       [--bulk-credentials-file FILE]
+--                       ((-s|--slots NUMBER) | (-b|--blocks NUMBER) |
+--                         (-e|--epochs NUMBER)) [-f]
+--
+-- Available options:
+--   --config FILE            Path to the node's config.json
+--   --db PATH                Path to the Chain DB
+--   --shelley-operational-certificate FILE
+--                            Path to the delegation certificate
+--   --shelley-vrf-key FILE   Path to the VRF signing key
+--   --shelley-kes-key FILE   Path to the KES signing key
+--   --bulk-credentials-file FILE
+--                            Path to the bulk credentials file
+--   -s,--slots NUMBER        Amount of slots to process
+--   -b,--blocks NUMBER       Amount of blocks to forge
+--   -e,--epochs NUMBER       Amount of epochs to process
+--   -f                       Force overwrite an existing Chain DB
 module Main (main) where
 
 import           System.Exit
