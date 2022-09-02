6a0589d835297b697f92d2041fc58acff97d5b58
Mark Tullsen <tullsen@galois.com>
2022-09-02 00:41:38 -0700

    galois consensus documentation: remove historical artifacts

--- ouroboros-consensus-test/test-examples/Main.hs
+++ ouroboros-consensus-test/test-examples/Main.hs
@@ -1,29 +1,7 @@
 module Main (main) where
 
--- pkgs tasty*:
-import           Test.Tasty
-import           Test.Tasty.QuickCheck
-
--- local modules:
-import           Test.Tutorial
-import           Test.Tutorial2
-import           Test.Example
-import           Test.ToyLedgerC
-import           Test.ToyLedgerD
-import           Test.ToyLedger1 -- defunct?
+import           Test.Tutorial  ()
+import           Test.Tutorial2 ()
 
 main :: IO ()
-main = defaultMain test
-
--- FIXME: add real [quickcheck] tests, ideally in separate modules.
-
-test :: TestTree
-test =
-  testGroup "ouroboros-examples"
-  [ testProperty "prop_example1" prop_example1
-  ]
-  where
-  prop_example1 :: Bool
-  prop_example1 = True
-  
-
+main = putStrLn "hello world"
