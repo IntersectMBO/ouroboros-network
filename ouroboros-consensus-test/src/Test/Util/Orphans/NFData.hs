350c5ab8b139724b5bb4fa7579eef6b178057bf6
Joris Dral <joris@well-typed.com>
2022-09-12 16:48:02 +0200

    Part 3: Benchmark sub-cached (aka: "legacy") implementation.

--- ouroboros-consensus-test/src/Test/Util/Orphans/NFData.hs
+++ ouroboros-consensus-test/src/Test/Util/Orphans/NFData.hs
@@ -11,6 +11,10 @@ import           Control.DeepSeq (NFData (..))
 import           Data.Foldable
 
 import qualified Data.FingerTree.Strict as FT
+import           Ouroboros.Consensus.Storage.LedgerDB.HD (SeqUtxoDiff (..),
+                     SudElement (..), SudMeasure (..), UtxoDiff (..),
+                     UtxoEntryDiff (..), UtxoEntryDiffState (..), UtxoKeys (..),
+                     UtxoValues (..))
 
 {------------------------------------------------------------------------------
   StrictFingerTree
@@ -20,4 +24,20 @@ import qualified Data.FingerTree.Strict as FT
 instance (NFData a, NFData v, FT.Measured v a)
       => NFData (FT.StrictFingerTree v a) where
   rnf ft = rnf (FT.measure ft) `seq` rnf (toList ft)
+
+{-------------------------------------------------------------------------------
+  SeqUtxoDiff
+-------------------------------------------------------------------------------}
+
+deriving newtype instance (NFData k, NFData v) => NFData (UtxoValues k v)
+
+deriving newtype instance NFData k => NFData (UtxoKeys k v)
+
+deriving anyclass instance NFData UtxoEntryDiffState
+deriving anyclass instance NFData v => NFData (UtxoEntryDiff v)
+deriving newtype instance (NFData k, NFData v) => NFData (UtxoDiff k v)
+
+deriving anyclass instance (NFData k, NFData v) => NFData (SudElement k v)
+deriving anyclass instance (NFData k, NFData v) => NFData (SudMeasure k v)
+deriving newtype instance (Ord k, NFData k, NFData v) => NFData (SeqUtxoDiff k v)
 
