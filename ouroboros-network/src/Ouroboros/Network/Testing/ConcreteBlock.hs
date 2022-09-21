d2ab4d94c86e54e08926110248f3e380a1b41379
John Ky <john.ky@iohk.io>
2022-09-22 09:05:46 +1000

    stuff

--- ouroboros-network/src/Ouroboros/Network/Testing/ConcreteBlock.hs
+++ ouroboros-network/src/Ouroboros/Network/Testing/ConcreteBlock.hs
@@ -121,7 +121,7 @@ deriving instance Hashable BlockNo
 --
 -- This requires @UndecidableInstances@ because @Hashable (HeaderHash b)@
 -- is no smaller than @Hashable (ChainHash b)@.
-instance Hashable (HeaderHash b) => Hashable (ChainHash b)
+instance (StandardHash b, Hashable (HeaderHash b)) => Hashable (ChainHash b)
  -- use generic instance
 
 -- | The hash of all the information in a 'BlockHeader'.
