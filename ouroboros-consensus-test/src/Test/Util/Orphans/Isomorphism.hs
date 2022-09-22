801d6fca2895b268b7fce09b3b03bb783caa6221
Joris Dral <joris@well-typed.com>
2022-09-22 11:14:03 +0200

    Update downstream dependencies in `ouroboros-consensus-test`.

--- ouroboros-consensus-test/src/Test/Util/Orphans/Isomorphism.hs
+++ ouroboros-consensus-test/src/Test/Util/Orphans/Isomorphism.hs
@@ -14,13 +14,15 @@ module Test.Util.Orphans.Isomorphism (
   ) where
 
 import           Data.Foldable (toList)
+import           Data.Maybe (fromJust)
 import           Data.Proxy
-import qualified Data.Sequence as Seq
+import           Data.Semigroupoid
+import           Data.Sequence (Seq (..))
+import           Data.Sequence.NonEmpty (NESeq (..))
 
 import qualified Data.FingerTree.RootMeasured.Strict as RMFT
 import qualified Data.FingerTree.Strict as FT
 import qualified Data.Map.Diff.Strict as MapDiff
-import qualified Data.Map.Diff.Strict.Internal as Internal
 
 import qualified Ouroboros.Consensus.Block as Block
 import qualified Ouroboros.Consensus.Storage.LedgerDB.HD as HD
@@ -83,25 +85,31 @@ instance (Ord k, Eq v)
       to' (HD.SudElement slot d) = DS.Element (to slot) (to d)
 
 instance Eq v => Isomorphism (MapDiff.Diff k v) (HD.UtxoDiff k v) where
-  to (Internal.Diff m) = HD.UtxoDiff (fmap to' m)
+  to (MapDiff.Diff m) = HD.UtxoDiff (fmap to' m)
     where
       to' = \case
-        Internal.DiffHistory (_xs Seq.:|> x) ->
+        MapDiff.NEDiffHistory (Empty :|> x :||> y) ->
+          to'' x <> to'' y
+        MapDiff.NEDiffHistory (Empty :||> x) ->
           to'' x
-        _ ->
-          error "A DiffHistory is isomorphic to a UtxoEntryDiff under the \
-                \ assumption that diff histories contain exactly one element."
+        _ -> error "A DiffHistory is isomorphic to a UtxoEntryDiff under the \
+                   \ assumption that diff histories contain exactly one      \
+                   \ insert, exactly one delete or exactly an insert AND a   \
+                   \ delete."
       to'' = \case
-        MapDiff.Insert v -> HD.UtxoEntryDiff v HD.UedsIns
-        MapDiff.Delete v -> HD.UtxoEntryDiff v HD.UedsDel
+        MapDiff.Insert v            -> HD.UtxoEntryDiff v HD.UedsIns
+        MapDiff.Delete v            -> HD.UtxoEntryDiff v HD.UedsDel
+        MapDiff.UnsafeAntiInsert _v -> error "UnsafeAntiInsert found."
+        MapDiff.UnsafeAntiDelete _v -> error "UnsafeAntiDelete found."
 
 instance Eq v => Isomorphism (HD.UtxoDiff k v) (MapDiff.Diff k v) where
-  to (HD.UtxoDiff m) = Internal.Diff $ fmap to' m
+  to (HD.UtxoDiff m) = MapDiff.Diff $ fmap to' m
     where
       to' (HD.UtxoEntryDiff x st) = case st of
         HD.UedsIns       -> MapDiff.singletonInsert x
         HD.UedsDel       -> MapDiff.singletonDelete x
-        HD.UedsInsAndDel -> MapDiff.singletonInsert x <> MapDiff.singletonDelete x
+        HD.UedsInsAndDel ->
+          fromJust (MapDiff.singletonInsert x <>? MapDiff.singletonDelete x)
 
 instance Isomorphism (MapDiff.Values k v) (HD.UtxoValues k v) where
   to (MapDiff.Values m) = HD.UtxoValues m
