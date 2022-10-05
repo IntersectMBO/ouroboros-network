4b08411824ba0e85ef20ee9982579b009299eeb8
Marcin Szamotulski <coot@coot.me>
2022-10-05 11:45:00 +0200

    peer-selection: added localRoots to PeerSelectionCounters

--- ouroboros-network/test/Test/Ouroboros/Network/Testnet.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/Testnet.hs
@@ -1985,7 +1985,7 @@ selectDiffusionPeerSelectionState :: Eq a
 selectDiffusionPeerSelectionState f =
     Signal.nub
   -- TODO: #3182 Rng seed should come from quickcheck.
-  . Signal.fromChangeEvents (f $ Governor.emptyPeerSelectionState $ mkStdGen 42)
+  . Signal.fromChangeEvents (f $ Governor.emptyPeerSelectionState (mkStdGen 42) [])
   . Signal.selectEvents
       (\case
         DiffusionDebugPeerSelectionTrace (TraceGovernorState _ _ st) -> Just (f st)
