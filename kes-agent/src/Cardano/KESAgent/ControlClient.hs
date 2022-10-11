a76bd0206fab7179bbab03db0b48ec101168bc05
Tobias Dammers <tdammers@gmail.com>
2022-10-11 17:14:31 +0200

    Clients

---
+++ kes-agent/src/Cardano/KESAgent/ControlClient.hs
@@ -0,0 +1,63 @@
+{-#LANGUAGE TypeApplications #-}
+{-#LANGUAGE FlexibleContexts #-}
+{-#LANGUAGE ScopedTypeVariables #-}
+{-#LANGUAGE DeriveAnyClass #-}
+
+-- | Clients for the KES Agent.
+module Cardano.KESAgent.ControlClient
+where
+
+import Cardano.KESAgent.Driver (driver)
+import Cardano.KESAgent.Peers (kesPusher, kesReceiver)
+import Cardano.KESAgent.Protocol
+import Cardano.Crypto.DirectSerialise
+
+import Cardano.Crypto.KES.Class
+
+import System.Socket (socket, SocketException, close, connect)
+import System.Socket.Type.Stream
+import System.Socket.Family.Unix
+import System.Socket.Protocol.Default
+import Network.TypedProtocol.Driver (runPeerWithDriver)
+import Data.Proxy (Proxy (..))
+
+import Control.Monad (forever, void)
+import Control.Exception (Exception, bracket, catch, throw)
+
+data ControlClientOptions =
+  ControlClientOptions
+    { controlClientSocketAddress :: SocketAddress Unix
+    }
+
+-- | Used to terminate a client session when we're done.
+data ControlClientTermination =
+  ControlClientTermination
+  deriving (Show, Exception)
+
+-- | A simple control client: push one KES key, then exit.
+runControlClient1 :: forall k
+                   . KESSignAlgorithm IO k
+                  => DirectDeserialise (SignKeyKES k)
+                  => DirectSerialise (SignKeyKES k)
+                  => VersionedProtocol (KESProtocol k)
+                  => Proxy k
+                  -> ControlClientOptions
+                  -> SignKeyKES k
+                  -> IO ()
+runControlClient1 proxy options key = do
+  void $ bracket
+    (socket @Unix @Stream @Default)
+    (\s -> do
+      close s
+      putStrLn "Control client socket closed"
+    )
+    (\s -> do
+      connect s (controlClientSocketAddress options)
+      putStrLn $ "Control client connected to " ++ show (controlClientSocketAddress options)
+      void $ runPeerWithDriver
+        (driver s)
+        (kesPusher (return key) (throw ControlClientTermination))
+        ()
+    ) `catch`
+    (\(e :: ControlClientTermination) -> putStrLn "Control client terminated normally")
+
---
+++ kes-agent/src/Cardano/KESAgent/ServiceClient.hs
@@ -0,0 +1,54 @@
+{-#LANGUAGE TypeApplications #-}
+{-#LANGUAGE FlexibleContexts #-}
+{-#LANGUAGE ScopedTypeVariables #-}
+
+-- | Clients for the KES Agent.
+module Cardano.KESAgent.ServiceClient
+where
+
+import Cardano.KESAgent.Driver (driver)
+import Cardano.KESAgent.Peers (kesPusher, kesReceiver)
+import Cardano.KESAgent.Protocol
+import Cardano.Crypto.DirectSerialise
+
+import Cardano.Crypto.KES.Class
+
+import System.Socket (socket, SocketException, close, connect)
+import System.Socket.Type.Stream
+import System.Socket.Family.Unix
+import System.Socket.Protocol.Default
+import Network.TypedProtocol.Driver (runPeerWithDriver)
+import Data.Proxy (Proxy (..))
+
+import Control.Monad (forever, void)
+import Control.Exception (bracket, catch)
+
+data ServiceClientOptions =
+  ServiceClientOptions
+    { serviceClientSocketAddress :: SocketAddress Unix
+    }
+
+runServiceClient :: forall k
+                  . KESSignAlgorithm IO k
+                 => DirectDeserialise (SignKeyKES k)
+                 => DirectSerialise (SignKeyKES k)
+                 => VersionedProtocol (KESProtocol k)
+                 => Proxy k
+                 -> ServiceClientOptions
+                 -> (SignKeyKES k -> IO ())
+                 -> IO ()
+runServiceClient proxy options handleKey = do
+  void $ bracket
+    (socket @Unix @Stream @Default)
+    (\s -> do
+      close s
+      putStrLn "Service client socket closed"
+    )
+    (\s -> do
+      connect s (serviceClientSocketAddress options)
+      putStrLn $ "Service client connected to " ++ show (serviceClientSocketAddress options)
+      void $ runPeerWithDriver
+        (driver s)
+        (kesReceiver handleKey)
+        ()
+    )
