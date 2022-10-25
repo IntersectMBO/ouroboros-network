91e457ecca201b82438cf7c64089b316ebf981c7
Marcin Szamotulski <coot@coot.me>
2022-10-25 18:02:00 +0200

    DNSActions: refactor code
    Hide local bindings under 'ioDNSActions'.

--- ouroboros-network/src/Ouroboros/Network/PeerSelection/RootPeersDNS/DNSActions.hs
+++ ouroboros-network/src/Ouroboros/Network/PeerSelection/RootPeersDNS/DNSActions.hs
@@ -170,85 +170,44 @@ getResolver resolvConf = do
     DNS.withResolver rs return
 
 
--- |
+-- | IO DNSActions which resolve domain names with 'DNS.Resolver'.
 --
--- TODO: it could be useful for `publicRootPeersProvider`.
+-- The IPv4 and IPv6 addresses the node will be using should determine the
+-- LookupReqs so that we can avoid lookups for address types that wont be used.
 --
-resolverResource :: DNS.ResolvConf
-                 -> IO (Resource IO (DNSorIOError IOException) DNS.Resolver)
-resolverResource resolvConf = do
-    rs <- DNS.makeResolvSeed resolvConf
-    case DNS.resolvInfo resolvConf of
-      DNS.RCFilePath filePath ->
-        pure $ go filePath NoResolver
-
-      _ -> DNS.withResolver rs (pure . constantResource)
-
+ioDNSActions :: LookupReqs
+             -> DNSActions DNS.Resolver IOException IO
+ioDNSActions =
+    \reqs -> DNSActions {
+               dnsResolverResource      = resolverResource,
+               dnsAsyncResolverResource = asyncResolverResource,
+               dnsLookupWithTTL         = lookupWithTTL reqs
+             }
   where
-    handlers :: [ Handler IO (Either (DNSorIOError IOException) a) ]
-    handlers  = [ Handler $ pure . Left . IOError
-                , Handler $ pure . Left . DNSError
-                ]
+    -- |
+    --
+    -- TODO: it could be useful for `publicRootPeersProvider`.
+    --
+    resolverResource :: DNS.ResolvConf
+                     -> IO (Resource IO (DNSorIOError IOException) DNS.Resolver)
+    resolverResource resolvConf = do
+        rs <- DNS.makeResolvSeed resolvConf
+        case DNS.resolvInfo resolvConf of
+          DNS.RCFilePath filePath ->
+            pure $ go filePath NoResolver
 
-    go :: FilePath
-       -> TimedResolver
-       -> Resource IO (DNSorIOError IOException) DNS.Resolver
-    go filePath tr@NoResolver = Resource $
-      do
-        result
-          <- (curry Right
-               <$> getTimeStamp filePath
-               <*> getResolver resolvConf)
-             `catches` handlers
-        case result of
-          Left err ->
-            pure (Left err, go filePath tr)
-          Right (modTime, resolver) -> do
-            pure (Right resolver, go filePath (TimedResolver resolver modTime))
+          _ -> DNS.withResolver rs (pure . constantResource)
 
-    go filePath tr@(TimedResolver resolver modTime) = Resource $ do
-      result <- runExceptT $ do
-        modTime' <- ExceptT $ (Right <$> getTimeStamp filePath)
-                              `catches` handlers
-        if modTime' <= modTime
-          then return (resolver, modTime)
-          else do
-            resolver' <- ExceptT $ (Right <$> getResolver resolvConf)
-                                   `catches` handlers
-            return (resolver', modTime')
-      case result of
-        Left err ->
-          return (Left err, go filePath tr)
-        Right (resolver', modTime') ->
-          return (Right resolver', go filePath (TimedResolver resolver' modTime'))
+      where
+        handlers :: [ Handler IO (Either (DNSorIOError IOException) a) ]
+        handlers  = [ Handler $ pure . Left . IOError
+                    , Handler $ pure . Left . DNSError
+                    ]
 
-
--- | `Resource` which passes the 'DNS.Resolver' through a 'StrictTVar'.  Better
--- than 'resolverResource' when using in multiple threads.
---
-asyncResolverResource :: DNS.ResolvConf
-                      -> IO (Resource IO (DNSorIOError IOException)
-                                         DNS.Resolver)
-
-asyncResolverResource resolvConf =
-    case DNS.resolvInfo resolvConf of
-      DNS.RCFilePath filePath -> do
-        resourceVar <- newTVarIO NoResolver
-        pure $ go filePath resourceVar
-      _ -> do
-        constantResource <$> getResolver resolvConf
-  where
-    handlers :: [ Handler IO (Either (DNSorIOError IOException) a) ]
-    handlers  = [ Handler $ pure . Left . IOError
-                , Handler $ pure . Left . DNSError
-                ]
-
-    go :: FilePath -> StrictTVar IO TimedResolver
-       -> Resource IO (DNSorIOError IOException) DNS.Resolver
-    go filePath resourceVar = Resource $ do
-      r <- readTVarIO resourceVar
-      case r of
-        NoResolver ->
+        go :: FilePath
+           -> TimedResolver
+           -> Resource IO (DNSorIOError IOException) DNS.Resolver
+        go filePath tr@NoResolver = Resource $
           do
             result
               <- (curry Right
@@ -257,118 +216,158 @@ asyncResolverResource resolvConf =
                  `catches` handlers
             case result of
               Left err ->
-                pure (Left err, go filePath resourceVar)
+                pure (Left err, go filePath tr)
               Right (modTime, resolver) -> do
-                atomically (writeTVar resourceVar (TimedResolver resolver modTime))
-                pure (Right resolver, go filePath resourceVar)
+                pure (Right resolver, go filePath (TimedResolver resolver modTime))
 
-        TimedResolver resolver modTime -> do
+        go filePath tr@(TimedResolver resolver modTime) = Resource $ do
           result <- runExceptT $ do
             modTime' <- ExceptT $ (Right <$> getTimeStamp filePath)
                                   `catches` handlers
             if modTime' <= modTime
-              then return resolver
+              then return (resolver, modTime)
               else do
-              resolver' <- ExceptT $ (Right <$> getResolver resolvConf)
-                                     `catches` handlers
-              atomically (writeTVar (castStrictTVar resourceVar)
-                         (TimedResolver resolver' modTime'))
-              return resolver'
+                resolver' <- ExceptT $ (Right <$> getResolver resolvConf)
+                                       `catches` handlers
+                return (resolver', modTime')
           case result of
             Left err ->
-              return (Left err, go filePath resourceVar)
-            Right resolver' ->
-              return (Right resolver', go filePath resourceVar)
+              return (Left err, go filePath tr)
+            Right (resolver', modTime') ->
+              return (Right resolver', go filePath (TimedResolver resolver' modTime'))
 
 
--- | Like 'DNS.lookupA' but also return the TTL for the results.
---
--- DNS library timeouts do not work reliably on Windows (#1873), hence the
--- additional timeout.
---
-lookupAWithTTL :: DNS.ResolvConf
-               -> DNS.Resolver
-               -> DNS.Domain
-               -> IO (Either DNS.DNSError [(IP, DNS.TTL)])
-lookupAWithTTL resolvConf resolver domain = do
-    reply <- timeout (microsecondsAsIntToDiffTime
-                       $ DNS.resolvTimeout resolvConf)
-                     (DNS.lookupRaw resolver domain DNS.A)
-    case reply of
-      Nothing          -> return (Left DNS.TimeoutExpired)
-      Just (Left  err) -> return (Left err)
-      Just (Right ans) -> return (DNS.fromDNSMessage ans selectA)
-      --TODO: we can get the SOA TTL on NXDOMAIN here if we want to
-  where
-    selectA DNS.DNSMessage { DNS.answer } =
-      [ (IPv4 addr, ttl)
-      | DNS.ResourceRecord {
-          DNS.rdata = DNS.RD_A addr,
-          DNS.rrttl = ttl
-        } <- answer
-      ]
+    -- | `Resource` which passes the 'DNS.Resolver' through a 'StrictTVar'.  Better
+    -- than 'resolverResource' when using in multiple threads.
+    --
+    asyncResolverResource :: DNS.ResolvConf
+                          -> IO (Resource IO (DNSorIOError IOException)
+                                             DNS.Resolver)
+
+    asyncResolverResource resolvConf =
+        case DNS.resolvInfo resolvConf of
+          DNS.RCFilePath filePath -> do
+            resourceVar <- newTVarIO NoResolver
+            pure $ go filePath resourceVar
+          _ -> do
+            constantResource <$> getResolver resolvConf
+      where
+        handlers :: [ Handler IO (Either (DNSorIOError IOException) a) ]
+        handlers  = [ Handler $ pure . Left . IOError
+                    , Handler $ pure . Left . DNSError
+                    ]
+
+        go :: FilePath -> StrictTVar IO TimedResolver
+           -> Resource IO (DNSorIOError IOException) DNS.Resolver
+        go filePath resourceVar = Resource $ do
+          r <- readTVarIO resourceVar
+          case r of
+            NoResolver ->
+              do
+                result
+                  <- (curry Right
+                       <$> getTimeStamp filePath
+                       <*> getResolver resolvConf)
+                     `catches` handlers
+                case result of
+                  Left err ->
+                    pure (Left err, go filePath resourceVar)
+                  Right (modTime, resolver) -> do
+                    atomically (writeTVar resourceVar (TimedResolver resolver modTime))
+                    pure (Right resolver, go filePath resourceVar)
+
+            TimedResolver resolver modTime -> do
+              result <- runExceptT $ do
+                modTime' <- ExceptT $ (Right <$> getTimeStamp filePath)
+                                      `catches` handlers
+                if modTime' <= modTime
+                  then return resolver
+                  else do
+                  resolver' <- ExceptT $ (Right <$> getResolver resolvConf)
+                                         `catches` handlers
+                  atomically (writeTVar (castStrictTVar resourceVar)
+                             (TimedResolver resolver' modTime'))
+                  return resolver'
+              case result of
+                Left err ->
+                  return (Left err, go filePath resourceVar)
+                Right resolver' ->
+                  return (Right resolver', go filePath resourceVar)
 
 
-lookupAAAAWithTTL :: DNS.ResolvConf
+    -- | Like 'DNS.lookupA' but also return the TTL for the results.
+    --
+    -- DNS library timeouts do not work reliably on Windows (#1873), hence the
+    -- additional timeout.
+    --
+    lookupAWithTTL :: DNS.ResolvConf
+                   -> DNS.Resolver
+                   -> DNS.Domain
+                   -> IO (Either DNS.DNSError [(IP, DNS.TTL)])
+    lookupAWithTTL resolvConf resolver domain = do
+        reply <- timeout (microsecondsAsIntToDiffTime
+                           $ DNS.resolvTimeout resolvConf)
+                         (DNS.lookupRaw resolver domain DNS.A)
+        case reply of
+          Nothing          -> return (Left DNS.TimeoutExpired)
+          Just (Left  err) -> return (Left err)
+          Just (Right ans) -> return (DNS.fromDNSMessage ans selectA)
+          --TODO: we can get the SOA TTL on NXDOMAIN here if we want to
+      where
+        selectA DNS.DNSMessage { DNS.answer } =
+          [ (IPv4 addr, ttl)
+          | DNS.ResourceRecord {
+              DNS.rdata = DNS.RD_A addr,
+              DNS.rrttl = ttl
+            } <- answer
+          ]
+
+
+    lookupAAAAWithTTL :: DNS.ResolvConf
+                      -> DNS.Resolver
+                      -> DNS.Domain
+                      -> IO (Either DNS.DNSError [(IP, DNS.TTL)])
+    lookupAAAAWithTTL resolvConf resolver domain = do
+        reply <- timeout (microsecondsAsIntToDiffTime
+                           $ DNS.resolvTimeout resolvConf)
+                         (DNS.lookupRaw resolver domain DNS.AAAA)
+        case reply of
+          Nothing          -> return (Left DNS.TimeoutExpired)
+          Just (Left  err) -> return (Left err)
+          Just (Right ans) -> return (DNS.fromDNSMessage ans selectAAAA)
+          --TODO: we can get the SOA TTL on NXDOMAIN here if we want to
+      where
+        selectAAAA DNS.DNSMessage { DNS.answer } =
+          [ (IPv6 addr, ttl)
+          | DNS.ResourceRecord {
+              DNS.rdata = DNS.RD_AAAA addr,
+              DNS.rrttl = ttl
+            } <- answer
+          ]
+
+
+    lookupWithTTL :: LookupReqs
+                  -> DNS.ResolvConf
                   -> DNS.Resolver
                   -> DNS.Domain
-                  -> IO (Either DNS.DNSError [(IP, DNS.TTL)])
-lookupAAAAWithTTL resolvConf resolver domain = do
-    reply <- timeout (microsecondsAsIntToDiffTime
-                       $ DNS.resolvTimeout resolvConf)
-                     (DNS.lookupRaw resolver domain DNS.AAAA)
-    case reply of
-      Nothing          -> return (Left DNS.TimeoutExpired)
-      Just (Left  err) -> return (Left err)
-      Just (Right ans) -> return (DNS.fromDNSMessage ans selectAAAA)
-      --TODO: we can get the SOA TTL on NXDOMAIN here if we want to
-  where
-    selectAAAA DNS.DNSMessage { DNS.answer } =
-      [ (IPv6 addr, ttl)
-      | DNS.ResourceRecord {
-          DNS.rdata = DNS.RD_AAAA addr,
-          DNS.rrttl = ttl
-        } <- answer
-      ]
+                  -> IO ([DNS.DNSError], [(IP, DNS.TTL)])
+    lookupWithTTL LookupReqAOnly resolvConf resolver domain = do
+        res <- lookupAWithTTL resolvConf resolver domain
+        case res of
+             Left err -> return ([err], [])
+             Right r  -> return ([], r)
 
+    lookupWithTTL LookupReqAAAAOnly resolvConf resolver domain = do
+        res <- lookupAAAAWithTTL resolvConf resolver domain
+        case res of
+             Left err -> return ([err], [])
+             Right r  -> return ([], r)
 
-lookupWithTTL :: LookupReqs
-              -> DNS.ResolvConf
-              -> DNS.Resolver
-              -> DNS.Domain
-              -> IO ([DNS.DNSError], [(IP, DNS.TTL)])
-lookupWithTTL LookupReqAOnly resolvConf resolver domain = do
-    res <- lookupAWithTTL resolvConf resolver domain
-    case res of
-         Left err -> return ([err], [])
-         Right r  -> return ([], r)
-
-lookupWithTTL LookupReqAAAAOnly resolvConf resolver domain = do
-    res <- lookupAAAAWithTTL resolvConf resolver domain
-    case res of
-         Left err -> return ([err], [])
-         Right r  -> return ([], r)
-
-lookupWithTTL LookupReqAAndAAAA resolvConf resolver domain = do
-    (r_ipv6, r_ipv4) <- concurrently (lookupAAAAWithTTL resolvConf resolver domain)
-                                     (lookupAWithTTL resolvConf resolver domain)
-    case (r_ipv6, r_ipv4) of
-         (Left  e6, Left  e4) -> return ([e6, e4], [])
-         (Right r6, Left  e4) -> return ([e4], r6)
-         (Left  e6, Right r4) -> return ([e6], r4)
-         (Right r6, Right r4) -> return ([], r6 <> r4)
-
-
--- | Bundle of DNS Actions that runs in IO
--- The IPv4 and IPv6 addresses the node will be using should determine the
--- LookupReqs so that we can avoid lookups for address types that wont be used.
---
-ioDNSActions :: LookupReqs
-             -> DNSActions DNS.Resolver IOException IO
-ioDNSActions reqs =
-  DNSActions {
-    dnsResolverResource      = resolverResource,
-    dnsAsyncResolverResource = asyncResolverResource,
-    dnsLookupWithTTL         = lookupWithTTL reqs
-  }
-
+    lookupWithTTL LookupReqAAndAAAA resolvConf resolver domain = do
+        (r_ipv6, r_ipv4) <- concurrently (lookupAAAAWithTTL resolvConf resolver domain)
+                                         (lookupAWithTTL resolvConf resolver domain)
+        case (r_ipv6, r_ipv4) of
+             (Left  e6, Left  e4) -> return ([e6, e4], [])
+             (Right r6, Left  e4) -> return ([e4], r6)
+             (Left  e6, Right r4) -> return ([e6], r4)
+             (Right r6, Right r4) -> return ([], r6 <> r4)
