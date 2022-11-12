ae26bb0640c28f7a964edae43708e810d0a0948b
Tobias Dammers <tdammers@gmail.com>
2022-11-12 14:58:31 +0100

    WIP KES evolution

---
+++ kes-agent/src/Cardano/KESAgent/Evolution.hs
@@ -0,0 +1,61 @@
+{-#LANGUAGE TypeFamilies #-}
+{-#LANGUAGE FlexibleContexts #-}
+{-#LANGUAGE RankNTypes #-}
+
+module Cardano.KESAgent.Evolution
+where
+
+import Cardano.Crypto.KES.Class
+import Cardano.Crypto.DSIGN.Class as DSIGN
+import Cardano.KESAgent.OCert
+
+import Data.Time.Clock.POSIX (POSIXTime, getPOSIXTime)
+import Data.Time (nominalDiffTimeToSeconds)
+
+getCurrentKESPeriod :: Integer -> IO KESPeriod
+getCurrentKESPeriod genesisTimestamp =
+  KESPeriod
+    . floor
+    .  (/ (36 * 3600))
+    . (`subtract` (realToFrac genesisTimestamp))
+    . nominalDiffTimeToSeconds
+    <$> getPOSIXTime
+
+updateKESToCurrent :: KESSignAlgorithm IO v
+                   => Integer
+                   -> ContextKES v
+                   -> OCert v
+                   -> SignKeyWithPeriodKES v
+                   -> IO (Maybe (SignKeyWithPeriodKES v))
+updateKESToCurrent genesisTimestamp context cert skp = do
+  currentPeriod <- getCurrentKESPeriod genesisTimestamp
+  updateKESTo context currentPeriod cert skp
+
+updateKESTo :: KESSignAlgorithm IO v
+            => ContextKES v
+            -> KESPeriod
+            -> OCert v
+            -> SignKeyWithPeriodKES v
+            -> IO (Maybe (SignKeyWithPeriodKES v))
+updateKESTo context currentPeriod cert skp = do
+  let targetEvolution =
+        unKESPeriod currentPeriod -
+        unKESPeriod (ocertKESPeriod cert)
+  if periodKES skp >= targetEvolution then
+    return (Just skp) -- key is already up to date or newer
+  else do
+    skp'May <- updateKESWithPeriod context skp
+    case skp'May of
+      Nothing ->
+        -- update failed (t >= totalPeriodsKES)
+        return Nothing
+      Just skp' ->
+        -- update succeeded
+        if periodKES skp' == targetEvolution then
+          -- reached target evolution, so we're done
+          return (Just skp')
+        else
+          -- Still not enough, keep going.
+          -- This also captures the (theoretical) case where we have updated
+          -- past the last evolution, but the recursive call will catch this.
+          updateKESTo context currentPeriod cert skp'
