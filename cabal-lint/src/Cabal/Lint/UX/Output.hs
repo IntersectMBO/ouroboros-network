bf90d546ebf24b99003d82c2f8f66b7f14127323
Nicolas Frisby <nick.frisby@iohk.io>
2022-12-01 18:41:33 -0800

    TOSQUASH calc intersection of inconsistent deps

--- cabal-lint/src/Cabal/Lint/UX/Output.hs
+++ cabal-lint/src/Cabal/Lint/UX/Output.hs
@@ -18,6 +18,7 @@ module Cabal.Lint.UX.Output (
 import           Cabal.Lint.Ids
 import           Cabal.Lint.Ord
 import           Cabal.Lint.Summygroup
+import           Control.Monad (join)
 import           Data.List (intercalate)
 import           Data.Map (Map)
 import qualified Data.Map as Map
@@ -26,6 +27,7 @@ import qualified Distribution.Compat.NonEmptySet as NES
 import qualified Distribution.Pretty                                       as C
 import qualified Distribution.Types.ComponentName                          as C
 import qualified Distribution.Types.UnqualComponentName                    as C
+import qualified Distribution.Types.Version                                as C
 import qualified Distribution.Types.VersionRange.Internal                  as C
 import qualified Language.Haskell.Extension                                as C
 
@@ -152,7 +154,7 @@ renderMessageOneLineText sho e = case e of
         EmptyProject -> "There were no .cabal files to analyze; pass at least one command-line argument, each a path to a .cabal file"
         ConsistentVersionRange (DepId pname libname) (MyVersionRange vrange) -> "Dependency " <> C.prettyShow pname <> ":" <> C.prettyShow (C.CLibName libname) <> " is consistently " <> prettyV vrange
         ConsistentDefaultLanguage mbLang -> "The default language is consistently " <> prettyL mbLang
-        InconsistentVersionRanges (DepId pname libname) (Occurrences m) -> "Dependency " <> C.prettyShow pname <> ":" <> C.prettyShow (C.CLibName libname) <> " is " <> differences (prettyV . unMyVersionRange) m
+        InconsistentVersionRanges (DepId pname libname) (Occurrences m) -> "Dependency " <> C.prettyShow pname <> ":" <> C.prettyShow (C.CLibName libname) <> " is " <> differences (prettyV . unMyVersionRange) m <> "; " <> theIntersection (Occurrences m)
         InconsistentDefaultLanguages (Occurrences m) -> "The default language is " <> differences prettyL m
     PackageMessage puid err -> case err of
         CouldNotParse -> "Package " <> sho puid <> " could not be parsed as a `.cabal` file"
@@ -181,3 +183,103 @@ renderMessageOneLineText sho e = case e of
             ]
         | (k, comps) <- Map.toList m
         ]
+
+    theIntersection (Occurrences m) =
+        let cons (MyVersionRange x) y = intersection <$> versionRangeToInterval x <*> y
+            nil                       = Just full
+        in
+        case foldr cons nil (Map.keys m) of
+            Nothing   -> "those constraints are not all simple intervals"
+            Just ival -> "the intersection of those constraints as simple intervals is " <> C.prettyShow (intervalToVersionRange ival)
+
+-----
+
+bottom :: C.Version
+bottom = C.version0   -- not C.nullVersion = C.mkVersion [] because our lower bound is inclusive
+
+next :: C.Version -> C.Version
+next = C.mkVersion . (++ [0]) . C.versionNumbers
+
+-- | Partial because the predecessor of @V.1@ could
+-- be @V.0.xyz@ where @xyz@ can be arbitrarily long
+--
+-- INVARIANT: @'prev' y = Just x@ if and only if @y = 'next' x@
+prev :: C.Version -> Maybe C.Version
+prev =
+    fmap C.mkVersion . go . C.versionNumbers
+  where
+    go = \case
+        [0]  -> Just []
+        x:xs -> (x:) <$> go xs
+        _    -> Nothing
+
+-----
+
+-- | A type that is either zero or negative epsilon
+--
+-- We use this to simplify the handling of strict upper bounds. If 'prev' were
+-- total, we could use that instead, and 'Nudge' would be unnecessary.
+data Nudge = LessEpsilon | NoNudge
+  deriving (Eq, Ord, Show)
+
+-- | An enriched type for the upper bound
+--
+-- The extra structure represents unboundedness and the distinction between @<@
+-- and @<=@ (see 'Nudge').
+data Upper a = Middle a Nudge | Top
+  deriving (Eq, Ord, Show)
+
+-- | Lower is inclusive, upper is exclusive
+data Interval a = Interval a (Upper a)
+  deriving (Show)
+
+empty :: a -> Interval a
+empty x = x `Interval` Middle x LessEpsilon
+
+intersection :: Ord a => Interval a -> Interval a -> Interval a
+intersection (loL `Interval` hiL) (loR `Interval` hiR)
+    | hiL < Middle loR NoNudge = empty loR   -- they don't abut
+    | hiR < Middle loL NoNudge = empty loR   -- they don't abut
+    | otherwise                = max loL loR `Interval` min hiL hiR
+
+-- | Fails when they don't abut
+union :: Ord a => Interval a -> Interval a -> Maybe (Interval a)
+union (loL `Interval` hiL) (loR `Interval` hiR)
+    | hiL < Middle loR NoNudge = Nothing   -- they don't abut
+    | hiR < Middle loL NoNudge = Nothing   -- they don't abut
+    | otherwise                = Just $ min loL loR `Interval` max hiL hiR
+
+-----
+
+full :: Interval C.Version
+full = bottom `Interval` Top
+
+intervalToVersionRange :: Interval C.Version -> C.VersionRange
+intervalToVersionRange (lo `Interval` ehi) = case ehi of
+    Middle hi nudge -> case compare lo hi of
+        LT -> (if lo == bottom then id else C.IntersectVersionRanges lower) $ case nudge of
+            NoNudge     -> C.OrEarlierVersion hi
+            LessEpsilon -> C.EarlierVersion   hi
+        EQ -> case nudge of
+            NoNudge     -> C.ThisVersion lo
+            LessEpsilon -> C.noVersion
+        GT -> C.noVersion
+
+    Top -> lower
+  where
+    lower = case prev lo of
+            Just v -> C.LaterVersion v
+            _      -> C.OrLaterVersion lo
+
+-- | Fails if there's a union of intervals that do not abut
+versionRangeToInterval :: C.VersionRange -> Maybe (Interval C.Version)
+versionRangeToInterval = \case
+    C.ThisVersion       v -> Just $ v      `Interval` Middle v NoNudge
+    C.LaterVersion      v -> Just $ next v `Interval` Top
+    C.OrLaterVersion    v -> Just $ v      `Interval` Top
+    C.EarlierVersion    v -> Just $ bottom `Interval` Middle v LessEpsilon
+    C.OrEarlierVersion  v -> Just $ bottom `Interval` Middle v NoNudge
+    C.MajorBoundVersion v -> Just $ v      `Interval` Middle (C.majorUpperBound v) LessEpsilon
+
+    C.UnionVersionRanges     l r -> join $ union        <$> versionRangeToInterval l <*> versionRangeToInterval r
+    C.IntersectVersionRanges l r ->        intersection <$> versionRangeToInterval l <*> versionRangeToInterval r
