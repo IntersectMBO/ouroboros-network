9cf409958176058e1c3e3ac15df837e0be9c56f6
Alexander Esgen <alexander.esgen@iohk.io>
2022-12-14 13:39:00 +0100

    Run stylish-haskell

--- ouroboros-consensus-cardano-tools/src/Cardano/Tools/DBAnalyser/Block/Byron.hs
+++ ouroboros-consensus-cardano-tools/src/Cardano/Tools/DBAnalyser/Block/Byron.hs
@@ -22,8 +22,8 @@ import qualified Cardano.Crypto as Crypto
 
 import qualified Cardano.Chain.Block as Chain
 import qualified Cardano.Chain.Genesis as Genesis
-import qualified Cardano.Chain.UTxO as Chain
 import qualified Cardano.Chain.Update as Update
+import qualified Cardano.Chain.UTxO as Chain
 
 import qualified Ouroboros.Consensus.Mempool.TxLimits as TxLimits
 import           Ouroboros.Consensus.Node.ProtocolInfo
