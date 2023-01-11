e13a69852e3aa4cabc896cc34e76814e63202d1f
Javier Sagredo <jasataco@gmail.com>
2023-01-11 16:20:50 +0100

    PBT for laws on TableStuff and StowableLedgerTables

---
+++ ouroboros-consensus-cardano-test/test/Test/Consensus/Cardano/LedgerTables.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeFamilies      #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Consensus.Cardano.LedgerTables (tests) where
+
+import           Data.Map.Diff.Strict (Values (..))
+import qualified Data.Map.Strict as Map
+import           Data.SOP.Strict
+
+import           Ouroboros.Consensus.Cardano.Block
+import           Ouroboros.Consensus.Cardano.CanHardFork
+import           Ouroboros.Consensus.HardFork.Combinator
+import           Ouroboros.Consensus.HardFork.Combinator.State.Types
+import           Ouroboros.Consensus.HardFork.Combinator.Util.Functors
+import           Ouroboros.Consensus.HardFork.Combinator.Util.Telescope
+import           Ouroboros.Consensus.Ledger.Tables
+import           Ouroboros.Consensus.Protocol.Praos.Translate ()
+import           Ouroboros.Consensus.Shelley.HFEras ()
+import           Ouroboros.Consensus.Shelley.Ledger.SupportsProtocol ()
+import           Ouroboros.Consensus.Shelley.ShelleyHFC (ShelleyTxOut (..))
+import           Ouroboros.Consensus.Util.SOP
+
+import qualified Cardano.Ledger.Shelley.API as SL
+
+import           Test.Cardano.Ledger.Alonzo.Serialisation.Generators ()
+import           Test.Cardano.Ledger.Babbage.Serialisation.Generators ()
+
+import           Test.LedgerTables
+
+import           Test.Consensus.Byron.Generators ()
+import           Test.Consensus.Cardano.MockCrypto (MockCryptoCompatByron)
+import           Test.Consensus.Shelley.Generators ()
+
+import           Test.Tasty
+import           Test.Tasty.QuickCheck
+
+type Crypto = MockCryptoCompatByron
+
+tests :: TestTree
+tests = testGroup "LedgerTables"
+  [ testGroup "Cardano"
+    [ testProperty "Stowable laws" (prop_stowable_laws @(CardanoBlock Crypto))
+    , testProperty "TableStuff laws"
+        (\(InSameEra ls tbs) -> prop_tablestuff_laws @(CardanoBlock Crypto) ls tbs)
+    ]
+  ]
+
+instance Arbitrary (LedgerTables (LedgerState (CardanoBlock Crypto)) ValuesMK) where
+  arbitrary = projectLedgerTables . unstowLedgerTables <$> arbitrary
+
+instance Show (LedgerTables (LedgerState (CardanoBlock Crypto)) EmptyMK) where
+  show x = showsLedgerState x ""
+
+instance Show (LedgerTables (LedgerState (CardanoBlock Crypto)) ValuesMK) where
+  show x = showsLedgerState x ""
+
+-- | We need to generate Ledger states and tables in the same era or otherwise
+-- it wouldn't make sense to try to attach them to a hard fork ledger state,
+-- this data definition does just that.
+data InSameEra =
+  InSameEra
+    !(LedgerState (CardanoBlock Crypto) EmptyMK)
+    !(LedgerTables (LedgerState (CardanoBlock Crypto)) ValuesMK)
+  deriving Show
+
+instance Arbitrary InSameEra where
+  arbitrary = do
+    ls <- arbitrary
+    tbs <- arbitrary `suchThat` (sameEra (getHardForkState $ hardForkLedgerStatePerEra ls) . cardanoUTxOTable)
+    pure $ InSameEra ls tbs
+
+-- | This function asserts that:
+--
+-- - Producing a table to be attached to a Byron HardFork State requires the
+--   table to be empty as that is the only kind of table such a state can have.
+--
+-- - Producing a table to be attached to a Shelley HardFork State requires all
+-- - the entries in the map to be on the same era as the state. We do so by
+-- - checking that all of them and the state produce the same index through
+-- - @'nsToIndex'@.
+sameEra ::
+     Telescope (K Past) (Current (Flip LedgerState EmptyMK)) (CardanoEras Crypto)
+  -> ApplyMapKind ValuesMK (SL.TxIn Crypto) (ShelleyTxOut (ShelleyBasedEras Crypto))
+  -> Bool
+sameEra (TZ{})      (ApplyValuesMK (Values m))      = Map.null m
+sameEra (TS _ tele) (ApplyValuesMK (Values values)) =
+    all (nsToIndex (tip tele) ==) $ map (nsToIndex . unShelleyTxOut . snd) (Map.toList values)
---
+++ ouroboros-consensus-test/src/Test/LedgerTables.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Test.LedgerTables (
+    prop_stowable_laws
+  , prop_tablestuff_laws
+  ) where
+
+import           Data.Function (on)
+
+import           Ouroboros.Consensus.Ledger.Basics
+import           Ouroboros.Consensus.Ledger.Tables
+
+import           Test.QuickCheck
+
+-- | We compare the Ledger Tables of the result because the comparison with the
+-- rest of the LedgerState takes considerably more time to run.
+(==?) ::
+  ( IsMapKind mk
+  , TableStuff (LedgerState blk)
+  , Eq (LedgerTables (LedgerState blk) mk)
+  , Show (LedgerTables (LedgerState blk) mk)
+  )
+  => LedgerState blk mk
+  -> LedgerState blk mk
+  -> Property
+(==?) = (===) `on` projectLedgerTables
+
+infixl 9 ==?
+
+-- | The StowableLedgerTables instances should follow these two laws:
+--
+-- > stow . unstow == id
+--
+-- > unstow . stow == id
+prop_stowable_laws ::
+     ( TableStuff (LedgerState blk)
+     , StowableLedgerTables (LedgerState blk)
+     , Eq (LedgerTables (LedgerState blk) EmptyMK)
+     , Show (LedgerTables (LedgerState blk) EmptyMK)
+     , Show (LedgerTables (LedgerState blk) ValuesMK)
+     )
+  => LedgerState blk EmptyMK
+  -> LedgerState blk ValuesMK
+  -> Property
+prop_stowable_laws = \ls ls' ->
+  stowLedgerTables (unstowLedgerTables ls) ==? ls .&&.
+  unstowLedgerTables (stowLedgerTables ls') ==? ls'
+
+-- | The TableStuff instances should follow these two laws:
+--
+-- > with . project == id
+--
+-- > project . with == id
+prop_tablestuff_laws ::
+     ( TableStuff (LedgerState blk)
+     , Eq (LedgerTables (LedgerState blk) EmptyMK)
+     , Show (LedgerTables (LedgerState blk) EmptyMK)
+     , Show (LedgerTables (LedgerState blk) ValuesMK)
+     )
+  => LedgerState blk EmptyMK
+  -> LedgerTables (LedgerState blk) ValuesMK
+  -> Property
+prop_tablestuff_laws = \ls tbs ->
+  (ls `withLedgerTables` (projectLedgerTables ls)) ==? ls .&&.
+  projectLedgerTables (ls `withLedgerTables` tbs) === tbs
