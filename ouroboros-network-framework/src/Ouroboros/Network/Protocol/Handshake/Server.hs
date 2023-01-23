1589e50984087a36432099503ef5f9a20beeb2b5
James Parker <james@galois.com>
2023-01-23 13:40:55 -0800

    Add `HandshakeResult` type for handshake results

--- ouroboros-network-framework/src/Ouroboros/Network/Protocol/Handshake/Server.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/Protocol/Handshake/Server.hs
@@ -10,7 +10,7 @@ import qualified Codec.CBOR.Term as CBOR
 
 import           Network.TypedProtocol.Core
 
-import           Ouroboros.Network.Protocol.Handshake.Client (acceptOrRefuse, encodeVersions)
+import           Ouroboros.Network.Protocol.Handshake.Client (acceptOrRefuse, decodeQueryResult, encodeVersions)
 import           Ouroboros.Network.Protocol.Handshake.Codec
 import           Ouroboros.Network.Protocol.Handshake.Type
 import           Ouroboros.Network.Protocol.Handshake.Version
@@ -28,21 +28,21 @@ handshakeServerPeer
   -> Versions vNumber vData r
   -> Peer (Handshake vNumber CBOR.Term)
           AsServer StPropose m
-          (Either (HandshakeProtocolError vNumber) (r, vNumber, vData))
-handshakeServerPeer codec@VersionDataCodec {encodeData} acceptVersion query versions =
+          (HandshakeResult r vNumber vData)
+handshakeServerPeer codec@VersionDataCodec {encodeData, decodeData} acceptVersion query versions =
     Await (ClientAgency TokPropose) $ \msg -> case msg of
       MsgProposeVersions vMap  ->
         case acceptOrRefuse codec acceptVersion versions vMap of
           (Right r@(_, vNumber, agreedData)) ->
-            let response = if query agreedData then
-                    MsgQueryReply $ encodeVersions encodeData versions
+            let (response, r') = if query agreedData then
+                    (MsgQueryReply $ encodeVersions encodeData versions, decodeQueryResult decodeData vMap)
                   else
-                    MsgAcceptVersion vNumber $ encodeData vNumber agreedData
+                    (MsgAcceptVersion vNumber $ encodeData vNumber agreedData, HandshakeNegotiationResult r)
             in
             Yield (ServerAgency TokConfirm)
                   response
-                  (Done TokDone (Right r))
+                  (Done TokDone r')
           (Left vReason) ->
             Yield (ServerAgency TokConfirm)
                   (MsgRefuse vReason)
-                  (Done TokDone (Left (HandshakeError vReason)))
+                  (Done TokDone (HandshakeResultError (HandshakeError vReason)))
