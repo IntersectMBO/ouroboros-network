49273237c76e28652e27e447d164bf5902113fd5
Alexander Esgen <alexander.esgen@iohk.io>
2023-01-30 12:09:38 +0100

    Cicero: double memory quota to 32 GB

--- nix/cells/automation/pipelines.nix
+++ nix/cells/automation/pipelines.nix
@@ -28,7 +28,7 @@
       skippedDescription = lib.escapeShellArg "No nix builder available for this system";
     };
 
-    memory = 1024 * 16;
+    memory = 1024 * 32;
     nomad.resources.cpu = 10000;
   };
 }
