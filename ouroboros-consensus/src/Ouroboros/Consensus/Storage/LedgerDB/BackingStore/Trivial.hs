5dfea856ba734e32a0560d890d09b79f4adde9f9
Javier Sagredo <javier.sagredo@iohk.io>
2023-02-03 15:49:42 +0100

    Introduce the BackingStore and the TVar implementation
    Co-authored-by: Nick Frisby <nick.frisby@iohk.io>
    Co-authored-by: Damian Nadales <damian.nadales@iohk.io>
    Co-authored-by: Joris Dral <joris@well-typed.com>

---
+++ ouroboros-consensus/src/Ouroboros/Consensus/Storage/LedgerDB/BackingStore/Trivial.hs
@@ -0,0 +1,35 @@
+-- | A Trivial backing store that just stores the slot number on a TVar. To be
+-- used by tests.
+module Ouroboros.Consensus.Storage.LedgerDB.BackingStore.Trivial (trivialBackingStore) where
+
+import           Control.Monad (void)
+
+import           Cardano.Slotting.Slot (WithOrigin (..))
+
+import           Ouroboros.Consensus.Util.IOLike (IOLike)
+import qualified Ouroboros.Consensus.Util.IOLike as IOLike
+
+import           Ouroboros.Consensus.Storage.LedgerDB.BackingStore
+
+{-------------------------------------------------------------------------------
+  A trivial backing store that holds only the slot number
+-------------------------------------------------------------------------------}
+
+trivialBackingStore :: IOLike m => values -> m (BackingStore m keys values diff)
+trivialBackingStore emptyValues = do
+  seqNo <- IOLike.newTVarIO Origin
+  pure $ BackingStore
+              (pure ())
+              (\_ _ -> pure ())
+              (IOLike.atomically $ do
+                  s <- IOLike.readTVar seqNo
+                  pure $ ( s
+                         , BackingStoreValueHandle
+                             (pure ())
+                             (\_ -> pure emptyValues)
+                             (\_ -> pure emptyValues)
+                         )
+              )
+              (\s _ -> void
+                     $ IOLike.atomically
+                     $ IOLike.modifyTVar seqNo (\_ -> At s))
