055a39234c828229dc4897d953ede05dbaa88142
Javier Sagredo <javier.sagredo@iohk.io>
2023-02-07 15:23:47 +0100

    Translate the ledger tables in the HFC
    Co-authored-by: Nick Frisby <nick.frisby@iohk.io>
    Co-authored-by: Damian Nadales <damian.nadales@iohk.io>
    Co-authored-by: Joris Dral <joris@well-typed.com>

--- ouroboros-consensus/src/Ouroboros/Consensus/HardFork/Combinator/State/Types.hs
+++ ouroboros-consensus/src/Ouroboros/Consensus/HardFork/Combinator/State/Types.hs
@@ -14,6 +14,7 @@ module Ouroboros.Consensus.HardFork.Combinator.State.Types (
   , TransitionInfo (..)
   , Translate (..)
   , TranslateForecast (..)
+  , TranslateLedgerState (..)
   ) where
 
 import           Prelude
@@ -26,6 +27,7 @@ import           NoThunks.Class (NoThunks (..))
 import           Ouroboros.Consensus.Block
 import           Ouroboros.Consensus.Forecast
 import           Ouroboros.Consensus.HardFork.History (Bound)
+import           Ouroboros.Consensus.Ledger.Basics
 import           Ouroboros.Consensus.Ticked
 
 import           Ouroboros.Consensus.HardFork.Combinator.Util.Telescope
@@ -93,8 +95,38 @@ newtype TranslateForecast f g x y = TranslateForecast {
       translateForecastWith ::
            Bound    -- 'Bound' of the transition (start of the new era)
         -> SlotNo   -- 'SlotNo' we're constructing a forecast for
-        -> f x
+        -> f x EmptyMK
         -> Except OutsideForecastRange (Ticked (g y))
+    }
+
+-- | Translate a LedgerState across an era transition.
+--
+-- As new tables might be populated on an era transition, the return map kind is
+-- @ValuesMK@. If no tables are populated, normally this will be filled with
+-- @polyEmptyLedgerTables@.
+data TranslateLedgerState x y = TranslateLedgerState {
+        -- | How to translate a Ledger State during the era transition
+        translateLedgerStateWith ::
+            EpochNo
+         -> LedgerState x EmptyMK
+         -> LedgerState y DiffMK
+
+        -- | How to translate tables on an era transition.
+        --
+        -- This is a rather technical subtlety. When performing a ledger state
+        -- translation, the provided input ledger state will be initially
+        -- populated with a @polyEmptyLedgerTables@. This step is required so
+        -- that the operation provided to 'Telescope.extend' is an automorphism.
+        --
+        -- If we only extend by one era, this function is a no-op, as the input
+        -- will be empty ledger states. However, if we extend across multiple
+        -- eras, previous eras might populate tables thus creating Values that
+        -- now need to be translated to newer eras. This function fills that
+        -- hole and allows us to promote tables from one era into tables from
+        -- the next era.
+      , translateLedgerTablesWith ::
+            LedgerTables (LedgerState x) DiffMK
+         -> LedgerTables (LedgerState y) DiffMK
     }
 
 -- | Knowledge in a particular era of the transition to the next era
