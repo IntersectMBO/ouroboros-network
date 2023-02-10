34dbf9db3c0f52a49276fcfd361a0139684d5690
Bart Frenk <bart.frenk@gmail.com>
2023-02-10 17:02:05 +0100

    Make import unqualified in Step

--- ouroboros-consensus-test/src/Test/StateMachine/Step.hs
+++ ouroboros-consensus-test/src/Test/StateMachine/Step.hs
@@ -6,48 +6,47 @@ module Test.StateMachine.Step (
   , stepCommands
   ) where
 
-
 import           Control.Monad.Reader
 import           Control.Monad.State
 import           Control.Monad.Writer
-import qualified Test.StateMachine.Sequential as QSM
-import qualified Test.StateMachine.Types as QSM
+import           Test.StateMachine.Sequential
+import           Test.StateMachine.Types
 import qualified Test.StateMachine.Types.Rank2 as Rank2
 
 newtype Step model cmd m resp a = Step {
-  _unStep :: StateT (model QSM.Symbolic)
-    (ReaderT (QSM.StateMachine model cmd m resp)
-    ((WriterT [QSM.Command cmd resp]) QSM.GenSym)) a
+  _unStep :: StateT (model Symbolic)
+    (ReaderT (StateMachine model cmd m resp)
+    ((WriterT [Command cmd resp]) GenSym)) a
 } deriving newtype (
       Functor
     , Applicative
     , Monad
-    , MonadState (model QSM.Symbolic)
-    , MonadWriter [QSM.Command cmd resp]
-    , MonadReader (QSM.StateMachine model cmd m resp))
+    , MonadState (model Symbolic)
+    , MonadWriter [Command cmd resp]
+    , MonadReader (StateMachine model cmd m resp))
 
-feed :: Rank2.Foldable resp => cmd QSM.Symbolic
-     -> Step model cmd m resp (QSM.Command cmd resp)
+feed :: Rank2.Foldable resp => cmd Symbolic
+     -> Step model cmd m resp (Command cmd resp)
 feed cmd = do
   sm <- ask
   model <- get
-  resp <- Step $ lift $ lift $ lift $ QSM.mock sm model cmd
-  let command = QSM.Command cmd resp (QSM.getUsedVars resp)
+  resp <- Step $ lift $ lift $ lift $ mock sm model cmd
+  let command = Command cmd resp (getUsedVars resp)
   tell [command]
-  put (QSM.transition sm model cmd resp)
+  put (transition sm model cmd resp)
   pure command
 
-stepCommands :: QSM.StateMachine model cmd m resp
+stepCommands :: StateMachine model cmd m resp
              -> Step model cmd m resp a
-             -> QSM.Commands cmd resp
+             -> Commands cmd resp
 stepCommands sm (Step action) =
-  QSM.Commands (evalGenSym
+  Commands (evalGenSym
                 (execWriterT
                  (runReaderT
-                  (evalStateT action (QSM.initModel sm))
+                  (evalStateT action (initModel sm))
                   sm))
-                QSM.newCounter)
+                newCounter)
   where
     evalGenSym action' counter =
-      let (x, _) = QSM.runGenSym action' counter
+      let (x, _) = runGenSym action' counter
       in x
