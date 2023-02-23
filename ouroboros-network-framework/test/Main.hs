1903a124f46a557013e620bc999992f23f3ba27d
Tobias Dammers <tdammers@gmail.com>
2023-02-23 14:04:19 +0100

    RawBearer API
    Lower-level send/receive API for Snockets, bypassing the normal Mux
    protocol. We need this for KES secure forgetting, as we cannot store
    secrets in intermediate data structures for serialization purposes; we
    must copy data directly between secure memory and file descriptors.

---
+++ ouroboros-network-framework/test/Main.hs
@@ -0,0 +1,30 @@
+module Main (main) where
+
+import           Test.Tasty
+
+import qualified Test.Ouroboros.Network.ConnectionManager as ConnectionManager
+import qualified Test.Ouroboros.Network.Driver as Driver
+import qualified Test.Ouroboros.Network.RateLimiting as RateLimiting
+import qualified Test.Ouroboros.Network.RawBearer as RawBearer
+import qualified Test.Ouroboros.Network.Server2 as Server2
+import qualified Test.Ouroboros.Network.Socket as Socket
+import qualified Test.Ouroboros.Network.Subscription as Subscription
+import qualified Test.Simulation.Network.Snocket as Snocket
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup "ouroboros-network-framework"
+  [ ConnectionManager.tests
+  , Driver.tests
+  , Server2.tests
+  , Socket.tests
+  , Subscription.tests
+  , RateLimiting.tests
+  , Snocket.tests
+  , RawBearer.tests
+  ]
+
+
