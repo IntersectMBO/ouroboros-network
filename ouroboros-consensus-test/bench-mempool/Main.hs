499efa65726ee454f58dc529cb41a2df5f3368cc
Damian Nadales <damian.only@gmail.com>
2023-03-02 13:40:24 +0100

    Output the mempool benchmark results as JSON
    ... which conforms to the input expected by
    https://github.com/benchmark-action/github-action-benchmark
    
    This patch also reduces the number of mempool benchmarks: we do not need
    that many data points for this benchmark. We should be austere in the
    benchmarks we produce, since the reports can easily get cluttered, and
    we will add more benchmarks in the future.

--- ouroboros-consensus-test/bench-mempool/Main.hs
+++ ouroboros-consensus-test/bench-mempool/Main.hs
@@ -1,15 +1,30 @@
-{-# LANGUAGE FlexibleContexts   #-}
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE OverloadedLists    #-}
-{-# LANGUAGE TupleSections      #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NumericUnderscores  #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 
 module Main (main) where
 
+import           Control.Arrow (first)
+import           Control.Monad (unless)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Csv as Csv
+import           Data.Maybe (fromMaybe)
 import           Data.Set ()
+import qualified Data.Text as Text
+import qualified Data.Text.Read as Text.Read
+import           System.Exit (die, exitFailure)
+import           Test.Tasty.Bench (CsvPath (CsvPath), bench, benchIngredients,
+                     bgroup, env, nfIO)
 import           Test.Tasty.HUnit (testCase, (@?=))
+import           Test.Tasty.Options (changeOption)
+import           Test.Tasty.Runners (parseOptions, tryIngredients)
+
+import           Data.Aeson
 
 import qualified Control.Tracer as Tracer
-import           Test.Tasty.Bench (bench, bgroup, defaultMain, env, nfIO)
 
 import           Bench.Consensus.Mempool
 import           Bench.Consensus.Mempool.TestBlock (TestBlock)
@@ -18,37 +33,95 @@ import           Bench.Consensus.MempoolWithMockedLedgerItf
 import qualified Ouroboros.Consensus.Mempool.Capacity as Mempool
 
 main :: IO ()
-main = defaultMain [ benchmarkJustAddingTransactions ]
+main = do
+    let csvFilePath = "mempool-benchmarks.csv"
+    runBenchmarks csvFilePath
+    rawValues <- parseBenchmarkResults csvFilePath
+    convertCsvRowsToJsonObjects rawValues "mempool-benchmarks.json"
   where
-    benchmarkJustAddingTransactions =
-        bgroup "Just adding transactions" $
-            fmap benchAddNTxs [1_000, 10_000, 100_000, 1_000_000]
+    runBenchmarks csvFilePath = do
+        opts <- parseOptions benchIngredients benchmarkJustAddingTransactions
+        let opts' = changeOption (Just . fromMaybe (CsvPath csvFilePath)) opts
+        case tryIngredients benchIngredients opts' benchmarkJustAddingTransactions of
+          Nothing               -> exitFailure
+          Just    runIngredient -> do
+            success <- runIngredient
+            unless success exitFailure
       where
-        benchAddNTxs n =
-          env (let txs = mkNTryAddTxs n in fmap (, txs) (openMempoolWithCapacipyFor txs))
-              -- The irrefutable pattern was added to avoid the
-              --
-              -- > Unhandled resource. Probably a bug in the runner you're using.
-              --
-              -- error reported here https://hackage.haskell.org/package/tasty-bench-0.3.3/docs/Test-Tasty-Bench.html#v:env
-              (\ ~(mempool, txs) -> bgroup (show n) [
-                  bench    "benchmark"     $ nfIO $ run        mempool txs
-                , testCase "test"          $        testAddTxs mempool txs
-                , testCase "txs length"    $ length txs @?= n
-                ]
-              )
+        benchmarkJustAddingTransactions =
+            bgroup "Just adding" $
+                fmap benchAddNTxs [10_000, 1_000_000]
           where
-            testAddTxs mempool txs = do
-                run mempool txs
-                mempoolTxs <- getTxs mempool
-                mempoolTxs @?= txsAddedInCmds txs
+            benchAddNTxs n =
+                env (let txs = mkNTryAddTxs n in fmap (, txs) (openMempoolWithCapacityFor txs))
+                -- The irrefutable pattern was added to avoid the
+                --
+                -- > Unhandled resource. Probably a bug in the runner you're using.
+                --
+                -- error reported here https://hackage.haskell.org/package/tasty-bench-0.3.3/docs/Test-Tasty-Bench.html#v:env
+                (\ ~(mempool, txs) -> bgroup (show n <> " transactions") [
+                    bench    "benchmark"     $ nfIO $ run        mempool txs
+                  , testCase "test"          $        testAddTxs mempool txs
+                  , testCase "txs length"    $ length txs @?= n
+                  ]
+                )
+              where
+                testAddTxs mempool txs = do
+                    run mempool txs
+                    mempoolTxs <- getTxs mempool
+                    mempoolTxs @?= txsAddedInCmds txs
+
+    parseBenchmarkResults csvFilePath = do
+        csvData <- BL.readFile csvFilePath
+        case Csv.decode Csv.HasHeader csvData of
+          Left err   -> die err
+          Right rows -> pure rows
+
+    -- Output the mempool benchmark results as a JSON file, which conforms to
+    -- the input expected by
+    -- https://github.com/benchmark-action/github-action-benchmark
+    convertCsvRowsToJsonObjects rows outFilePath =
+      encodeFile outFilePath $ fmap convertRowToJsonObject rows
+      where
+        convertRowToJsonObject (name:mean:_) =
+          object [ "name"  .= adjustName name
+                 , "value" .= adjustedMean
+                 , "unit"  .= unit
+                 ]
+          where
+            adjustName = Text.replace "."          " "
+                       . Text.replace ".benchmark" ""
+
+            adjustedMean :: Integer
+            (adjustedMean, unit) = first round
+                                 $ convertPicosecondsWithUnit
+                                 $ fromInteger
+                                 $ textToInt mean
+              where
+                textToInt = either error fst . Text.Read.decimal
+
+            -- Convert a number of picoseconds to the largest time unit that
+            -- makes the conversion greater or equal than one.
+            convertPicosecondsWithUnit :: Double -> (Double, String)
+            convertPicosecondsWithUnit n
+                |                        numberOfDigits  <= 4  = (n       , "picoseconds" )
+                | 4 <= numberOfDigits  && numberOfDigits <  7  = (n / 1e3 , "nanoseconds" )
+                | 7 <= numberOfDigits  && numberOfDigits <  10 = (n / 1e6 , "microseconds")
+                | 10 <= numberOfDigits && numberOfDigits <  13 = (n / 1e9 , "milliseconds")
+                | 13 <= numberOfDigits                         = (n / 1e12, "seconds"     )
+              where
+                numberOfDigits :: Int
+                numberOfDigits = floor (logBase 10 n) + 1
+            convertPicosecondsWithUnit _ = error "All the cases should be covered by the conditions above"
+
+        convertRowToJsonObject _             = error "Wrong format"
 
 {-------------------------------------------------------------------------------
   Adding TestBlock transactions to a mempool
 -------------------------------------------------------------------------------}
 
-openMempoolWithCapacipyFor :: [MempoolCmd TestBlock] ->  IO (MempoolWithMockedLedgerItf IO TestBlock)
-openMempoolWithCapacipyFor cmds =
+openMempoolWithCapacityFor :: [MempoolCmd TestBlock] ->  IO (MempoolWithMockedLedgerItf IO TestBlock)
+openMempoolWithCapacityFor cmds =
     openMempoolWithMockedLedgerItf capacityRequiredByCmds
                                    Tracer.nullTracer
                                    TestBlock.txSize
