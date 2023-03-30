1c98543c539c85b2f7d4b7be4ae4139553002147
Javier Sagredo <javier.sagredo@iohk.io>
2023-03-30 11:45:55 +0200

    Use `tools-index-state`

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,7 +1,14 @@
 { sources }:
 # our packages overlay
 pkgs: _:
-with pkgs; {
+with pkgs;
+let
+  tool = name: version: other:
+    haskell-nix.tool localConfig.ghcVersion name ({
+      version = version;
+      index-state = localConfig.tools-index-state;
+    } // other);
+in {
   ouroborosNetworkHaskellPackages = import ./ouroboros-network.nix {
     inherit config pkgs lib stdenv haskell-nix buildPackages;
     inherit (sources) CHaP;
@@ -16,16 +23,13 @@ with pkgs; {
 
   network-docs = callPackage ./network-docs.nix { };
 
-  cabal =
-    haskell-nix.tool localConfig.ghcVersion "cabal" { version = "latest"; };
+  cabal = tool "cabal" "latest" { };
 
-  stylish-haskell = haskell-nix.tool localConfig.ghcVersion "stylish-haskell" {
-    version = "0.14.4.0";
+  stylish-haskell = tool "stylish-haskell" "0.14.4.0" {
     cabalProjectLocal = "allow-older: ghc-lib-parser:base";
   };
 
-  cabal-fmt =
-    haskell-nix.tool localConfig.ghcVersion "cabal-fmt" { version = "latest"; };
+  cabal-fmt = tool "cabal-fmt" "latest" { };
 
   scriv = pkgs.callPackage ./scriv.nix { };
 
