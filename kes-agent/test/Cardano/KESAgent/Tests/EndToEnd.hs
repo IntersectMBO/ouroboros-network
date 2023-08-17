43547a4493a375d0d65688419963504d31c914ac
Tobias Dammers <tdammers@gmail.com>
2023-08-17 09:18:03 +0200

    Fix RTSOPTS bug properly

--- kes-agent/test/Cardano/KESAgent/Tests/EndToEnd.hs
+++ kes-agent/test/Cardano/KESAgent/Tests/EndToEnd.hs
@@ -6,7 +6,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import System.Process
 import System.Exit ( ExitCode (..) )
-import System.Environment ( unsetEnv )
+import System.Environment ( lookupEnv, setEnv, unsetEnv )
 
 tests :: TestTree
 tests =
@@ -17,6 +17,6 @@ tests =
 kesAgentHelp :: Assertion
 kesAgentHelp = do
   kesAgentPath <- readProcess "which" [ "kes-agent" ] ""
-  unsetEnv "GHCRTS"
+  print kesAgentPath
   (exitCode, stdout, stderr) <- readProcessWithExitCode "kes-agent" [ "--help" ] ""
   assertEqual (stdout ++ "\n" ++ stderr) ExitSuccess exitCode
