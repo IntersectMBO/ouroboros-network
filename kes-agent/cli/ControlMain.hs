00e636cbee0429b211b9c2bac7560eba3ee74eb7
Tobias Dammers <tdammers@gmail.com>
2023-08-21 22:25:53 +0200

    Control client CLI

---
+++ kes-agent/cli/ControlMain.hs
@@ -0,0 +1,144 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main
+where
+
+import Cardano.KESAgent.ControlClient
+import Cardano.KESAgent.Protocol
+import Cardano.KESAgent.Pretty
+import Cardano.KESAgent.TextEnvelope
+import Cardano.KESAgent.Serialization
+import Cardano.KESAgent.OCert
+import Cardano.KESAgent.RefCounting
+
+import Cardano.Crypto.Libsodium (sodiumInit)
+import Cardano.Crypto.KES.Class
+import Cardano.Crypto.Libsodium.MLockedSeed (mlockedSeedNewRandom, mlockedSeedFinalize)
+import Ouroboros.Network.RawBearer
+import Ouroboros.Network.Snocket
+
+import Control.Monad ( (>=>) )
+import Control.Monad.Class.MonadThrow (bracket, throwIO, catch, SomeException)
+import Control.Tracer
+import qualified Data.Aeson as JSON
+import Data.Maybe (fromMaybe)
+import Data.Proxy
+import Network.Socket
+import Options.Applicative
+import System.Environment
+import System.IO (hPutStrLn, stderr, hFlush)
+import System.IOManager
+
+data NewKeyOptions =
+  NewKeyOptions
+    { nkoControlPath :: Maybe String
+    , nkoKESVerificationKeyFile :: Maybe FilePath
+    , nkoOpCertFile :: Maybe FilePath
+    }
+    deriving (Show, Eq)
+
+instance Semigroup NewKeyOptions where
+  NewKeyOptions c1 vk1 oc1 <> NewKeyOptions c2 vk2 oc2 =
+    NewKeyOptions (c1 <|> c2) (vk1 <|> vk2) (oc1 <|> oc2)
+
+defNewKeyOptions :: NewKeyOptions =
+  NewKeyOptions
+    { nkoControlPath = Just "/tmp/kes-agent-control.socket"
+    , nkoKESVerificationKeyFile = Just "kes.vkey"
+    , nkoOpCertFile = Just "ocert.cert"
+    }
+
+nkoFromEnv :: IO NewKeyOptions
+nkoFromEnv = do
+  controlPath <- lookupEnv "KES_AGENT_CONTROL_PATH"
+  return NewKeyOptions
+    { nkoControlPath = controlPath
+    , nkoKESVerificationKeyFile = Nothing
+    , nkoOpCertFile = Nothing
+    }
+
+
+pNewKeyOptions =
+  NewKeyOptions
+    <$> option (Just <$> str)
+          (  long "control-address"
+          <> short 'c'
+          <> value Nothing
+          <> metavar "ADDR"
+          <> help "Socket address for 'control' connections to a running kes-agent process"
+          )
+    <*> option (Just <$> str)
+          (  long "kes-verification-key-file"
+          <> value Nothing
+          <> metavar "FILE"
+          <> help "File to write KES verification key to"
+          )
+    <*> option (Just <$> str)
+          (  long "opcert-file"
+          <> value Nothing
+          <> metavar "FILE"
+          <> help "File to read OpCert from"
+          )
+
+data ProgramOptions
+  = RunNewKey NewKeyOptions
+  deriving (Show, Eq)
+
+pProgramOptions = subparser
+  (  command "new-key" (info (RunNewKey <$> pNewKeyOptions) idm)
+  )
+
+runNewKey :: NewKeyOptions -> IO ()
+runNewKey nko' = withIOManager $ \ioManager -> do
+  nkoEnv <- nkoFromEnv
+  let nko = nko' <> nkoEnv <> defNewKeyOptions
+  verKeyFilename <- maybe (error "Missing KES VerKey file") return (nkoKESVerificationKeyFile nko)
+  ocertFilename <- maybe (error "Missing OpCert file") return (nkoOpCertFile nko)
+  controlPath <- maybe (error "No control address") return (nkoControlPath nko)
+  let controlClientOptions =
+        ControlClientOptions
+          { controlClientSnocket = socketSnocket ioManager
+          , controlClientAddress = SockAddrUnix controlPath
+          , controlClientLocalAddress = Nothing
+          }
+  sk <- genKeyKESIO
+  ( do
+      vkKES :: VerKeyKES (KES StandardCrypto) <- deriveVerKeyKES sk
+      let vkEnv = toTextEnvelope (KESVerKey vkKES :: KESVerKey StandardCrypto)
+
+      JSON.encodeFile verKeyFilename vkEnv
+      putStrLn $ "KES VerKey written to " ++ verKeyFilename
+      putStrLn $ "OpCert will be read from " ++ ocertFilename
+      hPutStrLn stderr "Press ENTER to continue..."
+      _ <- getLine
+      json <- maybe (error "Error decoding JSON envelope") return =<<
+                JSON.decodeFileStrict' ocertFilename
+      oc <- either error return $ fromTextEnvelope @(OpCert StandardCrypto) json
+      withNewCRef
+        (forgetSignKeyKES . skWithoutPeriodKES)
+        (SignKeyWithPeriodKES sk (unKESPeriod . ocertKESPeriod . opCert $ oc)) $ \skpVar -> do
+          runControlClient1
+            (Proxy @StandardCrypto)
+            makeSocketRawBearer
+            controlClientOptions
+            skpVar (opCert oc)
+            nullTracer
+    ) `catch` (\(e :: SomeException) -> forgetSignKeyKES sk >> throwIO e)
+
+genKeyKESIO :: KESAlgorithm k => IO (SignKeyKES k)
+genKeyKESIO =
+  bracket
+    mlockedSeedNewRandom
+    mlockedSeedFinalize
+    genKeyKES
+
+programDesc = fullDesc
+
+main :: IO ()
+main = do
+  sodiumInit
+  programOptions <- execParser (info (pProgramOptions <**> helper) programDesc)
+  case programOptions of
+    RunNewKey nko' -> runNewKey nko'
--- kes-agent/test/Cardano/KESAgent/Tests/EndToEnd.hs
+++ kes-agent/test/Cardano/KESAgent/Tests/EndToEnd.hs
@@ -2,19 +2,59 @@ module Cardano.KESAgent.Tests.EndToEnd
   ( tests )
   where
 
+import Paths_kes_agent
+
+import Control.Concurrent.Async
+import Control.Monad.Class.MonadThrow (finally)
 import Test.Tasty
 import Test.Tasty.HUnit
 import System.Process
 import System.Exit ( ExitCode (..) )
 import System.Environment ( lookupEnv, setEnv, unsetEnv )
+import System.IO
 
 tests :: TestTree
 tests =
   testGroup "end to end"
     [ testCase "kes-agent --help" kesAgentHelp
+    , testCase "kes-agent-control --help" kesAgentControlHelp
+    , testCase "kes-agent-control new-key" kesAgentControlNewKey
     ]
 
 kesAgentHelp :: Assertion
 kesAgentHelp = do
   (exitCode, stdout, stderr) <- readProcessWithExitCode "kes-agent" [ "--help" ] ""
   assertEqual (stdout ++ "\n" ++ stderr) ExitSuccess exitCode
+
+kesAgentControlHelp :: Assertion
+kesAgentControlHelp = do
+  (exitCode, stdout, stderr) <- readProcessWithExitCode "kes-agent-control" [ "--help" ] ""
+  assertEqual (stdout ++ "\n" ++ stderr) ExitSuccess exitCode
+
+kesAgentControlNewKey :: Assertion
+kesAgentControlNewKey = withRunningAgent $ do
+  opcertFile <- getDataFileName "fixtures/opcert.cert"
+  let cp' = proc "kes-agent-control" [ "new-key", "--opcert-file", opcertFile ]
+      cp = cp'
+            { std_in = CreatePipe
+            , std_out = CreatePipe
+            , std_err = CreatePipe
+            }
+  (Just hIn, Just hOut, Just hErr, ph) <- createProcess cp
+  (_, (err, out)) <-
+    hPutStrLn hIn "" `concurrently` (hGetContents hErr `concurrently` hGetContents hOut)
+
+  assertEqual "out"
+    [ "KES VerKey written to kes.vkey"
+    , "OpCert will be read from " ++ opcertFile
+    ]
+    (lines out)
+  assertEqual "err"
+    [ "Press ENTER to continue..."
+    ]
+    (lines err)
+
+withRunningAgent :: IO a -> IO a
+withRunningAgent action = do
+  agentProcess <- spawnProcess "kes-agent" [ "run" ]
+  action `finally` terminateProcess agentProcess
