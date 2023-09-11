b7c5f86fac7718648f7964d4938274ae274b4140
Sam Cowger <sam@galois.com>
2023-09-11 10:24:22 -0700

    O.N.Testing.Data.Script: remove error tag

--- ouroboros-network-testing/src/Ouroboros/Network/Testing/Data/Script.hs
+++ ouroboros-network-testing/src/Ouroboros/Network/Testing/Data/Script.hs
@@ -217,9 +217,9 @@ interpretPickScript :: (MonadSTM m, Ord peeraddr)
                     -> STM m (Set peeraddr)
 interpretPickScript scriptVar available pickNum
   | Set.null available
-  = error "interpretPickScript: given empty map to pick from" -- TODO
+  = error "interpretPickScript: given empty map to pick from"
   | pickNum <= 0
-  = error "interpretPickScript: given invalid pickNum" -- TODO
+  = error "interpretPickScript: given invalid pickNum"
 
   | otherwise
   = do pickmembers <- stepScriptSTM scriptVar
