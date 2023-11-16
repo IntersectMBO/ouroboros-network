5135abe524f73bb867447f3cd034d899ffe5dce2
Armando Santos <armando@well-typed.com>
2023-11-16 16:22:36 +0000

    Test GHActions

--- flake.nix
+++ flake.nix
@@ -152,6 +152,7 @@
               packages.ouroboros-network-protocols.components.tests.test.doCheck      = !pkgs.stdenv.hostPlatform.isWindows;
               packages.ouroboros-network-framework.components.tests.sim-tests.doCheck = !pkgs.stdenv.hostPlatform.isWindows;
               packages.ouroboros-network-framework.components.tests.io-tests.doCheck  = !pkgs.stdenv.hostPlatform.isWindows;
+              packages.ouroboros-network-framework.components.tests.bench.doCheck     = !pkgs.stdenv.hostPlatform.isWindows;
               packages.ouroboros-network.components.tests.sim-tests.doCheck           = !pkgs.stdenv.hostPlatform.isWindows;
               packages.ouroboros-network.components.tests.io-tests.doCheck            = !pkgs.stdenv.hostPlatform.isWindows;
               packages.cardano-client.components.tests.test.doCheck                   = !pkgs.stdenv.hostPlatform.isWindows;
