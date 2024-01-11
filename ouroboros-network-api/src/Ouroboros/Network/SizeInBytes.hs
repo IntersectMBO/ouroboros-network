9dd835a96ab31902c158008c51eb9570fdb1a46d
Marcin Szamotulski <coot@coot.me>
2024-01-11 21:28:47 +0100

    ouroboros-network-api: added monoid instance for SizeInBytes

--- ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
+++ ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
@@ -5,6 +5,7 @@
 module Ouroboros.Network.SizeInBytes (SizeInBytes (..)) where
 
 import Control.DeepSeq (NFData (..))
+import Data.Monoid (Sum (..))
 import Data.Word (Word32)
 
 import Data.Measure qualified as Measure
@@ -12,11 +13,13 @@ import NoThunks.Class (NoThunks (..))
 
 newtype SizeInBytes = SizeInBytes { getSizeInBytes :: Word32 }
   deriving (Show, Eq, Ord)
-  deriving Enum     via Word32
-  deriving Num      via Word32
-  deriving Real     via Word32
-  deriving Integral via Word32
-  deriving NoThunks via Word32
+  deriving Enum      via Word32
+  deriving Num       via Word32
+  deriving Real      via Word32
+  deriving Integral  via Word32
+  deriving NoThunks  via Word32
+  deriving Semigroup via Sum Word32
+  deriving Monoid    via Sum Word32
   deriving newtype NFData
   deriving Measure.Measure        via Word32
   deriving Measure.BoundedMeasure via Word32
