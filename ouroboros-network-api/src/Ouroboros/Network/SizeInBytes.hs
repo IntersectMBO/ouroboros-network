dba93de88b5b5e396025a5c8dba923439dfc8c23
Marcin Szamotulski <coot@coot.me>
2024-03-15 13:29:19 +0100

    ouroboros-network-api: SizeInBytes Show instance
    Use Quiet to derive the Show instance.

--- ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
+++ ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
@@ -1,17 +1,21 @@
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE DerivingVia                #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric              #-}
 
 module Ouroboros.Network.SizeInBytes (SizeInBytes (..)) where
 
 import Control.DeepSeq (NFData (..))
 import Data.Monoid (Sum (..))
 import Data.Word (Word32)
+import GHC.Generics
 
+import Quiet (Quiet (..))
 import NoThunks.Class (NoThunks (..))
 
 newtype SizeInBytes = SizeInBytes { getSizeInBytes :: Word32 }
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
+  deriving Show      via Quiet SizeInBytes
   deriving Enum      via Word32
   deriving Num       via Word32
   deriving Real      via Word32
@@ -19,4 +23,5 @@ newtype SizeInBytes = SizeInBytes { getSizeInBytes :: Word32 }
   deriving NoThunks  via Word32
   deriving Semigroup via Sum Word32
   deriving Monoid    via Sum Word32
+  deriving Generic
   deriving newtype NFData
