66756a601c35844235c5f75dc346d5acb6e2b87b
Marcin Szamotulski <coot@coot.me>
2024-04-14 14:58:55 +0200

    check-stylish: added ignore file
    `Ouroboros.Network.PeerSelection.Governor.Type` excluded from
    `check-stylish`, to preserve large export of record names in pattern
    synonyms.

--- scripts/ci/check-stylish.sh
+++ scripts/ci/check-stylish.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 export LC_ALL=C.UTF-8
 # TODO CPP pragmas in export lists are not supported by stylish-haskell
-fd . './network-mux' -e hs -E Setup.hs -E TCPInfo.hs -E Pipe.hs -E Channel.hs -E Bearer.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-api' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-framework' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-mock' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-protocols' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './cardano-client' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './network-mux' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './ouroboros-network-api' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './ouroboros-network-framework' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './ouroboros-network-mock' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './ouroboros-network-protocols' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './ouroboros-network' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+fd . './cardano-client' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
