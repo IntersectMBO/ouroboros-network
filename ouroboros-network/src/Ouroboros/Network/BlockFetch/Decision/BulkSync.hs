7cb9bf75bca147c22b4eb4a269b9b713b16fcc01
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-06-10 09:49:56 +0000

    Yay movin' forward

--- ouroboros-network/src/Ouroboros/Network/BlockFetch/Decision/BulkSync.hs
+++ ouroboros-network/src/Ouroboros/Network/BlockFetch/Decision/BulkSync.hs
@@ -17,6 +17,7 @@ import Ouroboros.Network.AnchoredFragment (AnchoredFragment, headBlockNo)
 import Ouroboros.Network.Block
 import Ouroboros.Network.BlockFetch.ClientState (FetchRequest (..),
            PeerFetchInFlight (..), PeerFetchStatus (..))
+import Ouroboros.Network.BlockFetch.ConsensusInterface (FetchMode(FetchModeBulkSync))
 
 import Ouroboros.Network.BlockFetch.Decision.Common
 -- REVIEW: We should not import anything from 'Decision.Deadline'; if the need
@@ -34,43 +35,78 @@ fetchDecisionsBulkSync
   -> [(FetchDecision (FetchRequest header), PeerInfo header peer extra)]
 
 fetchDecisionsBulkSync
-  _fetchDecisionPolicy@FetchDecisionPolicy{plausibleCandidateChain}
+  _fetchDecisionPolicy@FetchDecisionPolicy {plausibleCandidateChain}
   currentChain
   fetchedBlocks
   fetchedMaxSlotNo
-  =
-  -- FIXME: Wrap in a 'FetchRequest'.
-  map (first (fmap FetchRequest))
+  candidatesAndPeers =
+    -- Sort candidates from longest to shortest and filter out all unplausible
+    -- candidates. This gives a list of already-declined candidates and a list
+    -- of plausible candidates.
+    let (declinedCandidates, candidates) =
+          partitionEithersFirst
+            -- Filter to keep chains the consensus layer tells us are plausible.
+            . filterPlausibleCandidates
+              plausibleCandidateChain
+              currentChain
+            -- Sort the candidates by descending block number of their heads, that is
+            -- consider longest fragments first.
+            . sortOn (Down . headBlockNo . fst)
+            $ candidatesAndPeers
+        declinedCandidates' = map (first Left) declinedCandidates
+     in
+      -- If there are no candidates remaining, we are done. Otherwise, pick the
+      -- first one and try to fetch it. Decline all the others.
+      case candidates of
+          [] -> declinedCandidates'
+          ((theCandidate, _thePeer) : otherCandidates) ->
+            let declinedOtherCandidates = map (first (const (Left (FetchDeclineConcurrencyLimit FetchModeBulkSync 1)))) otherCandidates
+             in fetchTheCandidate
+                  candidatesAndPeers
+                  theCandidate
+                  : (declinedCandidates' ++ declinedOtherCandidates)
+    where
+      partitionEithersFirst :: [(Either a b, c)] -> ([(a, c)], [(b, c)])
+      partitionEithersFirst =
+        foldr
+          ( \(e, c) (as, bs) -> case e of
+              Left a -> ((a, c) : as, bs)
+              Right b -> (as, (b, c) : bs)
+          )
+          ([], [])
 
-    -- Filter to keep blocks that are not already in-flight with other peers.
-  . filterNotAlreadyInFlightWithOtherPeers
-  . map (swizzleSI . first (fmap snd))
+fetchTheCandidate ::
+  -- (HasHeader header, HeaderHash header ~ HeaderHash block) =>
+  [(AnchoredFragment header, PeerInfo header peer extra)] ->
+  AnchoredFragment header ->
+  (FetchDecision (FetchRequest header), PeerInfo header peer extra)
+fetchTheCandidate _candidatesAndPeers _theCandidate =
+  undefined
 
-    -- Filter to keep blocks that are not already in-flight for this peer.
-  . filterNotAlreadyInFlightWithPeer'
-  . map swizzleI
+-- -- FIXME: Wrap in a 'FetchRequest'.
+  -- map (first (fmap FetchRequest))
 
-    -- Filter to keep blocks that have not already been downloaded.
-  . filterNotAlreadyFetched'
-      fetchedBlocks
-      fetchedMaxSlotNo
+  --   -- Filter to keep blocks that are not already in-flight with other peers.
+  -- . filterNotAlreadyInFlightWithOtherPeers
+  -- . map (swizzleSI . first (fmap snd))
 
-    -- Select the suffix up to the intersection with the current chain.
-  . selectForkSuffixes
-      currentChain
+  --   -- Filter to keep blocks that are not already in-flight for this peer.
+  -- . filterNotAlreadyInFlightWithPeer'
+  -- . map swizzleI
 
-    -- Filter to keep chains the consensus layer tells us are plausible.
-  . filterPlausibleCandidates
-      plausibleCandidateChain
-      currentChain
+  --   -- Filter to keep blocks that have not already been downloaded.
+  -- . filterNotAlreadyFetched'
+  --     fetchedBlocks
+  --     fetchedMaxSlotNo
 
-    -- Sort the candidates by descending block number of their heads, that is
-    -- consider longest fragments first.
-   . sortOn (Down . headBlockNo . fst)
-  where
-    -- Data swizzling functions to get the right info into each stage.
-    swizzleI   (c, p@(_,     inflight,_,_,      _)) = (c,         inflight,       p)
-    swizzleSI  (c, p@(status,inflight,_,_,      _)) = (c, status, inflight,       p)
+  --   -- Select the suffix up to the intersection with the current chain.
+  -- . selectForkSuffixes
+  --     currentChain
+
+  -- where
+  --   -- Data swizzling functions to get the right info into each stage.
+  --   swizzleI   (c, p@(_,     inflight,_,_,      _)) = (c,         inflight,       p)
+  --   swizzleSI  (c, p@(status,inflight,_,_,      _)) = (c, status, inflight,       p)
 
 -- | A penultimate step of filtering, but this time across peers, rather than
 -- individually for each peer. If we're following the parallel fetch
