a5c268d1c23126e7c3dc5ef1b9454103500dd828
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-06-25 20:57:30 +0200

    Update the peers order following the blockfetch decision

--- ouroboros-network/src/Ouroboros/Network/BlockFetch/Decision.hs
+++ ouroboros-network/src/Ouroboros/Network/BlockFetch/Decision.hs
@@ -25,10 +25,11 @@ module Ouroboros.Network.BlockFetch.Decision
 import Data.Bifunctor (Bifunctor(..))
 import Data.Hashable
 import Data.List (singleton)
+import Data.Maybe (listToMaybe)
 
 import Ouroboros.Network.AnchoredFragment (AnchoredFragment)
 import Ouroboros.Network.Block
-import Ouroboros.Network.BlockFetch.ClientState (FetchRequest (..), PeersOrder)
+import Ouroboros.Network.BlockFetch.ClientState (FetchRequest (..), PeersOrder (..))
 import Ouroboros.Network.BlockFetch.ConsensusInterface (FetchMode (..))
 
 import Ouroboros.Network.BlockFetch.Decision.Common (FetchDecisionPolicy (..), PeerInfo, FetchDecision, FetchDecline (..),
@@ -43,13 +44,15 @@ fetchDecisions
       Hashable peer,
       HasHeader header,
       HeaderHash header ~ HeaderHash block,
-      Applicative m)
+      Monad m)
   => FetchDecisionPolicy header
   -> FetchMode
   -> AnchoredFragment header
   -> (Point block -> Bool)
   -> MaxSlotNo
-  -> PeersOrder peer
+  -> ( PeersOrder peer
+     , PeersOrder peer -> m ()
+     )
   -> [(AnchoredFragment header, PeerInfo header peer extra)]
   -> m [(FetchDecision (FetchRequest header), PeerInfo header peer extra)]
 
@@ -76,16 +79,47 @@ fetchDecisions
   currentChain
   fetchedBlocks
   fetchedMaxSlotNo
-  peersOrder
+  ( PeersOrder {peersOrderAll},
+    writePeersOrder
+    )
   candidatesAndPeers = do
+    -- Align the peers order with the actual peers; this consists in removing
+    -- all peers from the peers order that are not in the actual peers list and
+    -- adding at the end of the peers order all the actual peers that were not
+    -- there before.
+    let actualPeers = map (\(_, (_, _, _, peer, _)) -> peer) candidatesAndPeers
+        peersOrderAll' =
+          filter (`elem` actualPeers) peersOrderAll
+            ++ filter (`notElem` peersOrderAll) actualPeers
+        peersOrder' = PeersOrder peersOrderAll'
+
+    -- FIXME: If ChainSel was starved, push the current peer to the end of the
+    -- peers order priority queue.
+
+    -- Compute the actual block fetch decision. This contains only declines and
+    -- at most one request. 'theDecision' is therefore a 'Maybe'.
     let (theDecision, declines) =
           fetchDecisionsBulkSync
             fetchDecisionPolicy
             currentChain
             fetchedBlocks
             fetchedMaxSlotNo
-            peersOrder
+            peersOrder'
             candidatesAndPeers
+
+    -- If the peer that is supposed to fetch the block is not the first in the
+    -- peers order, then we have changed focused peer. We move the new peer at
+    -- the beginning of the queue, but we do not push the other one away,
+    -- because it has not done anything wrong.
+    case theDecision of
+      Just (_, (_, _, _, thePeer, _))
+        | Just thePeer /= listToMaybe peersOrderAll -> do
+            let peersOrder'' = PeersOrder $ thePeer : filter (/= thePeer) peersOrderAll'
+            -- FIXME: Record the current time as the first time we chose that
+            -- new peer.
+            writePeersOrder peersOrder''
+      _ -> pure ()
+
     pure $
       maybe [] (singleton . first Right) theDecision
         ++ map (first Left) declines
