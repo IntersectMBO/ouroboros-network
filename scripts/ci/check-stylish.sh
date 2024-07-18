e4a28e90be7894d08b06a106aa72acac5bc4646a
Marcin Szamotulski <coot@coot.me>
2024-07-18 09:53:12 +0200

    stylish-haskell: added flags
    * `-c` only check committed files in HEAD
    * `-u` only check uncommitted files
    * `-h` show help message
    
    Without any flag; check all files.

--- scripts/ci/check-stylish.sh
+++ scripts/ci/check-stylish.sh
@@ -2,13 +2,70 @@
 
 set -euo pipefail
 
+function usage {
+  echo "Usage $(basename "$0") [-ch]"
+  echo "Check files with 'stylish-haskell'; by default check all files."
+  echo
+  echo "        -u                        only check files uncommitted"
+  echo "        -c                        only check files committed in HEAD"
+  echo "        -h                        this help message"
+  exit
+}
+
 export LC_ALL=C.UTF-8
+
+STYLISH_HASKELL_ARGS="-c .stylish-haskell-network.yaml -i"
+
+optstring=":uch"
+while getopts ${optstring} arg; do
+  case ${arg} in
+    h)
+      usage;
+      exit 0
+      ;;
+    c)
+      PATHS=$(git show --pretty='' --name-only HEAD)
+      for path in $PATHS; do
+        if [ "${path##*.}" == "hs" ]; then
+          if grep -qE '^#' $path; then
+            echo "$path contains CPP.  Skipping."
+          else
+            echo $path
+            stylish-haskell $STYLISH_HASKELL_ARGS $path
+          fi
+        fi
+      done
+      exit 0
+      ;;
+    u)
+      PATHS=$(git diff --name-only HEAD)
+      for path in $PATHS; do
+        if [ "${path##*.}" == "hs" ]; then
+          if grep -qE '^#' $path; then
+            echo "$path contains CPP.  Skipping."
+          else
+            echo $path
+            stylish-haskell $STYLISH_HASKELL_ARGS $path
+          fi
+        fi
+      done
+      exit 0
+      ;;
+    ?)
+      echo "Invalid argument ${arg}"
+      exit 1
+      ;;
+  esac
+done
+
 # TODO CPP pragmas in export lists are not supported by stylish-haskell
-fd . './quickcheck-monoids' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './network-mux' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-api' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-framework' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-mock' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network-protocols' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './ouroboros-network' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
-fd . './cardano-client' -e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell -c .stylish-haskell-network.yaml -i
+FD_OPTS="-e hs --ignore-file ./scripts/ci/check-stylish-ignore -X stylish-haskell $STYLISH_HASKELL_ARGS"
+
+fd . './quickcheck-monoids'          $FD_OPTS
+fd . './network-mux'                 $FD_OPTS
+fd . './ouroboros-network-api'       $FD_OPTS
+fd . './ouroboros-network-framework' $FD_OPTS
+fd . './ouroboros-network-mock'      $FD_OPTS
+fd . './ouroboros-network-protocols' $FD_OPTS
+fd . './ouroboros-network'           $FD_OPTS
+fd . './cardano-client'              $FD_OPTS
