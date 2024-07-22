2039a55bc51e1d60fae66d5ea05cdaf23afa2c27
Marcin Szamotulski <coot@coot.me>
2024-07-22 18:41:17 +0200

    ouroboros-network: tx-submission module structure
    Export everything from the `Ouroboros.Network.TxSubmission.Inbound`
    module.

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound.hs
@@ -12,10 +12,14 @@
 -- | Legacy `tx-submission` inbound peer.
 --
 module Ouroboros.Network.TxSubmission.Inbound
-  ( txSubmissionInbound
-  , TxSubmissionMempoolWriter (..)
-  , TraceTxSubmissionInbound (..)
-  , TxSubmissionProtocolError (..)
+  ( -- * New Tx-Submission server
+    module Server
+  , module Types
+  , module Decision
+  , module Registry
+  , module Policy
+    -- * Legacy Tx-Submission server
+  , txSubmissionInbound
   , ProcessedTxCount (..)
   ) where
 
@@ -47,70 +51,19 @@ import Ouroboros.Network.NodeToNode.Version (NodeToNodeVersion)
 import Ouroboros.Network.Protocol.Limits
 import Ouroboros.Network.Protocol.TxSubmission2.Server
 import Ouroboros.Network.Protocol.TxSubmission2.Type
-import Ouroboros.Network.TxSubmission.Inbound.Decision (TxDecision)
+import Ouroboros.Network.TxSubmission.Inbound.Types
 import Ouroboros.Network.TxSubmission.Mempool.Reader (MempoolSnapshot (..),
            TxSubmissionMempoolReader (..))
 
--- | The consensus layer functionality that the inbound side of the tx
--- submission logic requires.
 --
--- This is provided to the tx submission logic by the consensus layer.
+-- re-exports
 --
-data TxSubmissionMempoolWriter txid tx idx m =
-     TxSubmissionMempoolWriter {
 
-       -- | Compute the transaction id from a transaction.
-       --
-       -- This is used in the protocol handler to verify a full transaction
-       -- matches a previously given transaction id.
-       --
-       txId          :: tx -> txid,
-
-       -- | Supply a batch of transactions to the mempool. They are either
-       -- accepted or rejected individually, but in the order supplied.
-       --
-       -- The 'txid's of all transactions that were added successfully are
-       -- returned.
-       mempoolAddTxs :: [tx] -> m [txid]
-    }
-
-data ProcessedTxCount = ProcessedTxCount {
-      -- | Just accepted this many transactions.
-      ptxcAccepted :: Int
-      -- | Just rejected this many transactions.
-    , ptxcRejected :: Int
-    }
-  deriving (Eq, Show)
-
-data TraceTxSubmissionInbound txid tx =
-    -- | Number of transactions just about to be inserted.
-    TraceTxSubmissionCollected Int
-    -- | Just processed transaction pass/fail breakdown.
-  | TraceTxSubmissionProcessed ProcessedTxCount
-    -- | Server received 'MsgDone'
-  | TraceTxInboundCanRequestMoreTxs Int
-  | TraceTxInboundCannotRequestMoreTxs Int
-
-  --
-  -- messages emitted by the new implementation of the server in
-  -- "Ouroboros.Network.TxSubmission.Inbound.Server"; some of them are also
-  -- used in this module.
-  --
-
-  | TraceTxInboundTerminated
-  | TraceTxInboundDecision (TxDecision txid tx)
-  deriving (Eq, Show)
-
-data TxSubmissionProtocolError =
-       ProtocolErrorTxNotRequested
-     | ProtocolErrorTxIdsNotRequested
-  deriving Show
-
-instance Exception TxSubmissionProtocolError where
-  displayException ProtocolErrorTxNotRequested =
-      "The peer replied with a transaction we did not ask for."
-  displayException ProtocolErrorTxIdsNotRequested =
-      "The peer replied with more txids than we asked for."
+import Ouroboros.Network.TxSubmission.Inbound.Types as Types
+import Ouroboros.Network.TxSubmission.Inbound.Decision as Decision
+import Ouroboros.Network.TxSubmission.Inbound.Registry as Registry
+import Ouroboros.Network.TxSubmission.Inbound.Policy as Policy
+import Ouroboros.Network.TxSubmission.Inbound.Server as Server
 
 
 -- | Information maintained internally in the 'txSubmissionInbound' server
