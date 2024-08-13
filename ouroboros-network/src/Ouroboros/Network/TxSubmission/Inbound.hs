539fee242bfb7847c68b77b29c9afc4830c6ba4b
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2024-08-13 11:36:28 +0200

    TxSubmission - delay initial request for transactions
    This change delays initial request for transactions from the client,
    whom may disconnect relatively quickly for reasons described in
    IntersectMBO/ouroboros-network issue #4927.

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE GADTs               #-}
@@ -20,6 +21,7 @@ import Data.Foldable (foldl', toList)
 import Data.List.NonEmpty qualified as NonEmpty
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
+import Data.Maybe
 import Data.Sequence.Strict (StrictSeq)
 import Data.Sequence.Strict qualified as Seq
 import Data.Set qualified as Set
@@ -34,11 +36,13 @@ import Control.Exception (assert)
 import Control.Monad (unless)
 import Control.Monad.Class.MonadSTM
 import Control.Monad.Class.MonadThrow
+import Control.Monad.Class.MonadTimer.SI
 import Control.Tracer (Tracer, traceWith)
 
 import Network.TypedProtocol.Pipelined (N, Nat (..), natToInt)
 
 import Ouroboros.Network.NodeToNode.Version (NodeToNodeVersion)
+import Ouroboros.Network.Protocol.Limits
 import Ouroboros.Network.Protocol.TxSubmission2.Server
 import Ouroboros.Network.TxSubmission.Mempool.Reader (MempoolSnapshot (..),
            TxSubmissionMempoolReader (..))
@@ -175,6 +179,7 @@ txSubmissionInbound
      , NoThunks tx
      , MonadSTM m
      , MonadThrow m
+     , MonadDelay m
      )
   => Tracer m (TraceTxSubmissionInbound txid tx)
   -> Word16         -- ^ Maximum number of unacknowledged txids allowed
@@ -183,7 +188,12 @@ txSubmissionInbound
   -> NodeToNodeVersion
   -> TxSubmissionServerPipelined txid tx m ()
 txSubmissionInbound tracer maxUnacked mpReader mpWriter _version =
-    TxSubmissionServerPipelined $
+    TxSubmissionServerPipelined $ do
+#ifdef TXSUBMISSION_DELAY
+      -- make the client linger before asking for tx's and expending
+      -- our resources as well, as he may disconnect for some reason
+      threadDelay (fromMaybe (-1) longWait)
+#endif    
       continueWithStateM (serverIdle Zero) initialServerState
   where
     -- TODO #1656: replace these fixed limits by policies based on
