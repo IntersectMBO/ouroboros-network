4a9432f357fd0944afb12d2932b832a1d04ccbda
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2024-08-19 17:13:34 +0200

    WIP txsub withbytes

--- ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
+++ ouroboros-network-api/src/Ouroboros/Network/SizeInBytes.hs
@@ -1,19 +1,48 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE DerivingVia                #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns             #-}
 
-module Ouroboros.Network.SizeInBytes (SizeInBytes (..)) where
+module Ouroboros.Network.SizeInBytes (SizeInBytes (..), WithBytes (..)) where
 
 import Control.DeepSeq (NFData (..))
 import Data.Monoid (Sum (..))
+import Data.Typeable
 import Data.Word (Word32)
 import GHC.Generics
+import Data.ByteString.Short (ShortByteString, fromShort, toShort)
+import Data.ByteString.Lazy
 
+import Cardano.Binary (ToCBOR (toCBOR))
+import Cardano.Binary qualified as Codec
+import Cardano.Ledger.Binary.Decoding
 import Data.Measure qualified as Measure
-import NoThunks.Class (NoThunks (..))
+import NoThunks.Class (AllowThunksIn (..), NoThunks (..))
 import Quiet (Quiet (..))
 
+data WithBytes a = WithBytes {
+  wbValue :: !a,
+  wbBytes :: ShortByteString }
+  deriving stock (Eq, Generic)
+  deriving (NoThunks) via AllowThunksIn '["wbBytes"] (WithBytes a)
+
+instance (Show a) => Show (WithBytes a) where
+  show (WithBytes { wbValue }) = show wbValue
+  
+instance (Typeable a, DecCBOR (Annotator a)) => DecCBOR (Annotator (WithBytes a)) where
+  decCBOR = do
+    (Annotator getValue, Annotator getBytes) <- withSlice decCBOR
+    return . Annotator $ \fullbytes -> WithBytes
+                                         (getValue fullbytes)
+                                         (toShort . toStrict . getBytes $ fullbytes)
+
+instance Typeable a => ToCBOR (WithBytes a) where
+  toCBOR (WithBytes { wbBytes })= Codec.encodePreEncoded (fromShort wbBytes)
+  
 newtype SizeInBytes = SizeInBytes { getSizeInBytes :: Word32 }
   deriving (Eq, Ord)
   deriving Show      via Quiet SizeInBytes
