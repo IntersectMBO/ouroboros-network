f4c2dc453c9ca4f57899534e364a22e4258295c8
Marcin Szamotulski <coot@coot.me>
2024-09-16 07:36:50 +0200

    tx-submission: added DebugTxLogic tracer
    Module structure needs to be reorganised to have just one debug tracer.

--- ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/TxSubmission/TxSubmissionV2.hs
+++ ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/TxSubmission/TxSubmissionV2.hs
@@ -137,6 +137,7 @@ runTxSubmission
      )
   => Tracer m (String, TraceSendRecv (TxSubmission2 txid (Tx txid)))
   -> Tracer m (DebugSharedTxState peeraddr txid (Tx txid))
+  -> Tracer m (DebugTxLogic peeraddr txid (Tx txid))
   -> Map peeraddr ( [Tx txid]
                   , ControlMessageSTM m
                   , Maybe DiffTime
@@ -144,7 +145,7 @@ runTxSubmission
                   )
   -> TxDecisionPolicy
   -> m ([Tx txid], [[Tx txid]])
-runTxSubmission tracer tracerDST state txDecisionPolicy = do
+runTxSubmission tracer tracerDST tracerTxLogic state txDecisionPolicy = do
 
     state' <- traverse (\(b, c, d, e) -> do
         mempool <- newMempool b
@@ -189,10 +190,10 @@ runTxSubmission tracer tracerDST state txDecisionPolicy = do
         -> m b
     run st txChannelsVar sharedTxStateVar
         inboundMempool gsvVar k =
-      withAsync (decisionLogicThread tracerDST txDecisionPolicy (Strict.readTVar gsvVar) txChannelsVar sharedTxStateVar) $ \a -> do
+      withAsync (decisionLogicThread tracerTxLogic txDecisionPolicy (Strict.readTVar gsvVar) txChannelsVar sharedTxStateVar) $ \a -> do
             -- Construct txSubmission outbound client
         let clients = (\(addr, (mempool, ctrlMsgSTM, outDelay, _, outChannel, _)) -> do
-                        let client = txSubmissionOutbound verboseTracer
+                        let client = txSubmissionOutbound (Tracer $ say . show)
                                                (NumTxIdsToAck $ getNumTxIdsToReq
                                                               $ maxUnacknowledgedTxIds
                                                               $ txDecisionPolicy)
@@ -273,9 +274,9 @@ txSubmissionSimulation (TxSubmissionState state txDecisionPolicy) = do
         threadDelay (simDelayTime + 1000)
         atomically (traverse_ (`writeTVar` Terminate) controlMessageVars)
 
-  let tracer = verboseTracer <> debugTracer
-      tracer' = verboseTracer <> debugTracer
-  runTxSubmission tracer tracer' state'' txDecisionPolicy
+  let tracer :: forall a. Show a => Tracer (IOSim s) a
+      tracer = verboseTracer <> debugTracer
+  runTxSubmission tracer tracer tracer state'' txDecisionPolicy
 
 -- | Tests overall tx submission semantics. The properties checked in this
 -- property test are the same as for tx submission v1. We need this to know we
