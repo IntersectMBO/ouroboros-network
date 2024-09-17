ddfa9bbb1cb34226cff257d423377309699e6d26
Marcin Szamotulski <coot@coot.me>
2024-09-17 19:45:47 +0200

    tx-submission: put common types in one place
    This allows us to have just one tracer for tx-submission decision logic.

--- ouroboros-network/testlib/Test/Ouroboros/Network/Diffusion/Testnet/Cardano/Simulation.hs
+++ ouroboros-network/testlib/Test/Ouroboros/Network/Diffusion/Testnet/Cardano/Simulation.hs
@@ -134,9 +134,8 @@ import Ouroboros.Network.Protocol.TxSubmission2.Codec (byteLimitsTxSubmission2,
 import Ouroboros.Network.Server qualified as Server
 import Ouroboros.Network.Snocket (Snocket, TestAddress (..))
 import Ouroboros.Network.TxSubmission.Inbound.Policy (TxDecisionPolicy)
-import Ouroboros.Network.TxSubmission.Inbound.Registry (DebugTxLogic)
-import Ouroboros.Network.TxSubmission.Inbound.State (DebugSharedTxState)
-import Ouroboros.Network.TxSubmission.Inbound.Types (TraceTxSubmissionInbound)
+import Ouroboros.Network.TxSubmission.Inbound.Types (TraceTxLogic,
+           TraceTxSubmissionInbound)
 
 import Ouroboros.Network.Mock.ConcreteBlock (Block (..), BlockHeader (..))
 import Simulation.Network.Snocket (BearerInfo (..), FD, SnocketTrace,
@@ -155,8 +154,8 @@ import Test.Ouroboros.Network.PeerSelection.RootPeersDNS (DNSLookupDelay (..),
            DNSTimeout (..), DomainAccessPoint (..), MockDNSMap, genDomainName)
 import Test.Ouroboros.Network.PeerSelection.RootPeersDNS qualified as PeerSelection hiding
            (tests)
-import Test.Ouroboros.Network.TxSubmission.Common (ArbTxDecisionPolicy (..),
-           Tx (..))
+import Test.Ouroboros.Network.TxSubmission.TxLogic (ArbTxDecisionPolicy (..))
+import Test.Ouroboros.Network.TxSubmission.Types (Tx (..))
 import Test.Ouroboros.Network.Utils
 import Test.QuickCheck
 
@@ -993,8 +992,7 @@ data DiffusionTestTrace =
     | DiffusionServerTrace (Server.Trace NtNAddr)
     | DiffusionFetchTrace (TraceFetchClientState BlockHeader)
     | DiffusionTxSubmissionInbound (TraceTxSubmissionInbound Int (Tx Int))
-    | DiffusionTxSubmissionDebug (DebugSharedTxState NtNAddr Int (Tx Int))
-    | DiffusionTxLogicDebug (DebugTxLogic NtNAddr Int (Tx Int))
+    | DiffusionTxLogic (TraceTxLogic NtNAddr Int (Tx Int))
     | DiffusionDebugTrace String
     | DiffusionDNSTrace DNSTrace
     deriving (Show)
@@ -1340,11 +1338,7 @@ diffusionSimulation
           . tracerWithName addr
           . tracerWithTime
           $ nodeTracer)
-          ( contramap DiffusionTxSubmissionDebug
-          . tracerWithName addr
-          . tracerWithTime
-          $ nodeTracer)
-          ( contramap DiffusionTxLogicDebug
+          ( contramap DiffusionTxLogic
           . tracerWithName addr
           . tracerWithTime
           $ nodeTracer)
