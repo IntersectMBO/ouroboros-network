b4def059a9217ca00acfb9124085ddea60581331
Marcin Szamotulski <coot@coot.me>
2024-09-17 19:45:47 +0200

    tx-submission: put common types in one place
    This allows us to have just one tracer for tx-submission decision logic.

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/Types.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/Types.hs
@@ -1,13 +1,256 @@
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Ouroboros.Network.TxSubmission.Inbound.Types
-  ( ProcessedTxCount (..)
+  ( -- * PeerTxState
+    PeerTxState (..)
+    -- * SharedTxState
+  , SharedTxState (..)
+    -- * Decisions
+  , TxDecision (..)
+  , emptyTxDecision
+  , SharedDecisionContext (..)
+    -- * Various
+  , ProcessedTxCount (..)
+    -- * Mempool API
   , TxSubmissionMempoolWriter (..)
+    -- * Traces
   , TraceTxSubmissionInbound (..)
+  , TraceTxLogic (..)
+    -- * Protocol Error
   , TxSubmissionProtocolError (..)
   ) where
 
 import Control.Exception (Exception (..))
+import Data.Map.Strict (Map)
+import Data.Sequence.Strict (StrictSeq)
+import Data.Set (Set)
+import Data.Set qualified as Set
+import GHC.Generics (Generic)
 
-import Ouroboros.Network.TxSubmission.Inbound.Decision (TxDecision (..))
+import NoThunks.Class (NoThunks (..))
+
+import Ouroboros.Network.DeltaQ (PeerGSV (..))
+import Ouroboros.Network.Protocol.TxSubmission2.Type
+
+--
+-- PeerTxState, SharedTxState
+--
+
+data PeerTxState txid tx = PeerTxState {
+       -- | Those transactions (by their identifier) that the client has told
+       -- us about, and which we have not yet acknowledged. This is kept in
+       -- the order in which the client gave them to us. This is the same order
+       -- in which we submit them to the mempool (or for this example, the final
+       -- result order). It is also the order we acknowledge in.
+       --
+       unacknowledgedTxIds      :: !(StrictSeq txid),
+
+       -- | Set of known transaction ids which can be requested from this peer.
+       --
+       availableTxIds           :: !(Map txid SizeInBytes),
+
+       -- | The number of transaction identifiers that we have requested but
+       -- which have not yet been replied to. We need to track this it keep
+       -- our requests within the limit on the number of unacknowledged txids.
+       --
+       requestedTxIdsInflight   :: !NumTxIdsToReq,
+
+       -- | The size in bytes of transactions that we have requested but which
+       -- have not yet been replied to. We need to track this it keep our
+       -- requests within the limit on the number of unacknowledged txids.
+       --
+       requestedTxsInflightSize :: !SizeInBytes,
+
+       -- | The set of requested `txid`s.
+       --
+       requestedTxsInflight     :: !(Set txid),
+
+       -- | A subset of `unacknowledgedTxIds` which were unknown to the peer.
+       -- We need to track these `txid`s since they need to be acknowledged.
+       --
+       -- We track these `txid` per peer, rather than in `bufferedTxs` map,
+       -- since that could potentially lead to corrupting the node, not being
+       -- able to download a `tx` which is needed & available from other nodes.
+       --
+       unknownTxs               :: !(Set txid)
+    }
+    deriving (Eq, Show, Generic)
+
+instance ( NoThunks txid
+         , NoThunks tx
+         ) => NoThunks (PeerTxState txid tx)
+
+
+-- | Shared state of all `TxSubmission` clients.
+--
+-- New `txid` enters `unacknowledgedTxIds` it is also added to `availableTxIds`
+-- and `referenceCounts` (see `acknowledgeTxIdsImpl`).
+--
+-- When a `txid` id is selected to be downloaded, it's added to
+-- `requestedTxsInflightSize` (see
+-- `Ouroboros.Network.TxSubmission.Inbound.Decision.pickTxsToDownload`).
+--
+-- When the request arrives, the `txid` is removed from `inflightTxs`.  It
+-- might be added to `unknownTxs` if the server didn't have that `txid`, or
+-- it's added to `bufferedTxs` (see `collectTxsImpl`).
+--
+-- Whenever we choose `txid` to acknowledge (either in `acknowledtxsIdsImpl`,
+-- `collectTxsImpl` or
+-- `Ouroboros.Network.TxSubmission.Inbound.Decision.pickTxsToDownload`, we also
+-- recalculate `referenceCounts` and only keep live `txid`s in other maps (e.g.
+-- `availableTxIds`, `bufferedTxs`, `unknownTxs`).
+--
+data SharedTxState peeraddr txid tx = SharedTxState {
+
+      -- | Map of peer states.
+      --
+      -- /Invariant:/ for peeraddr's which are registered using `withPeer`,
+      -- there's always an entry in this map even if the set of `txid`s is
+      -- empty.
+      --
+      peerTxStates    :: !(Map peeraddr (PeerTxState txid tx)),
+
+      -- | Set of transactions which are in-flight (have already been
+      -- requested) together with multiplicities (from how many peers it is
+      -- currently in-flight)
+      --
+      -- This set can intersect with `availableTxIds`.
+      --
+      inflightTxs     :: !(Map txid Int),
+
+      -- | Overall size of all `tx`s in-flight.
+      --
+      inflightTxsSize :: !SizeInBytes,
+
+      -- | Map of `tx` which:
+      --
+      --    * were downloaded,
+      --    * are already in the mempool (`Nothing` is inserted in that case),
+      --
+      -- We only keep live `txid`, e.g. ones which `txid` is unacknowledged by
+      -- at least one peer.
+      --
+      -- /Note:/ `txid`s which `tx` were unknown by a peer are tracked
+      -- separately in `unknownTxs`.
+      --
+      -- /Note:/ previous implementation also needed to explicitly tracked
+      -- `txid`s which were already acknowledged, but are still unacknowledged.
+      -- In this implementation, this is done due to reference counting.
+      --
+      -- This map is useful to acknowledge `txid`s, it's basically taking the
+      -- longest prefix which contains entries in `bufferedTxs` or `unknownTxs`.
+      --
+      bufferedTxs     :: !(Map txid (Maybe tx)),
+
+      -- | We track reference counts of all unacknowledged txids.  Once the
+      -- count reaches 0, a tx is removed from `bufferedTxs`.
+      --
+      -- The `bufferedTx` map contains a subset of `txid` which
+      -- `referenceCounts` contains.
+      --
+      -- /Invariants:/
+      --
+      --    * the txid count is equal to multiplicity of txid in all
+      --      `unacknowledgedTxIds` sequences;
+      --    * @Map.keysSet bufferedTxs `Set.isSubsetOf` Map.keysSet referenceCounts@;
+      --    * all counts are positive integers.
+      --
+      referenceCounts :: !(Map txid Int)
+    }
+    deriving (Eq, Show, Generic)
+
+instance ( NoThunks peeraddr
+         , NoThunks tx
+         , NoThunks txid
+         ) => NoThunks (SharedTxState peeraddr txid tx)
+
+
+--
+-- Decisions
+--
+
+-- | Decision made by the decision logic.  Each peer will receive a 'Decision'.
+--
+-- /note:/ it is rather non-standard to represent a choice between requesting
+-- `txid`s and `tx`'s as a product rather than a sum type.  The client will
+-- need to download `tx`s first and then send a request for more txids (and
+-- acknowledge some `txid`s).   Due to pipelining each client will request
+-- decision from the decision logic quite often (every two pipelined requests),
+-- but with this design a decision once taken will make the peer non-active
+-- (e.g. it won't be returned by `filterActivePeers`) for longer, and thus the
+-- expensive `makeDecision` computation will not need to take that peer into
+-- account.
+--
+data TxDecision txid tx = TxDecision {
+    txdTxIdsToAcknowledge :: !NumTxIdsToAck,
+    -- ^ txid's to acknowledge
+
+    txdTxIdsToRequest     :: !NumTxIdsToReq,
+    -- ^ number of txid's to request
+
+    txdPipelineTxIds      :: !Bool,
+    -- ^ the tx-submission protocol only allows to pipeline `txid`'s requests
+    -- if we have non-acknowledged `txid`s.
+
+    txdTxsToRequest       :: !(Set txid),
+    -- ^ txid's to download.
+
+    txdTxsToMempool       :: ![tx]
+    -- ^ list of `tx`s to submit to the mempool.
+  }
+  deriving (Show, Eq)
+
+-- | A non-commutative semigroup instance.
+--
+-- /note:/ this instance must be consistent with `pickTxsToDownload` and how
+-- `PeerTxState` is updated.  It is designed to work with `TMergeVar`s.
+--
+instance Ord txid => Semigroup (TxDecision txid tx) where
+    TxDecision { txdTxIdsToAcknowledge,
+                 txdTxIdsToRequest,
+                 txdPipelineTxIds = _ignored,
+                 txdTxsToRequest,
+                 txdTxsToMempool }
+      <>
+      TxDecision { txdTxIdsToAcknowledge = txdTxIdsToAcknowledge',
+                   txdTxIdsToRequest     = txdTxIdsToRequest',
+                   txdPipelineTxIds      = txdPipelineTxIds',
+                   txdTxsToRequest       = txdTxsToRequest',
+                   txdTxsToMempool       = txdTxsToMempool' }
+      =
+      TxDecision { txdTxIdsToAcknowledge = txdTxIdsToAcknowledge + txdTxIdsToAcknowledge',
+                   txdTxIdsToRequest     = txdTxIdsToRequest + txdTxIdsToRequest',
+                   txdPipelineTxIds      = txdPipelineTxIds',
+                   txdTxsToRequest       = txdTxsToRequest <> txdTxsToRequest',
+                   txdTxsToMempool       = txdTxsToMempool ++ txdTxsToMempool'
+                 }
+
+-- | A no-op decision.
+emptyTxDecision :: TxDecision txid tx
+emptyTxDecision = TxDecision {
+    txdTxIdsToAcknowledge = 0,
+    txdTxIdsToRequest     = 0,
+    txdPipelineTxIds      = False,
+    txdTxsToRequest       = Set.empty,
+    txdTxsToMempool       = []
+  }
+
+data SharedDecisionContext peeraddr txid tx = SharedDecisionContext {
+    -- TODO: check how to access it.
+    sdcPeerGSV       :: !(Map peeraddr PeerGSV),
+
+    sdcSharedTxState :: !(SharedTxState peeraddr txid tx)
+  }
+  deriving Show
+
+
+-- | TxLogic tracer.
+--
+data TraceTxLogic peeraddr txid tx =
+    TraceSharedTxState String (SharedTxState peeraddr txid tx)
+  | TraceTxDecisions (Map peeraddr (TxDecision txid tx))
+  deriving Show
 
 
 data ProcessedTxCount = ProcessedTxCount {
