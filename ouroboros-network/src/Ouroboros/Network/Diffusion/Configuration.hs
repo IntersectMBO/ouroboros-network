b9827420e785f9956faf64283c335d226eed33f9
Marcin Szamotulski <coot@coot.me>
2024-10-04 08:16:37 +0200

    chain-sync: randomised ProtocolTimeLimits
    This patch adds a way to randomise `ProtocolTimeLimits` per message,
    rather than per connection.   This is used in `ChainSync` to pick
    a random timeout in the `MustReply` state.

--- ouroboros-network/src/Ouroboros/Network/Diffusion/Configuration.hs
+++ ouroboros-network/src/Ouroboros/Network/Diffusion/Configuration.hs
@@ -11,7 +11,6 @@ module Ouroboros.Network.Diffusion.Configuration
   , defaultDiffusionMode
   , defaultPeerSharing
   , defaultBlockFetchConfiguration
-  , defaultChainSyncTimeout
   , defaultDeadlineTargets
   , defaultSyncTargets
   , defaultDeadlineChurnInterval
@@ -19,7 +18,6 @@ module Ouroboros.Network.Diffusion.Configuration
     -- re-exports
   , AcceptedConnectionsLimit (..)
   , BlockFetchConfiguration (..)
-  , ChainSyncTimeout (..)
   , ConsensusModePeerTargets (..)
   , DiffusionMode (..)
   , MiniProtocolParameters (..)
@@ -42,7 +40,6 @@ module Ouroboros.Network.Diffusion.Configuration
   ) where
 
 import Control.Monad.Class.MonadTime.SI
-import System.Random (randomRIO)
 
 import Ouroboros.Network.BlockFetch (BlockFetchConfiguration (..))
 import Ouroboros.Network.ConnectionManager.Core (defaultProtocolIdleTimeout,
@@ -50,8 +47,7 @@ import Ouroboros.Network.ConnectionManager.Core (defaultProtocolIdleTimeout,
 import Ouroboros.Network.ConsensusMode
 import Ouroboros.Network.Diffusion (P2P (..))
 import Ouroboros.Network.Diffusion.Policies (closeConnectionTimeout,
-           deactivateTimeout, maxChainSyncTimeout, minChainSyncTimeout,
-           peerMetricsConfiguration)
+           deactivateTimeout, peerMetricsConfiguration)
 import Ouroboros.Network.NodeToNode (DiffusionMode (..),
            MiniProtocolParameters (..), defaultMiniProtocolParameters)
 import Ouroboros.Network.PeerSelection.Governor.Types
@@ -61,9 +57,7 @@ import Ouroboros.Network.PeerSelection.LedgerPeers.Type
 import Ouroboros.Network.PeerSelection.PeerSharing (PeerSharing (..))
 import Ouroboros.Network.PeerSharing (ps_POLICY_PEER_SHARE_MAX_PEERS,
            ps_POLICY_PEER_SHARE_STICKY_TIME)
-import Ouroboros.Network.Protocol.ChainSync.Codec (ChainSyncTimeout (..))
 import Ouroboros.Network.Protocol.Handshake (handshake_QUERY_SHUTDOWN_DELAY)
-import Ouroboros.Network.Protocol.Limits (shortWait)
 import Ouroboros.Network.Server.RateLimiting (AcceptedConnectionsLimit (..))
 
 
@@ -146,33 +140,6 @@ defaultBlockFetchConfiguration bfcSalt =
     bfcMaxRequestsInflight    = fromIntegral $ blockFetchPipeliningMax defaultMiniProtocolParameters,
     bfcDecisionLoopInterval   = 0.01, -- 10ms
     bfcSalt }
-
-defaultChainSyncTimeout :: IO ChainSyncTimeout
-defaultChainSyncTimeout = do
-    -- These values approximately correspond to false positive
-    -- thresholds for streaks of empty slots with 99% probability,
-    -- 99.9% probability up to 99.999% probability.
-    -- t = T_s [log (1-Y) / log (1-f)]
-    -- Y = [0.99, 0.999...]
-    -- T_s = slot length of 1s.
-    -- f = 0.05
-    -- The timeout is randomly picked per bearer to avoid all bearers
-    -- going down at the same time in case of a long streak of empty
-    -- slots.
-    -- To avoid global synchronosation the timeout is picked uniformly
-    -- from the interval 135 - 269, corresponds to the a 99.9% to
-    -- 99.9999% thresholds.
-    -- TODO: The timeout should be drawn at random everytime chainsync
-    --       enters the must reply state. A static per connection timeout
-    --       leads to selection preassure for connections with a large
-    --       timeout, see #4244.
-    mustReplyTimeout <- Just . realToFrac <$> randomRIO ( realToFrac minChainSyncTimeout :: Double
-                                                        , realToFrac maxChainSyncTimeout :: Double
-                                                        )
-    return ChainSyncTimeout { canAwaitTimeout  = shortWait,
-                              intersectTimeout = shortWait,
-                              mustReplyTimeout,
-                              idleTimeout      = Just 3673 }
 
 defaultDeadlineChurnInterval :: DiffTime
 defaultDeadlineChurnInterval = 3300
