87e50f7c1cc69484b1d4aca1afb2eb52f93f9074
Karl Knutsson <karl.fb.knutsson@gmail.com>
2024-10-04 12:24:59 +0200

    tx-submission: ranking of peers
    DeltaQ metrics is only available for our warm and hot peers that also
    have us as hot. So a fraction of all downstream clients will have a
    metric.
    
    This change the ranking of peers to use simple scoring system. Deliver a
    new TX before in time before it gets into the block gives you one point.
    Delivering a TXs thats already in the mempool, is invalid, or fail
    because it was included in a recent blocks gives you a penalty.
    
    Only the peer's that downloaded a TX will attempt to add it to the
    mempool

--- ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
+++ ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
@@ -99,7 +99,7 @@ import Ouroboros.Network.Protocol.TxSubmission2.Type (NumTxIdsToAck (..),
            NumTxIdsToReq (..), TxSubmission2)
 import Ouroboros.Network.TxSubmission.Inbound.Policy (TxDecisionPolicy (..))
 import Ouroboros.Network.TxSubmission.Inbound.Registry (SharedTxStateVar,
-           TxChannelsVar, withPeer)
+           TxChannelsVar, TxMempoolSem, withPeer)
 import Ouroboros.Network.TxSubmission.Inbound.Server (txSubmissionInboundV2)
 import Ouroboros.Network.TxSubmission.Inbound.Types (TraceTxLogic,
            TraceTxSubmissionInbound)
@@ -354,6 +354,7 @@ applications debugTracer txSubmissionInboundTracer txSubmissionInboundDebug node
                     (txSubmissionInitiator aaTxDecisionPolicy (nkMempool nodeKernel))
                     (txSubmissionResponder (nkMempool nodeKernel)
                                           (nkTxChannelsVar nodeKernel)
+                                          (nkTxMempoolSem nodeKernel)
                                           (nkSharedTxStateVar nodeKernel))
             }
           ]
@@ -676,20 +677,24 @@ applications debugTracer txSubmissionInboundTracer txSubmissionInboundDebug node
     txSubmissionResponder
       :: Mempool m Int
       -> TxChannelsVar m NtNAddr Int (Tx Int)
+      -> TxMempoolSem m
       -> SharedTxStateVar m NtNAddr Int (Tx Int)
       -> MiniProtocolCb (ResponderContext NtNAddr) ByteString m ()
-    txSubmissionResponder mempool txChannelsVar sharedTxStateVar =
+    txSubmissionResponder mempool txChannelsVar txMempoolSem sharedTxStateVar =
       MiniProtocolCb $
         \ ResponderContext { rcConnectionId = connId@ConnectionId { remoteAddress = them }} channel
         -> do
           withPeer txSubmissionInboundDebug
                    txChannelsVar
+                   txMempoolSem
+                   aaTxDecisionPolicy
                    sharedTxStateVar
                    (getMempoolReader mempool)
                    getTxSize
                    them $ \api -> do
             let server = txSubmissionInboundV2
                            txSubmissionInboundTracer
+                           (getMempoolReader mempool)
                            (getMempoolWriter mempool)
                            api
             labelThisThread "TxSubmissionServer"
