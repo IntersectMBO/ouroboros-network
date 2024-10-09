b366a73dbaa70185cdd7e11c08471ee42de3ce2e
Damian Nadales <damian.nadales@iohk.io>
2024-10-09 12:56:31 +0200

    Remove field `blockForgeUTCTime` since `headerForgeUTCTime` is enough.

--- ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Testnet/Node.hs
+++ ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Testnet/Node.hs
@@ -321,8 +321,7 @@ run blockGeneratorArgs limits ni na tracersExtra tracerBlockFetch =
           blockFetchSize         = \_ -> 1000,
           blockMatchesHeader     = \_ _ -> True,
 
-          headerForgeUTCTime,
-          blockForgeUTCTime      = headerForgeUTCTime . fmap blockHeader
+          headerForgeUTCTime
         }
       where
         plausibleCandidateChain cur candidate =
