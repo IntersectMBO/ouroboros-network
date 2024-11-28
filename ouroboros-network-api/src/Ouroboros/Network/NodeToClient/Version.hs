53df9a2d0683c49f7ae9edecd844186e6c1aa4da
Fraser Murray <fraser.murray@iohk.io>
2024-11-28 13:21:29 +0000

    use as-yet unreleased NodeToClientV_19 instead of creating a new version for OneEraGenTxId change

--- ouroboros-network-api/src/Ouroboros/Network/NodeToClient/Version.hs
+++ ouroboros-network-api/src/Ouroboros/Network/NodeToClient/Version.hs
@@ -50,9 +50,7 @@ data NodeToClientVersion
     | NodeToClientV_18
     -- ^ added @GetFuturePParams@ query
     | NodeToClientV_19
-    -- ^ added @GetLedgerPeerSnapshot@
-    | NodeToClientV_20
-    -- ^ enabled @CardanoNodeToClientVersion15@ and @CardanoNodeToNodeVersion3@ in order to support changed serialization of @OneEraGenTxId@
+    -- ^ added @GetLedgerPeerSnapshot@, changed serialization of @OneEraGenTxId@
   deriving (Eq, Ord, Enum, Bounded, Show, Typeable, Generic, NFData)
 
 -- | We set 16ths bit to distinguish `NodeToNodeVersion` and
@@ -70,7 +68,6 @@ nodeToClientVersionCodec = CodecCBORTerm { encodeTerm, decodeTerm }
           NodeToClientV_17 -> enc 17
           NodeToClientV_18 -> enc 18
           NodeToClientV_19 -> enc 19
-          NodeToClientV_20 -> enc 20
         where
           enc :: Int -> CBOR.Term
           enc = CBOR.TInt . (`setBit` nodeToClientVersionBit)
@@ -81,7 +78,6 @@ nodeToClientVersionCodec = CodecCBORTerm { encodeTerm, decodeTerm }
             17 -> Right NodeToClientV_17
             18 -> Right NodeToClientV_18
             19 -> Right NodeToClientV_19
-            20 -> Right NodeToClientV_20
             n  -> Left (unknownTag n)
         where
           dec :: CBOR.Term -> Either (Text, Maybe Int) Int
