813cf18ec16a75cff8bd83b9b3d9612a396f0ddc
Armando Santos <armando@well-typed.com>
2024-12-16 15:26:27 +0000

    fx

--- ouroboros-network/src/Ouroboros/Cardano/Diffusion/Handlers.hs
+++ ouroboros-network/src/Ouroboros/Cardano/Diffusion/Handlers.hs
@@ -7,41 +7,47 @@
 module Ouroboros.Cardano.Diffusion.Handlers where
 
 import Control.Concurrent.Class.MonadSTM.Strict
-import Ouroboros.Cardano.Network.LedgerPeerConsensusInterface
 import Ouroboros.Cardano.Network.PeerSelection.Governor.PeerSelectionState
 import Ouroboros.Network.ConnectionManager.Types
 import Ouroboros.Network.Diffusion.Common
 import Ouroboros.Network.PeerSelection.Governor
 import Ouroboros.Network.PeerSelection.PeerMetric
 #ifdef POSIX
+import Cardano.Network.PeerSelection.Bootstrap (UseBootstrapPeers)
+import Cardano.Network.Types (LedgerStateJudgement)
 import Control.Monad.Class.MonadTime.SI
 import Control.Tracer (traceWith)
 import Ouroboros.Network.ConnectionManager.Core (Trace (..))
 import Ouroboros.Network.PeerSelection.Governor.Types
            (makeDebugPeerSelectionState)
-import Ouroboros.Network.PeerSelection.LedgerPeers.Type
+import Ouroboros.Network.PeerSelection.LedgerPeers.Type (UseLedgerPeers)
+import Ouroboros.Network.PeerSelection.PeerSharing (PeerSharing)
 import System.Posix.Signals qualified as Signals
 #endif
 
 #ifdef POSIX
 sigUSR1Handler
   :: Ord ntnAddr
-  => TracersExtra ntnAddr ntnVersion ntnVersionData ntcAddr ntcVersion
-     ntcVersionData resolverError CardanoPeerSelectionState
-     CardanoDebugPeerSelectionState extraFlags extraPeers extraCounters
-     IO
-  -> ArgumentsExtra extraArgs CardanoPeerSelectionState extraActions
-     (CardanoLedgerPeersConsensusInterface IO) extraPeers extraFlags
-     extraChurnArgs extraCounters exception peeraddr IO
-  -> Applications ntnAddr ntnVersion ntnVersionData ntcAddr ntcVersion
-     ntcVersionData (CardanoLedgerPeersConsensusInterface IO) IO a
-  -> ConnectionManager muxMode socket ntnAddr handle handleError IO
-  -> StrictTVar IO (PeerSelectionState CardanoPeerSelectionState
-     extraFlags extraPeers ntnAddr peerconn)
+  => TracersExtra ntnAddr ntnVersion ntnVersionData
+                  ntcAddr ntcVersion ntcVersionData
+                  resolverError extraState
+                  CardanoDebugPeerSelectionState
+                  extraFlags extraPeers extraCounters
+                  IO
+  -> STM IO UseLedgerPeers
+  -> PeerSharing
+  -> UseBootstrapPeers
+  -> STM IO LedgerStateJudgement
+  -> ConnectionManager muxMode socket ntnAddr
+                       handle handleError IO
+  -> StrictTVar IO (PeerSelectionState
+                   CardanoPeerSelectionState
+                   extraFlags extraPeers ntnAddr
+                   peerconn)
   -> PeerMetrics IO ntnAddr
   -> IO ()
-sigUSR1Handler tracersExtra argsExtra apps connectionManager
-                        dbgStateVar metrics = do
+sigUSR1Handler tracersExtra getUseLedgerPeers ownPeerSharing useBootstrapPeers
+               getLedgerStateJudgement connectionManager dbgStateVar metrics = do
   _ <- Signals.installHandler
          Signals.sigUSR1
          (Signals.Catch
@@ -53,13 +59,11 @@ sigUSR1Handler tracersExtra argsExtra apps connectionManager
                (up, bp, lsj, am) <- atomically $
                  (,,,) <$> upstreamyness metrics
                        <*> fetchynessBlocks metrics
-                       <*> (CardanoDebugPeerSelectionState <$>
-                             clpciGetLedgerStateJudgement
-                               (lpExtraAPI (daLedgerPeersCtx apps)))
+                       <*> (CardanoDebugPeerSelectionState <$> getLedgerStateJudgement)
                        <*> readAssociationMode
-                             (daReadUseLedgerPeers argsExtra)
-                             (daOwnPeerSharing argsExtra)
-                             (cpstBootstrapPeersFlag (extraState ps))
+                             getUseLedgerPeers
+                             ownPeerSharing
+                             useBootstrapPeers
                let dbgState = makeDebugPeerSelectionState ps up bp lsj am
                traceWith (dtTracePeerSelectionTracer tracersExtra)
                          (TraceDebugState now dbgState)
@@ -69,19 +73,6 @@ sigUSR1Handler tracersExtra argsExtra apps connectionManager
   return ()
 #else
 sigUSR1Handler
-  :: TracersExtra ntnAddr ntnVersion ntnVersionData ntcAddr ntcVersion
-     ntcVersionData resolverError CardanoPeerSelectionState
-     CardanoDebugPeerSelectionState extraFlags extraPeers extraCounters
-     IO
-  -> ArgumentsExtra extraArgs CardanoPeerSelectionState extraActions
-     (CardanoLedgerPeersConsensusInterface IO) extraPeers extraFlags
-     extraChurnArgs extraCounters exception peeraddr IO
-  -> Applications ntnAddr ntnVersion ntnVersionData ntcAddr ntcVersion
-     ntcVersionData (CardanoLedgerPeersConsensusInterface IO) IO a
-  -> ConnectionManager muxMode socket ntnAddr handle handleError IO
-  -> StrictTVar IO (PeerSelectionState CardanoPeerSelectionState
-     extraFlags extraPeers ntnAddr peerconn)
-  -> PeerMetrics IO ntnAddr
-  -> IO ()
-sigUSR1Handler _ _ _ _ _ _ = pure ()
+  :: IO ()
+sigUSR1Handler = pure ()
 #endif
