4ef89303d24967ec7fc37a619a0e2543600dee1b
Tobias Dammers <tdammers@gmail.com>
2024-12-19 16:42:37 +0100

    Use client / server setup

--- ouroboros-network-protocols/src/Ouroboros/Network/Protocol/ChainSync/PipeClient.hs
+++ ouroboros-network-protocols/src/Ouroboros/Network/Protocol/ChainSync/PipeClient.hs
@@ -10,6 +10,8 @@ where
 
 import Ouroboros.Network.Protocol.ChainSync.Client
 import Pipes
+import Pipes.Core
+import Control.Monad (forever)
 
 data ChainSyncResponse block point tip
   = RollForward block tip
@@ -17,37 +19,65 @@ data ChainSyncResponse block point tip
   | IntersectNotFound tip
   deriving (Show)
 
--- | Run a chain sync client as a 'Pipe'.
-chainSyncClientProducer :: forall block point tip m p
+data ChainSyncRequest point
+  = RequestIntersect [point]
+  | RequestNext
+  deriving (Show)
+
+example :: ( Monad m
+           , p ~ Proxy X () (ChainSyncRequest point) (ChainSyncResponse block point tip) m
+           )
+        => [point]
+        -> (ChainSyncResponse block point tip -> m ())
+        -> (ChainSyncClient block point tip p () -> p ())
+        -> m ()
+example initialPoints handleMutation runClient =
+  runEffect
+    $ chainSyncClient initialPoints handleMutation
+      <<+ chainSyncServer runClient
+  
+chainSyncClient :: forall block point tip m p
                          . ( Monad m
-                           , p ~ Pipe [point] (ChainSyncResponse block point tip) m
+                           , p ~ Client
+                                (ChainSyncRequest point)
+                                (ChainSyncResponse block point tip)
+                                m
+                           )
+                        => [point]
+                        -> (ChainSyncResponse block point tip -> m ())
+                        -> p ()
+chainSyncClient known handleResponse = do
+  request (RequestIntersect known) >>= lift . handleResponse
+  forever $ do
+    request RequestNext >>= lift . handleResponse
+
+
+chainSyncServer :: forall block point tip m p
+                         . ( Monad m
+                           , p ~ Server
+                                (ChainSyncRequest point)
+                                (ChainSyncResponse block point tip)
+                                m
                            )
                         => (ChainSyncClient block point tip p () -> p ())
-                        -> p ()
-chainSyncClientProducer runClient =
-  runClient $ ChainSyncClient runProtocol
+                        -> ChainSyncRequest point -> p ()
+chainSyncServer runClient rq0 =
+  runClient $ ChainSyncClient (handleRequest rq0)
   where
-    runProtocol :: p (ClientStIdle block point tip p ())
-    runProtocol = do
-      known <- await
-      pure . SendMsgFindIntersect known $
-        ClientStIntersect
-          { recvMsgIntersectFound = \p t -> ChainSyncClient $ do
-              yield (RollBackward p t)
-              runProducer
-          , recvMsgIntersectNotFound = \t -> ChainSyncClient $ do
-              yield (IntersectNotFound t)
-              runProducer
-          }
-
-    runProducer :: p (ClientStIdle block point tip p ())
-    runProducer =
-      pure . SendMsgRequestNext (pure ()) $
-        ClientStNext
-          { recvMsgRollForward = \h t -> ChainSyncClient $ do
-              yield (RollForward h t)
-              runProducer
-          , recvMsgRollBackward = \p t -> ChainSyncClient $ do
-              yield (RollBackward p t)
-              runProducer
-          }
+    handleRequest rq = case rq of
+        RequestIntersect known ->
+          pure . SendMsgFindIntersect known $
+            ClientStIntersect
+              { recvMsgIntersectFound = \p t -> ChainSyncClient $ do
+                  respond (RollBackward p t) >>= handleRequest
+              , recvMsgIntersectNotFound = \t -> ChainSyncClient $ do
+                  respond (IntersectNotFound t) >>= handleRequest
+              }
+        RequestNext ->
+          pure . SendMsgRequestNext (pure ()) $
+            ClientStNext
+              { recvMsgRollForward = \h t -> ChainSyncClient $ do
+                  respond (RollForward h t) >>= handleRequest
+              , recvMsgRollBackward = \p t -> ChainSyncClient $ do
+                  respond (RollBackward p t) >>= handleRequest
+              }
