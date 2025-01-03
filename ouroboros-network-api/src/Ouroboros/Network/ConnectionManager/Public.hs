e55ffbe723f8f8fdb84c20b5aba3d7cd2ef30110
Marcin Szamotulski <coot@coot.me>
2025-01-03 17:16:45 +0100

    NetworkState - ToJSON instances

--- ouroboros-network-api/src/Ouroboros/Network/ConnectionManager/Public.hs
+++ ouroboros-network-api/src/Ouroboros/Network/ConnectionManager/Public.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Ouroboros.Network.ConnectionManager.Public
   ( Provenance (..)
   , DataFlow (..)
   , TimeoutExpired (..)
   , AbstractState (..)
   ) where
+
+import Data.Aeson qualified as Aeson
+import Data.Aeson.Encoding qualified as Aeson
+import Data.String (fromString)
+import GHC.Generics
 
 
 -- | Each connection is is either initiated locally (outbound) or by a remote
@@ -17,8 +24,10 @@ data Provenance =
     -- | An outbound connection: one that was initiated by us.
     --
   | Outbound
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Generic)
 
+instance Aeson.ToJSON Provenance where
+  toEncoding = Aeson.string . show
 
 -- | Each connection negotiates if it is uni- or bi-directional.  'DataFlow'
 -- is a life time property of a connection, once negotiated it never changes.
@@ -31,13 +40,19 @@ data Provenance =
 data DataFlow
     = Unidirectional
     | Duplex
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Generic)
+
+instance Aeson.ToJSON DataFlow where
+  toEncoding = Aeson.string . show
 
 
 -- | Boolean like type which indicates if the timeout on 'OutboundStateDuplex'
 -- has expired.
 data TimeoutExpired = Expired | Ticking
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Generic)
+
+instance Aeson.ToJSON TimeoutExpired where
+  toEncoding = Aeson.string . show
 
 
 -- | Useful for tracing and error messages.
@@ -57,4 +72,35 @@ data AbstractState =
     | WaitRemoteIdleSt
     | TerminatingSt
     | TerminatedSt
-    deriving (Eq, Ord, Show)
+    deriving (Eq, Ord, Show, Generic)
+
+instance Aeson.ToJSON AbstractState where
+  toEncoding UnknownConnectionSt =
+    Aeson.pairs $ fromString "type" Aeson..= "UnknownConnectionState"
+  toEncoding ReservedOutboundSt =
+    Aeson.pairs $ fromString "type" Aeson..= "ReservedOutboundState"
+  toEncoding (UnnegotiatedSt a) =
+    Aeson.pairs $ fromString "type" Aeson..= "UnnegotiatedState"
+               <> fromString "provenance" Aeson..= a
+  toEncoding (InboundIdleSt a) =
+    Aeson.pairs $ fromString "type" Aeson..= "InboundIdleState"
+               <> fromString "dataFlow" Aeson..= a
+  toEncoding (InboundSt a) =
+    Aeson.pairs $ fromString "type" Aeson..= "InboundState"
+               <> fromString "dataFlow" Aeson..= a
+  toEncoding OutboundUniSt =
+    Aeson.pairs $ fromString "type" Aeson..= "OutboundUnidirectionalState"
+  toEncoding (OutboundDupSt a) =
+    Aeson.pairs $ fromString "type" Aeson..= "OutboundDuplexState"
+               <> fromString "timeout" Aeson..= a
+  toEncoding (OutboundIdleSt a) =
+    Aeson.pairs $ fromString "type" Aeson..= "OutboundIdleState"
+               <> fromString "dataFlow" Aeson..= a
+  toEncoding DuplexSt =
+    Aeson.pairs $ fromString "type" Aeson..= "DuplexState"
+  toEncoding WaitRemoteIdleSt =
+    Aeson.pairs $ fromString "type" Aeson..= "WaitRemoteIdleState"
+  toEncoding TerminatingSt =
+    Aeson.pairs $ fromString "type" Aeson..= "TerminatingState"
+  toEncoding TerminatedSt =
+    Aeson.pairs $ fromString "type" Aeson..= "TerminatedState"
