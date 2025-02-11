87c2c9937c497b1acfc8965113be19402c8a5a8d
Karl Knutsson <karl.fb.knutsson@gmail.com>
2025-02-11 08:37:44 +0100

    add a writeMany option for batch sending of SDUs
    Permit sending of multiple SDUs through a single call to sendMany for
    Socket bearers
    Bearers without vector IO support emulate it through multiple calls to
    write.

--- network-mux/src/Network/Mux/Types.hs
+++ network-mux/src/Network/Mux/Types.hs
@@ -237,10 +237,14 @@ msHeaderLength = 8
 data Bearer m = Bearer {
     -- | Timestamp and send SDU.
       write   :: TimeoutFn m -> SDU -> m Time
+    -- | Timestamp and send many SDUs.
+    , writeMany   :: TimeoutFn m -> [SDU] -> m Time
     -- | Read a SDU
     , read    :: TimeoutFn m -> m (SDU, Time)
     -- | Return a suitable SDU payload size.
     , sduSize :: SDUSize
+    -- | Return a suitable batch size
+    , batchSize :: Int
     -- | Name of the bearer
     , name    :: String
     }
