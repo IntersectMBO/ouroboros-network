4ea7cbc2127a2030921fde37c41408a0294b53a9
Karl Knutsson <karl.fb.knutsson@gmail.com>
2025-02-11 08:37:44 +0100

    add a writeMany option for batch sending of SDUs
    Permit sending of multiple SDUs through a single call to sendMany for
    Socket bearers
    Bearers without vector IO support emulate it through multiple calls to
    write.

--- network-mux/src/Network/Mux/Bearer/Queues.hs
+++ network-mux/src/Network/Mux/Bearer/Queues.hs
@@ -40,10 +40,12 @@ queueChannelAsBearer
   -> Bearer m
 queueChannelAsBearer sduSize tracer QueueChannel { writeQueue, readQueue } = do
       Mx.Bearer {
-        Mx.read    = readMux,
-        Mx.write   = writeMux,
-        Mx.sduSize = sduSize,
-        Mx.name    = "queue-channel"
+        Mx.read      = readMux,
+        Mx.write     = writeMux,
+        Mx.writeMany = writeMuxMany,
+        Mx.sduSize   = sduSize,
+        Mx.batchSize = 2 * (fromIntegral $ Mx.getSDUSize sduSize),
+        Mx.name      = "queue-channel"
       }
     where
       readMux :: Mx.TimeoutFn m -> m (Mx.SDU, Time)
@@ -69,4 +71,10 @@ queueChannelAsBearer sduSize tracer QueueChannel { writeQueue, readQueue } = do
           atomically $ writeTBQueue writeQueue buf
           traceWith tracer Mx.TraceSendEnd
           return ts
+
+      writeMuxMany :: Mx.TimeoutFn m -> [Mx.SDU] -> m Time
+      writeMuxMany timeoutFn sdus = do
+        ts <- getMonotonicTime
+        mapM_ (writeMux timeoutFn) sdus
+        return ts
 
