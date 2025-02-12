797f14ec6a09a574cd186c9b824a66fd46792514
Karl Knutsson <karl.fb.knutsson@gmail.com>
2025-02-12 11:36:31 +0100

    size limits for self connect test

--- ouroboros-network-framework/sim-tests/Test/Simulation/Network/Snocket.hs
+++ ouroboros-network-framework/sim-tests/Test/Simulation/Network/Snocket.hs
@@ -37,6 +37,7 @@ import Codec.Serialise (Serialise)
 import Codec.Serialise qualified as Serialise
 
 import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Lazy qualified as BL
 import Data.Foldable (traverse_)
 import Data.Functor (void)
 import Data.Map qualified as Map
@@ -558,6 +559,7 @@ prop_simultaneous_open defaultBearerInfo =
 --
 prop_self_connect :: ByteString -> Property
 prop_self_connect payload =
+  BL.length payload > 0 && BL.length payload <= 0xffff ==>
     runSimOrThrow sim
   where
     addr :: TestAddress Int
