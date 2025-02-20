800167a56ce06e8d7bf9b66dc067340beaa54179
Armando Santos <armando@well-typed.com>
2025-02-20 13:08:47 +0000

    Moved Topology files to ouroboros-network

---
+++ ouroboros-network/src/Ouroboros/Cardano/Node/Topology.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE NamedFieldPuns #-}
+module Ouroboros.Cardano.Node.Topology where
+
+import Cardano.Network.PeerSelection.Bootstrap (UseBootstrapPeers (..))
+import Cardano.Network.PeerSelection.PeerTrustable (PeerTrustable (..))
+import Ouroboros.Network.Diffusion.Topology
+
+type CardanoNetworkTopology =
+  NetworkTopology UseBootstrapPeers PeerTrustable
+
+-- | This function returns false if non-trustable peers are configured
+--
+isValidTrustedPeerConfiguration :: CardanoNetworkTopology -> Bool
+isValidTrustedPeerConfiguration
+  NetworkTopology { localRootPeersGroups = LocalRootPeersGroups lprgs
+                  , extraConfig         = ubp
+                  } =
+    case ubp of
+      DontUseBootstrapPeers   -> True
+      UseBootstrapPeers []    -> anyTrustable
+      UseBootstrapPeers (_:_) -> True
+  where
+    anyTrustable =
+      any (\LocalRootPeersGroup { localRoots
+                                , extraFlags = trustable
+                                } ->
+            case trustable of
+              IsNotTrustable -> False
+              IsTrustable    -> not
+                              . null
+                              . rootAccessPoints
+                              $ localRoots
+          ) lprgs
