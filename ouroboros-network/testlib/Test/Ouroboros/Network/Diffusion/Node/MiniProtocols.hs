c8c766ca66f8147b864d961702cd5c0d9fbff7ed
Marcin Szamotulski <coot@coot.me>
2025-03-14 15:36:23 +0100

    tx-submission: refactored PeerTxAPI
    Removed `withMempoolSem` and added instead `addTxToMempool`.

--- ouroboros-network/testlib/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
+++ ouroboros-network/testlib/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
@@ -708,11 +708,11 @@ applications debugTracer txSubmissionInboundTracer txSubmissionInboundDebug node
                    aaTxDecisionPolicy
                    sharedTxStateVar
                    (getMempoolReader mempool)
+                   (getMempoolWriter mempool)
                    getTxSize
                    them $ \api -> do
             let server = txSubmissionInboundV2
                            txSubmissionInboundTracer
-                           (getMempoolReader mempool)
                            (getMempoolWriter mempool)
                            api
             labelThisThread "TxSubmissionServer"
