8e6b397f80a61cac85a33d1380988fcdb5f3a24a
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2025-03-31 11:38:32 +0200

    Information Channel changes

--- ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/InformationChannel.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/InformationChannel.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE RankNTypes     #-}
 module Ouroboros.Network.ConnectionManager.InformationChannel
   ( InformationChannel (..)
-  , InboundGovernorInfoChannel
   , newInformationChannel
   ) where
 
@@ -11,10 +10,6 @@ import Control.Concurrent.Class.MonadSTM.Strict
 
 import Data.Functor (($>))
 import GHC.Natural (Natural)
-import Network.Mux qualified as Mux
-import Ouroboros.Network.ConnectionHandler (Handle)
-import Ouroboros.Network.Context (ResponderContext)
-import Ouroboros.Network.InboundGovernor.Event (NewConnectionInfo)
 
 -- | Information channel.
 --
@@ -24,19 +19,13 @@ data InformationChannel a m =
     --
     readMessage  :: STM m a,
 
+    -- | Efficiently flush all values from the channel
+    readMessages :: STM m [a],
+
     -- | Write a value to the channel.
     --
     writeMessage :: a -> STM m ()
   }
-
--- | A channel which instantiates to 'NewConnectionInfo' and
--- 'Handle'.
---
--- * /Producer:/ connection manger for duplex outbound connections.
--- * /Consumer:/ inbound governor.
---
-type InboundGovernorInfoChannel (muxMode :: Mux.Mode) initiatorCtx peerAddr versionData bytes m a b =
-    InformationChannel (NewConnectionInfo peerAddr (Handle muxMode initiatorCtx (ResponderContext peerAddr) versionData bytes m a b)) m
 
 
 -- | Create a new 'InformationChannel' backed by a `TBQueue`.
@@ -50,6 +39,7 @@ newInformationChannel = do
         >>= \q -> labelTBQueue q "server-cc" $> q
     pure $ InformationChannel {
         readMessage  = readTBQueue channel,
+        readMessages = flushTBQueue channel,
         writeMessage = writeTBQueue channel
       }
 
@@ -57,4 +47,4 @@ newInformationChannel = do
 -- | The 'InformationChannel's 'TBQueue' depth.
 --
 cc_QUEUE_BOUND :: Natural
-cc_QUEUE_BOUND = 10
+cc_QUEUE_BOUND = 100
