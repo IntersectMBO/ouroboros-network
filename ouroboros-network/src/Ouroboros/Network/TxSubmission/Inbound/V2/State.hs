b884ccb6e974230a046a7d046117ba2423eb4f59
Marcin Szamotulski <coot@coot.me>
2025-03-31 18:16:06 +0200

    Compilation with ghc >= 9.10 without CPPs

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/State.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/State.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE BlockArguments      #-}
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE NamedFieldPuns      #-}
@@ -24,15 +23,12 @@ module Ouroboros.Network.TxSubmission.Inbound.V2.State
   ) where
 
 import Control.Concurrent.Class.MonadSTM.Strict
-import Control.Monad.Class.MonadTime.SI
 import Control.Exception (assert)
+import Control.Monad.Class.MonadTime.SI
 import Control.Tracer (Tracer, traceWith)
 
-import Data.Foldable (fold,
-#if !MIN_VERSION_base(4,20,0)
-         foldl',
-#endif
-         toList)
+import Data.Foldable (fold, toList)
+import Data.Foldable qualified as Foldable
 import Data.Functor (($>))
 import Data.Map.Merge.Strict qualified as Map
 import Data.Map.Strict (Map)
@@ -42,6 +38,7 @@ import Data.Sequence.Strict (StrictSeq)
 import Data.Sequence.Strict qualified as StrictSeq
 import Data.Set (Set)
 import Data.Set qualified as Set
+import Data.Typeable (Typeable)
 import System.Random (StdGen)
 
 import GHC.Stack (HasCallStack)
@@ -258,7 +255,7 @@ tickTimedTxs now st@SharedTxState{ timedTxs
     fn :: Map txid Int
        -> [txid]
        -> Map txid Int
-    fn m txids = foldl' gn m txids
+    fn m txids = Foldable.foldl' gn m txids
 
     gn :: Map txid Int
        -> txid
@@ -353,11 +350,12 @@ receivedTxIdsImpl
                     <> Map.map (const Nothing) ignoredTxIds
 
         referenceCounts' =
-          foldl' (flip $ Map.alter (\case
-                                       Nothing  -> Just $! 1
-                                       Just cnt -> Just $! succ cnt))
-                 referenceCounts
-                 txidsSeq
+          Foldable.foldl'
+            (flip $ Map.alter (\case
+                                 Nothing  -> Just $! 1
+                                 Just cnt -> Just $! succ cnt))
+            referenceCounts
+            txidsSeq
 
         st' = st { bufferedTxs     = bufferedTxs',
                    referenceCounts = referenceCounts' }
@@ -372,6 +370,7 @@ collectTxsImpl
        ( Ord peeraddr
        , Ord txid
        , Show txid
+       , Typeable txid
        )
     => (tx -> SizeInBytes) -- ^ compute tx size
     -> peeraddr
@@ -547,7 +546,7 @@ receivedTxIds tracer sharedVar getMempoolSnapshot peeraddr reqNo txidsSeq txidsM
 collectTxs
   :: forall m peeraddr tx txid.
      (MonadSTM m, Ord txid, Ord peeraddr,
-      Show txid)
+      Show txid, Typeable txid)
   => Tracer m (TraceTxLogic peeraddr txid tx)
   -> (tx -> SizeInBytes)
   -> SharedTxStateVar m peeraddr txid tx
