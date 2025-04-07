5bc716326bf14ec7482dfeba14ea3c98f284e129
Tobias Dammers <tdammers@gmail.com>
2025-04-07 11:30:27 +0200

    Remove generated files from 'playground'.
    Keys and certs can be generated using the provided playground scripts
    and should not be committed to the repository.

--- playground/run-agent.sh
+++ playground/run-agent.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-cabal run kes-agent:exe:kes-agent -- run \
-    --cold-verification-key ./cold.vkey \
-    --genesis-file ../kes-agent/fixtures/mainnet-shelley-genesis.json
+case "$1" in
+    "1")
+            cabal run kes-agent:exe:kes-agent -- run \
+                --cold-verification-key ./cold.vkey \
+                --genesis-file ../kes-agent/fixtures/mainnet-shelley-genesis.json \
+            -s /tmp/kes-agent-service-1.socket \
+            -c /tmp/kes-agent-control.socket \
+            -b /tmp/kes-agent-service.socket \
+            -l notice
+        ;;
+
+    "2")
+            cabal run kes-agent:exe:kes-agent -- run \
+                --cold-verification-key ./cold.vkey \
+                --genesis-file ../kes-agent/fixtures/mainnet-shelley-genesis.json \
+            -s /tmp/kes-agent-service.socket \
+            -c /tmp/kes-agent-control-2.socket \
+            -b /tmp/kes-agent-service-1.socket \
+            -l notice
+        ;;
+
+    "")
+        cabal run kes-agent:exe:kes-agent -- run \
+            --cold-verification-key ./cold.vkey \
+            --genesis-file ../kes-agent/fixtures/mainnet-shelley-genesis.json \
+            -l notice
+        ;;
+esac
