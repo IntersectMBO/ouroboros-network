a767725ec7cc3dc0e0f4014657dcde787b965e8c
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2025-05-11 12:15:55 +0200

    bugfix connection manager

--- ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
+++ ouroboros-network-framework/src/Ouroboros/Network/ConnectionManager/Core.hs
@@ -653,7 +653,7 @@ with args@Arguments {
           -- hits there we will update `connVar`.
           uninterruptibleMask_ do
             traceWith tracer (TrConnectionCleanup connId)
-            mbTransition <- modifyTMVar stateVar $ \state -> do
+            mbTransition <- modifyTMVar id stateVar $ \state -> do
               eTransition <- atomically $ do
                 connState <- readTVar connVar
                 let connState' = TerminatedState Nothing
@@ -711,7 +711,7 @@ with args@Arguments {
                            { fromState = Known (TerminatedState Nothing)
                            , toState   = Unknown
                            }
-                mbTransition' <- modifyTMVar stateVar $ \state ->
+                mbTransition' <- modifyTMVar id stateVar $ \state ->
                   case State.lookup connId state of
                     Nothing -> pure (state, Nothing)
                     Just v  ->
@@ -878,7 +878,7 @@ with args@Arguments {
                                  hardLimit
                                  socket
                                  connId = do
-        r <- modifyTMVar stateVar $ \state -> do
+        r <- mask \unmask -> modifyTMVar unmask stateVar \state -> do
           numberOfCons <- atomically $ countIncomingConnections state
 
           let -- Check if after accepting this connection we get above the
@@ -952,8 +952,7 @@ with args@Arguments {
                         labelTVar (connVar v') ("conn-state-" ++ show connId)
                         return (v', Just connState0')
 
-                connThread' <-
-                  mask \unmask -> forkConnectionHandler
+                connThread' <- mask_ $ forkConnectionHandler
                      id stateVar mutableConnVar' socket connId writer unmask handler
                 return (connThread', mutableConnVar', connState0', connState')
 
@@ -2046,14 +2045,16 @@ with args@Arguments {
                              )
             <> FirstToFinish (do b <- LazySTM.readTVar timeoutVar
                                  check b
-                                 Right <$> readTVar connVar
+                                 connState <- readTVar connVar
+                                 let connState' = TerminatingState connId connThread Nothing
+                                     trTrace    = TransitionTrace connStateId
+                                                    (mkTransition connState connState')
+                                 writeTVar connVar connState'
+                                 return . Right $ (connState', trTrace)
                              )
           case r of
-            Right connState -> do
-              let connState' = TerminatingState connId connThread Nothing
-              atomically $ writeTVar connVar connState'
-              traceWith trTracer (TransitionTrace connStateId
-                                   (mkTransition connState connState'))
+            Right (connState', trace') -> do
+              traceWith trTracer trace'
               traceCounters stateVar
               -- We rely on the `finally` handler of connection thread to:
               --
@@ -2398,13 +2399,13 @@ modifyTMVar :: ( MonadEvaluate m
                , MonadMask     m
                , MonadSTM      m
                )
-            => StrictTMVar m a
+            => (forall x. m x -> m x)
+            -> StrictTMVar m a
             -> (a -> m (a, b))
             -> m b
-modifyTMVar v k =
-  mask $ \restore -> do
+modifyTMVar unmask v k = do
     a <- atomically (takeTMVar v)
-    (a',b) <- restore (k a >>= evaluate)
+    (a',b) <- unmask (k a >>= evaluate)
       `onException`
         atomically (putTMVar v a)
     atomically (putTMVar v a')
