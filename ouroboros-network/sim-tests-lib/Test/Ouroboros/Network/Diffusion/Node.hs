60117971bf9e7a8832be6a94b7273c12856947a1
Marcin Szamotulski <coot@coot.me>
2025-06-25 16:42:42 +0200

    tx-submission: run drainRejectionThread

--- ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Diffusion/Node.hs
+++ ouroboros-network/sim-tests-lib/Test/Ouroboros/Network/Diffusion/Node.hs
@@ -117,7 +117,7 @@ import Ouroboros.Network.Snocket (MakeBearer, Snocket, TestAddress (..),
            invalidFileDescriptor)
 
 import Ouroboros.Network.TxSubmission.Inbound.V2.Policy (TxDecisionPolicy)
-import Ouroboros.Network.TxSubmission.Inbound.V2.Registry (decisionLogicThread)
+import Ouroboros.Network.TxSubmission.Inbound.V2.Registry (decisionLogicThreads)
 import Ouroboros.Network.TxSubmission.Inbound.V2.Types (TraceTxLogic,
            TraceTxSubmissionInbound)
 
@@ -367,7 +367,7 @@ run blockGeneratorArgs limits ni na
            $ \ diffusionThread ->
                withAsync (blockFetch nodeKernel) $ \blockFetchLogicThread ->
 
-                 withAsync (decisionLogicThread
+                 withAsync (decisionLogicThreads
                               tracerTxLogic
                               sayTracer
                               (aTxDecisionPolicy na)
