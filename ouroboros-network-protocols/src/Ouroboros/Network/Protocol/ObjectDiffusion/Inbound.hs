8594b77aa408eb2b78a5012acfc3bd12a00f7308
Nicolas “Niols” Jeannerod <nicolas.jeannerod@moduscreate.com>
2025-06-27 13:03:19 +0200

    Add ObjectDiffusion mini-protocol for Peras
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Alexander Esgen <alexander.esgen@iohk.io>
    Co-authored-by: Georgy Lukyanov <georgy.lukyanov@iohk.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    
    Backporting notes: removed ObjectDiffusion tests as they depend on a
    newer version of typed-protocols (0.1.1.0).

---
+++ ouroboros-network-protocols/src/Ouroboros/Network/Protocol/ObjectDiffusion/Inbound.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- | A view of the object diffusion protocol from the point of view of
+-- the inbound/client peer.
+--
+-- This provides a view that uses less complex types and should be easier to
+-- use than the underlying typed protocol itself.
+--
+-- For execution, a conversion into the typed protocol is provided.
+module Ouroboros.Network.Protocol.ObjectDiffusion.Inbound
+  ( -- * Protocol type for the inbound
+    ObjectDiffusionInboundPipelined (..)
+  , InboundStIdle (..)
+  , Collect (..)
+    -- * Execution as a typed protocol
+  , objectDiffusionInboundPeerPipelined
+  ) where
+
+import Data.List.NonEmpty (NonEmpty)
+import Network.TypedProtocol.Core
+import Network.TypedProtocol.Peer (Peer, PeerPipelined (..))
+import Network.TypedProtocol.Peer.Client
+import Ouroboros.Network.Protocol.ObjectDiffusion.Type
+
+data ObjectDiffusionInboundPipelined objectId object m a where
+  ObjectDiffusionInboundPipelined
+    :: m (InboundStIdle Z objectId object m a)
+    -> ObjectDiffusionInboundPipelined objectId object m a
+
+-- | This is the type of the pipelined results, collected by 'CollectPipelined'.
+-- This protocol can pipeline requests for object ids and objects,
+-- so we use a sum of either for collecting the responses.
+data Collect objectId object
+  = -- | The result of 'SendMsgRequestObjectIdsPipelined'. It also carries
+    -- the number of objectIds originally requested.
+    CollectObjectIds NumObjectIdsReq [objectId]
+  | -- | The result of 'SendMsgRequestObjectsPipelined'. The actual reply only
+    -- contains the objects sent, but this pairs them up with the
+    -- objects requested. This is because the peer can determine that
+    -- some objects are no longer needed.
+    CollectObjects [objectId] [object]
+
+data InboundStIdle (n :: N) objectId object m a where
+  SendMsgRequestObjectIdsBlocking
+    :: NumObjectIdsAck -- ^ number of objectIds to acknowledge
+    -> NumObjectIdsReq -- ^ number of objectIds to request
+    -> ( NonEmpty objectId -> m (InboundStIdle Z objectId object m a))
+    -> InboundStIdle Z objectId object m a
+  SendMsgRequestObjectIdsPipelined
+    :: NumObjectIdsAck
+    -> NumObjectIdsReq
+    -> m (InboundStIdle (S n) objectId object m a)
+    -> InboundStIdle n objectId object m a
+  SendMsgRequestObjectsPipelined
+    :: [objectId]
+    -> m (InboundStIdle (S n) objectId object m a)
+    -> InboundStIdle n objectId object m a
+  CollectPipelined
+    :: Maybe (InboundStIdle (S n) objectId object m a)
+    -> (Collect objectId object -> m (InboundStIdle n objectId object m a))
+    -> InboundStIdle (S n) objectId object m a
+  SendMsgDone
+    :: m a
+    -> InboundStIdle Z objectId object m a
+
+inboundRun
+  :: forall (n :: N) objectId object m a.
+     (Functor m)
+  => InboundStIdle n objectId object m a
+  -> Peer (ObjectDiffusion objectId object) AsClient (Pipelined n (Collect objectId object)) StIdle m a
+
+inboundRun (SendMsgRequestObjectIdsBlocking ackNo reqNo k) =
+      Yield (MsgRequestObjectIds SingBlocking ackNo reqNo)
+        $ Await
+        $ \case
+            MsgReplyObjectIds (BlockingReply objectIds) ->
+              Effect (inboundRun <$> k objectIds)
+inboundRun (SendMsgRequestObjectIdsPipelined ackNo reqNo k) =
+      YieldPipelined
+        (MsgRequestObjectIds SingNonBlocking ackNo reqNo)
+        (ReceiverAwait
+          $ \(MsgReplyObjectIds (NonBlockingReply objectIds)) ->
+              ReceiverDone (CollectObjectIds reqNo objectIds)
+        )
+        (Effect (inboundRun <$> k))
+inboundRun (SendMsgRequestObjectsPipelined objectIds k) =
+      YieldPipelined
+        (MsgRequestObjects objectIds)
+        (ReceiverAwait
+          $ \(MsgReplyObjects objects) ->
+              ReceiverDone (CollectObjects objectIds objects)
+        )
+        (Effect (inboundRun <$> k))
+inboundRun (CollectPipelined mNone collect) =
+      Collect
+        (fmap inboundRun mNone)
+        (Effect . fmap inboundRun . collect)
+inboundRun (SendMsgDone kDone) =
+      Yield MsgDone $ Effect $ Done <$> kDone
+
+-- | Transform a 'ObjectDiffusionInboundPipelined' into a 'PeerPipelined'.
+objectDiffusionInboundPeerPipelined
+  :: forall objectId object m a.
+     (Functor m)
+  => ObjectDiffusionInboundPipelined objectId object m a
+  -> PeerPipelined (ObjectDiffusion objectId object) AsClient StInit m a
+objectDiffusionInboundPeerPipelined (ObjectDiffusionInboundPipelined inboundSt) =
+  PeerPipelined $
+    Yield MsgInit $
+      Effect $ inboundRun <$> inboundSt
