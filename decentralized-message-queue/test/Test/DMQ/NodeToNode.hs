86400b27ed3c098859ccebe71ce5fdc4b464bf44
Marcin Szamotulski <coot@coot.me>
2025-07-01 17:29:05 +0200

    dmq: added nodeToNodeVersionCodec test

--- decentralized-message-queue/test/Test/DMQ/NodeToNode.hs
+++ decentralized-message-queue/test/Test/DMQ/NodeToNode.hs
@@ -17,7 +17,8 @@ import Test.Tasty.QuickCheck (testProperty)
 
 tests :: TestTree
 tests = testGroup "DMQ.Network.NodeToNode"
-    [ testProperty "nodeToNodeCodecCBORTerm" prop_nodeToNodeCodec
+    [ testProperty "nodeToNodeVersionCodec"  prop_nodeToNodeVersionCodec
+    , testProperty "nodeToNodeCodecCBORTerm" prop_nodeToNodeCodec
     ]
 
 instance Arbitrary NodeToNodeVersion where
@@ -38,6 +39,17 @@ instance Arbitrary NodeToNodeVersionData where
                      , PeerSharingEnabled
                      ]
         <*> arbitrary
+
+
+prop_nodeToNodeVersionCodec :: NodeToNodeVersion
+                            -> Bool
+prop_nodeToNodeVersionCodec version =
+    case decodeTerm (encodeTerm version) of
+        Right version' -> version == version'
+        Left {}        -> False
+  where
+      CodecCBORTerm { encodeTerm, decodeTerm } = nodeToNodeVersionCodec
+
 
 prop_nodeToNodeCodec :: NodeToNodeVersion -> NodeToNodeVersionData -> Bool
 prop_nodeToNodeCodec ntnVersion ntnData =
