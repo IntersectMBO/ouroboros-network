d480142e92aebfbbef73d49b5f7aed3ff9872e40
Marcin Szamotulski <coot@coot.me>
2025-07-22 11:00:11 +0200

    rebase artifact
    * stylish-haskell
    * cabal-gild
    * GHC options

--- ouroboros-network/src/Ouroboros/Network/NodeToNode.hs
+++ ouroboros-network/src/Ouroboros/Network/NodeToNode.hs
@@ -154,9 +154,9 @@ import Ouroboros.Network.Subscription.Ip qualified as Subscription
 import Ouroboros.Network.Subscription.Worker (LocalAddresses (..),
            SubscriberError)
 import Ouroboros.Network.Tracers
-import Ouroboros.Network.TxSubmission.Inbound.V2.Types qualified as TxInbound
 import Ouroboros.Network.TxSubmission.Inbound.V2.Policy (TxDecisionPolicy (..),
            defaultTxDecisionPolicy, max_TX_SIZE)
+import Ouroboros.Network.TxSubmission.Inbound.V2.Types qualified as TxInbound
 import Ouroboros.Network.TxSubmission.Outbound qualified as TxOutbound
 import Ouroboros.Network.Util.ShowProxy (ShowProxy, showProxy)
 
