e0c98c55c1a2e76a0b7a72017670cba5f4de4e28
Marcin Szamotulski <coot@coot.me>
2025-07-23 17:59:45 +0200

    tx-submission: check tx-sizes up to _MAX_SIZE_DISCREPENCY

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/State.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/State.hs
@@ -15,6 +15,7 @@ module Ouroboros.Network.TxSubmission.Inbound.V2.State
   , acknowledgeTxIds
   , splitAcknowledgedTxIds
   , tickTimedTxs
+  , const_MAX_TX_SIZE_DISCREPENCY
     -- * Internals, only exported for testing purposes:
   , RefCountDiff (..)
   , updateRefCounts
@@ -360,6 +361,11 @@ receivedTxIdsImpl
                    unacknowledgedTxIds    = unacknowledgedTxIds',
                    requestedTxIdsInflight = requestedTxIdsInflight - reqNo }
 
+-- | We check advertised sizes up in a fuzzy way.  The advertised and received
+-- sizes need to agree up to `const_MAX_TX_SIZE_DISCREPENCY`.
+--
+const_MAX_TX_SIZE_DISCREPENCY :: SizeInBytes
+const_MAX_TX_SIZE_DISCREPENCY = 32
 
 collectTxsImpl
     :: forall peeraddr tx txid.
@@ -415,12 +421,21 @@ collectTxsImpl txSize peeraddr requestedTxIdsMap receivedTxs
               Map.dropMissing
               Map.dropMissing
               (Map.zipWithMaybeMatched \_ receivedSize advertisedSize ->
-                if receivedSize == advertisedSize
+                if receivedSize `checkTxSize` advertisedSize
                   then Nothing
                   else Just (receivedSize, advertisedSize)
               )
               (txSize `Map.map` receivedTxs)
               requestedTxIdsMap
+
+        checkTxSize :: SizeInBytes
+                    -> SizeInBytes
+                    -> Bool
+        checkTxSize received advertised
+          | received > advertised
+          = received - advertised <= const_MAX_TX_SIZE_DISCREPENCY
+          | otherwise
+          = advertised - received <= const_MAX_TX_SIZE_DISCREPENCY
 
         requestedTxIds = Map.keysSet requestedTxIdsMap
         notReceived    = requestedTxIds Set.\\ Map.keysSet receivedTxs
