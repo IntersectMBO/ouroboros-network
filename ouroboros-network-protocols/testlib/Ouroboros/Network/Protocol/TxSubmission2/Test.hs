84b8dd1e838bd4a2410ec9a3f1c46684de67b433
Marcin Szamotulski <coot@coot.me>
2025-08-09 11:10:19 +0200

    tx-submission: expose AnnotatedCodec with polymorphic withBytes functor

--- ouroboros-network-protocols/testlib/Ouroboros/Network/Protocol/TxSubmission2/Test.hs
+++ ouroboros-network-protocols/testlib/Ouroboros/Network/Protocol/TxSubmission2/Test.hs
@@ -347,7 +347,7 @@ anncodec_v2 :: MonadST m
                               m ByteString
 anncodec_v2 = anncodecTxSubmission2
               Serialise.encode Serialise.decode
-              Serialise.decode
+              (const <$> Serialise.decode)
 
 
 
