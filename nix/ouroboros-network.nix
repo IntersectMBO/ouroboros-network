a2b77d6924d36ac49d285ce5182cb7f3ecc3680f
Marcin Szamotulski <coot@coot.me>
2025-08-27 11:17:20 +0200

    quickcheck-monoids: compatibility with QuickCheck-2.16

--- nix/ouroboros-network.nix
+++ nix/ouroboros-network.nix
@@ -43,6 +43,8 @@ let
     # stdenv.hostPlatform.isWindows will work as expected
     src = ./..;
     name = "ouroboros-network";
+    index-state = "2025-07-16T09:24:19Z";
+    index-sha256 = "sha256-fmnSRF68/UIQYzzdmNs3UT0cbYhn9d5nlhb3BnVXe48=";
     compiler-nix-name = lib.mkDefault defaultCompiler;
     cabalProjectLocal =
       if pkgs.stdenv.hostPlatform.isWindows
