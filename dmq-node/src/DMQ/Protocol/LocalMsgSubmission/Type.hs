baa1d9e3840d012bb548f139dce95c9b374f14f6
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2025-09-11 16:14:44 +0200

    mempool: adapt for generalized validation

--- dmq-node/src/DMQ/Protocol/LocalMsgSubmission/Type.hs
+++ dmq-node/src/DMQ/Protocol/LocalMsgSubmission/Type.hs
@@ -13,35 +13,10 @@ module DMQ.Protocol.LocalMsgSubmission.Type
   , module Ouroboros
   ) where
 
-import Data.Aeson
-import Data.Text (Text)
 import Network.TypedProtocol.Core as Core
 import Ouroboros.Network.Protocol.LocalTxSubmission.Type as Ouroboros
-import Ouroboros.Network.Util.ShowProxy
+import Ouroboros.Network.TxSubmission.Mempool.Simple
 
 -- | The LocalMsgSubmission protocol is an alias for the LocalTxSubmission
 --
-type LocalMsgSubmission sig = Ouroboros.LocalTxSubmission sig SigMempoolFail
-
--- | The type of failures when adding to the mempool
---
-data SigMempoolFail =
-    SigInvalid Text
-  | SigDuplicate
-  | SigExpired
-  | SigResultOther Text
-  deriving (Eq, Show)
-
-instance ShowProxy SigMempoolFail where
-
-instance ToJSON SigMempoolFail where
-  toJSON SigDuplicate = String "duplicate"
-  toJSON SigExpired   = String "expired"
-  toJSON (SigInvalid txt) = object
-    [ "type" .= String "invalid"
-    , "reason" .= txt
-    ]
-  toJSON (SigResultOther txt) = object
-    [ "type" .= String "other"
-    , "reason" .= txt
-    ]
+type LocalMsgSubmission sig = Ouroboros.LocalTxSubmission sig (MempoolAddFail sig)
