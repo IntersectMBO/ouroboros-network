ffe29f161037fa93f723ad82f358b91938e87fe3
Marcin Szamotulski <coot@coot.me>
2025-09-16 13:02:36 +0200

    ouroboros-network:api-tests - moved NodeTo{Client,Node}.Version tests

--- ouroboros-network/api/tests/Main.hs
+++ ouroboros-network/api/tests/Main.hs
@@ -3,6 +3,8 @@ module Main (main) where
 import Main.Utf8 (withUtf8)
 import Test.Tasty
 
+import Test.Cardano.Network.NodeToClient.Version qualified as NodeToClient.Version
+import Test.Cardano.Network.NodeToNode.Version qualified as NodeToNode.Version
 import Test.Ouroboros.Network.PeerSelection.RelayAccessPoint qualified as RelayAccessPoint
 
 main :: IO ()
@@ -11,7 +13,9 @@ main = withUtf8 $ defaultMain tests
 tests :: TestTree
 tests =
   testGroup "ouroboros-network-api"
-  [ RelayAccessPoint.tests
+  [ NodeToClient.Version.tests
+  , NodeToNode.Version.tests
+  , RelayAccessPoint.tests
   ]
 
 
