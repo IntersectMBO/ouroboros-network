86d7302bedec1248e832aeb62b0ee558723e2395
Marcin Szamotulski <coot@coot.me>
2025-09-30 10:01:38 +0200

    cardano-diffusion:ping - using ouroboros-network and cardano-diffusion

--- cardano-diffusion/api/tests/Test/Cardano/Network/NodeToNode/Version.hs
+++ cardano-diffusion/api/tests/Test/Cardano/Network/NodeToNode/Version.hs
@@ -6,7 +6,6 @@ module Test.Cardano.Network.NodeToNode.Version (tests) where
 import Cardano.Network.NodeToNode.Version
 
 import Ouroboros.Network.CodecCBORTerm
-import Ouroboros.Network.Magic
 
 import Ouroboros.Network.PeerSelection.PeerSharing (PeerSharing (..))
 import Test.QuickCheck
