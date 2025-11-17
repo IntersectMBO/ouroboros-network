5292cccd4b8e2fb9db35e143fd74b3c27136be97
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2025-11-17 11:26:49 +0100

    TCP_NOTSENT_LOWAT

--- network-mux/src/Network/Mux/Bearer.hs
+++ network-mux/src/Network/Mux/Bearer.hs
@@ -27,6 +27,7 @@ import           Control.Tracer (Tracer)
 
 import           Data.ByteString.Lazy qualified as BL
 import           Network.Socket (Socket)
+import           Network.Socket qualified as Socket
 #if defined(mingw32_HOST_OS)
 import           System.Win32 (HANDLE)
 #endif
@@ -66,8 +67,10 @@ makeSocketBearer = makeSocketBearer' 0
 
 makeSocketBearer' :: DiffTime -> MakeBearer IO Socket
 makeSocketBearer' pt = MakeBearer $ (\sduTimeout tr fd rb -> do
+    Socket.setSocketOption fd opt (fromIntegral size)
     return $ socketAsBearer size batch rb sduTimeout pt tr fd)
   where
+    opt = Socket.SockOpt 6 25 -- IPPROTO_TCP and TCP_NOTSENT_LOWAT copy-pasted from my system's linux .h files
     size = SDUSize 12_288
     batch = 131_072
 
