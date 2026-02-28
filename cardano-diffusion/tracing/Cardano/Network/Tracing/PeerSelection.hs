78b6c3ee02a13ce2c1d93d5ae8572c11046f0bc2
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2026-02-28 17:07:56 +0100

    cardano-diffusion: trace peer selection extra traces

---
+++ cardano-diffusion/tracing/Cardano/Network/Tracing/PeerSelection.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Cardano.Network.Tracing.PeerSelection () where
+
+import Data.Aeson
+import Data.Text qualified  as Text
+
+import Cardano.Logging
+import Cardano.Network.PeerSelection.ExtraRootPeers
+import Cardano.Network.LedgerPeerConsensusInterface
+import Cardano.Network.Tracing.Churn ()
+
+instance LogFormatting (ToExtraTrace (ExtraPeers peeraddr)) where
+  forMachine _dtal (TraceLedgerStateJudgementChanged new) =
+    mconcat [ "kind" .= String "LedgerStateJudgementChanged"
+            , "new"  .= Text.pack (show new) ]
+
+  forMachine _dtal (TraceUseBootstrapPeersChanged ubp) =
+    mconcat [ "kind" .= String "UseBootstrapPeersChanged"
+            , "useBootstrapPeers" .= toJSON ubp ]
+
+  forHuman = Text.pack . show
+
+
+instance MetaTrace (ToExtraTrace (ExtraPeers peeraddr)) where
+  namespaceFor TraceLedgerStateJudgementChanged {} =
+    Namespace [] ["LedgerStateJudgementChanged"]
+
+  namespaceFor TraceUseBootstrapPeersChanged {} =
+    Namespace [] ["UseBootstrapPeersChanged"]
+
+  severityFor (Namespace [] ["LedgerStateJudgement"])
+              (Just (TraceLedgerStateJudgementChanged lsj))
+                | TooOld <- lsj      = Just Warning
+                | YoungEnough <- lsj = Just Notice
+  severityFor (Namespace [] ["UseBootstrapPeersChanged"]) _ =
+    Just Notice
+  severityFor _ _ = Nothing
+
+  documentFor (Namespace [] ["LedgerStateJudgementChanged"]) =
+    Just "Indicates whether the node is caught up or needs to sync"
+  documentFor (Namespace [] ["UseBootstrapPeersChanged"]) =
+    Just "Reacts to configuration changes related to bootstrap peers in the topology file"
+  documentFor _ = Nothing
+
+  allNamespaces = [
+    Namespace [] ["LedgerStateJudgementChanged"],
+    Namespace [] ["UseBootstrapPeersChanged"]
+    ]
