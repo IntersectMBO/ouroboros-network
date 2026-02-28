29afccdded36033c9da7c4acde0f41ff28869c51
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2026-02-28 17:40:04 +0100

    SizeInBytes ToJSON instance

--- ouroboros-network/api/lib/Ouroboros/Network/SizeInBytes.hs
+++ ouroboros-network/api/lib/Ouroboros/Network/SizeInBytes.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE DerivingVia                #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 
 module Ouroboros.Network.SizeInBytes (SizeInBytes (..)) where
 
 import Control.DeepSeq (NFData (..))
+import Data.Aeson.Types (ToJSON)
 import Data.Monoid (Sum (..))
 import Data.Word (Word32)
 import GHC.Generics
@@ -26,6 +26,6 @@ newtype SizeInBytes = SizeInBytes { getSizeInBytes :: Word32 }
   deriving Semigroup via Sum Word32
   deriving Monoid    via Sum Word32
   deriving Generic
-  deriving newtype NFData
+  deriving newtype (NFData, ToJSON)
   deriving Measure.Measure        via Word32
   deriving Measure.BoundedMeasure via Word32
