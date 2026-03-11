d6bf18c91fcb332fc4e17adb76d417d12c662aea
Karl Knutsson <karl.fb.knutsson@gmail.com>
2026-03-11 15:33:34 +0100

    WIP: remove the 5m loop timer
    Cut down on the number of wakeups by only depending on the debouncer.

--- ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/Registry.hs
+++ ouroboros-network/src/Ouroboros/Network/TxSubmission/Inbound/V2/Registry.hs
@@ -510,7 +510,7 @@ decisionLogicThread tracer policy txChannelsVar sharedStateVar = do
     maxCoalesce = 0.050
 
     debounceTime :: DiffTime
-    debounceTime = 2 * _DECISION_LOOP_DELAY
+    debounceTime = 0.01
 
     debounce :: Lazy.TVar m Bool -> Time -> Word64 -> m ()
     debounce txTimer deadline lastGen = do
@@ -537,10 +537,6 @@ decisionLogicThread tracer policy txChannelsVar sharedStateVar = do
 
     go :: Word64 -> m Void
     go lastGen = do
-      -- We rate limit the decision making process, it could overwhelm the CPU
-      -- if there are too many inbound connections.
-      threadDelay _DECISION_LOOP_DELAY
-
       now <- getMonotonicTime
       nextDelay <- atomically $ do
         sharedTxState <- readTVar sharedStateVar
@@ -661,6 +657,4 @@ decisionLogicThreads tracer counterTracer policy txChannelsVar sharedStateVar =
     decisionLogicThread tracer policy txChannelsVar sharedStateVar
 
 
--- `5ms` delay
-_DECISION_LOOP_DELAY :: DiffTime
-_DECISION_LOOP_DELAY = 0.005
+-- No fixed delay: decision loop blocks on STM/timers and uses debounce.
