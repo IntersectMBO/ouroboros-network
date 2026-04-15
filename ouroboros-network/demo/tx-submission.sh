7cf4dd8490007fc756b521e409f9f6e6aedace5f
Marcin Szamotulski <coot@coot.me>
2026-04-15 15:51:08 +0200

    tx-demo: txs-to-request and unacked-txids

--- ouroboros-network/demo/tx-submission.sh
+++ ouroboros-network/demo/tx-submission.sh
@@ -6,6 +6,9 @@ OUTBOUND_ADDR=10.0.0.1
 OUTBOUND_PORT=4001
 TXS_FILE="/tmp/txs.cbor"
 
+TXS_TO_REQUEST=3
+UNACKED_TXIDS=10
+
 # network shaping parameters
 THROUPUT="10mbit"
 DELAY="10ms"
@@ -18,7 +21,7 @@ echo "Using temporary directory: $TMP_DIR"
 cabal build exe:demo-tx-submission
 CMD=$(cabal list-bin exe:demo-tx-submission)
 
-INBOUND_CMD="${CMD} inbound --addr ${INBOUND_ADDR} --port ${INBOUND_PORT}"
-OUTBOUND_CMD="${CMD} outbound --addr ${INBOUND_ADDR} --port ${INBOUND_PORT} --bind-addr ${OUTBOUND_ADDR} --bind-port ${OUTBOUND_PORT} --input ${TXS_FILE} -n 20"
+INBOUND_CMD="${CMD} inbound --addr ${INBOUND_ADDR} --port ${INBOUND_PORT} --txids-to-request ${TXS_TO_REQUEST} --unacked-txids ${UNACKED_TXIDS}"
+OUTBOUND_CMD="${CMD} outbound --addr ${INBOUND_ADDR} --port ${INBOUND_PORT} --bind-addr ${OUTBOUND_ADDR} --bind-port ${OUTBOUND_PORT} --input ${TXS_FILE} -n 20 --unacked-txids ${UNACKED_TXIDS}"
 
 ./network-mux/demo/ns-setup.sh "${THROUPUT}" "${DELAY}" "${TMP_DIR}" "${OUTBOUND_CMD}" "${INBOUND_CMD}"
