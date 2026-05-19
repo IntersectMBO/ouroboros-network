34b0fd25b7c1f0bb9434ec4d3f87c44e02d977ce
Marcin Szamotulski <coot@coot.me>
2026-05-19 10:24:47 +0200

    framework-io-tests: use ephemeral ports

--- ouroboros-network/framework/io-tests/Test/Ouroboros/Network/Socket.hs
+++ ouroboros-network/framework/io-tests/Test/Ouroboros/Network/Socket.hs
@@ -135,7 +135,7 @@ prop_socket_send_recv_ipv4
   -> [Int]
   -> Property
 prop_socket_send_recv_ipv4 f xs = ioProperty $ do
-    server:_ <- Socket.getAddrInfo Nothing (Just "127.0.0.1") (Just "6061")
+    server:_ <- Socket.getAddrInfo Nothing (Just "127.0.0.1") (Just "0")
     client:_ <- Socket.getAddrInfo Nothing (Just "127.0.0.1") (Just "0")
     prop_socket_send_recv (Socket.addrAddress client)
                           (Socket.addrAddress server)
@@ -149,7 +149,7 @@ prop_socket_send_recv_ipv6 :: (Int ->  Int -> (Int, Int))
                            -> [Int]
                            -> Property
 prop_socket_send_recv_ipv6 request response = ioProperty $ do
-    server:_ <- Socket.getAddrInfo Nothing (Just "::1") (Just "6061")
+    server:_ <- Socket.getAddrInfo Nothing (Just "::1") (Just "0")
     client:_ <- Socket.getAddrInfo Nothing (Just "::1") (Just "0")
     prop_socket_send_recv (Socket.addrAddress client)
                           (Socket.addrAddress server)
@@ -256,7 +256,7 @@ prop_socket_send_recv initiatorAddr responderAddr configureSock f xs =
 
         }
         (unversionedProtocol (SomeResponderApplication responderApp))
-        $ \_ _ -> do
+        $ \localAddress _ -> do
           void $ connectToNode
             snocket
             Mx.makeSocketBearer
@@ -270,7 +270,7 @@ prop_socket_send_recv initiatorAddr responderAddr configureSock f xs =
             (`configureSock` Nothing)
             (unversionedProtocol initiatorApp)
             (Just initiatorAddr)
-            responderAddr
+            localAddress
           atomically $ (,) <$> takeTMVar sv <*> takeTMVar cv
 
     return (res == mapAccumL f 0 xs)
