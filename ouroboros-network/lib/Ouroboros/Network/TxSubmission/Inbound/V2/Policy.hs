77c974cd840b0eb8a7799c431312b430aa07da4f
Karl Knutsson <karl.fb.knutsson@gmail.com>
2026-05-25 15:42:30 +0200

    WIP: bump interTxSpace to 125ms
    50ms was too low in my mainnet relay. Most tx requests took longer than
    that. 125ms should be long enough for 90% of all requests.

--- ouroboros-network/lib/Ouroboros/Network/TxSubmission/Inbound/V2/Policy.hs
+++ ouroboros-network/lib/Ouroboros/Network/TxSubmission/Inbound/V2/Policy.hs
@@ -91,7 +91,7 @@ defaultTxDecisionPolicy =
     scoreRate              = 0.001,
     scoreMax               = 15 * 60,
     scoreAcceptDecrement   = 3,
-    interTxSpace           = 0.050,
-    inflightTimeout        = 0.5,
+    interTxSpace           = 0.125,
+    inflightTimeout        = 0.500,
     maxPeerClaimDelay      = 0.250
   }
